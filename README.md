f9917386735e345c196ecdd6a95c16dcc475b6c2
Dima Ryazanov <dima@gmail.com>
2017-01-18 14:57:17 -0800

    Add an API for refreshing the access token.
    Also switch from Flask-OAuthLib to requests-oauthlib.

--- README.md
+++ README.md
@@ -16,6 +16,10 @@
 
         export FLASK_APP=quilt_server
         export FLASK_DEBUG=1
+        export OAUTH_BASE_URL='https://quilt-heroku.herokuapp.com'
+        export OAUTH_CLIENT_ID='chrOhbIPVtJAey7LcT1ez7PnIaV9tFLqNYXapcG3'
+        export OAUTH_CLIENT_SECRET='ihhjjcPioqbdsNyo6xfjMmTALqsJzSLgVWd5SgPfAJ5gxRBUCjZR7jT8Yy2IJrVpNbd0UHaKJHoBlFgjwwokTiaOEnmjGtS6KwaPDaXRb1jbrHkvpX82CNNAtwV44Nt3'
+
 
 * Activate the environment again - make sure the variables got set
 * Initialize the database tables:
--- quilt_server/config.py
+++ quilt_server/config.py
@@ -10,20 +10,6 @@ if PRODUCTION:
     assert False
 else:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/quilt'
-    SECRET_KEY = 'this is very secret'  # Used to sign cookies.
-
-    QUILT_AUTH = dict(
-        base_url='https://quilt-heroku.herokuapp.com',
-        request_token_url=None,
-        access_token_url='/o/token/',
-        authorize_url='/o/authorize/',
-        consumer_key='chrOhbIPVtJAey7LcT1ez7PnIaV9tFLqNYXapcG3',
-        # This one is really sensitive, but it's for the stage environment, so whatever.
-        consumer_secret=(
-            'ihhjjcPioqbdsNyo6xfjMmTALqsJzSLgVWd5SgPfAJ5gxRBUCjZR7jT8Yy2IJrVp' +
-            'Nbd0UHaKJHoBlFgjwwokTiaOEnmjGtS6KwaPDaXRb1jbrHkvpX82CNNAtwV44Nt3'
-        )
-    )
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False  # Turn it on for debugging.
