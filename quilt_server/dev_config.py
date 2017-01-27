88881dd33e3aead7537b9ec406c14183035bace0
Dima Ryazanov <dima@gmail.com>
2017-01-26 17:34:57 -0800

    Make quilt server work with ELB
    - Make nginx listen on plain http instead of https
    - Add an nginx config that simulates ELB (for stage, testing, etc.)
    - Add a /healthcheck route for ELB
    - Use hard-coded URLs for oauth redirects instead of `SERVER_NAME`. (`SERVER_NAME` is broken cause it does two unrelated things: 1) sets the hostname for `url_for` calls, and 2) 404s requests with any other hostname - such as ELB health checks.)

--- quilt_server/dev_config.py
+++ quilt_server/dev_config.py
@@ -5,6 +5,7 @@ import os
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/quilt'
 OAUTH = dict(
+    redirect_uri='http://localhost:5000/oauth_callback',
     base_url='https://quilt-heroku.herokuapp.com',
     client_id='chrOhbIPVtJAey7LcT1ez7PnIaV9tFLqNYXapcG3',
     client_secret=os.getenv('OAUTH_CLIENT_SECRET')
