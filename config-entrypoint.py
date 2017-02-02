9062406165c718aa05f0183eac42d7a554984d72
Dima Ryazanov <dima@gmail.com>
2017-02-01 20:13:21 -0800

    Allow using S3 config outside of EC2

--- config-entrypoint.py
+++ config-entrypoint.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 import boto3
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, NoCredentialsError
 
 def main(argv):
     config_bucket = os.getenv('QUILT_SERVER_CONFIG_S3_BUCKET')
@@ -26,11 +26,7 @@ def main(argv):
         print("Error: need QUILT_SERVER_CONFIG_S3_BUCKET when running on EC2.", file=sys.stderr)
         return 1
 
-    if not is_ec2 and config_bucket:
-        print("Error: QUILT_SERVER_CONFIG_S3_BUCKET should not be used in dev.", file=sys.stderr)
-        return 1
-
-    if is_ec2:
+    if config_bucket:
         config_path = '/config.py'
         if not os.path.exists(config_path):
             s3_client = boto3.client('s3')
@@ -39,7 +35,7 @@ def main(argv):
                 config = config_obj['Body'].read()
                 with open(config_path, 'wb') as config_file:
                     config_file.write(config)
-            except ClientError as ex:
+            except (ClientError, NoCredentialsError) as ex:
                 print("Failed to read s3://%s/config.py: %s" % (config_bucket, ex))
                 return 1
     else:
