ff031413396e01df4186170ce089a946c343f30a
Aneesh Karve <akarve@users.noreply.github.com>
2017-05-29 21:33:39 -0700

    make integration tests safer, more general (#113)
    * make integration test safer, more general
    
    * use unittest.mock.patch
    
    * use @patch decorator to patch entire `IntegrationTest` class
    
    * fetch patch in 2.7 friendly way
    
    * correct supported versions

--- quilt/test/test_integration.py
+++ quilt/test/test_integration.py
@@ -1,19 +1,23 @@
 """
 Integration test of installation from known public package(s)
 """
-import pytest
+from .utils import patch
 
+import pytest
 
 from ..tools import command
 from .utils import BasicQuiltTestCase
 from .integration import skip
 
+ENV = pytest.config.getoption("--integration")
+
 @skip
+@patch('quilt.tools.command.QUILT_PKG_URL', ENV)
 class IntegrationTest(BasicQuiltTestCase):
-    def test_prod_install(self):
+    def test_env_install(self):
         # public package
-        command.install('akarve/days')
+        command.install('akarve/days') # package exists on both stage and prod
         from quilt.data.akarve import days
         df = days.names.data()
         # check for expected datum
-        assert(df.loc[3]['Day'] == 'Wednesday')
+        assert df.loc[3]['Day'] == 'Wednesday', 'unexpected value in days df at loc[3]'
