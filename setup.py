99464e65bf4fd076c0d62c22b2ad92e61436598b
Kevin Moore <kevinemoore@gmail.com>
2017-07-13 21:25:53 -0700

    Version 2.6.0 (#139)
    Updating version to 2.6.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setup(
     name="quilt",
-    version="2.5.2",
+    version="2.6.0",
     packages=find_packages(),
     description='Quilt is an open-source data frame registry',
     long_description=readme(),
@@ -32,7 +32,7 @@ setup(
     author_email='contact@quiltdata.io',
     license='LICENSE',
     url='https://github.com/quiltdata/quilt',
-    download_url='https://github.com/quiltdata/quilt/releases/tag/2.5.2',
+    download_url='https://github.com/quiltdata/quilt/releases/tag/2.6.0',
     keywords='quilt quiltdata shareable data dataframe package platform pandas',
     install_requires=[
         'appdirs>=1.4.0',
