36a5ec12e151af7a644508a03a9bc3961dfd16bb
Dima Ryazanov <dima@gmail.com>
2017-08-18 12:08:24 -0700

    Increment version to 2.7.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setup(
     name="quilt",
-    version="2.6.3",
+    version="2.7.0",
     packages=find_packages(),
     description='Quilt is a data package manager',
     long_description=readme(),
@@ -32,7 +32,7 @@ setup(
     author_email='contact@quiltdata.io',
     license='LICENSE',
     url='https://github.com/quiltdata/quilt',
-    download_url='https://github.com/quiltdata/quilt/releases/tag/2.6.1',
+    download_url='https://github.com/quiltdata/quilt/releases/tag/2.7.0',
     keywords='quilt quiltdata shareable data dataframe package platform pandas',
     install_requires=[
         'appdirs>=1.4.0',
