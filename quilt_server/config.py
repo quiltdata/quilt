8425efaf60b642418786c523d142a370dae3c3a0
Dima Ryazanov <dima@gmail.com>
2017-11-15 10:33:45 +0700

    Change the S3 URL expiration time from 12 to 24 hours

--- quilt_server/config.py
+++ quilt_server/config.py
@@ -10,7 +10,7 @@ See `app.config.from_object('...')` in __init__.py.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False  # Turn it on for debugging.
 
-PACKAGE_URL_EXPIRATION = 60*60*12 # 12 Hours
+PACKAGE_URL_EXPIRATION = 60*60*24 # 24 hours
 
 JSON_USE_ENCODE_METHODS = True  # Support the __json__ method in Node
 
