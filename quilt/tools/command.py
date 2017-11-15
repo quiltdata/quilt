684c7852854ecafbf0f444aab540f88af0b81027
Adam Sah <asah@bbfdirect.com>
2017-11-15 09:04:26 -0800

    a better fix - CLI unittests forthcoming

--- quilt/tools/command.py
+++ quilt/tools/command.py
@@ -753,7 +753,7 @@ def install_via_requirements(requirements_str, force=False):
             package += '/' + subpath
         install(package, hash, version, tag, force=force)
     
-def install(package, hash=None, version=None, tag=None, force=False, reqfile=False):
+def install(package, hash=None, version=None, tag=None, force=False):
     """
     Download a Quilt data package from the server and install locally.
 
@@ -769,7 +769,7 @@ def install(package, hash=None, version=None, tag=None, force=False, reqfile=Fal
     if len(package) == 0:
         raise CommandException("package name is empty.")
 
-    if reqfile or package[0] == '@' or '\n' in package:
+    if package[0] == '@' or '\n' in package:
         return install_via_requirements(package, force=force)
         
     assert [hash, version, tag].count(None) == 2
