b25a4b473dce3d641e74f6dc8d14ad8fb794840b
Adam Sah <asah@midgard.net>
2017-11-26 18:52:29 +0100

    test fix: spurious _mock_log() due to responses lib upgrade

--- quilt/test/test_install.py
+++ quilt/test/test_install.py
@@ -101,7 +101,6 @@ class InstallTest(QuiltTestCase):
         self._mock_tag('foo/bar', 'mytag', contents_hash[0:6], cmd=responses.PUT)
         command.tag_add('foo/bar', 'mytag', contents_hash[0:6])
 
-        self._mock_log('foo/bar', contents_hash)
         self._mock_version('foo/bar', '1.0', contents_hash[0:6], cmd=responses.PUT)
         command.version_add('foo/bar', '1.0', contents_hash[0:6], force=True)
 
