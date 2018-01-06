6da3e7b02c9ebda2a57fea76345eeddad8566aa5
Brian <eode@eptitude.net>
2018-01-06 17:12:52 -0500

    CLI Tests: Update `quilt push` for team args
     * test mutual exclusion of quilt push --team and --public
     * add `team` arg to cli push tests
     * use dict comparison instead of selecting individual keys (more complete, simpler)

--- compiler/quilt/test/test_cli.py
+++ compiler/quilt/test/test_cli.py
@@ -672,6 +672,7 @@ class TestCLI(BasicQuiltTestCase):
             [0, 'push', 0],
             [0, 'push', '--public'],
             [0, 'push', '--reupload'],
+            [0, 'push', '--team'],
         ])
 
         ## This section tests for circumstances expected to be rejected by argparse.
@@ -680,9 +681,11 @@ class TestCLI(BasicQuiltTestCase):
             'push --public'.split(),
             'push --reupload'.split(),
             'push --public --reupload'.split(),
+            'push --public --team'.split(),
+            'push --public --team fakeuser/fakepackage'.split(),  # mutually exclusive options
             ]
         for args in expect_fail_2_args:
-            assert self.execute(args)['return code'] == 2
+            assert self.execute(args)['return code'] == 2, "using args: " + str(args)
 
         ## This section tests for appropriate types and values.
         cmd = 'push fakeuser/fakepackage'.split()
@@ -696,11 +699,16 @@ class TestCLI(BasicQuiltTestCase):
         # Specific tests
         assert not result['args']
         assert result['func'] == 'push'
-        assert result['kwargs']['reupload'] is False
-        assert result['kwargs']['public'] is False
-        assert result['kwargs']['package'] == 'fakeuser/fakepackage'
+        kwargs = result['kwargs']
+        assert kwargs == {
+            'reupload': False,
+            'public': False,
+            'package': 'fakeuser/fakepackage',
+            'team': False,
+        }
 
         ## Test the flags as well..
+        # public (and reupload)
         cmd = 'push --reupload --public fakeuser/fakepackage'.split()
         result = self.execute(cmd)
 
@@ -712,9 +720,34 @@ class TestCLI(BasicQuiltTestCase):
         # Specific tests
         assert not result['args']
         assert result['func'] == 'push'
-        assert result['kwargs']['reupload'] is True
-        assert result['kwargs']['public'] is True
-        assert result['kwargs']['package'] == 'fakeuser/fakepackage'
+        kwargs = result['kwargs']
+        assert kwargs == {
+            'reupload': True,
+            'public': True,
+            'package': 'fakeuser/fakepackage',
+            'team': False,
+        }
+
+        # team (without reupload)
+        cmd = 'push --reupload --team blah:fakeuser/fakepackage'.split()
+        result = self.execute(cmd)
+
+        # General tests
+        assert result['return code'] == 0
+        assert result['matched'] is True  # func name recognized by MockObject class?
+        assert not result['bind failure']
+
+        # Specific tests
+        assert not result['args']
+        assert result['func'] == 'push'
+        kwargs = result['kwargs']
+        assert kwargs == {
+            'reupload': True,
+            'public': False,
+            'package': 'blah:fakeuser/fakepackage',
+            'team': True,
+        }
+
 
     def test_cli_option_dev(self):
         if os.name == 'nt':
