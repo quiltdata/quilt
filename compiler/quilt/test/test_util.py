81cfe287b8e41948f2b2b55eb0815093dfa8acdc
Dima Ryazanov <dima@gmail.com>
2018-01-10 13:23:41 -0800

    Simplify the test

--- compiler/quilt/test/test_util.py
+++ compiler/quilt/test/test_util.py
@@ -12,40 +12,12 @@ class UtilTest(QuiltTestCase):
         mydir = os.path.dirname(__file__)
         path = os.path.join(mydir, './listdir')
 
-        files = ['foo.txt']
-        invis_files = ['.invisible.txt']
-        children = ['dir/child.txt']
-        dirs = ['dir']
-        invis_dirs = ['.invisible_dir']
-
-        for f in sub_files(path):
-            assert f in files
-            assert f not in invis_files
-            assert f not in children
-            assert f not in dirs and f not in invis_dirs
-
-        for f in sub_files(path, invisible=True):
-            assert f in files or f in invis_files
-            assert f not in children
-            assert f not in dirs and f not in invis_dirs
+        assert set(sub_files(path)) == set(['foo.txt'])
+        assert set(sub_files(path, invisible=True)) == set(['.invisible.txt', 'foo.txt'])
 
     def test_sub_dirs(self):
         mydir = os.path.dirname(__file__)
         path = os.path.join(mydir, './listdir')
 
-        files = ['foo.txt']
-        invis_files = ['.invisible.txt']
-        children = ['dir/child.txt']
-        dirs = ['dir']
-        invis_dirs = ['.invisible_dir']
-
-        for d in sub_dirs(path):
-            assert d in dirs
-            assert d not in invis_dirs
-            assert d not in files and d not in invis_files
-            assert d not in children
-
-        for d in sub_dirs(path, invisible=True):
-            assert d in dirs or d in invis_dirs
-            assert d not in files and d not in invis_files
-            assert d not in children
+        assert set(sub_dirs(path)) == set(['dir'])
+        assert set(sub_dirs(path, invisible=True)) == set(['.invisible_dir', 'dir'])
