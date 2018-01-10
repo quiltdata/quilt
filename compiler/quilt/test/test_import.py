2a4621f861bd2382044d0db5403a2a27858de63d
eode <aeon.descriptor@gmail.com>
2018-01-10 05:03:06 -0600

    team demo -- bugfixes, tests for pkgname parsing and imports (#268)
    * Fix 'Built ... successfully' messsage for teams
    
    * Team display for quilt ls
    
    * build_empty() to _build_empty() (not public-facing)
    
    * Test for test_parse_package_names and associated fixes
    
    Found and fixed:
     * trailing slash when install_via_requirements handles yaml, but no
    subpath is present (invalid None comparison on blank list)
     * invalid subpath elements permitted by store.parse_packagename()
    
    * test_import: add team versions of all functions
    
    * test_cli: `quilt login` command, including team param
    
    * Fix and test that `quilt login teamname` rejects invalid team names
    
    * Test: cli logout() (including team arg)
    
    * CLI: Make `quilt push` --team and --public mutually exclusive
    
    * CLI Tests: Update `quilt push` for team args
    
     * test mutual exclusion of quilt push --team and --public
     * add `team` arg to cli push tests
     * use dict comparison instead of selecting individual keys (more complete, simpler)
    
    * Nixed: copymodded outdated test for use with teams
    
    * Update .gitignore, bump travis and appveyor

--- compiler/quilt/test/test_import.py
+++ compiler/quilt/test/test_import.py
@@ -74,6 +74,65 @@ class ImportTest(QuiltTestCase):
         with self.assertRaises(ImportError):
             from quilt.data.foo.baz import blah
 
+    def test_team_imports(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('test:bar/package', build_path)
+
+        # Good imports
+
+        from quilt.team.test.bar import package
+        from quilt.team.test.bar.package import dataframes
+        from quilt.team.test.bar.package import README
+
+        # Contents of the imports
+
+        assert isinstance(package, GroupNode)
+        assert isinstance(dataframes, GroupNode)
+        assert isinstance(dataframes.csv, DataNode)
+        assert isinstance(README, DataNode)
+
+        assert package.dataframes == dataframes
+        assert package.README == README
+
+        assert set(dataframes._keys()) == {'xls', 'csv', 'tsv', 'xls_skip', 'nulls'}
+        assert set(dataframes._group_keys()) == set()
+        assert set(dataframes._data_keys()) == {'xls', 'csv', 'tsv', 'xls_skip', 'nulls'}
+
+        assert isinstance(README(), string_types)
+        assert isinstance(README._data(), string_types)
+        assert isinstance(dataframes.csv(), pd.DataFrame)
+        assert isinstance(dataframes.csv._data(), pd.DataFrame)
+
+        str(package)
+        str(dataframes)
+        str(README)
+
+        # Bad attributes of imported packages
+
+        with self.assertRaises(AttributeError):
+            package.foo
+
+        with self.assertRaises(AttributeError):
+            package.dataframes.foo
+
+        with self.assertRaises(AttributeError):
+            package.dataframes.csv.foo
+
+        # Bad imports
+
+        with self.assertRaises(ImportError):
+            import quilt.team.test.bar.bad_package
+
+        with self.assertRaises(ImportError):
+            import quilt.team.test.bad_user.bad_package
+
+        with self.assertRaises(ImportError):
+            from quilt.team.test.bar.dataframes import blah
+
+        with self.assertRaises(ImportError):
+            from quilt.team.test.bar.baz import blah
+
     def test_import_group_as_data(self):
         mydir = os.path.dirname(__file__)
         build_path = os.path.join(mydir, './build_group_data.yml')
@@ -87,6 +146,22 @@ class ImportTest(QuiltTestCase):
 
         # Incompatible Schema
         from quilt.data.foo.grppkg import incompatible
+        with self.assertRaises(PackageException):
+            incompatible._data()
+
+    def test_team_import_group_as_data(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build_group_data.yml')
+        command.build('test:bar/grppkg', build_path)
+
+        # Good imports
+        from quilt.team.test.bar.grppkg import dataframes
+        assert isinstance(dataframes, GroupNode)
+        assert isinstance(dataframes.csvs.csv, DataNode)
+        assert isinstance(dataframes._data(), pd.DataFrame)
+
+        # Incompatible Schema
+        from quilt.team.test.bar.grppkg import incompatible
         with self.assertRaises(PackageException):
             incompatible._data()
 
@@ -180,6 +255,76 @@ class ImportTest(QuiltTestCase):
         new_file = package3.new.file._data()
         assert isinstance(new_file, string_types)
 
+    def test_team_save(self):
+        mydir = os.path.dirname(__file__)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('test:bar/package1', build_path)
+
+        from quilt.team.test.bar import package1
+
+        # Build an identical package
+        command.build('test:bar/package2', package1)
+
+        from quilt.team.test.bar import package2
+        teststore = PackageStore(self._store_dir)
+        contents1 = open(os.path.join(teststore.package_path('test', 'bar', 'package1'),
+                                      Package.CONTENTS_DIR,
+                                      package1._package.get_hash())).read()
+        contents2 = open(os.path.join(teststore.package_path('test', 'bar', 'package2'),
+                                      Package.CONTENTS_DIR,
+                                      package2._package.get_hash())).read()
+        assert contents1 == contents2
+
+        # Rename an attribute
+        package1.dataframes2 = package1.dataframes
+        del package1.dataframes
+
+        # Modify an existing dataframe
+        csv = package1.dataframes2.csv._data()
+        csv.at[0, 'Int0'] = 42
+
+        # Add a new dataframe
+        df = pd.DataFrame(dict(a=[1, 2, 3]))
+        package1._set(['new', 'df'], df)
+        assert package1.new.df._data() is df
+
+        # Add a new file
+        file_path = os.path.join(mydir, 'data/foo.csv')
+        package1._set(['new', 'file'], file_path)
+        assert package1.new.file._data() == file_path
+
+        # Add a new group
+        package1._add_group('newgroup')
+        assert isinstance(package1.newgroup, GroupNode)
+        package1.newgroup._add_group('foo')
+        assert isinstance(package1.newgroup.foo, GroupNode)
+
+        # Overwrite a leaf node
+        new_path = os.path.join(mydir, 'data/nuts.csv')
+        package1._set(['newgroup', 'foo'], new_path)
+        assert package1.newgroup.foo._data() == new_path
+
+        # Overwrite the whole group
+        package1._set(['newgroup'], new_path)
+        assert package1.newgroup._data() == new_path
+
+        # Built a new package and verify the new contents
+        command.build('test:bar/package3', package1)
+
+        from quilt.team.test.bar import package3
+
+        assert hasattr(package3, 'dataframes2')
+        assert not hasattr(package3, 'dataframes')
+
+        new_csv = package3.dataframes2.csv._data()
+        assert new_csv.xs(0)['Int0'] == 42
+
+        new_df = package3.new.df._data()
+        assert new_df.xs(2)['a'] == 3
+
+        new_file = package3.new.file._data()
+        assert isinstance(new_file, string_types)
+
     def test_set_non_node_attr(self):
         mydir = os.path.dirname(__file__)
         build_path = os.path.join(mydir, './build.yml')
@@ -193,17 +338,15 @@ class ImportTest(QuiltTestCase):
         with self.assertRaises(AttributeError):
             package4.newdf = df
 
-    def test_team_imports(self):
+    def test_team_set_non_node_attr(self):
         mydir = os.path.dirname(__file__)
-        build_path1 = os.path.join(mydir, './build_simple.yml')
-        command.build('my_team:foo/team_imports', build_path1)
-        build_path2 = os.path.join(mydir, './build_empty.yml')
-        command.build('foo/team_imports', build_path2)
+        build_path = os.path.join(mydir, './build.yml')
+        command.build('test:bar/package4', build_path)
 
-        # Verify that both imports work, and packages are in fact different.
+        from quilt.team.test.bar import package4
 
-        from quilt.team.my_team.foo import team_imports as pkg1
-        from quilt.data.foo import team_imports as pkg2
-
-        assert hasattr(pkg1, 'foo')
-        assert not hasattr(pkg2, 'foo')
+        # Assign a DataFrame as a node
+        # (should throw exception)
+        df = pd.DataFrame(dict(a=[1, 2, 3]))
+        with self.assertRaises(AttributeError):
+            package4.newdf = df
