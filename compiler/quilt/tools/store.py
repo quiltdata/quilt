cfaecf41864cbcc7984374298546dc0467d302ca
Aneesh Karve <akarve@users.noreply.github.com>
2018-01-10 13:40:48 -0800

    Fix `quilt ls` (#284)
    * fix quilt ls
    * refactor children to be clearer and more correct
    * Use literal examples in sub_files()/sub_dirs() tests
    * Simplify the test

--- compiler/quilt/tools/store.py
+++ compiler/quilt/tools/store.py
@@ -9,7 +9,7 @@ from shutil import rmtree
 from .const import PACKAGE_DIR_NAME
 from .core import FileNode, RootNode, TableNode, CommandException
 from .package import Package, PackageException
-from .util import BASE_DIR
+from .util import BASE_DIR, sub_dirs, sub_files
 
 # start with alpha (_ may clobber attrs), continue with alphanumeric or _
 VALID_NAME_RE = re.compile(r'^[a-zA-Z]\w*$')
@@ -186,6 +186,7 @@ class PackageStore(object):
 
         path = self.package_path(user, package)
         remove_objs = set()
+        # TODO: do we really want to delete invisible dirs?
         if os.path.isdir(path):
             # Collect objects from all instances for potential cleanup
             contents_path = os.path.join(path, Package.CONTENTS_DIR)
@@ -205,10 +206,10 @@ class PackageStore(object):
         Return an iterator over all the packages in the PackageStore.
         """
         pkgdir = os.path.join(self._path, self.PKG_DIR)
-        for user in os.listdir(pkgdir):
-            for pkg in os.listdir(os.path.join(pkgdir, user)):
+        for user in sub_dirs(pkgdir):
+            for pkg in sub_dirs(os.path.join(pkgdir, user)):
                 pkgpath = os.path.join(pkgdir, user, pkg)
-                for hsh in os.listdir(os.path.join(pkgpath, Package.CONTENTS_DIR)):
+                for hsh in sub_files(os.path.join(pkgpath, Package.CONTENTS_DIR)):
                     yield Package(self, user, pkg, pkgpath, pkghash=hsh)
 
     def ls_packages(self):
@@ -217,11 +218,11 @@ class PackageStore(object):
         """
         packages = []
         pkgdir = os.path.join(self._path, self.PKG_DIR)
-        for user in os.listdir(pkgdir):
-            for pkg in os.listdir(os.path.join(pkgdir, user)):
+        for user in sub_dirs(pkgdir):
+            for pkg in sub_dirs(os.path.join(pkgdir, user)):
                 pkgpath = os.path.join(pkgdir, user, pkg)           
-                pkgmap = {h : [] for h in os.listdir(os.path.join(pkgpath, Package.CONTENTS_DIR))}
-                for tag in os.listdir(os.path.join(pkgpath, Package.TAGS_DIR)):
+                pkgmap = { h : [] for h in sub_files(os.path.join(pkgpath, Package.CONTENTS_DIR)) }
+                for tag in sub_files(os.path.join(pkgpath, Package.TAGS_DIR)):
                     with open(os.path.join(pkgpath, Package.TAGS_DIR, tag), 'r') as tagfile:
                         pkghash = tagfile.read()
                         pkgmap[pkghash].append(tag)
