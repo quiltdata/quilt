c956c3349cf5cd567171a83ab91cd9e2051e1036
Brian <eode@eptitude.net>
2018-01-09 19:25:22 -0500

    Remove unused class

--- compiler/quilt/tools/main.py
+++ compiler/quilt/tools/main.py
@@ -43,15 +43,6 @@ class UsageAction(argparse.Action):
         parser.print_usage()
         exit()
 
-#
-# class SubcommandHelpFormatter(argparse.RawDescriptionHelpFormatter):
-#     """Avoid showing the"""
-#     def _format_action(self, action):
-#         parts = super(argparse.RawDescriptionHelpFormatter, self)._format_action(action)
-#         if action.nargs == argparse.PARSER:
-#             parts = "\n".join(parts.split("\n")[1:])
-#         return parts
-#
 
 class CustomHelpParser(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
