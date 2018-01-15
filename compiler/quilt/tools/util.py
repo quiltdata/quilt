f1de29e7577ac7b4d63b0ef5c44b1c8c8f6f4a26
Brian <eode@eptitude.net>
2018-01-15 12:33:44 -0500

    Copied files from tensorflow_virtual_files branch
    * Provides Python2.7 compatibility
    * Provides helper functions from tools/util.py

--- compiler/quilt/tools/util.py
+++ compiler/quilt/tools/util.py
@@ -1,11 +1,17 @@
 """
 Helper functions.
 """
+import re
 import gzip
 import os
+import keyword
+import tokenize
 
 from appdirs import user_config_dir, user_data_dir
 from six import BytesIO, string_types, Iterator
+
+from .compat import pathlib
+
 
 APP_NAME = "QuiltCli"
 APP_AUTHOR = "QuiltData"
@@ -73,6 +79,7 @@ def file_to_str(fname):
         data = fd.read()
     return data
 
+
 def gzip_compress(data):
     """
     Compress a string. Same as gzip.compress in Python3.
@@ -81,6 +88,7 @@ def gzip_compress(data):
     with gzip.GzipFile(fileobj=buf, mode='wb') as fd:
         fd.write(data)
     return buf.getvalue()
+
 
 def sub_dirs(path, invisible=False):
     """
@@ -92,6 +100,7 @@ def sub_dirs(path, invisible=False):
 
     return dirs
 
+
 def sub_files(path, invisible=False):
     """
     Child files (non-recursive)
@@ -101,3 +110,117 @@ def sub_files(path, invisible=False):
         files = [x for x in files if not x.startswith('.')]
 
     return files
+
+
+def is_identifier(string):
+    """Check if string could be a valid python identifier
+
+    :param string: string to be tested
+    :returns: True if string can be a python identifier, False otherwise
+    :rtype: bool
+    """
+    val = re.match(r'^[a-zA-Z_]\w*$', string) and not keyword.iskeyword(string)
+    return True if val else False
+
+
+def is_nodename(string):
+    """Check if string could be a valid node name
+
+    Convenience, and a good place to aggregate node-name related checks.
+
+    :param string: string to be tested
+    :returns: True if string could be used as a node name, False otherwise
+    :rtype: bool
+    """
+    if string.startswith('_'):
+        return False
+    return is_identifier(string)
+
+
+def to_identifier(string):
+    """Makes a python identifier (perhaps an ugly one) out of any string.
+
+    This isn't an isomorphic change, the original filename can't be recovered
+    from the change in all cases, so it must be stored separately.
+
+    Examples:
+    >>> to_identifier('#if') -> '_if'
+    >>> to_identifier('global') -> 'global_'
+    >>> to_identifier('9foo') -> 'n9foo'
+
+    :param string: string to convert
+    :returns: `string`, converted to python identifier if needed
+    :rtype: string
+    """
+    # Not really useful to expose as a CONSTANT, and python will compile and cache
+    string = re.sub(r'[^0-9a-zA-Z_]', '_', string)
+
+    if string[0].isdigit():
+        string = "n" + string
+    if keyword.iskeyword(string):
+        string = string + '_'
+
+    return string
+
+
+def to_nodename(string, invalid=None, raise_exc=False):
+    """Makes a Quilt Node name (perhaps an ugly one) out of any string.
+
+    This isn't an isomorphic change, the original filename can't be recovered
+    from the change in all cases, so it must be stored separately (`FileNode`
+    metadata)
+
+    If `invalid` is given, it should be an iterable of names that the returned
+    string cannot match -- for example, other node names.
+
+    If `raise_exc` is False (default), an exception is raised when the
+    converted string is present in `invalid`.  Otherwise, the converted string
+    will have a number appended to its name.
+
+    Example:
+    # replace special chars -> remove prefix underscores -> rename keywords
+    # '!if' -> '_if' -> 'if' -> 'if_'
+    >>> to_nodename('!if') -> 'if_'
+    >>> to_nodename('if', ['if_']) -> 'if__2'
+    >>> to_nodename('9#blah') -> 'n9_blah'
+    >>> to_nodename('9:blah', ['n9_blah', 'n9_blah_2']) -> 'n9_blah_3'
+
+    :param string: string to convert to a nodename
+    :param invalid: iterable of names to avoid
+    :type invalid: iterable
+    :param raise_exc: Raise an exception on name conflicts if truthy.
+    :type raise_exc: bool
+    :returns: valid node name
+    :rtype: string
+    """
+    string = to_identifier(to_identifier(string).lstrip('_'))
+
+    if string[0].isdigit():  # for valid cases like '_903'.lstrip('_') == invalid '903'
+        string = 'n' + string
+
+    if invalid is None:
+        return string
+
+    if not isinstance(invalid, set):
+        invalid = set(invalid)
+
+    if string in invalid and raise_exc:
+        raise ValueError("Conflicting node name after string conversion: {!r}".format(string))
+
+    result = string
+    counter = 1
+    while result in invalid:
+        # first conflicted name will be "somenode_2"
+        # The result is "somenode", "somenode_2", "somenode_3"..
+        counter += 1
+        result = "{}_{}".format(string, counter)
+
+    return result
+
+
+def filepath_to_nodepath(filepath, nodepath_separator='.'):
+    filepath = pathlib.Path(filepath)
+    if filepath.anchor:
+        raise ValueError("Invalid filepath (relative file path required): " + str(filepath))
+
+    return nodepath_separator.join(to_nodename(part) for part in filepath.parts)
