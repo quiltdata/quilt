48daad7d805a474e51d963b86be96f278e372187
Aneesh Karve <aneesh.karve@gmail.com>
2018-01-17 10:55:36 -0800

    revert team changes (to be moved to config.js.tmpl)

--- catalog/static/config.js
+++ catalog/static/config.js
@@ -1,14 +1,12 @@
 /* eslint-disable */
-const { hostname } = window.location;
-
-if (hostname === 'quiltdata.com') {
+if (window.location.hostname === 'quiltdata.com') {
   window.__CONFIG = {
     api: 'https://pkg.quiltdata.com',
     stripeKey: 'pk_live_aV44tCGHpBZr5FfFCUqbXqid',
     userApi: 'https://app.quiltdata.com/api-root',
     signOutUrl: 'https://app.quiltdata.com/api-auth/logout?next=%2F'
   };
-} else if (hostname === 'packages-stage.firebaseapp.com' || hostname === 'localhost') {
+} else {
   window.__CONFIG = {
     api: 'https://pkg-stage.quiltdata.com',
     stripeKey: 'pk_test_DzvjoWzXwIB1DRtQqywxDjWp',
@@ -20,22 +18,5 @@ if (hostname === 'quiltdata.com') {
     // GitHub
     // userApi: 'https://api.github.com/user',
     // signOutUrl: '/'
-  };
-} else {
-  // TODO: This should come from Docker variables on team instances
-  window.__CONFIG = {
-    api: 'https://pkg-stage.quiltdata.com',
-    stripeKey: 'pk_test_DzvjoWzXwIB1DRtQqywxDjWp',
-
-    // Quilt auth
-    userApi: 'https://quilt-heroku.herokuapp.com/api-root',
-    signOutUrl: 'https://quilt-heroku.herokuapp.com/api-auth/logout?next=%2F',
-
-    // GitHub
-    // userApi: 'https://api.github.com/user',
-    // signOutUrl: '/'
-    team: {
-      name: 'team_name'
-    },
   };
 }
