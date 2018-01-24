53edec530dc63ce70b18699db41fc14252dc236d
Aneesh Karve <aneesh.karve@gmail.com>
2018-01-23 17:51:08 -0800

    remove dead env file

--- catalog/env.sh
+++
@@ -1,4 +0,0 @@
-export REGISTRY_URL=http://localhost:5000
-export USER_API=http://localhost:5002/accounts/api-root
-export SIGN_OUT_URL=http://localhost:5002/accounts/logout?next=%2F
-export STRIPE_KEY=NOSTRIPE
