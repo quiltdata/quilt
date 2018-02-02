f752e3814f919e680e94f5b549d5b7cf359155d9
Brian <eode@eptitude.net>
2018-02-01 20:09:43 -0500

    include 'compat.py'

---
+++ compiler/quilt/tools/compat.py
@@ -0,0 +1,23 @@
+"""
+Python 2 / 3 compatibility
+
+In cases where the `six` lib covers an item, use `six`.
+
+For others, import the module here under the python3 name (if there are naming differences).
+
+Unfortunately, this *doesn't* allow for `from .tools.compat.foo import bar`, so if needed,
+import frequently-used objects here for convenience.
+"""
+# unused imports -- pylint: disable=W0611
+
+import sys as _sys
+
+# XXX: This originally had a few modules, but I've managed to remove the others.
+#      I'm tentatively still keeping compat.py, otherwise the try: except: block ends up in
+#      each module's import stanzas, instead of 'from compat import pathlib'.
+
+try:
+    import pathlib2 as pathlib
+except ImportError:
+    assert _sys.version_info >= (3, 6)   # use `pip install --upgrade quilt` if needed
+    import pathlib
