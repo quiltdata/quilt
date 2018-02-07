87f40e5d4a421b0ec5d201d73162dfb6bb5ef0b1
Aneesh Karve <aneesh.karve@gmail.com>
2018-02-07 11:03:17 -0800

    new lock file; debug comment in config

--- catalog/static/config.js
+++ catalog/static/config.js
@@ -12,8 +12,15 @@ if (window.location.hostname === 'quiltdata.com') {
     stripeKey: 'pk_test_DzvjoWzXwIB1DRtQqywxDjWp',
 
     // Quilt auth
+    userApi: 'https://quilt-heroku.herokuapp.com/api-root',
+    signOutUrl: 'https://quilt-heroku.herokuapp.com/api-auth/logout?next=%2F',
+    /* Dev config
+    team: {
+      name: "TEST"
+    },
     userApi: 'http://localhost:5002/accounts/api-root',
     signOutUrl: 'http://localhost:5002/accounts/logout?next=%2F',
+    */ 
     // GitHub
     // userApi: 'https://api.github.com/user',
     // signOutUrl: '/'
