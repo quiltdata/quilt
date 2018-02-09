e292ab4a9f69af57b99c107f6e9e99edd8fd0b11
nivlac <meffij@gmail.com>
2018-02-09 14:22:56 -0800

    Tests for audit endpoints

--- registry/tests/admin_test.py
+++ registry/tests/admin_test.py
@@ -59,3 +59,85 @@ class AdminTestCase(QuiltTestCase):
         )
         assert resp.status_code == requests.codes.forbidden
 
+    def testAuditUser(self):
+        # actions as user test_user
+        self._mock_admin()
+        resp = self.app.get(
+            '/api/audit/{usr}/'.format(
+                usr=self.user
+            ),
+            headers={
+                'Authorization': self.user
+            }
+        )
+        data = json.loads(resp.get_data()).get('events')
+        assert(len(data) == 3)
+        for event in data:
+            assert(event.get('type') == 'PUSH')
+            assert(event.get('package_owner') == self.user)
+            assert(event.get('user') == self.user)
+
+        self.put_package(self.user, self.pkg, self.contents_list[0])
+        resp = self.app.get(
+            '/api/audit/{usr}/'.format(
+                usr=self.user
+            ),
+            headers={
+                'Authorization': self.user
+            }
+        )
+        data = json.loads(resp.get_data()).get('events')
+        assert(len(data) == 4)
+        pass
+
+    def testAuditPackage(self):
+        self._mock_admin()
+        resp = self.app.get(
+            '/api/audit/{usr}/{pkg}/'.format(
+                usr=self.user,
+                pkg=self.pkg
+            ),
+            headers={
+                'Authorization': self.user
+            }
+        )
+        data = json.loads(resp.get_data()).get('events')
+        assert(len(data) == 3)
+        for event in data:
+            assert(event.get('type') == 'PUSH')
+            assert(event.get('package_owner') == self.user)
+            assert(event.get('user') == self.user)
+
+        self.put_package(self.user, self.pkg, self.contents_list[0])
+        resp = self.app.get(
+            '/api/audit/{usr}/{pkg}/'.format(
+                usr=self.user,
+                pkg=self.pkg
+            ),
+            headers={
+                'Authorization': self.user
+            }
+        )
+        data = json.loads(resp.get_data()).get('events')
+        assert(len(data) == 4)
+
+        self.put_package(self.user, self.pkg + '2', self.contents_list[0])
+        resp = self.app.get(
+            '/api/audit/{usr}/{pkg}/'.format(
+                usr=self.user,
+                pkg=self.pkg
+            ),
+            headers={
+                'Authorization': self.user
+            }
+        )
+        data = json.loads(resp.get_data()).get('events')
+        assert(len(data) == 4)
+        pass
+
+    def testAdminListUserUI(self):
+        pass
+
+    # test user with no packages and some events
+    # test user with packages and no events
+    # test disabled users
