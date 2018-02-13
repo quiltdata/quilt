e0fb90668313808ad45a5d8ae1a848db352ffb80
Brian <eode@eptitude.net>
2018-02-13 18:04:09 -0500

    Remove requirement for FileNode metadata and handling for q_path of None

--- compiler/quilt/tools/core.py
+++ compiler/quilt/tools/core.py
@@ -13,14 +13,6 @@ import struct
 from six import iteritems, itervalues, string_types
 
 
-# Backward compatibility
-# Created circa 2.10.0
-# It's possible some older package stores or registries have FileNodes in them with no
-# metadata['q_path'].  If data is loaded from disk or network with no metadata['q_path'],
-# metadata['q_path'] is set to this token.
-FILENAME_MISSING = "::FILENAME_MISSING::"
-
-
 class PackageFormat(Enum):
     HDF5 = 'HDF5'
     PARQUET = 'PARQUET'
@@ -96,11 +88,13 @@ class TableNode(Node):
 class FileNode(Node):
     json_type = 'FILE'
 
-    def __init__(self, hashes, metadata):
+    def __init__(self, hashes, metadata=None):
+        if metadata is None:
+            metadata = {}
+
         assert isinstance(hashes, list)
         assert isinstance(metadata, dict)
-        assert metadata.get('q_path')   # metadata for a FileNode requires 'q_path' to be set.
-        assert not os.path.isabs(metadata['q_path'])
+        assert not os.path.isabs(metadata.get('q_path') or '')  # handles q_path when None
 
         self.hashes = hashes
         self.metadata = metadata
@@ -117,14 +111,6 @@ def decode_node(value):
     if type_str is None:
         return value
     node_cls = NODE_TYPE_TO_CLASS[type_str]
-
-    # See comment at definition of FILENAME_MISSING
-    if node_cls is FileNode:
-        if 'metadata' not in value:
-            value['metadata'] = {}
-        if not value['metadata'].get('q_path'):
-            value['metadata']['q_path'] = FILENAME_MISSING
-
     return node_cls(**value)
 
 def hash_contents(contents):
