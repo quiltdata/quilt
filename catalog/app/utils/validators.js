1922f3b541613858fae1224d6144c75943011505
nl_0 <nl.imbecile@gmail.com>
2018-02-16 15:45:44 +0500

    util/validators: rm unneeded validators

--- catalog/app/utils/validators.js
+++ catalog/app/utils/validators.js
@@ -1,15 +1,5 @@
-const mkValidator = (name, test) => (v) => v && !test(v) ? name : undefined;
+export const mkValidator = (name, test) => (v) => v && !test(v) ? name : undefined;
 
-const matches = (re) => (str) => re.test(str);
+export const matches = (re) => (str) => re.test(str);
 
 export const required = (v) => v ? undefined : 'required';
-
-// taken from angular
-const EMAIL_RE =
-    /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/;
-
-export const email = mkValidator('email', matches(EMAIL_RE));
-
-// TODO
-const USERNAME_RE = /^[a-z][a-z0-9]+$/;
-export const username = mkValidator('username', matches(USERNAME_RE));
