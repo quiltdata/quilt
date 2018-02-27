1e51450ce0170f6c083fb57b5a7ea09d7e461aa3
Aneesh Karve <aneesh.karve@gmail.com>
2018-02-27 10:48:05 -0800

    enumerate keys on mustHaveType, check for empty strings

--- catalog/app/constants/config.js
+++ catalog/app/constants/config.js
@@ -1,28 +1,25 @@
 /* Config - environment-specific parameters */
 /* eslint-disable no-underscore-dangle */
-const config = window.__CONFIG; // eslint-disable-line no-underscore-dangle
+const config = window.__CONFIG;
 Object.freeze(config);
 
-const mustHave = {
+const mustHaveType = {
   alwaysRequireAuth: 'boolean',
   api: 'string',
-  stripeKey: 'string',
   userApi: 'string',
   // eslint-disable-next-line comma-dangle
   signOutUrl: 'string'
 };
 
 // test the config object
-const keys = Object.keys(window.__CONFIG);
-// eslint has good reasons not to like for .. of, so do it the old-fashioned way:
-for (let i = 0; i < keys.length; i += 1) {
-  const k = keys[i];
-  const v = window.__CONFIG[k];
-  // eslint-disable-next-line valid-typeof
-  if (k in mustHave && typeof v !== mustHave[k]) {
-    throw new Error(`Unexpected config['${k}'}]: ${v}`);
+Object.keys(mustHaveType).forEach((key) => {
+  const expectedType = mustHaveType[key];
+  const actualValue = window.__CONFIG[key];
+  const actualType = typeof actualValue;
+  if ((actualType !== expectedType) || (actualType === 'string' && actualValue.length < 1)) {
+    throw new Error(`Unexpected config['${key}'}]: ${actualValue}`);
   }
-}
+});
 
 if (window.__CONFIG.team && !window.__CONFIG.team.id) {
   // eslint-disable-next-line no-console
