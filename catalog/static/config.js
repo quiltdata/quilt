2610a25340a37022f9de36d56f8ea1b5c5150bb6
Aneesh Karve <akarve@users.noreply.github.com>
2018-02-27 12:03:13 -0800

    Show team prefix for package author (#423)

--- catalog/static/config.js
+++ catalog/static/config.js
@@ -25,8 +25,8 @@ if (window.location.hostname === 'quiltdata.com') {
     // team: {
     //   id: "SuperCorp",
     // },
-    // userApi: 'http://localhost:5002/accounts/api-root',
-    // signOutUrl: 'http://localhost:5002/accounts/logout?next=%2F',
+    // userApi: 'http://auth:5002/accounts/api-root',
+    // signOutUrl: 'http://auth:5002/accounts/logout?next=%2F',
 
     // GitHub
     // userApi: 'https://api.github.com/user',
