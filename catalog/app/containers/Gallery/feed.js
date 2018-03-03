536dc5551358e45b40b16c00ac9c66bbf0ef8500
Aneesh Karve <aneesh.karve@gmail.com>
2018-03-02 20:08:12 -0800

    consistent key order

--- catalog/app/containers/Gallery/feed.js
+++ catalog/app/containers/Gallery/feed.js
@@ -1,7 +1,7 @@
 const feed = [
   {
-    owner: 'aicsjackson',
     name: 'prod',
+    owner: 'aicsjackson',
     description: 'This package contains many of the cells seen, imaged, measured, and cataloged by the Allen Institute for Cell Science.',
   },
   {
