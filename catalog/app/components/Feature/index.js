7869b7c7ab8e9e974194542f054decd8042a2453
Aneesh Karve <aneesh.karve@gmail.com>
2018-03-04 15:38:27 -0700

    refresh landing page

--- catalog/app/components/Feature/index.js
+++ catalog/app/components/Feature/index.js
@@ -3,18 +3,17 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 
+import RaisedButton from 'material-ui/RaisedButton';
 import ImageRow from 'components/ImageRow';
-import background from 'img/back/polygon-light.jpg';
-import { breaks } from 'constants/style';
-import TakeAction from 'components/TakeAction';
+import background from 'img/back/black-hex.jpg';
+import config from 'constants/config';
+import { docs } from 'constants/urls';
 
 import strings from './messages';
 
-const xs = `${breaks.sm - 1}px`;
-
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-const Feature = ({ header, tagline, signUp }) => (
-  <ImageRow backgroundColor="#F6B500" src={background}>
+const Feature = ({ header, tagline }) => (
+  <ImageRow backgroundColor="black" src={background}>
     <Content>
       <h1 className="main">
         { header }
@@ -22,17 +21,23 @@ const Feature = ({ header, tagline, signUp }) => (
       <h2 className="main">
         { tagline }
       </h2>
-      <TakeAction signUp={signUp} />
-      <div className="framer">
-        <iframe
-          title="Star Quilt on GitHub"
-          src="https://ghbtns.com/github-btn.html?user=quiltdata&repo=quilt&type=star&count=true&size=large"
-          frameBorder="0"
-          scrolling="0"
-          width="160px"
-          height="30px"
-        />
+      <div className="left">
+        <RaisedButton href={docs} label="Get started" />
       </div>
+      {
+        config.team ? null : (
+          <div className="right">
+            <iframe
+              title="Star Quilt on GitHub"
+              src="https://ghbtns.com/github-btn.html?user=quiltdata&repo=quilt&type=star&count=true&size=large"
+              frameBorder="0"
+              scrolling="0"
+              width="160px"
+              height="30px"
+            />
+          </div>
+        )
+      }
     </Content>
   </ImageRow>
 );
@@ -47,7 +52,6 @@ Feature.defaultProps = {
 Feature.propTypes = {
   header: PropTypes.string,
   tagline: PropTypes.string,
-  signUp: PropTypes.bool,
 };
 
 const Content = styled.div`
@@ -56,45 +60,37 @@ const Content = styled.div`
   position: relative;
   text-align: center;
 
-  br {
-    background-color: red;
-    height: 64px;
-  }
-
   h1.main, h2.main {
-    color: rgba(8, 8, 8, .8);
+    color: white;
+    font-size: 4em;
     margin: 0;
+    text-align: left;
+    text-shadow: 0px 0px 0px black;
+
   }
 
   h1.main {
-    font-size: 5vw;
     font-weight: bold;
+    margin-top: 0;
   }
 
   h2.main {
-    font-size: 5vw;
+    color: #F88500;
     margin-bottom: 64px;
   }
 
-  .framer {
-    position: relative;
+  .left {
+    text-align: left;
+  }
+
+  .right {
     margin-top: 64px;
     text-align: right;
   }
 
-  .framer iframe {
-    display: inline-block
+  iframe {
+    display: inline-block;
     width: 135px;
-  }
-
-  @media (max-width:${xs}) {
-    h1.main {
-      font-size: 1.5em;
-    }
-
-    h2.main {
-      font-size: 1.5em;
-    }
   }
 `;
 
