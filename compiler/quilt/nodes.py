ef0293ae676f0b0d1ca99055a890a3f7b1c56964
Brian <eode@eptitude.net>
2018-03-14 18:05:58 -0400

    Remove _iteritems/_iterpaths, add optional recursion to _items()/_keys()

--- compiler/quilt/nodes.py
+++ compiler/quilt/nodes.py
@@ -47,7 +47,7 @@ class DataNode(Node):
 
     @property
     def _datafile(self):
-        if isinstance(getattr(self, '_node', None), core.FileNode):
+        if isinstance(self._node, core.FileNode):
             return self._data()
 
     def _data(self):
@@ -73,7 +73,18 @@ class GroupNode(DataNode):
         data_info = '\n'.join(sorted(self._data_keys()))
         return '%s\n%s%s' % (pinfo, group_info, data_info)
 
-    def _items(self):
+    def _items(self, recursive=False):
+        """Iterate over paths and nodes in this node
+
+        Yields child `(path, node)` pairs.  Paths are slash-separated
+        strings.  If `recursive` is True, also yields all child nodes
+        and their children, recursively.
+
+        :param recursive: recursively include child nodes, as well
+        :returns: iterator of (<path string>, <node>) pairs
+        """
+        if recursive:
+            return self.__iteritems(self, pathlib.PurePath(), recursive)
         return ((name, child) for name, child in iteritems(self.__dict__)
                 if not name.startswith('_'))
 
@@ -89,10 +100,25 @@ class GroupNode(DataNode):
         """
         return [name for name, child in self._items() if isinstance(child, GroupNode)]
 
-    def _keys(self):
+    def _keys(self, recursive=False, iterator=False):
+        """Iterate over paths in this node
+
+        Returns iterable of keys directly accessible on this object via
+        getattr or dot.notation.
+
+        If `recursive` is True, also yields key paths for child nodes and
+        their children, recursively, as slash-separated strings.
+
+        :param recursive: recursively include child nodes, as well
+        :param iterator: return an iterator if True, list otherwise
+        :returns: iterable (list or iterator) of path strings
         """
-        keys directly accessible on this object via getattr or .
-        """
+        if recursive or iterator:
+            items = self.__iteritems(self, pathlib.PurePath(), recursive)
+            result = (str(path) for path, node in items)
+            if iterator:
+                return result
+            return list(result)
         return [name for name in self.__dict__ if not name.startswith('_')]
 
     def _add_group(self, groupname):
@@ -101,7 +127,8 @@ class GroupNode(DataNode):
 
     @classmethod
     def __iteritems(cls, node, base_path, recursive=False):
-        # worker for _iteritems and _iterpaths.
+        # double underscore since we can't mark methods private with a single underscore
+        # worker for _items and _keys recursion
         assert isinstance(base_path, pathlib.PurePath)
         assert isinstance(node, GroupNode)
 
@@ -111,33 +138,6 @@ class GroupNode(DataNode):
             if recursive and isinstance(child_node, GroupNode):
                 for subpath, subnode in cls.__iteritems(child_node, child_path, recursive):
                     yield subpath, subnode
-
-    def _iteritems(self, recursive=False):
-        """Iterate over paths and nodes in this node
-
-        Yields child `(path, node)` pairs.  Paths are slash-separated
-        strings.  If `recursive` is True, also yields all child nodes
-        and their children, recursively.
-
-        :param recursive: iterate recursively over child nodes as well
-        :returns: iterator of (<path string>, <node>) pairs
-        """
-        for path, node in self.__iteritems(self, pathlib.PurePath(), recursive):
-            yield str(path), node
-
-    def _iterpaths(self, recursive=False):
-        """Iterate over paths in this node
-
-        Yields paths in this node, as slash-separated strings.  If
-        `recursive` is True, also yields all child nodes and their
-        children, recursively.
-
-        :param recursive: iterate recursively over child nodes, as well
-        :returns: iterator of path strings
-        """
-        # Only _iteritems was needed, but _iterpaths is a gimme.
-        for path, node in self.__iteritems(self, pathlib.PurePath(), recursive):
-            yield str(path)
 
 
 class PackageNode(GroupNode):
