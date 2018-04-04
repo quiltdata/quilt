5e104c535ece7ac156638bb57b19eeb109d22b48
Dima Ryazanov <dima@gmail.com>
2018-04-03 19:04:23 -0700

    Use wordsegment instead

--- registry/quilt_server/search.py
+++ registry/quilt_server/search.py
@@ -5,14 +5,26 @@ Full-text search helper functions.
 """
 
 from functools import reduce
-import os
 import re
 
 import sqlalchemy as sa
-from sqlalchemy.dialects.postgresql import TSVECTOR
+import wordsegment
 
 from .const import FTS_LANGUAGE
 from .core import GroupNode
+
+try:
+    import uwsgi  # pylint:disable=W0611
+
+    # If we're running in uwsgi, load word segment data at import time, before uwsgi forks.
+    # That way, the 100MB of data is shared between worker processes (copy-on-write, etc.)
+
+    # In theory, this shouldn't actually work because ref-counting would modify the data.
+    # In practice, it appears to work?
+
+    wordsegment.load()
+except ImportError:
+    pass
 
 def tsvector_concat(*args):
     return reduce(sa.sql.operators.custom_op('||'), args)
@@ -23,22 +35,33 @@ def find_node_keywords(obj):
             yield name
             yield from find_node_keywords(child)
 
-TOKEN_RE = re.compile(r'[a-z]+|\d+', re.I)
-
-with open(os.path.join(os.path.dirname(__file__), 'resources', 'words.txt')) as fd:
-    DICTIONARY = frozenset(w.strip().lower() for w in fd)
+ALPHA_NUM_RE = re.compile(r'[a-z]+|\d+', re.I)
 
 def tokenize(string):
-    min_length = 3
-    for match in TOKEN_RE.finditer(string):
-        value = match.group()
-        yield value
-        length = len(value)
-        for start in range(0, length - min_length + 1):
-            for end in range(start + min_length, length + 1):
-                substr = value[start:end]
-                if substr != value and substr in DICTIONARY:
-                    yield substr
+    """
+    Split the input several times, returning intermediate results at each level:
+    - delimited by underscores
+    - letter/number boundaries
+    - word segments
+    E.g., tokenize('landuse_austin_tx_24dates') ->
+        ['landuse', 'land', 'use', 'austin', 'tx', '24dates', '24', 'dates']
+
+    (Don't need a token for the original string because to_tsvector splits on underscores.)
+    """
+    if not wordsegment.BIGRAMS:
+        # Should only happen in dev.
+        wordsegment.load()
+
+    lvl1_parts = string.split('_')
+    for lvl1 in lvl1_parts:
+        lvl2_parts = ALPHA_NUM_RE.findall(lvl1)
+        if len(lvl2_parts) > 1:
+            yield lvl1
+        for lvl2 in lvl2_parts:
+            lvl3_parts = wordsegment.segment(lvl2)
+            if len(lvl3_parts) > 1:
+                yield lvl2
+            yield from lvl3_parts
 
 def keywords_tsvector(owner, name, contents):
     full_name = '%s/%s' % (owner, name)
@@ -46,25 +69,9 @@ def keywords_tsvector(owner, name, contents):
     def _to_tsvector(keywords):
         return sa.func.to_tsvector(FTS_LANGUAGE, ' '.join(keywords))
 
-    # "tokenize" may return keywords that will become duplicates, such as "dog" and "dogs",
-    # giving them more weight during search ranking. Clean them up, and reset their position to 1.
-    # "dogscats dog dogs cat cats" -> "dogscat:1 dog:1 cat:1"
-    # TODO(dima): Upgrade to Postgres 10 to make this less painful.
-    def _keywords_tsvector(keywords):
-        return sa.select([
-            sa.func.coalesce(
-                sa.func.array_to_string(
-                    sa.func.array_agg(sa.column('lexeme') + ':1'), ' '
-                ), ''
-            )
-            .cast(TSVECTOR)
-        ]).select_from(
-            sa.func.unnest(_to_tsvector(keywords))
-        )
-
     return tsvector_concat(
         sa.func.setweight(_to_tsvector([full_name, owner, name]), 'A'),
-        sa.func.setweight(_keywords_tsvector(tokenize(owner)), 'B'),
-        sa.func.setweight(_keywords_tsvector(tokenize(name)), 'B'),
+        sa.func.setweight(_to_tsvector(tokenize(owner)), 'B'),
+        sa.func.setweight(_to_tsvector(tokenize(name)), 'B'),
         sa.func.setweight(_to_tsvector(find_node_keywords(contents)), 'C')
     )
