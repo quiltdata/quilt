21f2e87ab27f5841c86a2eb19dee17c6d6d3f7ae
nl 0 <nl.imbecile@gmail.com>
2018-05-14 23:27:14 +0500

    utils/immutableTools: remove(), removeIn(), fromJS() (#589)
    * utils/immutableTools: add remove and removeIn helpers
    
    * utils/immutableTools: fromJS

--- catalog/app/utils/immutableTools.js
+++ catalog/app/utils/immutableTools.js
@@ -1,3 +1,5 @@
+import { fromJS as iFromJS } from 'immutable';
+
 export const invoke = (method) => (...args) => (obj) =>
   obj && method in obj ? obj[method](...args) : obj;
 
@@ -6,6 +8,9 @@ export const get = invoke('get');
 export const getIn = invoke('getIn');
 export const update = invoke('update');
 export const updateIn = invoke('updateIn');
+export const remove = invoke('remove');
+export const removeIn = invoke('removeIn');
 export const sortBy = invoke('sortBy');
 export const push = invoke('push');
 export const toJS = invoke('toJS');
+export const fromJS = (...args) => (obj) => iFromJS(obj, ...args);
