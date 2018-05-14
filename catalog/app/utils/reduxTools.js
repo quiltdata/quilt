355640d9ef6ae7604a18eefbe38f8b24059e7ed5
nl 0 <nl.imbecile@gmail.com>
2018-05-14 23:21:36 +0500

    utils/reduxTools: refactoring and new helpers (#599)
    * `unset` and `noop` constants
    
    * rename `composeHandlers` => `handleActions` and adjust behaviour
    
    * `handleTransitions` helper
    
    * `withInitialState` helper
    
    * adjust dependent code

--- catalog/app/utils/reduxTools.js
+++ catalog/app/utils/reduxTools.js
@@ -1,16 +1,28 @@
+import get from 'lodash/get';
 import isFunction from 'lodash/isFunction';
 
-const reduceHandlers = ({ payload, meta, ...rest }) => (state, [key, handler]) =>
-  state.update(key, (val) => {
-    if (!isFunction(handler)) return handler;
-    const updater = handler(payload, meta, rest);
-    return isFunction(updater) ? updater(val) : updater;
-  });
+export const unset = Symbol('reduxTools/unset');
+export const noop = Symbol('reduxTools/noop');
 
-export const composeHandlers = (initial, handlers) => (state = initial, action) =>
-  action.type in handlers
-    ? Object.entries(handlers[action.type]).reduce(reduceHandlers(action), state)
-    : state;
+const handleAction = (getHandlers) => (state, action) =>
+  Object.entries(getHandlers(action, state) || {}).reduce((acc, [key, handler]) => {
+    const updater = isFunction(handler) ? handler(action.payload, action.meta, action) : handler;
+    const updated = isFunction(updater) ? updater(acc.get(key)) : updater;
+    switch (updated) {
+      case noop: return acc;
+      case unset: return acc.remove(key);
+      default: return acc.set(key, updated);
+    }
+  }, state);
+
+export const handleActions = (handlers) =>
+  handleAction(({ type }) => handlers[type]);
+
+export const handleTransitions = (getState, handlers) =>
+  handleAction(({ type }, state) => get(handlers, [getState(state), type]));
 
 export const createActions = (scope, ...actions) =>
   actions.reduce((acc, action) => ({ ...acc, [action]: `${scope}/${action}` }), {});
+
+export const withInitialState = (initialState, reducer) =>
+  (state = initialState, action) => reducer(state, action);
