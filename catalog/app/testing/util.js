bf028d43cc02d17d586d4d0d1133de32c669ca70
nl_0 <nl.imbecile@gmail.com>
2018-06-13 02:45:30 +0500

    testing/util: better react utils

--- catalog/app/testing/util.js
+++ catalog/app/testing/util.js
@@ -1,6 +1,6 @@
 import id from 'lodash/identity';
 import pick from 'lodash/pick';
-import React from 'react';
+import { createElement } from 'react';
 import { take } from 'redux-saga/effects';
 
 export const getLocation = ({ location: l }) =>
@@ -9,11 +9,28 @@ export const getLocation = ({ location: l }) =>
 const mapProps = (props, mapper = id) =>
   mapper instanceof Array ? pick(props, mapper) : mapper(props);
 
-export const mockComponent = (name, mapper) => (props) =>
-  <div __name={name} {...mapProps(props, mapper)} />;
+const defaultRender = ({ children }) => [children];
 
-export const findMockComponent = (html, name) =>
-  html.find(`[__name="${name}"]`);
+const renderProps = (props) =>
+  Object.entries(props).map(([key, value]) =>
+    createElement('div', { __prop: key }, value));
+
+const mapRender = (props, mapper = defaultRender) =>
+  mapper instanceof Array ? renderProps(pick(props, mapper)) : mapper(props);
+
+export const mockComponent = (name, propMapper, renderMapper) => (props) =>
+  createElement(
+    'div',
+    { __name: name, ...mapProps(props, propMapper) },
+    ...mapRender(props, renderMapper)
+  );
+
+export const findMockComponent = (html, name, prop) => {
+  const comp = html.find(`[__name="${name}"]`);
+  return prop
+    ? comp.find(`[__prop="${prop}"]`)
+    : comp;
+};
 
 export const flushPromises = () => new Promise((resolve) => setImmediate(resolve));
 
