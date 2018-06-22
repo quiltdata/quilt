f8f92d279ed42c518cd02dfaf908b94d9e5627fc
nl_0 <nl.imbecile@gmail.com>
2018-06-22 22:55:31 +0500

    containers/Auth/tests/support/requests: take req map as an argument

--- catalog/app/containers/Auth/tests/support/requests.js
+++ catalog/app/containers/Auth/tests/support/requests.js
@@ -4,89 +4,16 @@ import defer from 'utils/defer';
 import { step } from 'testing/feature';
 import { flushPromises } from 'testing/util';
 
-import { tokens, tokensRaw, tokensStale, user, api } from './fixtures';
-
-
-const requests = {
-  signUp: {
-    setup: () => ['postOnce', '/register'],
-    expect: (ctx) =>
-      expect.objectContaining({
-        body: JSON.stringify(ctx.credentials),
-      }),
-  },
-  resetPassword: {
-    setup: () => ['postOnce', '/reset_password'],
-    expect: ({ email }) =>
-      expect.objectContaining({
-        body: JSON.stringify({ email }),
-      }),
-  },
-  changePassword: {
-    setup: () => ['postOnce', '/reset_password'],
-    expect: ({ link, password }) =>
-      expect.objectContaining({
-        body: JSON.stringify({ link, password }),
-      }),
-  },
-  getCode: {
-    setup: () => ['getOnce', '/api/code'],
-    expect: () =>
-      expect.objectContaining({
-        headers: expect.objectContaining({
-          Authorization: `Bearer ${tokens.token}`,
-        }),
-      }),
-    success: () => ({ code: 'the code' }),
-  },
-  refreshTokens: {
-    setup: () => ['postOnce', '/api/refresh'],
-    expect: () =>
-      expect.objectContaining({
-        headers: expect.objectContaining({
-          Authorization: `Bearer ${tokensStale.token}`,
-        }),
-      }),
-    success: () => tokensRaw,
-  },
-  signIn: {
-    setup: () => ['postOnce', '/login'],
-    expect: (ctx) =>
-      expect.objectContaining({
-        body: JSON.stringify(ctx.credentials),
-      }),
-    success: () => tokensRaw,
-  },
-  fetchUser: {
-    setup: () => ['getOnce', '/api-root'],
-    expect: () =>
-      expect.objectContaining({
-        headers: expect.objectContaining({
-          Authorization: `Bearer ${tokens.token}`,
-        }),
-      }),
-    success: () => user,
-  },
-  signOut: {
-    setup: () => ['postOnce', '/logout'],
-    expect: () =>
-      expect.objectContaining({
-        body: JSON.stringify({ token: tokens.token }),
-        headers: expect.objectContaining({
-          Authorization: `Bearer ${tokens.token}`,
-        }),
-      }),
-  },
-};
+import { api } from './fixtures';
 
 
 const defaultSuccess = () => ({ sendAsJson: false });
 
-export default [
+export default (requests) => [
   step(/(.+) request is expected/, (ctx, name) => {
     const result = defer();
-    const [method, endpoint] = requests[name].setup(ctx);
-    fetchMock[method](`${api}${endpoint}`, result.promise, { name });
+    const [method, endpoint, opts] = requests[name].setup(ctx);
+    fetchMock[method](`${api}${endpoint}`, result.promise, { name, ...opts });
     return {
       ...ctx,
       requestResolvers: {
