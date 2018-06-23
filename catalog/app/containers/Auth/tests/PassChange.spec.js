adf76630f7ad7996da59f3171f8aa85f180fe7f2
nl_0 <nl.imbecile@gmail.com>
2018-06-24 02:12:05 +0500

    Auth: PassChange spec: use reusable steps

--- catalog/app/containers/Auth/tests/PassChange.spec.js
+++ catalog/app/containers/Auth/tests/PassChange.spec.js
@@ -1,6 +1,3 @@
-/* eslint-disable import/first, global-require */
-
-import { mount } from 'enzyme';
 import createHistory from 'history/createMemoryHistory';
 import { fromJS } from 'immutable';
 import React from 'react';
@@ -12,10 +9,7 @@ import StoreProvider from 'utils/StoreProvider';
 import configureStore from 'store';
 
 import feature from 'testing/feature';
-import {
-  mockComponentSelector,
-  getPropName,
-} from 'testing/util';
+import { mockComponentSelector } from 'testing/util';
 
 import {
   Provider as AuthProvider,
@@ -23,43 +17,20 @@ import {
   errors,
 } from '..';
 
-import {
-  api,
-  date,
-} from './support/fixtures';
+import { api } from './support/fixtures';
+import errorSteps from './support/error';
+import formSteps from './support/form';
+import reactSteps from './support/react';
 import requestsSteps from './support/requests';
 
-jest.mock('constants/config', () => ({}));
+jest.mock('material-ui/RaisedButton');
+jest.mock('material-ui/TextField');
+jest.mock('react-router-dom');
 
-jest.mock('utils/time');
-import { timestamp } from 'utils/time';
-
+jest.mock('components/Spinner');
+jest.mock('constants/config');
 jest.mock('utils/errorReporting');
-import { captureError } from 'utils/errorReporting';
-
-jest.mock('material-ui/RaisedButton', () =>
-  require('testing/util').mockComponent('RaisedButton', {
-    children: ['label', 'children'],
-  }));
-jest.mock('material-ui/TextField', () =>
-  require('testing/util').mockComponentClass('TextField', {
-    children: ['floatingLabelText', 'errorText'],
-    methods: {
-      getInputNode() { return null; },
-    },
-  }));
-
-jest.mock('react-router-dom', () => ({
-  Link: require('testing/util').mockComponent('Link', { el: 'a' }),
-}));
-
-jest.mock('components/Spinner', () =>
-  require('testing/util').mockComponent('Spinner'));
-
-const values = {
-  password: 'valid',
-  'invalid password': 'invalid',
-};
+jest.mock('utils/time');
 
 const requests = {
   changePassword: {
@@ -71,6 +42,64 @@ const requests = {
   },
 };
 
+const values = {
+  password: 'valid',
+  'invalid password': 'invalid',
+};
+
+const fields = ['password', 'passwordCheck'];
+
+const onSubmit = (ctx) => ({
+  ...ctx,
+  password:
+    ctx.mounted
+      .find(`${mockComponentSelector('TextField')}[name="password"]`)
+      .prop('value'),
+});
+
+const screens = {
+  success: (html) => {
+    const msgs = html.find('p');
+    expect(msgs.eq(0).text()).toMatch('Your password has been changed');
+    expect(msgs.eq(1).text()).toMatch('Now you can sign in');
+    expect(msgs.eq(1).find(mockComponentSelector('Link')).attr('to')).toBe('/signin');
+  },
+};
+
+const setup = () => {
+  const history = createHistory({ initialEntries: ['/'] });
+  const store = configureStore(fromJS({}), history);
+  const storage = {
+    load: () => ({}),
+    set: () => {},
+    remove: () => {},
+  };
+  const link = 'test-link';
+  const tree = (
+    <StoreProvider store={store}>
+      <FormProvider>
+        <LanguageProvider messages={messages}>
+          <AuthProvider
+            storage={storage}
+            api={api}
+          >
+            <PassChange match={{ params: { link } }} />
+          </AuthProvider>
+        </LanguageProvider>
+      </FormProvider>
+    </StoreProvider>
+  );
+  return { tree, link };
+};
+
+const steps = [
+  ...requestsSteps(requests),
+  ...formSteps({ values, fields, onSubmit }),
+  ...errorSteps(errors.AuthError),
+  ...reactSteps({ setup, screens }),
+];
+
+
 feature('containers/Auth/PassChange')
   .scenario('Trying to change the password when the link is invalid')
 
@@ -78,11 +107,13 @@ feature('containers/Auth/PassChange')
 
   .when('the component tree is mounted')
   .then('I should see the form in interactive state')
+  .then('the rendered markup should match the snapshot')
 
   .when('I submit the form')
+  .then('I should see the form in invalid state')
+  .then('the rendered markup should match the snapshot')
   .then('I should see error on password field: "Enter a password"')
   .then('I should see error on passwordCheck field: "Enter the password again"')
-  .then('I should see the form in error state')
 
   .when('I enter password into password field')
   .then('I should see no error on password field')
@@ -92,27 +123,28 @@ feature('containers/Auth/PassChange')
 
   .when('I enter password into passwordCheck field')
   .then('I should see no error on passwordCheck field')
+  .then('I should see the form in valid state')
 
   .when('I submit the form')
   .then('I should see the form in waiting state')
   .then('changePassword request should be made')
 
   .when('changePassword request fails with 400, error: "Invalid link."')
-  .then('I should see the form in error state')
+  .then('I should see the form in invalid state')
+  .then('the rendered markup should match the snapshot')
   .then('I should see form error: "This reset link is invalid"')
-  .then('I should see submit button disabled')
 
   .back()
   .when('changePassword request fails with 404, error: "User not found."')
-  .then('I should see the form in error state')
+  .then('I should see the form in invalid state')
+  .then('the rendered markup should match the snapshot')
   .then('I should see form error: "This reset link is invalid"')
-  .then('I should see submit button disabled')
 
   .back()
   .when('changePassword request fails with 500')
-  .then('I should see the form in error state')
+  .then('I should see the form in invalid state')
+  .then('the rendered markup should match the snapshot')
   .then('I should see form error: "Something went wrong"')
-  .then('I should see submit button disabled')
   .then('the error should be captured')
 
 
@@ -121,8 +153,6 @@ feature('containers/Auth/PassChange')
   .given('changePassword request is expected')
 
   .when('the component tree is mounted')
-  .then('I should see the form in interactive state')
-
   .when('I enter invalid password into password field')
   .when('I enter invalid password into passwordCheck field')
   .when('I submit the form')
@@ -130,155 +160,21 @@ feature('containers/Auth/PassChange')
   .then('changePassword request should be made')
 
   .when('changePassword request fails with 400, error: "Password must be"')
-  .then('I should see the form in error state')
+  .then('I should see the form in invalid state')
+  .then('the rendered markup should match the snapshot')
   .then('I should see error on password field: "Password must be"')
-  .then('I should see submit button disabled')
 
   .when('I enter password into password field')
   .then('I should see no error on password field')
 
   .when('I enter password into passwordCheck field')
   .then('I should see no error on passwordCheck field')
+  .then('I should see the form in valid state')
 
   .when('I submit the form')
   .when('changePassword request succeeds')
-  .then('I should see the success message')
+  .then('I should see the success screen')
+  .then('the rendered markup should match the snapshot')
 
-  .steps([...requestsSteps(requests)])
-
-  .step(/I (re-)?enter (.+) into (.+) field/, (ctx, re, valueName, fieldName) => {
-    const field = ctx.mounted.find(`${mockComponentSelector('TextField')}[name="${fieldName}"]`);
-    const value = values[valueName];
-    field.simulate('focus');
-    if (re) field.simulate('change', { target: { value: '' } });
-    field.simulate('change', { target: { value } });
-    field.simulate('blur');
-  })
-
-  .step(/I submit the form/, (ctx) => {
-    const password = ctx.mounted.find(`${mockComponentSelector('TextField')}[name="password"]`).prop('value');
-    ctx.mounted.find('form').simulate('submit');
-    return { ...ctx, password };
-  })
-
-  .step(/I should see no error on (.+) field/, (ctx, fieldName) => {
-    const field = ctx.mounted
-      .find(`${mockComponentSelector('TextField')}[name="${fieldName}"]`);
-    expect(field.find(getPropName('errorText'))).toHaveLength(0);
-  })
-
-  .step(/I should see error on (.+) field: "(.+)"/, (ctx, fieldName, errorText) => {
-    ctx.mounted.update();
-    const field = ctx.mounted
-      .find(`${mockComponentSelector('TextField')}[name="${fieldName}"]`);
-    expect(field.find(getPropName('errorText')).text()).toMatch(errorText);
-  })
-
-  .step(/I should see form error: "(.+)"/, (ctx, errorText) => {
-    const html = ctx.mounted.render();
-    expect(html.find(`${mockComponentSelector('TextField')} + p`).text())
-      .toMatch(errorText);
-  })
-
-  .step(/I should see no form error/, (ctx) => {
-    const html = ctx.mounted.render();
-    expect(html.find(`${mockComponentSelector('TextField')} + p`)).toHaveLength(0);
-  })
-
-  .step(/I should see the form in interactive state/, (ctx) => {
-    const html = ctx.mounted.render();
-    expect(html).toMatchSnapshot();
-
-    const fields = ctx.mounted.find(mockComponentSelector('TextField'));
-    const passwordField = fields.at(0);
-    const passwordCheckField = fields.at(1);
-
-    expect(passwordField.prop('name')).toBe('password');
-    expect(passwordField.prop('disabled')).toBe(false);
-
-    expect(passwordCheckField.prop('name')).toBe('passwordCheck');
-    expect(passwordCheckField.prop('disabled')).toBe(false);
-
-    expect(ctx.mounted.find(mockComponentSelector('RaisedButton', 'label')).text())
-      .toBe('Change Password');
-    expect(ctx.mounted.find(mockComponentSelector('RaisedButton')).prop('disabled'))
-      .toBe(false);
-  })
-
-  .step(/I should see the form in error state/, (ctx) => {
-    const html = ctx.mounted.render();
-    expect(html).toMatchSnapshot();
-
-    expect(ctx.mounted.find(mockComponentSelector('RaisedButton')).prop('disabled'))
-      .toBe(true);
-  })
-
-  .step(/I should see the form in waiting state/, (ctx) => {
-    const html = ctx.mounted.render();
-    expect(html).toMatchSnapshot();
-
-    const fields = ctx.mounted.find(mockComponentSelector('TextField'));
-    const passwordField = fields.at(0);
-    const passwordCheckField = fields.at(1);
-
-    expect(passwordField.prop('disabled')).toBe(true);
-    expect(passwordCheckField.prop('disabled')).toBe(true);
-
-    const btn = ctx.mounted.find(mockComponentSelector('RaisedButton'));
-    expect(btn.prop('disabled')).toBe(true);
-    expect(btn.find(`${getPropName('children')} ${mockComponentSelector('Spinner')}`))
-      .toHaveLength(1);
-  })
-
-  .step(/I should see submit button enabled/, (ctx) => {
-    expect(ctx.mounted.find(mockComponentSelector('RaisedButton')).prop('disabled'))
-      .toBe(false);
-  })
-
-  .step(/I should see submit button disabled/, (ctx) => {
-    expect(ctx.mounted.find(mockComponentSelector('RaisedButton')).prop('disabled'))
-      .toBe(true);
-  })
-
-  .step(/I should see the success message/, (ctx) => {
-    const html = ctx.mounted.render();
-    const msgs = html.find('p');
-    expect(msgs.eq(0).text()).toMatch('Your password has been changed');
-    expect(msgs.eq(1).text()).toMatch('Now you can sign in');
-    expect(msgs.eq(1).find(mockComponentSelector('Link')).attr('to')).toBe('/signin');
-    expect(html).toMatchSnapshot();
-  })
-
-  .step(/the error should be captured/, () => {
-    expect(captureError).toBeCalledWith(expect.any(errors.AuthError));
-  })
-
-  .step(/the component tree is mounted/, (ctx) => {
-    const history = createHistory({ initialEntries: ['/'] });
-    const store = configureStore(fromJS({}), history);
-    const storage = {
-      load: () => ({}),
-      set: () => {},
-      remove: () => {},
-    };
-    const link = 'test-link';
-    const tree = (
-      <StoreProvider store={store}>
-        <FormProvider>
-          <LanguageProvider messages={messages}>
-            <AuthProvider
-              storage={storage}
-              api={api}
-            >
-              <PassChange match={{ params: { link } }} />
-            </AuthProvider>
-          </LanguageProvider>
-        </FormProvider>
-      </StoreProvider>
-    );
-    timestamp.mockReturnValue(date);
-    const mounted = mount(tree);
-    return { ...ctx, mounted, link };
-  })
-
+  .steps(steps)
   .run();
