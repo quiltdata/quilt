c5f5562fd90976d7bd743e06fef21c303b0526b4
nl_0 <nl.imbecile@gmail.com>
2018-07-01 20:49:16 +0500

    server: serve config in production mode

--- catalog/server/middlewares/frontendMiddleware.js
+++ catalog/server/middlewares/frontendMiddleware.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 const compression = require('compression');
 const pkg = require(path.resolve(process.cwd(), 'package.json'));
+const serveConfig = require('./config');
 
 // Dev middleware
 const addDevMiddlewares = (app, webpackConfig) => {
-  const serveConfig = require('./config');
   app.get('/config.js',
     serveConfig(path.resolve(process.cwd(), 'config.js.tmpl'), process.env));
 
@@ -49,11 +49,13 @@ const addDevMiddlewares = (app, webpackConfig) => {
 const addProdMiddlewares = (app, options) => {
   const publicPath = options.publicPath || '/';
   const outputPath = options.outputPath || path.resolve(process.cwd(), 'build');
+  const configPath = options.configPath || path.resolve(process.cwd(), 'config.js.tmpl');
 
   // compression middleware compresses your server responses which makes them
   // smaller (applies also to assets). You can read more about that technique
   // and other good practices on official Express.js docs http://mxs.is/googmy
   app.use(compression());
+  app.get('/config.js', serveConfig(configPath, process.env));
   app.use(publicPath, express.static(outputPath));
 
   app.get('*', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')));
