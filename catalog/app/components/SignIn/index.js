0e394d010032c494fe604f55c02a00a621d7b255
nl_0 <nl.imbecile@gmail.com>
2018-07-12 16:26:14 +0500

    components/SignIn: fix display issue, refactor

--- catalog/app/components/SignIn/index.js
+++ catalog/app/components/SignIn/index.js
@@ -1,48 +1,50 @@
 /* SignIn */
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
-import PropTypes from 'prop-types';
+import PT from 'prop-types';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
+import { setPropTypes } from 'recompose';
 
 import MIcon from 'components/MIcon';
 import Spinner from 'components/Spinner';
 import { authButtonStyle } from 'constants/style';
+import { composeComponent } from 'utils/reactTools';
 
 import strings from './messages';
 
-const SignIn = ({ error, useNavStyle, waiting }) => {
-  if (waiting) {
-    return <Spinner className="fa-2x" drop="8px" />;
-  }
-  let icon = null;
-  if (error) {
-    const title = `${error.message}\n${JSON.stringify(error)}`;
-    icon = <MIcon drop="6px" title={title}>error_outline</MIcon>;
-  }
-  const Button = useNavStyle ? FlatButton : RaisedButton;
-  return (
-    <div>
-      {icon}
-      <Button
-        containerElement={<Link to="/signin" />}
-        style={useNavStyle ? authButtonStyle : undefined}
-      >
-        <FormattedMessage {...strings.logIn} />
-      </Button>
-    </div>
-  );
-};
-
-SignIn.propTypes = {
-  error: PropTypes.object,
-  useNavStyle: PropTypes.bool,
-  waiting: PropTypes.bool.isRequired,
-};
-
-SignIn.defaultProps = {
-  useNavStyle: true,
-};
-
-export default SignIn;
+export default composeComponent('SignIn',
+  setPropTypes({
+    error: PT.object,
+    useNavStyle: PT.bool,
+    waiting: PT.bool.isRequired,
+  }),
+  ({ error, useNavStyle = true, waiting }) => {
+    if (waiting) {
+      return <Spinner className="fa-2x" />;
+    }
+    const Button = useNavStyle ? FlatButton : RaisedButton;
+    const btnStyle = useNavStyle ? authButtonStyle : undefined;
+    return (
+      <div>
+        {error
+          ? (
+            <MIcon
+              title={`${error.message}\n${JSON.stringify(error)}`}
+              style={{ verticalAlign: 'middle' }}
+            >
+              error_outline
+            </MIcon>
+          )
+          : null
+        }
+        <Button
+          containerElement={<Link to="/signin" />}
+          style={{ ...btnStyle, verticalAlign: 'middle' }}
+        >
+          <FormattedMessage {...strings.logIn} />
+        </Button>
+      </div>
+    );
+  });
