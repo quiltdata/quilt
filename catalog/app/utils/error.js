da8e70873748db32ed57668148985741a0028a0b
nl_0 <nl.imbecile@gmail.com>
2018-07-14 19:21:50 +0500

    utils/error: ErrorDisplay, rm makeError

--- catalog/app/utils/error.js
+++ catalog/app/utils/error.js
@@ -1,31 +1,14 @@
-/* Quilt Error constructor with fields designed to to feed <Error />
- * via object rest spread */
-export default function makeError(primary, secondary = '', response = {}) {
-  const error = new Error(primary);
-  // need to do this explicitly so that object rest spread will catch it
-  // when it goes into <Error />
-  error.headline = primary;
-  error.detail = secondary;
-  // Response objects do not work with JSON.stringify() so we copy
-  // some fields into a fresh Object
-  error.object = {
-    ok: response.ok,
-    redirected: response.redirected,
-    status: response.status,
-    statusText: response.statusText,
-    type: response.type,
-    url: response.url,
-  };
-
-  return error;
-}
-
 /**
- * Extensible error class
+ * Extensible error class.
  */
 export class BaseError {
   static displayName = 'BaseError';
 
+  /**
+   * @param {string} message Message to pass to the Error constructor.
+   *
+   * @param {Object} props Properties to assign to the created instance.
+   */
   constructor(msg, props) {
     const e = new Error(msg);
     Object.setPrototypeOf(e, Object.getPrototypeOf(this));
@@ -41,3 +24,21 @@ Object.defineProperty(BaseError.prototype, 'name', {
   enumerable: false,
   get() { return this.constructor.displayName; },
 });
+
+/**
+ * Error class with fields designed to feed <Error /> via object rest spread.
+ */
+export class ErrorDisplay extends BaseError {
+  static displayName = 'ErrorDisplay';
+
+  /**
+   * @param {string} headline
+   *
+   * @param {string} detail
+   *
+   * @param {Object} object
+   */
+  constructor(headline, detail, object) {
+    super(headline, { headline, detail, object });
+  }
+}
