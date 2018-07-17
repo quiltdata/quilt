22bfbf078381c6bd91b4655f22f9156aed01135a
Kevin Moore <kevin@quiltdata.io>
2018-07-16 18:49:22 -0700

    Allow insecure catalog connection
    Adding logic to allow overriding the default catalog URL, which assumes HTTPS. Print a warning if the URL isn't HTTPS.

--- registry/quilt_server/prod_config.py
+++ registry/quilt_server/prod_config.py
@@ -11,7 +11,9 @@ REGISTRY_HOST = os.environ['REGISTRY_HOST']
 REGISTRY_URL = os.environ['REGISTRY_URL']
 CATALOG_HOST = os.environ['CATALOG_HOST']
 
-CATALOG_URL = 'https://%s' % CATALOG_HOST
+CATALOG_URL = os.environ.get('CATALOG_URL', 'https://%s' % CATALOG_HOST)
+if not CATALOG_URL.startswith("https"):
+    print("WARNING: INSECURE CONNECTION TO CATALOG")
 
 PACKAGE_BUCKET_NAME = os.environ['PACKAGE_BUCKET_NAME']
 
