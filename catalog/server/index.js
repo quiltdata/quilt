ce247e819f7d868d961de75e9242a0918790a928
nl_0 <nl.imbecile@gmail.com>
2018-08-09 20:40:04 +0500

    server: fix new eslint warnings

--- catalog/server/index.js
+++ catalog/server/index.js
@@ -2,14 +2,18 @@
 
 require('dotenv').config();
 
-const express = require('express');
-const logger = require('./logger');
-
-const argv = require('minimist')(process.argv.slice(2));
-const setup = require('./middlewares/frontendMiddleware');
-const isDev = process.env.NODE_ENV !== 'production';
-const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const { resolve } = require('path');
+
+const express = require('express');
+const argv = require('minimist')(process.argv.slice(2));
+
+const logger = require('./logger');
+const setup = require('./middlewares/frontendMiddleware');
+
+const isDev = process.env.NODE_ENV !== 'production';
+// eslint-disable-next-line import/order
+const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
+
 const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
--- catalog/server/logger.js
+++ catalog/server/logger.js
@@ -27,8 +27,8 @@ const logger = {
     console.log(`
 ${chalk.bold('Access URLs:')}${divider}
 Localhost: ${chalk.magenta(`http://${host}:${port}`)}
-      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
-(tunnelStarted ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}` : '')}${divider}
+      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`)
++ (tunnelStarted ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}` : '')}${divider}
 ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
     `);
   },
