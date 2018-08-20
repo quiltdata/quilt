c5a4a243ec534ec44389ce9db81e04b4df8eb6b3
Diego Scarabelli <diego@rigetti.com>
2018-08-20 14:34:33 -0700

    Fix bug

--- compiler/quilt/util.py
+++ compiler/quilt/util.py
@@ -9,7 +9,7 @@ from .tools.command import build_from_node, build, push
 def save(package, data, params={}, is_public=False):
     """Build and push data to Quilt registry at user/package/data_node,
     associating params as metadata for the data node.
-    :param package: short package specifier string, i.e. 'team:user/pkg'
+    :param package: short package specifier string, i.e. 'team:user/pkg/subpath'
     :param data: data to save (np.ndarray or pd.DataFrame)
     :param params: metadata dictionary
     :param is_public: boolean kwarg to push the packages publicly
@@ -19,6 +19,5 @@ def save(package, data, params={}, is_public=False):
             value = value.astype(float)
             params[key] = value.tolist()
     build_from_node(package, nodes.DataNode(None, None, data, params))
-    build('{}'.format(package))
     push('{}'.format(package), is_public=is_public)
 
