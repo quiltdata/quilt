016423859e1f4c911ffbfe49f496e6c2fa5ad44b
Brian <eode@eptitude.net>
2018-10-20 01:56:06 -0400

    update env_config defaults.

--- registry/quilt_server/env_config.py
+++ registry/quilt_server/env_config.py
@@ -3,40 +3,81 @@ Config file that takes configuration from the environment.
 Overrides values in config.py.
 """
 import os
+import socket
+
+Required = object()
 
 def to_bool(str):
     if str in ['true', '1']:
         return True
     if str in ['false', '0']:
         return False
-    raise ValueError(("Invalid value '%s' supplied for boolean env var, "
+    raise EnvironmentError(("Invalid value '%s' supplied for boolean env var, "
         "should be one of: 'true', '1', 'false', '0'.") % str)
 
-DEBUG = to_bool(os.getenv('DEBUG', 'false'))
+def getenv(name, default):
+    value = os.getenv(name)
+    if value in [None, '']:
+        if default is Required:
+            raise EnvironmentError("Expected variable {!r}, but it wasn't present.".format(name))
+        else:
+            return default
+    return value
 
-SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+# Development
+DEBUG = to_bool(getenv('DEBUG', 'false'))
+
+DEV_USERNAME = getenv('DEV_USERNAME', None)
+DEV_PASSWORD = getenv('DEV_PASSWORD', None)
+DEV_EMAIL = getenv('DEV_EMAIL', 'support@quiltdata.io')
+
+MAIL_DEV = to_bool(getenv('MAIL_DEV', 'false'))
+
+# DB
+SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_ECHO = DEBUG
 
-REGISTRY_URL = os.environ['REGISTRY_URL']
-CATALOG_URL = os.environ['CATALOG_URL']
+# Base URLs
+REGISTRY_URL = getenv('REGISTRY_URL', Required)
+CATALOG_URL = getenv('CATALOG_URL', Required)
+if not CATALOG_URL.startswith("https"):
+    print("WARNING: INSECURE CONNECTION TO CATALOG")
+    # require verbose environment variable to be defined
+    assert to_bool(getenv('ALLOW_INSECURE_CATALOG_ACCESS', 'false'))
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-S3_ENDPOINT = os.environ['S3_ENDPOINT']
-PACKAGE_BUCKET_NAME = os.getenv('PACKAGE_BUCKET_NAME', 'package')
+STAGE_AUTH_URL = getenv('STAGE_AUTH_URL', '')
+QUILT_AUTH_URL = getenv('QUILT_AUTH_URL', STAGE_AUTH_URL)
 
-MIXPANEL_PROJECT_TOKEN = os.getenv('MIXPANEL_PROJECT_TOKEN', '')
-DEPLOYMENT_ID = ''
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 
+# S3
+S3_ENDPOINT = getenv('S3_ENDPOINT', Required)
+PACKAGE_BUCKET_NAME = getenv('PACKAGE_BUCKET_NAME', 'package')
+# `None` uses the environment/AWS config.
+AWS_ACCESS_KEY_ID = getenv('AWS_ACCESS_KEY_ID', None)
+AWS_SECRET_ACCESS_KEY = getenv('AWS_SECRET_ACCESS_KEY', None)
+
+# Mixpanel config
+MIXPANEL_PROJECT_TOKEN = getenv('MIXPANEL_PROJECT_TOKEN', '')
+DEPLOYMENT_ID = getenv('DEPLOYMENT_ID', socket.gethostname())
+
+# Stripe config -- Falsey values will disable payments.
+STRIPE_SECRET_KEY = getenv('STRIPE_SECRET_KEY', '')
+
+# Quilt features
 ENABLE_USER_ENDPOINTS = to_bool(os.getenv('ENABLE_USER_ENDPOINTS', 'false'))
 
-MAIL_SERVER = os.getenv('SMTP_HOST')
-MAIL_PORT = os.getenv('SMTP_PORT')
-MAIL_USERNAME = os.getenv('SMTP_USERNAME')
-MAIL_PASSWORD = os.getenv('SMTP_PASSWORD')
-MAIL_USE_TLS = to_bool(os.getenv('SMTP_USE_TLS', 'true'))
 
-SECRET_KEY = os.environ['QUILT_SECRET_KEY']
+# Mail
+DEFAULT_SENDER = getenv('QUILT_DEFAULT_SENDER')
+MAIL_SERVER = getenv('SMTP_HOST', '' if DEBUG else Required)
+MAIL_PORT = getenv('SMTP_PORT', None)
+MAIL_USERNAME = getenv('SMTP_USERNAME', '')
+MAIL_PASSWORD = getenv('SMTP_PASSWORD', '')
+MAIL_USE_TLS = to_bool(getenv('SMTP_USE_TLS', 'true'))
 
-DEFAULT_SENDER = os.environ['QUILT_DEFAULT_SENDER']
+
+# deprecated or inactive (?)
+# Only referenced in configs
+AUTH_PROVIDER = os.getenv('AUTH_PROVIDER', 'quilt')
+# This is Only referenced in configs, appears to do nothing, but is a required value..
+SECRET_KEY = getenv('QUILT_SECRET_KEY')
