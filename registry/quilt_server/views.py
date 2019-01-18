6a256deeb898bc3d5e7c657e5daac737b66ec4b6
nivlac <meffij@gmail.com>
2019-01-18 14:00:10 -0800

    Add region to cognito identity client

--- registry/quilt_server/views.py
+++ registry/quilt_server/views.py
@@ -91,6 +91,7 @@ s3_client = boto3.client(
 
 cognito_identity_client = boto3.client(
     'cognito-identity',
+    region='us-east-1',
     aws_access_key_id=app.config.get('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=app.config.get('AWS_SECRET_ACCESS_KEY')
 )
