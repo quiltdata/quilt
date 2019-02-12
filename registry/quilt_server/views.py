7df556cee0176882bc5831373fead2df90898e33
stevededalus <michael.sindelar@gmail.com>
2019-02-11 16:34:28 -0800

    remove region in sts client

--- registry/quilt_server/views.py
+++ registry/quilt_server/views.py
@@ -93,7 +93,6 @@ s3_client = boto3.client(
 
 sts_client = boto3.client(
     'sts',
-    region_name='us-east-1',
     aws_access_key_id=app.config.get('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=app.config.get('AWS_SECRET_ACCESS_KEY')
 )
