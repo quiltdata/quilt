d535cdeef8d99f47a58521d0b4a0515b9b312264
Kevin Moore <kevinemoore@gmail.com>
2019-06-04 09:26:26 -0700

    Releasing version 3.0.4 (#847)

--- api/python/setup.py
+++ api/python/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = "3.0.3"
+VERSION = "3.0.4"
 
 def readme():
     readme_short = """
