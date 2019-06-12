546fea23cc9a485cba9b8603b44617f8e46140b8
nl_0 <nl.imbecile@gmail.com>
2019-06-12 15:21:35 +0500

    website/components/Illustration: prettify

--- catalog/app/website/components/Illustration.js
+++ catalog/app/website/components/Illustration.js
@@ -4,11 +4,13 @@ import { styled } from '@material-ui/styles'
 
 import img2x from 'utils/img2x'
 
-export default styled(({ srcs, dir, offset, centerOffset, width, alwaysAbsolute, boxProps, ...props }) => (
-  <Box position="relative" {...props} {...boxProps}>
-    <img alt="" src={img2x(...srcs)} />
-  </Box>
-))(({ theme: t, dir = 'left', offset, centerOffset, width, alwaysAbsolute }) => ({
+export default styled(
+  ({ srcs, dir, offset, centerOffset, width, alwaysAbsolute, boxProps, ...props }) => (
+    <Box position="relative" {...props} {...boxProps}>
+      <img alt="" src={img2x(...srcs)} />
+    </Box>
+  ),
+)(({ theme: t, dir = 'left', offset, centerOffset, width, alwaysAbsolute }) => ({
   '& img': {
     [alwaysAbsolute ? '&' : t.breakpoints.up('md')]: {
       position: 'absolute',
