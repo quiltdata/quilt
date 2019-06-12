2869ae2fc8dbef26179e76268db5763ba7ccb3fd
nl_0 <nl.imbecile@gmail.com>
2019-06-12 15:22:30 +0500

    website/components/Backgrounds: adjust Backlight4

--- catalog/app/website/components/Backgrounds/Backlight4.js
+++ catalog/app/website/components/Backgrounds/Backlight4.js
@@ -1,13 +1,13 @@
 import { Box } from '@material-ui/core'
 import { styled } from '@material-ui/styles'
 
-import backlight4 from './backlight1.png'
+import backlight4 from './backlight4.png'
 
 export default styled(Box)({
   backgroundImage: `url(${backlight4})`,
   backgroundPosition: 'center',
   backgroundSize: 'cover',
-  height: 1449, // TODO
+  height: 2059,
   left: 0,
   position: 'absolute',
   right: 0,
