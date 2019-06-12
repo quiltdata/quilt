37feea4ded5c906031ec4edbd1d27b17c1811586
nl_0 <nl.imbecile@gmail.com>
2019-06-12 18:53:54 +0500

    website/components/Bullet: use styledBy

--- catalog/app/website/components/Bullet.js
+++ catalog/app/website/components/Bullet.js
@@ -2,34 +2,37 @@ import * as React from 'react'
 import { Typography } from '@material-ui/core'
 import { styled } from '@material-ui/styles'
 
+import styledBy from 'utils/styledBy'
+
 const bulletColors = {
   primary: {
     shadow: '#ffafaa',
-    gradient: ['#f1b39d', '#f78881'],
+    gradient: '#f1b39d, #f78881',
   },
   secondary: {
     shadow: '#6072e9',
-    gradient: ['#5c83ea', '#6752e6'],
+    gradient: '#5c83ea, #6752e6',
   },
   tertiary: {
     shadow: '#a7d5d6',
-    gradient: ['#c2ead6', '#3c85da'],
+    gradient: '#c2ead6, #3c85da',
   },
 }
 
 export default styled(({ color, ...props }) => (
   <Typography variant="body1" color="textSecondary" {...props} />
-))(({ color }) => ({
+))({
   marginBottom: '2em',
   paddingLeft: 32,
   position: 'relative',
 
   '&::before': {
-    backgroundImage: `linear-gradient(to top, ${bulletColors[color].gradient.join(
-      ', ',
-    )})`,
+    backgroundImage: styledBy(
+      'color',
+      (c) => `linear-gradient(to top, ${bulletColors[c].gradient})`,
+    ),
     borderRadius: '50%',
-    boxShadow: [[0, 0, 16, 0, bulletColors[color].shadow]],
+    boxShadow: styledBy('color', (c) => [[0, 0, 16, 0, bulletColors[c].shadow]]),
     content: '""',
     display: 'inline-flex',
     height: 8,
@@ -38,4 +41,4 @@ export default styled(({ color, ...props }) => (
     left: 0,
     width: 8,
   },
-}))
+})
