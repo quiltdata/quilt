f241920fb29ed0bf6af12551dbe4d6f2efe6b751
nl_0 <nl.imbecile@gmail.com>
2019-06-12 15:34:50 +0500

    utils/styledBy: adjust callback params

--- catalog/app/utils/styledBy.js
+++ catalog/app/utils/styledBy.js
@@ -3,9 +3,7 @@ const PLACEHOLDER = '_'
 export default (propName, variants) => (props) => {
   const propVal = props[propName]
   if (!variants) return propVal
-  const variant =
-    typeof variants === 'function'
-      ? variants
-      : variants[propVal in variants ? propVal : PLACEHOLDER]
-  return typeof variant === 'function' ? variant(props, propVal, propName) : variant
+  if (typeof variants === 'function') return variants(propVal, props)
+  const variant = variants[propVal in variants ? propVal : PLACEHOLDER]
+  return typeof variant === 'function' ? variant(props) : variant
 }
