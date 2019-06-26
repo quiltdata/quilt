0d4a2885d3f6f8275911b8347dae619a14e8c09d
nl_0 <nl.imbecile@gmail.com>
2019-06-27 00:29:11 +0500

    typo

--- catalog/app/utils/AWS/S3.js
+++ catalog/app/utils/AWS/S3.js
@@ -38,7 +38,7 @@ export const use = useS3
 export const useRequest = (extra) => {
   const cfg = useConfig()
   const s3RegularClient = useS3(extra)
-  const s3ProxiyngClient = useS3({
+  const s3ProxyingClient = useS3({
     endpoint: cfg.s3Proxy,
     s3ForcePathStyle: true,
     ...extra,
@@ -47,12 +47,12 @@ export const useRequest = (extra) => {
   return React.useMemo(
     () => ({ bucket, operation, params }) => {
       const useProxy = bucket !== cfg.defaultBucket
-      const client = useProxy ? s3ProxiyngClient : s3RegularClient
+      const client = useProxy ? s3ProxyingClient : s3RegularClient
       const method =
         authenticated && !useProxy ? 'makeRequest' : 'makeUnauthenticatedRequest'
       return client[method](operation, params).promise()
     },
-    [s3RegularClient, s3ProxiyngClient, authenticated, cfg.defaultBucket],
+    [s3RegularClient, s3ProxyingClient, authenticated, cfg.defaultBucket],
   )
 }
 
