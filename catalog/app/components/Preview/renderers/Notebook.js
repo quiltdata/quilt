8a734a804c2e735eca03690ebebb6ceba7aada0f
Alexei Mochalov <nl.imbecile@gmail.com>
2019-07-03 03:34:14 +0500

    Make notebook rendering fast again + fix attempts to render math in inappropriate places (e.g. code cells) (#923)

--- catalog/app/components/Preview/renderers/Notebook.js
+++ catalog/app/components/Preview/renderers/Notebook.js
@@ -1,12 +1,36 @@
 import cx from 'classnames'
+import renderMathInEl from 'katex/contrib/auto-render/auto-render'
 import PT from 'prop-types'
 import * as React from 'react'
 import * as RC from 'recompose'
-import { unstable_Box as Box } from '@material-ui/core/Box'
+import { makeStyles } from '@material-ui/styles'
 
 import 'katex/dist/katex.css'
 
 import * as RT from 'utils/reactTools'
+
+const MATH_DELIMITERS = [
+  { left: '$$', right: '$$', display: true },
+  { left: '$', right: '$', display: false },
+  { left: '\\(', right: '\\)', display: false },
+  { left: '\\[', right: '\\]', display: true },
+]
+
+const renderMath = (el) => {
+  if (!el) return
+  renderMathInEl(el, { delimiters: MATH_DELIMITERS })
+}
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    // workaround to speed-up browser rendering / compositing
+    '& div.input_area > div.highlight > pre': {
+      overflow: 'hidden',
+      whiteSpace: 'pre-wrap',
+    },
+  },
+})
 
 const Notebook = RT.composeComponent(
   'Preview.renderers.Notebook',
@@ -14,15 +38,18 @@ const Notebook = RT.composeComponent(
     children: PT.string,
     className: PT.string,
   }),
-  ({ children, className, ...props } = {}) => (
-    <Box
-      width="100%"
-      className={cx(className, 'ipynb-preview')}
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: children }}
-      {...props}
-    />
-  ),
+  ({ children, className, ...props } = {}) => {
+    const classes = useStyles()
+    return (
+      <div
+        className={cx(classes.root, className, 'ipynb-preview')}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: children }}
+        ref={renderMath}
+        {...props}
+      />
+    )
+  },
 )
 
 export default ({ preview }, props) => <Notebook {...props}>{preview}</Notebook>
