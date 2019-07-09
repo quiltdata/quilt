46098f4f5a0f12444ab164e0e6579633fd131e6f
Aneesh Karve <akarve@users.noreply.github.com>
2019-07-08 17:23:44 -0700

    Remove navigator_config lambda (#941)

--- lambdas/navigator_config/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='lambda_function',
-    version='0.0.1',
-    py_modules=['index', 'cfnresponse'],
-    data_files=[('', ['config-schema.json', 'federation-schema.json'])]
-)
