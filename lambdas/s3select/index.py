5aa5f29c212b5a86d14c5a5710d13bdcde8f8683
Dima Ryazanov <dima@gmail.com>
2019-07-17 12:04:00 -0700

    Check that the object is public. Add a unit test.

--- lambdas/s3select/index.py
+++ lambdas/s3select/index.py
@@ -1,4 +1,8 @@
 """
+Sign S3 select requests (because S3 select does not allow anonymous access).
+
+The implementation doesn't care what the request is, and just signs it using
+the current AWS credentials.
 """
 import os
 from urllib.parse import urlencode
@@ -13,25 +17,34 @@ from t4_lambda_shared.decorator import api
 from t4_lambda_shared.utils import get_default_origins
 
 SERVICE = 's3'
-REGION = os.environ['AWS_REGION']
+REGION = os.environ.get('AWS_REGION', '')
 
 REQUEST_HEADERS_TO_FORWARD = {'content-type', 'x-amz-content-sha256', 'x-amz-user-agent'}
 REQUEST_HEADERS_TO_SIGN = {'host', 'x-amz-content-sha256', 'x-amz-user-agent'}
 RESPONSE_HEADERS_TO_FORWARD = {'content-type'}
 
 credentials = Session().get_credentials()
-auth = SigV4Auth(credentials, SERVICE, REGION)
 session = requests.Session()
 
 
 @api(cors_origins=get_default_origins())
 def lambda_handler(request):
     """
+    Sign the request and forward it to S3.
     """
     bucket, key = request.pathParameters['proxy'].split('/', 1)
     host = f'{bucket}.s3.amazonaws.com'
 
-    url = f'https://{host}/{key}?{urlencode(request.args)}'
+    # Make an unsigned HEAD request to test anonymous access.
+
+    object_url = f'https://{host}/{key}'
+    head_response = session.head(object_url)
+    if not head_response.ok:
+        return requests.codes.forbidden, 'Not allowed', {'content-type': 'text/plain'}
+
+    # Sign the full S3 select request.
+
+    url = f'{object_url}?{urlencode(request.args)}'
 
     headers = { k: v for k, v in request.headers.items() if k in REQUEST_HEADERS_TO_FORWARD }
     headers['host'] = host
@@ -42,6 +55,7 @@ def lambda_handler(request):
         data=request.data,
         headers={ k: v for k, v in headers.items() if k in REQUEST_HEADERS_TO_SIGN }
     )
+    auth = SigV4Auth(credentials, SERVICE, REGION)
     auth.add_auth(aws_request)
 
     headers.update(aws_request.headers)
@@ -53,8 +67,8 @@ def lambda_handler(request):
         headers=headers,
     )
 
-    response_headers = {
-        k: v for k, v in response.headers.items() if k in RESPONSE_HEADERS_TO_FORWARD
-    }
+    response_headers = { k: v for k, v in response.headers.items() if k in RESPONSE_HEADERS_TO_FORWARD }
+    # Add a default content type to prevent API Gateway from setting it to application/json.
+    response_headers.setdefault('content-type', 'application/octet-stream')
 
     return response.status_code, response.content, response_headers
