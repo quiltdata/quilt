069576bb0afa2df081062f42106d03df45d98202
nl_0 <nl.imbecile@gmail.com>
2019-07-18 17:32:24 +0500

    rm Feature component

--- catalog/app/containers/HomePage/HomePage.js
+++ catalog/app/containers/HomePage/HomePage.js
@@ -1,11 +1,23 @@
 import * as React from 'react'
-import { Box, Button } from '@material-ui/core'
+import * as M from '@material-ui/core'
 
 import FAIcon from 'components/FAIcon'
-import Feature from 'components/Feature'
 import * as Intercom from 'components/Intercom'
 import Layout from 'components/Layout'
-import * as urls from 'constants/urls'
+import * as URLS from 'constants/urls'
+
+import background from './background.png'
+
+const Back = M.styled(M.Box)({
+  backgroundImage: `url(${background})`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'contain',
+})
+
+const Thin = M.styled(M.Typography)({
+  fontWeight: 300,
+})
 
 export default () => {
   const intercom = Intercom.use()
@@ -13,29 +25,33 @@ export default () => {
     <Layout
       dark
       pre={
-        <React.Fragment>
-          <Feature
-            header="Collaborate in S3"
-            tagline="Search, visualize, and version data with Quilt"
-          />
-          <Box color="common.white" display="flex" p={2}>
-            <Box flexGrow={1} />
-            <Button href={urls.slackInvite} color="inherit">
+        <>
+          <Back p={8} height={600} color="white">
+            <Thin variant="h3" color="inherit" gutterBottom>
+              Collaborate in S3
+            </Thin>
+            <Thin variant="body1" color="inherit">
+              Search, visualize, and version data with Quilt
+            </Thin>
+          </Back>
+          <M.Box color="common.white" display="flex" p={2}>
+            <M.Box flexGrow={1} />
+            <M.Button href={URLS.slackInvite} color="inherit">
               <FAIcon type="slack" />
               &nbsp;Join Slack
-            </Button>
-            <Box width="1em" />
-            <Button href={urls.docs} color="inherit">
+            </M.Button>
+            <M.Box width="1em" />
+            <M.Button href={URLS.docs} color="inherit">
               <FAIcon type="book" />
               &nbsp;Read Docs
-            </Button>
-            <Box width="1em" />
-            <Button onClick={() => intercom('show')} color="inherit">
+            </M.Button>
+            <M.Box width="1em" />
+            <M.Button onClick={() => intercom('show')} color="inherit">
               <FAIcon type="chatBubble" />
               &nbsp;Chat
-            </Button>
-          </Box>
-        </React.Fragment>
+            </M.Button>
+          </M.Box>
+        </>
       }
     />
   )
