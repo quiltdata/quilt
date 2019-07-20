df49f32b1907a4163a07627a4d0022ba33b38caf
Aneesh Karve <aneesh.karve@gmail.com>
2019-07-19 20:41:06 -0700

    nicer condition for notebook extraction

--- lambdas/es/indexer/index.py
+++ lambdas/es/indexer/index.py
@@ -249,9 +249,8 @@ def extract_text(notebook_str):
     formatted = nbformat.reads(notebook_str, as_version=NB_VERSION)
     text = []
     for cell in formatted.get("cells", []):
-        if "source" in cell and "cell_type" in cell:
-            if cell["cell_type"] == "code" or cell["cell_type"] == "markdown":
-                text.append(cell["source"])
+        if 'source' in cell and cell.get('cell_type') in ('code', 'markdown'):
+            text.append(cell["source"])
 
     return "\n".join(text)
 
