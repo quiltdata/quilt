259b5c0fcf66b263705b4022e6ee7e76c732f290
nl_0 <nl.imbecile@gmail.com>
2019-07-23 17:02:10 +0500

    clean up global styles

--- catalog/app/global-styles.js
+++ catalog/app/global-styles.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import { CssBaseline } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 
-// TODO: clean up
 const useGlobalStyles = makeStyles({
   '@global': {
     'html, body': {
@@ -12,77 +11,12 @@ const useGlobalStyles = makeStyles({
       width: '100%',
       wordBreak: 'normal',
     },
-
-    body: {
-      // font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif,
-    },
-
     a: {
       color: 'inherit',
       textDecoration: 'none',
     },
-    'code, pre': {
-      // backgroundColor: '#eee',
-    },
-    dt: {
-      // color: headerColor,
-    },
-    dd: {
-      // marginLeft: 0,
-      // marginBottom: '1em',
-    },
-    'h1, h2, h3, h4, h5, h6': {
-      // color: headerColor,
-      // fontWeight: 'lighter',
-    },
-    h1: {
-      // fontSize: '2em',
-      // overflow: 'hidden',
-      // textOverflow: 'ellipsis',
-    },
-    h2: {
-      // fontSize: '1.5em',
-    },
-    h3: {
-      // fontSize: '1.4em',
-    },
-    h4: {
-      // fontSize: '1.3em',
-    },
-    h5: {
-      // fontSize: '1.2em',
-    },
-    h6: {
-      // fontSize: '1.1em',
-    },
-    pre: {
-      // border: '1px solid rgb(220, 220, 220)',
-      // borderRadius: '5px',
-      // overflow: 'auto',
-      // padding: '.5em',
-    },
-    'p, label, li, dd': {
-      // fontSize: bodySize,
-      // lineHeight: '1.5em',
-    },
-
-    /* per app.js fontObservers, fontLoaded is fired once Roboto is ready */
-    /*
-    'body.fontLoaded': {
-      // fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif",
-
-      '.fixed, code, kbd, pre, samp': {
-        // fontFamily: "'Roboto Mono', monospace",
-      },
-
-      'p, label, li, dd': {
-        // font-weight: lighter,
-      },
-    },
-    */
-
-    '#app': {
-      // minWidth: '100%',
+    'code, kbd, pre, samp': {
+      fontFamily: "'Roboto Mono', monospace",
     },
   },
 })
