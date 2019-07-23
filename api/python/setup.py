c19b71f7ee9d2e38afc03c39ffc712f3199500ad
Kevin Moore <kevinemoore@gmail.com>
2019-07-23 14:09:21 -0700

    Update version to 3.0.5 (#1030)

--- api/python/setup.py
+++ api/python/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = "3.0.4"
+VERSION = "3.0.5"
 
 def readme():
     readme_short = """
