9137463310f6fa1fa8c3869bb13dadf8156baf2d
Dima Ryazanov <dima@gmail.com>
2019-07-29 14:22:35 +0300

    Add a video transcode lambda

---
+++ lambdas/transcode/index.py
@@ -0,0 +1,1 @@
+from transcode.index import lambda_handler
---
+++ lambdas/transcode/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='t4_lambda_transcode',
+    version='0.0.1',
+    packages=['transcode'],
+    py_modules=['index'],
+)
