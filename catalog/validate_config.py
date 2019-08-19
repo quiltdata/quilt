836d4564cbb124460a20d1e48677ed9d5b209d74
Kevin Moore <kevin@quiltdata.io>
2019-08-19 15:36:24 -0700

    WIP: fix validator script

--- catalog/validate_config.py
+++ catalog/validate_config.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+import json
 from jsonschema import Draft4Validator
 
+schema = None
 schema_path = "./config-schema.json"
-schema = json.loads(open(schema_path).read().decode("utf-8"))
+with open(schema_path, "r") as schema_file:
+    schema = json.loads(schema_file.read())
 
 config_path = "./config.json.tmpl"
-catalog_config = json.loads(open(config_path).read().decode("utf-8"))
+with open(config_path, "r") as config_file:
+    catalog_config = json.loads(config_file.read())
+
+assert schema and catalog_config
 Draft4Validator(schema).validate(catalog_config)
+
+print(f"Successfully validated {config_path} against {schema_path}")
