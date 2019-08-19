86e963bc8ad324a9bc2519a7819eaa986367f787
Jackson Brown <jmaxfieldbrown@gmail.com>
2019-08-19 11:50:21 -0700

    Downsample test data

--- lambdas/thumbnail/tests/test_thumbnail.py
+++ lambdas/thumbnail/tests/test_thumbnail.py
@@ -33,7 +33,7 @@ def _make_event(query, headers=None):
     ("cell.tiff", "w640h480", "cell-480.png", [15, 1, 158, 100], [514, 480]),
     ("cell.png", "w64h64", "cell-64.png", [168, 104, 3], [39, 64]),
     ("sat_greyscale.tiff", "w640h480",  "sat_greyscale-480.png", [512, 512], [480, 480]),
-    ("sat_rgb.tiff", "w1024h768", "sat_rgb-768.png", [2048, 2048, 3], [768, 768]),
+    ("sat_rgb.tiff", "w256h256", "sat_rgb-256.png", [256, 256, 4], [256, 256]),
     # Test for statusCode error
     pytest.param("cell.png", "w1h1", None, None, None, marks=pytest.mark.raises(exception=AssertionError))
 ])
