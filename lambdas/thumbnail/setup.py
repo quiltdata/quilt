549529329c64c39bc8d28ad10b1fe2145216a986
Jackson Brown <jmaxfieldbrown@gmail.com>
2019-08-26 10:22:53 -0700

    Remove packages from setup

--- lambdas/thumbnail/setup.py
+++ lambdas/thumbnail/setup.py
@@ -3,6 +3,5 @@ from setuptools import setup
 setup(
     name='t4_lambda_thumbnail',
     version='0.0.1',
-    py_modules=['index'],
-    packages=['tifffile', 'aicsimageio', 'aicsimageio.readers', 'aicsimageio.writers', 'aicsimageio.vendor']
+    py_modules=['index']
 )
