6db51cfd6a92b2ccf1754ac885cd3bfff7ae3293
nl_0 <nl.imbecile@gmail.com>
2019-08-27 23:15:36 +0500

    utils/SVG: usePaint: support collections

--- catalog/app/utils/SVG.js
+++ catalog/app/utils/SVG.js
@@ -1,24 +1,38 @@
+import * as R from 'ramda'
 import * as React from 'react'
 import uuid from 'uuid/v1'
 
 import tagged from 'utils/tagged'
+import useMemoEq from 'utils/useMemoEq'
 
 export const Paint = tagged(['Color', 'Server', 'None'])
 
 export const pointsToSVG = (points) => points.map(({ x, y }) => `${x} ${y}`).join(' ')
 
-export const usePaint = (paint) => {
-  const id = React.useMemo(uuid, [])
-  return React.useMemo(
-    () =>
-      Paint.case(
-        {
+const memoEq = (fn) => (input) => useMemoEq(input, fn)
+
+export const usePaint = memoEq(
+  R.pipe(
+    (paint) => {
+      if (Paint.is(paint)) {
+        const id = uuid()
+        return (fn) => fn(paint, id)
+      }
+      // assuming object if not array
+      const map = Array.isArray(paint) ? R.addIndex(R.map) : R.mapObjIndexed
+      const ids = map(uuid, paint)
+      return (fn) => map((p, k) => fn(p, ids[k]), paint)
+    },
+    (map) =>
+      map(
+        Paint.case({
           Color: (value) => ({ ref: value, def: null }),
-          Server: (el) => ({ ref: `url(#${id})`, def: React.cloneElement(el, { id }) }),
+          Server: (el, id) => ({
+            ref: `url(#${id})`,
+            def: React.cloneElement(el, { id, key: id }),
+          }),
           None: () => ({ ref: 'none', def: null }),
-        },
-        paint,
+        }),
       ),
-    [paint, id],
-  )
-}
+  ),
+)
