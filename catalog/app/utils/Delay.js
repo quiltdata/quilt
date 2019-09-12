70e189ddda3c2ba3241be949974588f39c77b75a
nl_0 <nl.imbecile@gmail.com>
2019-09-12 23:21:22 +0500

    utils/Delay: refactor to hooks, adjust API

--- catalog/app/utils/Delay.js
+++ catalog/app/utils/Delay.js
@@ -1,34 +1,11 @@
-import PT from 'prop-types'
 import * as React from 'react'
 
-export default class Delay extends React.Component {
-  static propTypes = {
-    ms: PT.number,
-    children: PT.func.isRequired,
-  }
-
-  static defaultProps = {
-    ms: 1000,
-  }
-
-  constructor() {
-    super()
-    this.state = { ready: false }
-  }
-
-  componentDidMount() {
-    this.timeout = setTimeout(() => {
-      clearTimeout(this.timeout)
-      delete this.timeout
-      this.setState({ ready: true })
-    }, this.props.ms)
-  }
-
-  componentWillUnmount() {
-    if (this.timeout) clearTimeout(this.timeout)
-  }
-
-  render() {
-    return this.state.ready ? this.props.children() : null
-  }
+export default function Delay({ ms = 1000, alwaysRender = false, children }) {
+  const [ready, setReady] = React.useState(false)
+  React.useEffect(() => {
+    const timeout = setTimeout(() => setReady(true), ms)
+    return () => clearTimeout(timeout)
+  }, [])
+  // eslint-disable-next-line no-nested-ternary
+  return alwaysRender ? children(ready) : ready ? children() : null
 }
