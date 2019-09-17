dd67469dd190a2374a9f52751be81ecb8d3bcb82
Dima Ryazanov <dima@gmail.com>
2019-09-17 15:14:52 -0700

    Remove a TODO

--- catalog/app/utils/BucketConfig.js
+++ catalog/app/utils/BucketConfig.js
@@ -4,7 +4,6 @@ import * as Cache from 'utils/ResourceCache'
 import { useRoute } from 'utils/router'
 
 const fetchBuckets = async ({ registryUrl }) => {
-  // TODO: use api/buckets endpoint once it's working
   const res = await fetch(`${registryUrl}/api/buckets`)
   const text = await res.text()
   if (!res.ok) {
