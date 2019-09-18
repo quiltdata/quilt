d542b23e7df24b5048f9e25d2770a58929f96519
nl_0 <nl.imbecile@gmail.com>
2019-09-18 22:25:34 +0500

    OpenLanding: complete Showcase section

--- catalog/app/website/pages/OpenLanding/Showcase/Carousel.js
+++ catalog/app/website/pages/OpenLanding/Showcase/Carousel.js
@@ -8,34 +8,64 @@ import * as M from '@material-ui/core'
 
 const Swipeable = autoPlay(virtualize(SwipeableViews))
 
-const SLIDE_COUNT_FACTOR = 10000
+const SLIDE_COUNT_FACTOR = 1000000
 
 const useStyles = M.makeStyles((t) => ({
+  overflow: {
+    overflowX: 'hidden',
+  },
+  container: {
+    width: `calc(100% + ${t.spacing(2)}px)`,
+  },
   slide: {
     width: '100%',
+    paddingRight: t.spacing(2),
+  },
+  captionContainer: {
+    height: t.typography.pxToRem(32),
+    marginTop: t.spacing(5),
+    position: 'relative',
   },
   caption: {
-    marginTop: t.spacing(4.5),
+    position: 'absolute',
+    width: '100%',
   },
   dots: {
     display: 'flex',
     justifyContent: 'center',
-    marginTop: t.spacing(4.5),
+    marginTop: t.spacing(4),
   },
   dot: {
-    // TODO
-    background: '#999',
+    background: M.colors.blueGrey[500],
     border: 'none',
     borderRadius: '50%',
+    cursor: 'pointer',
     height: 12,
+    outline: 'none',
     padding: 0,
+    position: 'relative',
     width: 12,
+    '&::before': {
+      background: `linear-gradient(to top, #5c83ea, #6752e6)`,
+      borderRadius: '50%',
+      bottom: 0,
+      boxShadow: [[0, 0, 16, 0, '#6072e9']],
+      content: '""',
+      left: 0,
+      opacity: 0,
+      position: 'absolute',
+      right: 0,
+      top: 0,
+      transition: 'opacity 400ms',
+    },
     '& + &': {
       marginLeft: t.spacing(2),
     },
   },
   current: {
-    background: '#99f',
+    '&::before': {
+      opacity: 1,
+    },
   },
 }))
 
@@ -43,8 +73,10 @@ export default function Carousel({ className, slides }) {
   const classes = useStyles()
   const [index, setIndex] = React.useState(0)
   const onChangeIndex = React.useCallback(R.unary(setIndex), [])
-  const actualIndex = mod(index, slides.length)
-  const current = slides[actualIndex]
+  const current = slides[mod(index, slides.length)]
+  const maxSlides = slides.length * SLIDE_COUNT_FACTOR
+  const nearestZero = Math.floor(index / slides.length) * slides.length
+  const goToNearestIndex = (i) => setIndex(nearestZero + i)
 
   const slideRenderer = React.useCallback(
     ({ index: i, key }) => (
@@ -60,31 +92,41 @@ export default function Carousel({ className, slides }) {
 
   return (
     <div className={className}>
-      <Swipeable
-        disableLazyLoading
-        enableMouseEvents
-        index={index}
-        onChangeIndex={onChangeIndex}
-        slideRenderer={slideRenderer}
-        slideCount={slides.length * SLIDE_COUNT_FACTOR}
-      />
-      <M.Typography
-        variant="body1"
-        color="textSecondary"
-        align="center"
-        className={classes.caption}
-      >
-        {current.caption}
-      </M.Typography>
+      <div className={classes.overflow}>
+        <Swipeable
+          disableLazyLoading
+          enableMouseEvents
+          index={index}
+          interval={7000}
+          onChangeIndex={onChangeIndex}
+          slideRenderer={slideRenderer}
+          slideCount={maxSlides}
+          className={classes.container}
+        />
+      </div>
+      <div className={classes.captionContainer}>
+        {slides.map((s, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <M.Fade in={s === current} key={i}>
+            <M.Typography
+              variant="body1"
+              color="textSecondary"
+              align="center"
+              className={classes.caption}
+            >
+              {s.caption}
+            </M.Typography>
+          </M.Fade>
+        ))}
+      </div>
       <div className={classes.dots}>
         {slides.map((s, i) => (
           <button
             type="button"
             // eslint-disable-next-line react/no-array-index-key
             key={i}
-            // TODO: set nearest index
-            onClick={() => setIndex(i)}
-            className={cx(classes.dot, actualIndex === i && classes.current)}
+            onClick={() => goToNearestIndex(i)}
+            className={cx(classes.dot, current === s && classes.current)}
           />
         ))}
       </div>
--- catalog/app/website/pages/OpenLanding/Showcase/Showcase.js
+++ catalog/app/website/pages/OpenLanding/Showcase/Showcase.js
@@ -2,6 +2,8 @@ import * as React from 'react'
 import * as M from '@material-ui/core'
 
 import Bar from 'website/components/Bar'
+import Overlay1Full from 'website/components/Backgrounds/Overlay1Full'
+import Overlay2 from 'website/components/Backgrounds/Overlay2'
 
 import Carousel from './Carousel'
 
@@ -17,7 +19,7 @@ const slides = [
   },
   {
     src: slide2,
-    caption: 'Choose from one of 25 visualizations',
+    caption: 'Lorem ipsum dolor sit amet',
   },
   {
     src: slide3,
@@ -31,13 +33,21 @@ const slides = [
 
 const useStyles = M.makeStyles((t) => ({
   root: {
+    position: 'relative',
+  },
+  container: {
     display: 'grid',
-    [t.breakpoints.up('sm')]: {
+    paddingBottom: t.spacing(5),
+    position: 'relative',
+    [t.breakpoints.up('md')]: {
       gridColumnGap: t.spacing(10),
-      gridTemplateColumns: '1fr 640px',
+      gridTemplateColumns: '1fr 1fr',
       gridTemplateAreas: '"text carousel"',
     },
-    [t.breakpoints.down('xs')]: {
+    [t.breakpoints.up('lg')]: {
+      gridTemplateColumns: '1fr 640px',
+    },
+    [t.breakpoints.down('sm')]: {
       gridRowGap: t.spacing(4),
       gridTemplateRows: 'auto auto',
       gridTemplateAreas: `
@@ -48,12 +58,12 @@ const useStyles = M.makeStyles((t) => ({
   },
   text: {
     gridArea: 'text',
-    paddingBottom: t.spacing(4),
-    paddingTop: t.spacing(6),
+    [t.breakpoints.up('lg')]: {
+      paddingTop: t.spacing(6),
+    },
   },
   carousel: {
     gridArea: 'carousel',
-    paddingBottom: t.spacing(5),
   },
 }))
 
@@ -61,33 +71,37 @@ export default function Showcase() {
   const classes = useStyles()
 
   return (
-    <M.Container maxWidth="lg" className={classes.root}>
-      <div className={classes.text}>
-        <Bar color="primary" />
-        <M.Box mt={5}>
-          <M.Typography variant="h1" color="textPrimary">
-            Publish and discover unlimited data
-          </M.Typography>
-        </M.Box>
-        <M.Box mt={4}>
-          <M.Typography variant="body1" color="textSecondary">
-            {/* TODO: copy */}
-            Quilt is a web catalog and Python client powered by AWS services in your
-            private cloud. Quilt is a web catalog and Python client powered by AWS
-            services in your private cloud. Quilt is a web catalog and Python client
-            powered by AWS services in your private cloud.
-          </M.Typography>
-        </M.Box>
-        <M.Box mt={4}>
-          <M.Link color="textPrimary" underline="none" href="TODO">
-            Learn more
-            <M.Icon color="primary" style={{ verticalAlign: 'middle' }}>
-              chevron_right
-            </M.Icon>
-          </M.Link>
-        </M.Box>
-      </div>
-      <Carousel className={classes.carousel} slides={slides} />
-    </M.Container>
+    <div className={classes.root}>
+      <Overlay2 />
+      <Overlay1Full top={-80} />
+      <M.Container maxWidth="lg" className={classes.container}>
+        <div className={classes.text}>
+          <Bar color="primary" />
+          <M.Box mt={5}>
+            <M.Typography variant="h1" color="textPrimary">
+              Publish and discover unlimited data
+            </M.Typography>
+          </M.Box>
+          <M.Box mt={4}>
+            <M.Typography variant="body1" color="textSecondary">
+              {/* TODO: copy */}
+              Quilt is a web catalog and Python client powered by AWS services in your
+              private cloud. Quilt is a web catalog and Python client powered by AWS
+              services in your private cloud. Quilt is a web catalog and Python client
+              powered by AWS services in your private cloud.
+            </M.Typography>
+          </M.Box>
+          <M.Box mt={4}>
+            <M.Link color="textPrimary" underline="none" href="TODO">
+              Learn more
+              <M.Icon color="primary" style={{ verticalAlign: 'middle' }}>
+                chevron_right
+              </M.Icon>
+            </M.Link>
+          </M.Box>
+        </div>
+        <Carousel className={classes.carousel} slides={slides} />
+      </M.Container>
+    </div>
   )
 }
