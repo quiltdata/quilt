3d45a113d6158f507c517b61ce84da62437fc71f
nl_0 <nl.imbecile@gmail.com>
2019-09-24 03:56:25 +0500

    Landing: testimonials

--- catalog/app/website/pages/Landing/Landing.js
+++ catalog/app/website/pages/Landing/Landing.js
@@ -5,6 +5,7 @@ import Layout from 'website/components/Layout'
 import CaseStudies from './CaseStudies'
 import Platform from './Platform'
 import Showcase from './Showcase'
+import Testimonials from './Testimonials'
 import Videos from './Videos'
 
 export default () => (
@@ -13,5 +14,6 @@ export default () => (
     <Videos />
     <Platform />
     <CaseStudies />
+    <Testimonials />
   </Layout>
 )
