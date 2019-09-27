9bc0148fd483b8225f4a6e1ab2dd0392a87094ce
Aneesh Karve <aneesh.karve@gmail.com>
2019-09-27 12:16:12 -0700

    Link to more examples on open homepage

--- catalog/app/website/pages/OpenLanding/Showcase/Showcase.js
+++ catalog/app/website/pages/OpenLanding/Showcase/Showcase.js
@@ -71,7 +71,24 @@ export default function Showcase() {
             </M.Typography>
           </M.Box>
           <M.Box mt={4}>
-            <ChevronLink href="#demo-video">Learn more</ChevronLink>
+            <ChevronLink href="https://open.quiltdata.com/b/quilt-example/tree/robnewman/us_county_smoking_vs_poverty/">
+              Explore visualizations and notebooks
+            </ChevronLink>
+          </M.Box>
+          <M.Box mt={2}>
+            <ChevronLink href="https://open.quiltdata.com/b/quilt-example/tree/akarve/previews/storms.parquet">
+              Preview file formats (Parquet, Excel, VCF, and more)
+            </ChevronLink>
+          </M.Box>
+          <M.Box mt={2}>
+            <ChevronLink href="https://open.quiltdata.com/b/quilt-example/tree/akarve/previews/terrain/">
+              Browse directories of images
+            </ChevronLink>
+          </M.Box>
+          <M.Box mt={4}>
+            <M.Button variant="contained" color="secondary" href="#demo-video">
+              Learn more
+            </M.Button>
           </M.Box>
         </div>
         <Screenshots className={classes.carousel} />
