6fe37f6d4365afe942aae0b87e6a30a3338b5cfa
stevededalus <michael.sindelar@gmail.com>
2019-10-09 14:29:01 -0700

    Use in

--- api/python/tests/integration/quilt_data_check_example_images.py
+++ api/python/tests/integration/quilt_data_check_example_images.py
@@ -6,8 +6,6 @@ from urllib.parse import urlparse
 
 from PIL import Image
 
-from .s3file import S3File
-
 LABEL_SET = ['🍎', '🍊']
 
 class CheckPackageOfImages(data_checker.TestCase):
@@ -25,11 +23,8 @@ class CheckPackageOfImages(data_checker.TestCase):
 class CheckValidImages(data_checker.TestCase):
     @data_checker.TestCase.test_key('images_cropped/Apple/10049481156_6cdc5d23be_o_23651.jpg') # Logical key
     def quilt_data_check_ImageParsable(self, pkg_entry):
-        s3 = boto3.resource("s3")
-
+        s3 = boto3.client("s3")
         bucket, key, version_id = parse_s3_url(urlparse(pkg_entry.physical_keys[0])) 
-        s3_object = s3.Object(bucket_name=bucket, key=key)
-        # Version ID not supported on s3.Object by boto
-        img = Image.open(S3File(s3_object, version_id=version_id))
+        img = Image.open(s3.get_object(Bucket=bucket, Key=key, VersionId=version_id)["Body"])
         self.assertTrue(img.format == 'JPEG', f"Found format: {img.format}")
         self.assertTrue(img.mode == 'RGB', f"Found mode: {img.mode}")
