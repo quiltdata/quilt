857b965cc87863068f9d7c34544b1fce1d9fff58
stevededalus <michael.sindelar@gmail.com>
2019-10-09 19:07:10 -0700

    use sys.exit

--- api/python/tests/integration/runner_data_tests.py
+++ api/python/tests/integration/runner_data_tests.py
@@ -1,11 +1,12 @@
 import click 
+import sys
 
 from quilt3 import check_packages, config
 
 @click.command()
 @click.argument('pkg_names') # open-data-registry/datasets
 def run_tests_on_packages(pkg_names):
-    check_packages([pkg_names], registry='s3://quilt-example') # pylint: disable=no-value-for-parameter#set by click
+    sys.exit(check_packages([pkg_names], registry='s3://quilt-example')) # pylint: disable=no-value-for-parameter#set by click
 
 if __name__ == '__main__':
     config('https://open.quiltdata.com')
