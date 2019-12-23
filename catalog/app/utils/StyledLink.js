a2faec33e6812f542c707bff57121f89f1ebde8f
nl_0 <nl.imbecile@gmail.com>
2019-12-23 18:14:32 +0500

    StyledLink: forward ref

--- catalog/app/utils/StyledLink.js
+++ catalog/app/utils/StyledLink.js
@@ -1,26 +1,25 @@
 import cx from 'classnames'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import * as colors from '@material-ui/core/colors'
-import { withStyles } from '@material-ui/styles'
-
-import * as RT from 'utils/reactTools'
+import * as M from '@material-ui/core'
 
 export const linkStyle = {
   '&, &:visited': {
-    color: colors.blue[900],
+    color: M.colors.blue[900],
     cursor: 'pointer',
   },
   '&:hover, &:focus': {
-    color: colors.blue[500],
+    color: M.colors.blue[500],
   },
 }
 
-export default RT.composeComponent(
-  'StyledLink',
-  withStyles(() => ({ root: linkStyle })),
-  ({ component, className, classes, ...props }) => {
-    const Component = component || props.to ? Link : 'a'
-    return <Component className={cx(className, classes.root)} {...props} />
-  },
-)
+const useStyles = M.makeStyles(() => ({ root: linkStyle }))
+
+export default React.forwardRef(function StyledLink(
+  { component, className, ...props },
+  ref,
+) {
+  const classes = useStyles()
+  const Component = component || props.to ? Link : 'a'
+  return <Component className={cx(className, classes.root)} {...props} ref={ref} />
+})
