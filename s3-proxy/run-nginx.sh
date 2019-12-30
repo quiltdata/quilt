f170865346015bc5a1d00160c35cace476604370
Dima Ryazanov <dima@gmail.com>
2019-12-30 14:31:53 -0800

    Fix IPv6 addresses in nginx config
    nginx gets confused by IPv6 addresses without brackets.

--- s3-proxy/run-nginx.sh
+++ s3-proxy/run-nginx.sh
@@ -3,6 +3,12 @@
 # Get the DNS server from /etc/resolv.conf
 nameserver=$(awk '{if ($1 == "nameserver") { print $2; exit;}}' < /etc/resolv.conf)
 
+if [[ "$nameserver" == *:*:* ]]
+then
+   # IPv6; put brackets around it.
+   nameserver="[$nameserver]"
+fi
+
 # Add it to the NGINX config
 echo "resolver $nameserver;" > /etc/nginx/conf.d/resolver.conf
 
