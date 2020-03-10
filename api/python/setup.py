d7542a78b02ede1f9c4b34fe91995683fd5294d6
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2020-03-10 10:01:46 +0500

    Add Python 3.8 trove classifier

--- api/python/setup.py
+++ api/python/setup.py
@@ -47,6 +47,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     author='quiltdata',
     author_email='contact@quiltdata.io',
