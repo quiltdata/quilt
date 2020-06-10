quiltdata/quilt

commit 0240a659f8692d1512533eaf3c654a42d2305776

Make pylint 2.5 happy

lambdas/access_counts/index.py
@@ -117,7 +117,7 @@ REPAIR_OBJECT_ACCESS_LOG = textwrap.dedent("""
     MSCK REPAIR TABLE object_access_log
 """)
 
-INSERT_INTO_OBJECT_ACCESS_LOG = textwrap.dedent(f"""\
+INSERT_INTO_OBJECT_ACCESS_LOG = textwrap.dedent("""\
     INSERT INTO object_access_log
     SELECT eventname, bucket, key, date_format(eventtime, '%Y-%m-%d') AS date
     FROM (

lambdas/preview/index.py
@@ -89,7 +89,7 @@ def lambda_handler(request):
         max_bytes = int(request.args.get('max_bytes', CATALOG_LIMIT_BYTES))
     except ValueError as error:
         return make_json_response(400, {
-            'title': f'Unexpected max_bytes= value',
+            'title': 'Unexpected max_bytes= value',
             'detail': str(error)
         })
 

api/python/quilt3/packages.py
@@ -850,8 +850,8 @@ class Package:
         no such entry exists.
         """
         if "README.md" not in self:
-            ex_msg = f"This Package is missing a README file. A Quilt recognized README file is a  file named " \
-                     f"'README.md' (case-insensitive)"
+            ex_msg = "This Package is missing a README file. A Quilt recognized README file is a  file named " \
+                     "'README.md' (case-insensitive)"
             raise QuiltException(ex_msg)
 
         return self["README.md"]
@@ -1089,8 +1089,8 @@ class Package:
                 error_message = f'Quilt does not know how to serialize a {type(entry)}'
                 if ext is not None:
                     error_message += f' as a {ext} file.'
-                error_message += f'. If you think this should be supported, please open an issue or PR at ' \
-                                 f'https://github.com/quiltdata/quilt'
+                error_message += '. If you think this should be supported, please open an issue or PR at ' \
+                                 'https://github.com/quiltdata/quilt'
                 raise QuiltException(error_message)
 
             if serialization_format_opts is None:

api/python/quilt3/session.py
@@ -173,9 +173,9 @@ def login():
     registry_url = get_registry_url()
     if registry_url is None:
         raise QuiltException(
-            f"You attempted to authenticate to a Quilt catalog, but your home catalog is "
-            f"currently set to None. Please first specify your home catalog by running "
-            f"\"quilt3.config('$URL')\", replacing '$URL' with your catalog homepage."
+            "You attempted to authenticate to a Quilt catalog, but your home catalog is "
+            "currently set to None. Please first specify your home catalog by running "
+            "\"quilt3.config('$URL')\", replacing '$URL' with your catalog homepage."
         )
 
     login_url = "%s/login" % get_registry_url()

lambdas/preview/test/test_index.py
@@ -296,7 +296,7 @@ class TestIndex():
 
         warnings = body['info']['warnings']
         assert warnings, f"expected warnings when parsing {csv}"
-        assert warnings.count("Skipping line") == 43, f"expected to skip 43 lines"
+        assert warnings.count("Skipping line") == 43, "expected to skip 43 lines"
 
     @responses.activate
     def test_tsv_as_csv(self):

pylintrc
@@ -13,6 +13,7 @@ disable=attribute-defined-outside-init,
         import-outside-toplevel,
         inconsistent-return-statements,
         invalid-name,
+        invalid-sequence-index,
         line-too-long,
         missing-class-docstring,
         missing-function-docstring,

commit ee9af6e1f9613818d5845e53547cb0a5fc766dc8

Fix INSERT INTO object_access_log query
Regression in 0240a659f8692d1512533eaf3c654a42d2305776

lambdas/access_counts/index.py
@@ -132,7 +132,7 @@ INSERT_INTO_OBJECT_ACCESS_LOG = textwrap.dedent("""\
     -- Filter out non-S3 events, or S3 events like ListBucket that have no object
     -- Select the correct time range
     WHERE bucket IS NOT NULL AND key IS NOT NULL AND
-          eventtime >= from_unixtime({{start_ts:f}}) AND eventtime < from_unixtime({{end_ts:f}})
+          eventtime >= from_unixtime({start_ts:f}) AND eventtime < from_unixtime({end_ts:f})
 """)
 
 CREATE_PACKAGE_HASHES = textwrap.dedent(f"""\