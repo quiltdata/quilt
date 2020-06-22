a1891241ee0ece1276ccce55d35e81a8d04b622f
nl_0 <nl.imbecile@gmail.com>
2020-06-22 17:33:41 +0500

    landing: a/b testing system stub

---
+++ catalog/app/website/pages/Landing/Experiments/Experiments.js
@@ -0,0 +1,29 @@
+import * as R from 'ramda'
+import * as React from 'react'
+
+import useConstant from 'utils/useConstant'
+
+const Ctx = React.createContext()
+
+const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)]
+
+// variants: map of experiment name to array of variants
+export function ExperimentsProvider({ variants, children }) {
+  const selected = useConstant(() => {
+    // choose random variant for every experiment
+    R.map(pickRandom, variants)
+    // TODO: save as mixpanel user data
+  })
+
+  // eslint-disable-next-line no-underscore-dangle
+  const get = React.useCallback(R.prop(R.__, selected), [selected])
+
+  return <Ctx.Provider value={get}>{children}</Ctx.Provider>
+}
+
+export function useExperiments(experiment) {
+  const get = React.useContext(Ctx)
+  return experiment ? get(experiment) : get
+}
+
+export { ExperimentsProvider as Provider, useExperiments as use }
--- catalog/app/website/pages/Landing/Landing.js
+++ catalog/app/website/pages/Landing/Landing.js
@@ -10,6 +10,7 @@ import Videos from 'website/components/Videos'
 
 import Buckets from './Buckets'
 import CaseStudies from './CaseStudies'
+import { ExperimentsProvider } from './Experiments'
 import Highlights from './Highlights'
 import LocalMode from './LocalMode'
 import Platform from './Platform'
@@ -19,26 +20,33 @@ import { TalkToUsProvider } from './TalkToUs'
 import Testimonials from './Testimonials'
 import UseQuilt from './UseQuilt'
 
+const variants = {
+  cta: ['CTA 1', 'CTA 2'],
+  lede: ['LEDE 1', 'LEDE 2'],
+}
+
 export default function Landing() {
   const cfg = Config.useConfig()
   return (
-    <TalkToUsProvider>
-      <Layout>
-        <React.Suspense fallback={null}>
-          <LinkedData.CatalogData />
-        </React.Suspense>
-        {cfg.mode !== 'LOCAL' && <Dots />}
-        {cfg.mode === 'PRODUCT' && <Buckets />}
-        {cfg.mode === 'LOCAL' ? <LocalMode /> : <Showcase />}
-        {cfg.mode !== 'LOCAL' && <Videos />}
-        {cfg.mode !== 'LOCAL' && <Platform />}
-        {cfg.mode !== 'LOCAL' && <CaseStudies />}
-        {cfg.mode !== 'LOCAL' && <Testimonials />}
-        {cfg.mode !== 'LOCAL' && <UseQuilt />}
-        {cfg.mode !== 'LOCAL' && <Highlights />}
-        {cfg.mode === 'MARKETING' && <Pricing />}
-        {cfg.mode !== 'LOCAL' && <Contribute />}
-      </Layout>
-    </TalkToUsProvider>
+    <ExperimentsProvider variants={variants}>
+      <TalkToUsProvider>
+        <Layout>
+          <React.Suspense fallback={null}>
+            <LinkedData.CatalogData />
+          </React.Suspense>
+          {cfg.mode !== 'LOCAL' && <Dots />}
+          {cfg.mode === 'PRODUCT' && <Buckets />}
+          {cfg.mode === 'LOCAL' ? <LocalMode /> : <Showcase />}
+          {cfg.mode !== 'LOCAL' && <Videos />}
+          {cfg.mode !== 'LOCAL' && <Platform />}
+          {cfg.mode !== 'LOCAL' && <CaseStudies />}
+          {cfg.mode !== 'LOCAL' && <Testimonials />}
+          {cfg.mode !== 'LOCAL' && <UseQuilt />}
+          {cfg.mode !== 'LOCAL' && <Highlights />}
+          {cfg.mode === 'MARKETING' && <Pricing />}
+          {cfg.mode !== 'LOCAL' && <Contribute />}
+        </Layout>
+      </TalkToUsProvider>
+    </ExperimentsProvider>
   )
 }
