bc836371612be6f815617ecc6c0312d70d811d8c
nl_0 <nl.imbecile@gmail.com>
2020-06-23 12:49:16 +0500

    experiments: track to mixpanel

--- catalog/app/components/Experiments/Experiments.js
+++ catalog/app/components/Experiments/Experiments.js
@@ -1,30 +1,51 @@
 import * as R from 'ramda'
 import * as React from 'react'
 
-import useConstant from 'utils/useConstant'
+// TODO: ensure proper variants
+// map of experiment name to array of variants
+const EXPERIMENTS = {
+  cta: ['Ready to synchronize your data?'],
+  lede: [
+    'Maximize your return on data by managing data like code',
+    'Experiment faster by managing data like code',
+  ],
+}
 
 const Ctx = React.createContext()
 
 const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)]
 
-// variants: map of experiment name to array of variants
-export function ExperimentsProvider({ variants, children }) {
-  const selected = useConstant(() => {
-    // choose random variant for every experiment
-    const sel = R.map(pickRandom, variants)
-    // TODO: save as mixpanel user data
-    return sel
-  })
+const mapKeys = (fn) =>
+  R.pipe(
+    R.toPairs,
+    R.map(([k, v]) => [fn(k, v), v]),
+    R.fromPairs,
+  )
 
-  // eslint-disable-next-line no-underscore-dangle
-  const get = React.useCallback(R.prop(R.__, selected), [selected])
+export function ExperimentsProvider({ children }) {
+  const ref = React.useRef({})
 
-  return <Ctx.Provider value={get}>{children}</Ctx.Provider>
+  const get = React.useCallback(
+    (name) => {
+      if (!(name in ref.current)) {
+        ref.current[name] = pickRandom(EXPERIMENTS[name])
+      }
+      return ref.current[name]
+    },
+    [ref.current],
+  )
+
+  const getSelectedVariants = React.useCallback(
+    (prefix = '') => mapKeys((k) => `${prefix}${k}`)(ref.current),
+    [ref.current],
+  )
+
+  return <Ctx.Provider value={{ get, getSelectedVariants }}>{children}</Ctx.Provider>
 }
 
 export function useExperiments(experiment) {
-  const get = React.useContext(Ctx)
-  return experiment ? get(experiment) : get
+  const exps = React.useContext(Ctx)
+  return experiment ? exps.get(experiment) : exps
 }
 
 export { ExperimentsProvider as Provider, useExperiments as use }
--- catalog/app/website/components/Layout.js
+++ catalog/app/website/components/Layout.js
@@ -1,28 +1,14 @@
 import * as React from 'react'
 import { ThemeProvider } from '@material-ui/styles'
 
-import { ExperimentsProvider } from 'components/Experiments'
 import * as Layout from 'components/Layout'
 import { TalkToUsProvider } from 'components/TalkToUs'
 import * as style from 'constants/style'
 
-// TODO: ensure proper variants
-const variants = {
-  cta: [
-    'Ready to synchronize your data?',
-  ],
-  lede: [
-    'Maximize your return on data by managing data like code',
-    'Experiment faster by managing data like code',
-  ],
-}
-
 export default ({ children, ...props }) => (
   <ThemeProvider theme={style.websiteTheme}>
-    <ExperimentsProvider variants={variants}>
-      <TalkToUsProvider>
-        <Layout.Layout pre={children} {...props} />
-      </TalkToUsProvider>
-    </ExperimentsProvider>
+    <TalkToUsProvider>
+      <Layout.Layout pre={children} {...props} />
+    </TalkToUsProvider>
   </ThemeProvider>
 )
