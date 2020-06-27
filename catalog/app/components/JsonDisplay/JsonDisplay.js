4a9d3fd25351dc123fce024d32acdf803989d5b1
nl_0 <nl.imbecile@gmail.com>
2020-06-27 23:50:45 +0500

    JsonDisplay: handle undefined values

--- catalog/app/components/JsonDisplay/JsonDisplay.js
+++ catalog/app/components/JsonDisplay/JsonDisplay.js
@@ -7,24 +7,31 @@ const IconBlank = () => <Box pr={2.5} />
 const IconExpand = () => <Icon fontSize="small">chevron_right</Icon>
 const IconCollapse = () => <Icon fontSize="small">expand_more</Icon>
 
-const Key = ({ children, ...props }) => !!children && <Box {...props}>{children}: </Box>
+function Key({ children, ...props }) {
+  return !!children && <Box {...props}>{children}: </Box>
+}
 
-const formatValue = R.cond([[R.is(String), (s) => `"${s}"`], [R.T, (v) => `${v}`]])
+const formatValue = R.cond([
+  [R.is(String), (s) => `"${s}"`],
+  [R.T, (v) => `${v}`],
+])
 
 // TODO: add some highlighting
-const PrimitiveValue = ({ children, ...props }) => (
-  <Box {...props}>{formatValue(children)}</Box>
-)
+function PrimitiveValue({ children, ...props }) {
+  return <Box {...props}>{formatValue(children)}</Box>
+}
 
-const PrimitiveEntry = ({ name, value, topLevel = true, ...props }) => (
-  <Box display="flex" alignItems="center" {...props}>
-    {!topLevel && <IconBlank />}
-    <Key>{name}</Key>
-    <PrimitiveValue>{value}</PrimitiveValue>
-  </Box>
-)
+function PrimitiveEntry({ name, value, topLevel = true, ...props }) {
+  return (
+    <Box display="flex" alignItems="center" {...props}>
+      {!topLevel && <IconBlank />}
+      <Key>{name}</Key>
+      <PrimitiveValue>{value}</PrimitiveValue>
+    </Box>
+  )
+}
 
-const CompoundEntry = ({ name, value, topLevel = true, ...props }) => {
+function CompoundEntry({ name, value, topLevel = true, ...props }) {
   const braces = Array.isArray(value) ? '[]' : '{}'
   const entries = React.useMemo(() => Object.entries(value), [value])
   // TODO: default expanded state heuristics?
@@ -53,21 +60,25 @@ const CompoundEntry = ({ name, value, topLevel = true, ...props }) => {
       </Box>
       {!!expanded && (
         <Box pl={2}>
-          <React.Fragment>
-            {entries.map(([k, v]) => (
-              <JsonDisplay key={k} name={k} value={v} topLevel={false} />
-            ))}
-            <Box onClick={toggle}>{braces[1]}</Box>
-          </React.Fragment>
+          {entries.map(([k, v]) => (
+            <JsonDisplayInner key={k} name={k} value={v} topLevel={false} />
+          ))}
+          <Box onClick={toggle}>{braces[1]}</Box>
         </Box>
       )}
     </Box>
   )
 }
 
-const isPrimitive = R.anyPass([R.is(String), R.is(Number), R.is(Boolean), R.equals(null)])
+const isPrimitive = R.anyPass([
+  R.is(String),
+  R.is(Number),
+  R.is(Boolean),
+  R.equals(null),
+  R.equals(undefined),
+])
 
-const JsonDisplay = ({ value, ...props }) => {
+function JsonDisplayInner({ value, ...props }) {
   const Component = isPrimitive(value) ? PrimitiveEntry : CompoundEntry
   return <Component value={value} {...props} />
 }
@@ -77,13 +88,15 @@ const Container = styled(Box)({
   whiteSpace: 'pre',
 })
 
-export default ({ name, value, topLevel, ...props }) => (
-  <Container
-    fontFamily="monospace.fontFamily"
-    fontSize="body2.fontSize"
-    width="100%"
-    {...props}
-  >
-    <JsonDisplay {...{ name, value, topLevel }} />
-  </Container>
-)
+export default function JsonDisplay({ name, value, topLevel, ...props }) {
+  return (
+    <Container
+      fontFamily="monospace.fontFamily"
+      fontSize="body2.fontSize"
+      width="100%"
+      {...props}
+    >
+      <JsonDisplayInner {...{ name, value, topLevel }} />
+    </Container>
+  )
+}
