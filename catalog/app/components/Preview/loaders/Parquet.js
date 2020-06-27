b409c1c042cc84aabe57125a2f92bb5cb047c822
nl_0 <nl.imbecile@gmail.com>
2020-06-27 23:51:42 +0500

    Parquet preview: dont process schema

--- catalog/app/components/Preview/loaders/Parquet.js
+++ catalog/app/components/Preview/loaders/Parquet.js
@@ -19,16 +19,7 @@ export const load = utils.previewFetcher(
       formatVersion: info.format_version,
       metadata: info.metadata,
       numRowGroups: info.num_row_groups,
-      schema: R.map(
-        (i) => ({
-          path: i.path,
-          logicalType: i.logical_type,
-          physicalType: i.physical_type,
-          maxDefinitionLevel: i.max_definition_level,
-          maxRepetitionLevel: i.max_repetition_level,
-        }),
-        info.schema,
-      ),
+      schema: info.schema,
       serializedSize: info.serialized_size,
       shape: { rows: info.shape[0], columns: info.shape[1] },
       note: info.note,
