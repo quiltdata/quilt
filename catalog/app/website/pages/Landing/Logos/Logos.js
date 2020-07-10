ba40d1da6c151b27b4941b06bd7e4950b7c4dd05
nl_0 <nl.imbecile@gmail.com>
2020-07-10 22:39:34 +0500

    Landing/Logos: auto-play, loop

--- catalog/app/website/pages/Landing/Logos/Logos.js
+++ catalog/app/website/pages/Landing/Logos/Logos.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Carousel from 'react-multi-carousel'
 import * as M from '@material-ui/core'
 
 import logoAics from './logo-aics.png'
@@ -8,43 +9,36 @@ import logoHudl from './logo-hudl.png'
 import logoRibon from './logo-ribon.png'
 import logoSight from './logo-sight.png'
 
-// TODO: inesrt relevant hrefs, double-check titles
 const logos = [
   {
     src: logoAics,
     height: 55,
     title: 'Allen Institute for Cell Science',
-    // href: '',
   },
   {
     src: logoSight,
     title: 'Sighthound',
     height: 60,
-    // href: '',
   },
   {
     src: logoHudl,
     height: 55,
     title: 'hudl',
-    // href: '',
   },
   {
     src: logoCelsius,
     height: 48,
     title: 'Celsius',
-    // href: '',
   },
   {
     src: logoRibon,
     title: 'Ribon Therapeutics',
     height: 65,
-    // href: '',
   },
   {
     src: logoCredit,
     height: 61,
     title: 'The Credit Junction',
-    // href: '',
   },
 ]
 
@@ -53,35 +47,40 @@ const useStyles = M.makeStyles((t) => ({
     display: 'flex',
     flexDirection: 'column',
     paddingBottom: t.spacing(14),
-    paddingTop: t.spacing(2),
+    paddingTop: t.spacing(12),
     position: 'relative',
   },
-  logos: {
+  sliderContainer: {
     alignItems: 'center',
     display: 'flex',
-    marginLeft: 'auto',
-    marginRight: 'auto',
     marginTop: t.spacing(9),
     maskImage:
-      'linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,1) 64px calc(100% - 64px), rgba(0,0,0,0))',
-    maxWidth: '100%',
-    overflowX: 'auto',
-    scrollbarWidth: 'none',
-    MsOverflowStyle: 'none',
-    '&::-webkit-scrollbar': {
-      display: 'none',
+      'linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,1) 150px calc(100% - 150px), rgba(0,0,0,0))',
+    overflow: 'hidden',
+    position: 'relative',
+    [t.breakpoints.down('xs')]: {
+      marginLeft: 'calc((100vw - 600px) / 2)',
+      marginRight: 'calc((100vw - 600px) / 2)',
+      minWidth: 600,
     },
   },
-  link: {
-    cursor: 'pointer',
-    paddingLeft: t.spacing(4),
-    paddingRight: t.spacing(4),
-    '&:first-child': {
-      paddingLeft: t.spacing(8),
-    },
-    '&:last-child': {
-      paddingRight: t.spacing(8),
-    },
+  slider: {
+    alignItems: 'center',
+    backfaceVisibility: 'hidden',
+    display: 'flex',
+    flexDirection: 'row',
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+    position: 'relative',
+    transformStyle: 'preserve-3d',
+    willChange: 'transform, transition',
+  },
+  item: {
+    display: 'flex',
+    justifyContent: 'center',
+    transformStyle: 'preserve-3d',
+    backfaceVisibility: 'hidden',
   },
   img: {
     display: 'block',
@@ -90,6 +89,7 @@ const useStyles = M.makeStyles((t) => ({
 
 export default function Logos() {
   const classes = useStyles()
+
   return (
     <div className={classes.root}>
       <M.Container maxWidth="lg">
@@ -97,19 +97,50 @@ export default function Logos() {
           Companies that love Quilt
         </M.Typography>
       </M.Container>
-      <div className={classes.logos}>
+      <Carousel
+        responsive={{
+          xxl: {
+            breakpoint: { max: 3000, min: 1800 },
+            items: 5,
+          },
+          xl: {
+            breakpoint: { max: 1800, min: 1500 },
+            items: 4,
+          },
+          lg: {
+            breakpoint: { max: 1500, min: 1200 },
+            items: 3,
+          },
+          md: {
+            breakpoint: { max: 1200, min: 800 },
+            items: 2,
+          },
+          sm: {
+            breakpoint: { max: 800, min: 0 },
+            items: 1,
+          },
+        }}
+        sliderClass={classes.slider}
+        itemClass={classes.item}
+        containerClass={classes.sliderContainer}
+        arrows={false}
+        infinite
+        centerMode
+        focusOnSelect
+        autoPlay
+        autoPlaySpeed={5000}
+      >
         {logos.map((l) => (
-          <a href={l.href} className={classes.link} key={l.title}>
-            <img
-              title={l.title}
-              alt={l.title}
-              src={l.src}
-              className={classes.img}
-              style={{ height: l.height }}
-            />
-          </a>
+          <img
+            key={l.title}
+            title={l.title}
+            alt={l.title}
+            src={l.src}
+            className={classes.img}
+            style={{ height: l.height }}
+          />
         ))}
-      </div>
+      </Carousel>
     </div>
   )
 }
