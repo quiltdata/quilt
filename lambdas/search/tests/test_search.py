65a61d5b588d217f81007ed5a759d21680e9e345
Aneesh Karve <aneesh.karve@gmail.com>
2020-07-22 12:46:41 -0700

    lint: strip dead import

--- lambdas/search/tests/test_search.py
+++ lambdas/search/tests/test_search.py
@@ -5,7 +5,7 @@ import json
 import os
 from unittest import TestCase
 from unittest.mock import patch
-from urllib.parse import quote, urlencode
+from urllib.parse import urlencode
 
 import responses
 
