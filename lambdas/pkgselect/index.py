dc0f61fdf33930dc5ed0f8a84a7626949fe414b5
Kevin Moore <kevinemoore@gmail.com>
2020-07-30 17:13:57 -0700

    Package select lambda (#1714)
    ## API to browse a package contents w/o downloading the manifest
    Use s3 select to filter package manifests and return only logical keys. Then turn the flat list of keys into a virtual folder view using Pandas.
    * Check for an end event from s3 to ensure that all results are counted.
    * Unit tests
    * Use SUBSTRING in S3 Select. Replacing the use of LIKE with SUBSTRING and exact match in calls to S3 Select. Also, using SUBSTRING to strip folder prefixes in S3 Select (replacing stripping the prefix in pandas).
    * Add detail view for single files that returns an entire row of a manifest
    * Separate prefixes and objects in response
    * Support quotes in logical keys
    * Buffer all s3 responses just in case s3 returns a partial-row response, load the complete response into a buffer before processing.
    * Add session token to temp credentials
    * Add circle ci test 
    * test requirements
    * adding an end-to-end test and taking suggestions from code reviews.
    * Move buffer_s3response to shared
    * Add type hints
    
    Co-authored-by: Kevin Moore <kevin@quiltdata.io>
    Co-authored-by: Sergey Fedoseev <fedoseev.sergey@quiltdata.io>

---
+++ lambdas/pkgselect/index.py
@@ -0,0 +1,160 @@
+"""
+Provide a virtual-file-system view of a package's logical keys.
+"""
+
+import json
+
+import boto3
+import pandas as pd
+
+from t4_lambda_shared.decorator import api, validate
+from t4_lambda_shared.utils import buffer_s3response, get_default_origins, make_json_response
+
+SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'bucket': {
+            'type': 'string'
+        },
+        'manifest': {
+            'type': 'string'
+        },
+        'access_key': {
+            'type': 'string'
+        },
+        'secret_key': {
+            'type': 'string'
+        },
+        'session_token': {
+            'type': 'string'
+        },
+        'prefix': {
+            'type': 'string'
+        },
+        'logical_key': {
+            'type': 'string'
+        }
+    },
+    'required': ['bucket', 'manifest', 'access_key', 'secret_key'],
+    'additionalProperties': False
+}
+
+
+def file_list_to_folder(df: pd.DataFrame) -> dict:
+    """
+    Post process a set of logical keys to return only the
+    top-level folder view (a special case of the s3-select
+    lambda).
+    """
+    # matches all strings; everything before and including the first
+    # / is extracted
+    folder = pd.Series(df.logical_key.dropna().str.extract('([^/]+/?).*')[0].unique())
+    return dict(
+        prefixes=folder[folder.str.endswith('/')].sort_values().tolist(),
+        objects=folder[~folder.str.endswith('/')].sort_values().tolist()
+    )
+
+
+def get_s3_client(
+    aws_access_key_id: str,
+    aws_secret_access_key: str,
+    aws_session_token: str
+):
+    """
+    Create an S3 Client using the provided credentials
+    """
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token
+    )
+    s3_client = session.client('s3')
+    return s3_client
+
+
+def call_s3_select(
+    s3_client: str,
+    bucket: str,
+    key: str,
+    logical_key_prefix: str,
+    detail=False
+):
+    """
+    Call S3 Select to read only the logical keys from a
+    package manifest that match the desired folder path
+    prefix
+    """
+
+    if logical_key_prefix is None:
+        logical_key_prefix = ""
+
+    prefix_length = len(logical_key_prefix)
+    sanitized = logical_key_prefix.replace("'", "''")
+
+    if detail:
+        logical_key = sanitized
+        sql_stmt = f"SELECT s.* FROM s3object s WHERE s.logical_key = '{logical_key}' LIMIT 1"
+    else:
+        prefix = sanitized
+        sql_stmt = f"SELECT SUBSTRING(s.logical_key, {prefix_length + 1}) AS logical_key FROM s3object s"
+        if prefix:
+            sql_stmt += f" WHERE SUBSTRING(s.logical_key, 1, {prefix_length}) = '{prefix}'"
+
+    print(sql_stmt)
+    response = s3_client.select_object_content(
+        Bucket=bucket,
+        Key=key,
+        ExpressionType='SQL',
+        Expression=sql_stmt,
+        InputSerialization={
+            'JSON': {'Type': 'LINES'},
+            'CompressionType': 'NONE'
+        },
+        OutputSerialization={'JSON': {'RecordDelimiter': '\n'}}
+    )
+    return response
+
+
+@api(cors_origins=get_default_origins())
+@validate(SCHEMA)
+def lambda_handler(request):
+    """
+    Parse a manifest to return a folder-like view of its contents (logical keys).
+
+    Returns:
+        JSON response
+    """
+    bucket = request.args['bucket']
+    key = request.args['manifest']
+    aws_access_key_id = request.args['access_key']
+    aws_secret_access_key = request.args['secret_key']
+    aws_session_token = request.args['session_token']
+    prefix = request.args.get('prefix')
+    logical_key = request.args.get('logical_key')
+
+    # Create an s3 client using the provided credentials
+    s3_client = get_s3_client(aws_access_key_id, aws_secret_access_key, aws_session_token)
+
+    # Get details of a single file in the package
+    if logical_key is not None:
+        response = call_s3_select(s3_client, bucket, key, logical_key, detail=True)
+        # parse and prep response
+        response_data = json.load(buffer_s3response(response))
+    else:
+        # Call s3 select to fetch only logical keys matching the
+        # desired prefix (folder path)
+        response = call_s3_select(s3_client, bucket, key, prefix)
+        result = buffer_s3response(response)
+
+        # Parse the response into a logical folder view
+        df = pd.read_json(result, lines=True)
+        response_data = file_list_to_folder(df)
+
+    ret_val = make_json_response(
+        200,
+        {
+            'contents': response_data
+        }
+    )
+
+    return ret_val
---
+++ lambdas/pkgselect/test/test_pkgselect.py
@@ -0,0 +1,313 @@
+"""
+Test functions for pkgselect endpoint
+"""
+
+import json
+import os
+from unittest import TestCase
+from unittest.mock import patch
+
+import boto3
+import pandas as pd
+import responses
+
+from t4_lambda_shared.utils import (
+    buffer_s3response, read_body, IncompleteResultException
+)
+
+from ..index import (
+    call_s3_select, file_list_to_folder,
+    lambda_handler
+)
+
+
+class TestPackageSelect(TestCase):
+    """
+    Unit tests for the PackageSelect API endpoint.
+    """
+    def setUp(self):
+        """
+        Mocks to tests calls to S3 Select
+        """
+
+        logical_keys = [
+            "foo.csv",
+            "bar/file1.txt",
+            "bar/file2.txt",
+            "bar/baz/file3.txt",
+            "bar/baz/file4.txt"
+        ]
+        jsonl = ""
+        for key in logical_keys:
+            jsonl += "{\"logical_key\": \"%s\"}\n" % key
+        print(jsonl)
+        streambytes = jsonl.encode()
+
+        self.s3response = {
+            'Payload': [
+                {
+                    'Records': {
+                        'Payload': streambytes
+                    }
+                },
+                {
+                    'Progress': {
+                        'Details': {
+                            'BytesScanned': 123,
+                            'BytesProcessed': 123,
+                            'BytesReturned': 123
+                        }
+                    }
+                },
+                {
+                    'Stats': {
+                        'Details': {
+                            'BytesScanned': 123,
+                            'BytesProcessed': 123,
+                            'BytesReturned': 123
+                        }
+                    }
+                },
+                {
+                    'End': {}
+                }
+            ]
+        }
+
+        self.s3response_incomplete = {
+            'Payload': [
+                {
+                    'Records': {
+                        'Payload': streambytes
+                    }
+                },
+                {
+                    'Stats': {
+                        'Details': {
+                            'BytesScanned': 123,
+                            'BytesProcessed': 123,
+                            'BytesReturned': 123
+                        }
+                    }
+                }
+            ]
+        }
+
+        requests_mock = responses.RequestsMock(assert_all_requests_are_fired=False)
+        requests_mock.start()
+        self.addCleanup(requests_mock.stop)
+
+        env_patcher = patch.dict(os.environ, {
+            'AWS_ACCESS_KEY_ID': 'test_key',
+            'AWS_SECRET_ACCESS_KEY': 'test_secret',
+        })
+        env_patcher.start()
+        self.addCleanup(env_patcher.stop)
+
+    @classmethod
+    def _make_event(cls, params, headers=None):
+        return {
+            'httpMethod': 'POST',
+            'path': '/foo',
+            'pathParameters': {},
+            'queryStringParameters': params or None,
+            'headers': headers or None,
+            'body': None,
+            'isBase64Encoded': False,
+        }
+
+    def test_call_s3select_top_level(self):
+        """
+        Test that parameters are correctly passed to
+        S3 Select (without a prefix)
+        """
+        bucket = "bucket"
+        key = ".quilt/packages/manifest_hash"
+
+        expected_args = {
+            'Bucket': bucket,
+            'Key': key,
+            'Expression': "SELECT SUBSTRING(s.logical_key, 1) AS logical_key FROM s3object s",
+            'ExpressionType': 'SQL',
+            'InputSerialization': {
+                'CompressionType': 'NONE',
+                'JSON': {'Type': 'LINES'}
+                },
+            'OutputSerialization': {'JSON': {'RecordDelimiter': '\n'}},
+        }
+
+        mock_s3 = boto3.client('s3')
+        with patch.object(
+                mock_s3,
+                'select_object_content',
+                return_value=self.s3response_incomplete
+        ) as patched:
+            call_s3_select(mock_s3, bucket, key, "")
+            patched.assert_called_once_with(**expected_args)
+
+    def test_call_s3select_prefix(self):
+        """
+        Test that parameters are correctly passed to
+        S3 Select (with a prefix)
+        """
+        bucket = "bucket"
+        key = ".quilt/packages/manifest_hash"
+        prefix = "bar/"
+
+        expected_sql = "SELECT SUBSTRING(s.logical_key, 5) AS logical_key FROM s3object s"
+        expected_sql += f" WHERE SUBSTRING(s.logical_key, 1, 4) = '{prefix}'"
+        expected_args = {
+            'Bucket': bucket,
+            'Key': key,
+            'Expression': expected_sql,
+            'ExpressionType': 'SQL',
+            'InputSerialization': {
+                'CompressionType': 'NONE',
+                'JSON': {'Type': 'LINES'}
+            },
+            'OutputSerialization': {'JSON': {'RecordDelimiter': '\n'}},
+        }
+
+        mock_s3 = boto3.client('s3')
+        with patch.object(
+                mock_s3,
+                'select_object_content',
+                return_value=self.s3response
+        ) as patched:
+            call_s3_select(mock_s3, bucket, key, prefix)
+            patched.assert_called_once_with(**expected_args)
+
+    def test_call_s3select_quote_in_prefix(self):
+        """
+        Test that parameters are correctly passed to
+        S3 Select (with a prefix)
+        """
+        bucket = "bucket"
+        key = ".quilt/packages/manifest_hash"
+        prefix = "Alice's files/"
+
+        expected_sql = "SELECT SUBSTRING(s.logical_key, 15) AS logical_key FROM s3object s"
+        expected_sql += " WHERE SUBSTRING(s.logical_key, 1, 14) = 'Alice''s files/'"
+        expected_args = {
+            'Bucket': bucket,
+            'Key': key,
+            'Expression': expected_sql,
+            'ExpressionType': 'SQL',
+            'InputSerialization': {
+                'CompressionType': 'NONE',
+                'JSON': {'Type': 'LINES'}
+                },
+            'OutputSerialization': {'JSON': {'RecordDelimiter': '\n'}},
+        }
+
+        mock_s3 = boto3.client('s3')
+        with patch.object(
+                mock_s3,
+                'select_object_content',
+                return_value=self.s3response
+        ) as patched:
+            call_s3_select(mock_s3, bucket, key, prefix)
+            patched.assert_called_once_with(**expected_args)
+
+    def test_incomplete_response(self):
+        """
+        Test that an incomplete response from S3 Select is
+        detected and an exception is raised.
+        """
+        with self.assertRaises(IncompleteResultException):
+            buffer_s3response(self.s3response_incomplete)
+
+    def test_browse_top_level(self):
+        """
+        Test that the S3 Select response is parsed
+        into the correct top-level folder view.
+        """
+        df = pd.read_json(buffer_s3response(self.s3response), lines=True)
+        assert isinstance(df, pd.DataFrame)
+
+        folder = file_list_to_folder(df)
+        assert len(folder['prefixes']) == 1
+        assert len(folder['objects']) == 1
+        assert 'foo.csv' in folder['objects']
+        assert 'bar/' in folder['prefixes']
+
+    def test_browse_subfolder(self):
+        """
+        Test that the S3 Select response is parsed
+        into the correct sub-folder view.
+        """
+        prefix = "bar/"
+        df = pd.read_json(buffer_s3response(self.s3response), lines=True)
+        assert isinstance(df, pd.DataFrame)
+
+        filtered_df = df[df['logical_key'].str.startswith(prefix)]
+        stripped = filtered_df['logical_key'].str.slice(start=len(prefix))
+        folder = file_list_to_folder(stripped.to_frame('logical_key'))
+        print(folder)
+        assert len(folder['prefixes']) == 1
+        assert len(folder['objects']) == 2
+        assert "file1.txt" in folder['objects']
+        assert "file2.txt" in folder['objects']
+        assert "baz/" in folder['prefixes']
+
+    def test_browse_subsubfolder(self):
+        """
+        Test that the S3 Select response is parsed
+        into the correct sub-sub-folder view.
+        """
+        prefix = "bar/baz/"
+        df = pd.read_json(buffer_s3response(self.s3response), lines=True)
+        assert isinstance(df, pd.DataFrame)
+        filtered_df = df[df['logical_key'].str.startswith(prefix)]
+        stripped = filtered_df['logical_key'].str.slice(start=len(prefix))
+        folder = file_list_to_folder(stripped.to_frame('logical_key'))
+        assert "objects" in folder
+        assert "prefixes" in folder
+        assert not folder['prefixes']
+        assert len(folder['objects']) == 2
+        assert "file3.txt" in folder['objects']
+        assert "file4.txt" in folder['objects']
+
+    def test_lambda(self):
+        """
+        End-to-end test (folder view without a prefix)
+        """
+        bucket = "bucket"
+        key = ".quilt/packages/manifest_hash"
+        params = dict(
+            bucket=bucket,
+            manifest=key,
+            access_key="TESTKEY",
+            secret_key="TESTSECRET",
+            session_token="TESTSESSION"
+        )
+
+        expected_args = {
+            'Bucket': bucket,
+            'Key': key,
+            'Expression': "SELECT SUBSTRING(s.logical_key, 1) AS logical_key FROM s3object s",
+            'ExpressionType': 'SQL',
+            'InputSerialization': {
+                'CompressionType': 'NONE',
+                'JSON': {'Type': 'LINES'}
+                },
+            'OutputSerialization': {'JSON': {'RecordDelimiter': '\n'}},
+        }
+
+        mock_s3 = boto3.client('s3')
+        client_patch = patch.object(
+            mock_s3,
+            'select_object_content',
+            return_value=self.s3response
+        )
+        client_patch.start()
+        with patch('boto3.Session.client', return_value=mock_s3):
+            response = lambda_handler(self._make_event(params), None)
+            print(response)
+            assert response['statusCode'] == 200
+            folder = json.loads(read_body(response))['contents']
+            assert len(folder['prefixes']) == 1
+            assert len(folder['objects']) == 1
+            assert 'foo.csv' in folder['objects']
+            assert 'bar/' in folder['prefixes']
+        client_patch.stop()
