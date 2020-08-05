8abce3d010713625a88f03bc7139bd5fb97e3e7d
nl_0 <nl.imbecile@gmail.com>
2020-08-05 12:09:42 +0500

    internal/webpack: update CopyWebpackPlugin opts

--- catalog/internals/webpack/webpack.base.babel.js
+++ catalog/internals/webpack/webpack.base.babel.js
@@ -90,11 +90,13 @@ module.exports = (options) => ({
     ],
   },
   plugins: options.plugins.concat([
-    new CopyWebpackPlugin([
-      {
-        from: 'static',
-      },
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'static',
+        },
+      ],
+    }),
 
     // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
     // inside your code for any environment checks; Terser will automatically
