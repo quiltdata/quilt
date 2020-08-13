949c8eaf18258ad6f6d097326ff63c3a8eab96f9
nl_0 <nl.imbecile@gmail.com>
2020-08-13 18:44:07 +0500

    NotFoundPage: pass props, refactoring, cleanup

--- catalog/app/containers/NotFoundPage/index.js
+++ catalog/app/containers/NotFoundPage/index.js
@@ -5,15 +5,14 @@ import Error from 'components/Error'
 import Layout from 'components/Layout'
 import { createBoundary } from 'utils/ErrorBoundary'
 import { BaseError } from 'utils/error'
-import { composeComponent } from 'utils/reactTools'
 
-export const NotFoundPage = composeComponent('NotFoundPage', () => (
-  <Layout>
-    <Error headline="Nothing here" detail="Do you need to log in?" />
-  </Layout>
-))
-
-export default NotFoundPage
+export default function NotFoundPage() {
+  return (
+    <Layout>
+      <Error headline="Nothing here" detail="Do you need to log in?" />
+    </Layout>
+  )
+}
 
 export class NotFoundError extends BaseError {}
 
@@ -22,6 +21,10 @@ export const ThrowNotFound = () => {
 }
 
 export const createNotFound = (Component) =>
-  createBoundary(() => R.when(R.is(NotFoundError), () => <Component />), 'CatchNotFound')
+  createBoundary(
+    (props) =>
+      R.when(R.is(NotFoundError), (error) => <Component {...props} error={error} />),
+    'CatchNotFound',
+  )
 
 export const CatchNotFound = createNotFound(NotFoundPage)
