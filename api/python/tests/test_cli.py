42657544fbc6721a5eb0e933193fe20101576627
Greg  Ezema <gregobinna@gmail.com>
2020-09-10 17:39:57 +0100

    add meta argument to the push CLI

--- api/python/tests/test_cli.py
+++ api/python/tests/test_cli.py
@@ -2,11 +2,12 @@ import tempfile
 from pathlib import Path
 from unittest import mock
 
+import pytest
 import quilt3
 from quilt3 import main
+from quilt3.util import QuiltException
 
 from .utils import QuiltTestCase
-
 
 create_parser = main.create_parser
 
@@ -15,6 +16,7 @@ class CommandLineTestCase(QuiltTestCase):
     """
     Base TestCase class, sets up a CLI parser
     """
+
     @classmethod
     def setUpClass(cls):
         parser = create_parser()
@@ -36,10 +38,43 @@ class QuiltCLITestCase(CommandLineTestCase):
             (Path(tmp_dir) / 'bar' / 'baz')
 
             with mock.patch('quilt3.Package.__new__', return_value=pkg) as mocked_package_class, \
-                 mock.patch.object(pkg, 'set_dir', wraps=pkg.set_dir) as mocked_set_dir, \
-                 mock.patch.object(pkg, 'push') as mocked_push:
+                mock.patch.object(pkg, 'set_dir', wraps=pkg.set_dir) as mocked_set_dir, \
+                mock.patch.object(pkg, 'push') as mocked_push:
                 main.main(('push', '--dir', tmp_dir, name))
 
                 mocked_package_class.assert_called_once_with(quilt3.Package)
                 mocked_set_dir.assert_called_once_with('.', tmp_dir)
                 mocked_push.assert_called_once_with(name, registry=None, dest=None, message=None)
+
+
+@pytest.mark.parametrize(
+    'arg, meta, expected_set_dir_count, expected_push_count, expected_meta',
+    [
+        (None, None, 1, 1, None),
+        ('--meta', '{invalid: meta}', 0, 0, {}),
+        ('--meta', "{'single': 'quotation'}", 0, 0, {}),
+        ('--meta', '{"test": "meta", }', 0, 0, {}),
+        ('--meta', '{"test": "meta"}', 1, 1, {"test": "meta"}),
+    ]
+)
+def test_push_with_meta_data(arg, meta, expected_set_dir_count, expected_push_count, expected_meta):
+    name = 'test/name'
+    pkg = quilt3.Package()
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        (Path(tmp_dir) / 'foo').touch()
+        (Path(tmp_dir) / 'bar').mkdir()
+        (Path(tmp_dir) / 'bar' / 'baz')
+
+        with mock.patch('quilt3.Package.__new__', return_value=pkg) as mocked_package_class, \
+            mock.patch.object(pkg, 'set_dir', wraps=pkg.set_dir) as mocked_set_dir, \
+            mock.patch.object(pkg, 'push') as mocked_push:
+
+            if arg:
+                main.main(('push', '--dir', tmp_dir, name, arg, meta))
+            else:
+                main.main(('push', '--dir', tmp_dir, name))
+            mocked_package_class.assert_called_once_with(quilt3.Package)
+            assert mocked_set_dir.call_count == expected_set_dir_count
+            assert mocked_push.call_count == expected_push_count
+            assert pkg.meta == expected_meta
