3c3e2a267bfa6c0bae494bd1d6b5df524710bc30
nl_0 <nl.imbecile@gmail.com>
2020-09-11 16:39:43 +0500

    image preview: better progress / error display

--- catalog/app/components/Preview/loaders/Image.js
+++ catalog/app/components/Preview/loaders/Image.js
@@ -7,7 +7,6 @@ import * as utils from './utils'
 
 export const detect = utils.extIn(SUPPORTED_EXTENSIONS)
 
-// TODO: issue a head request to ensure existance and get storage class
 export const Loader = function ImageLoader({ handle, children }) {
   return children(AsyncResult.Ok(PreviewData.Image({ handle })))
 }
