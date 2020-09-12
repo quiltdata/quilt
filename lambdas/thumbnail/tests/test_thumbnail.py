5fd24c3f25f167399334eef8608094f95340d7df
Aneesh Karve <akarve@users.noreply.github.com>
2020-09-12 11:08:15 -0700

    Fix --poppler unit tests, add test for empty images (#1798)

--- lambdas/thumbnail/tests/test_thumbnail.py
+++ lambdas/thumbnail/tests/test_thumbnail.py
@@ -12,8 +12,7 @@ import responses
 from aicsimageio import AICSImage
 
 from t4_lambda_shared.utils import read_body
-
-from ..index import lambda_handler, set_pdf_env
+from .. import index
 
 
 HEADER_403 = {
@@ -58,38 +57,36 @@ def test_403():
     }
     event = _make_event({"url": url, **params})
     # Get the response
-    response = lambda_handler(event, None)
+    response = index.lambda_handler(event, None)
     assert response["statusCode"] == 403
     body = json.loads(response["body"])
     assert "text" in body
     assert "error" in body
 
 
-@patch.dict(os.environ, {
-    'LAMBDA_TASK_ROOT': str(Path('/var/task')),
-    # need the real OS path so that we can find pdftoppm, but patch to avoid side-effects
-    'PATH': os.environ.get("PATH"),
-    'LD_LIBRARY_PATH': str(Path('/lib64')) + os.pathsep + str(Path('/usr/lib64')),
-    # set_pdf_env() will blow this away
-    # it's only here to prevent side-effects on the test host
-    'FONT_CONFIG_PATH': ''
-})
 @responses.activate
 @pytest.mark.parametrize(
-    "input_file, params, expected_thumb, expected_original_size, expected_thumb_size, num_pages",
+    "input_file, params, expected_thumb, expected_original_size, expected_thumb_size, num_pages, status",
     [
-        ("penguin.jpg", {"size": "w256h256"}, "penguin-256.jpg", [1526, 1290, 3], [217, 256], None),
-        ("cell.tiff", {"size": "w640h480"}, "cell-480.png", [15, 1, 158, 100], [514, 480], None),
-        ("cell.png", {"size": "w64h64"}, "cell-64.png", [168, 104, 3], [39, 64], None),
-        ("sat_greyscale.tiff", {"size": "w640h480"}, "sat_greyscale-480.png", [512, 512], [480, 480], None),
-        ("generated.ome.tiff", {"size": "w256h256"}, "generated-256.png", [6, 36, 76, 68], [224, 167], None),
-        ("sat_rgb.tiff", {"size": "w256h256"}, "sat_rgb-256.png", [256, 256, 4], [256, 256], None),
-        ("single_cell.ome.tiff", {"size": "w256h256"}, "single_cell.png", [6, 40, 152, 126], [256, 205], None),
+        ("penguin.jpg", {"size": "w256h256"}, "penguin-256.jpg", [1526, 1290, 3], [217, 256], None, 200),
+        ("cell.tiff", {"size": "w640h480"}, "cell-480.png", [15, 1, 158, 100], [514, 480], None, 200),
+        ("cell.png", {"size": "w64h64"}, "cell-64.png", [168, 104, 3], [39, 64], None, 200),
+        ("sat_greyscale.tiff", {"size": "w640h480"}, "sat_greyscale-480.png", [512, 512], [480, 480], None, 200),
+        ("generated.ome.tiff", {"size": "w256h256"}, "generated-256.png", [6, 36, 76, 68], [224, 167], None, 200),
+        ("sat_rgb.tiff", {"size": "w256h256"}, "sat_rgb-256.png", [256, 256, 4], [256, 256], None, 200),
+        ("single_cell.ome.tiff", {"size": "w256h256"}, "single_cell.png", [6, 40, 152, 126], [256, 205], None, 200),
         # Test for statusCode error
+        pytest.param(
+            "empty.png",
+            {"size": "w32h32"},
+            None, None, None, None, 500,
+            marks=pytest.mark.xfail(raises=AssertionError)
+        ),
+        # Test known bad file
         pytest.param(
             "cell.png",
             {"size": "w1h1"},
-            None, None, None, None,
+            None, None, None, None, 400,
             marks=pytest.mark.xfail(raises=AssertionError)
         ),
         # The following PDF tests should only run if poppler-utils is installed;
@@ -97,19 +94,19 @@ def test_403():
         pytest.param(
             "MUMmer.pdf",
             {"size": "w1024h768", "input": "pdf", "page": "4"},
-            "pdf-page4-1024w.jpeg", None, [1024, 1450], None,
+            "pdf-page4-1024w.jpeg", None, [1024, 1450], None, 200,
             marks=pytest.mark.poppler
         ),
         pytest.param(
             "MUMmer.pdf",
             {"size": "w256h256", "input": "pdf", "page": "8"},
-            "pdf-page8-256w.jpeg", None, [256, 363], None,
+            "pdf-page8-256w.jpeg", None, [256, 363], None, 200,
             marks=pytest.mark.poppler
         ),
         pytest.param(
             "MUMmer.pdf",
             {"size": "w1024h768", "input": "pdf", "page": "4", "countPages": "true"},
-            "pdf-page4-1024w.jpeg", None, [1024, 1450], 8,
+            "pdf-page4-1024w.jpeg", None, [1024, 1450], 8, 200,
             marks=pytest.mark.poppler
         ),
     ]
@@ -121,40 +118,49 @@ def test_generate_thumbnail(
         expected_thumb,
         expected_original_size,
         expected_thumb_size,
-        num_pages
+        num_pages,
+        status
 ):
-    # Resolve the input file path
-    input_file = data_dir / input_file
-    # Mock the request
-    url = f"https://example.com/{input_file}"
-    responses.add(
-        responses.GET,
-        url=url,
-        body=input_file.read_bytes(),
-        status=200
-    )
-    # Create the lambda request event
-    event = _make_event({"url": url, **params})
-    # Get the response
-    response = lambda_handler(event, None)
-    # Assert the request was handled with no errors
-    assert response["statusCode"] == 200, f"response: {response}"
-    # Parse the body / the returned thumbnail
-    body = json.loads(read_body(response))
-    # Assert basic metadata was filled properly
-    assert body["info"]["thumbnail_size"] == expected_thumb_size
-    if expected_original_size:  # PDFs don't have an expected size
-        assert body["info"]["original_size"] == expected_original_size
-    if "countPages" in params:
-        assert body["info"]["page_count"] == num_pages
-    # Assert the produced image is the same as the expected
-    if params.get('input') == 'pdf':
-        actual = Image.open(BytesIO(base64.b64decode(body['thumbnail'])))
-        expected = Image.open(data_dir / expected_thumb)
-    else:
-        actual = AICSImage(base64.b64decode(body['thumbnail'])).reader.data
-        expected = AICSImage(data_dir / expected_thumb).reader.data
-    assert np.array_equal(actual, expected)
+    # don't actually modify the environment in tests
+    with patch.object(index, 'set_pdf_env', return_value=None) as set_env:
+        # Resolve the input file path
+        input_file = data_dir / input_file
+        # Mock the request
+        url = f"https://example.com/{input_file}"
+        responses.add(
+            responses.GET,
+            url=url,
+            body=input_file.read_bytes(),
+            status=200
+        )
+        # Create the lambda request event
+        event = _make_event({"url": url, **params})
+        # Get the response
+        response = index.lambda_handler(event, None)
+        # Assert the request was handled with no errors
+        assert response["statusCode"] == 200, f"response: {response}"
+        # only check the body and expected image if it's a successful call
+        # Parse the body / the returned thumbnail
+        body = json.loads(read_body(response))
+        # Assert basic metadata was filled properly
+        assert body["info"]["thumbnail_size"] == expected_thumb_size
+        if expected_original_size:  # PDFs don't have an expected size
+            assert body["info"]["original_size"] == expected_original_size
+        if "countPages" in params:
+            assert body["info"]["page_count"] == num_pages
+        # Assert the produced image is the same as the expected
+        if params.get('input') == 'pdf':
+            actual = Image.open(BytesIO(base64.b64decode(body['thumbnail'])))
+            expected = Image.open(data_dir / expected_thumb)
+            actual_array = np.array(actual)
+            expected_array = np.array(expected)
+            assert set_env.call_count == 1
+            assert actual_array.shape == expected_array.shape
+            assert np.allclose(expected_array, actual_array, atol=15, rtol=0.1)
+        else:
+            actual = AICSImage(base64.b64decode(body['thumbnail'])).reader.data
+            expected = AICSImage(data_dir / expected_thumb).reader.data
+            assert np.array_equal(actual, expected)
 
 
 @patch.dict(os.environ, {
@@ -163,11 +169,11 @@ def test_generate_thumbnail(
     'LD_LIBRARY_PATH': str(Path('/lib64')) + os.pathsep + str(Path('/usr/lib64')),
     # set_pdf_env() will blow this away
     # it's only here to prevent side-effects on the test host
-    'FONT_CONFIG_PATH': ''
+    'FONTCONFIG_FILE': ''
 })
 def test_pdf_env():
     """test that env vars are set so that poppler, pdf2image work properly"""
-    set_pdf_env()
+    index.set_pdf_env()
     assert os.environ.get('FONTCONFIG_FILE') == os.path.join(
         os.environ.get('LAMBDA_TASK_ROOT'),
         'quilt_binaries',
