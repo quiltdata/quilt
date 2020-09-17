655c08a7ac40297c85ef0ddbd48305b8b7e8ad6b
nl_0 <nl.imbecile@gmail.com>
2020-09-17 17:42:13 +0500

    Footer: always show current year

--- catalog/app/components/Footer/messages.js
+++
@@ -1,9 +0,0 @@
-/* Footer Messages */
-import { defineMessages } from 'react-intl'
-
-export default defineMessages({
-  copy: {
-    id: 'app.components.Footer.copy',
-    defaultMessage: '2019 Quilt Data, Inc.',
-  },
-})
