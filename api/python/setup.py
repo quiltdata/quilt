44d0d8008973391e3b258df2fe7b8e924d61c9cc
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2020-10-07 20:21:30 +0500

    Add CircleCI job for Python 3.9

--- api/python/setup.py
+++ api/python/setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     author='quiltdata',
     author_email='contact@quiltdata.io',
