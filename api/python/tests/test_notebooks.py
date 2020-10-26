0b0b0284350c06f1afaa6c6415cc4785dd926534
Greg  Ezema <gregobinna@gmail.com>
2020-10-26 13:34:43 +0100

    test all documentation notebooks

--- api/python/tests/test_notebooks.py
+++ api/python/tests/test_notebooks.py
@@ -1,7 +1,9 @@
+import re
 import shutil
 import subprocess
 from pathlib import Path
 
+from tests.utils import QuiltTestCase
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
@@ -16,7 +18,8 @@ class NotebookFilesHandler(FileSystemEventHandler):
         self.new_files.append(Path(event.src_path))
 
     def clean_up(self):
-        print("cleaning up files...")
+        """Delete newly created files and directories."""
+        print("deleting newly created files and directories...")
         for path in self.new_files:
             if path.exists():
                 if path.is_file():
@@ -25,31 +28,95 @@ class NotebookFilesHandler(FileSystemEventHandler):
                     shutil.rmtree(path)
 
 
-def test_walk_through_notebooks():
-    cwd = Path(__file__).parent
-    docs_path = list(cwd.parts)[:-3]
-    paths = [Path(*docs_path) / 'docs/Walkthrough/Editing a Package.ipynb', ]
+def execute_notebook(notebook):
+    """Execute a given notebook in a sub shell."""
+    notebook = re.escape(str(notebook))
+    cmd = f"jupyter nbconvert --to notebook --execute {notebook}"
+    subprocess.call(cmd, shell=True)
 
-    handler = NotebookFilesHandler()
-    observer = Observer()
 
-    try:
-        # start watcher for path 'docs'
-        path_to_watch = str(Path(*docs_path) / 'docs')
-        observer.schedule(handler, path=path_to_watch, recursive=True)
-        observer.start()
+class NotebookDocumentationTestCase(QuiltTestCase):
 
-        for path in paths:
-            path = str(path).replace(' ', '\\ ')
-            cmd = f"jupyter nbconvert --to notebook --execute {path}"
-            subprocess.call(cmd, shell=True)
+    @classmethod
+    def setUpClass(cls):
+        cwd = Path(__file__).parent
+        cls.quilt_path = list(cwd.parts)[:-3]
+        cls.handler = NotebookFilesHandler()
+        cls.observer = Observer()
 
-            # delete created files after each notebook run
-            handler.clean_up()
+        path_to_watch = str(Path(*cls.quilt_path) / 'docs')
+        cls.observer.schedule(cls.handler, path=path_to_watch, recursive=True)
+        cls.observer.start()
 
-        # stop observer to clean up resource
-        observer.stop()
-    except KeyboardInterrupt:
-        # stop observer and cleanup on keyboard interruption
-        observer.stop()
-        handler.clean_up()
+    @classmethod
+    def tearDownClass(cls):
+        """Stop observer to clean up resource"""
+        cls.observer.stop()
+        cls.observer.join()
+
+    def tearDown(self):
+        """Delete created files/directories after each notebook run."""
+        super().tearDown()
+        self.handler.clean_up()
+
+    def test_working_with_manifests_notebook(self):
+        docs_path = Path(*self.quilt_path) / 'docs/Advanced Features/'
+        notebook = docs_path / 'Working with Manifests.ipynb'
+        execute_notebook(notebook)
+
+        assert (docs_path / 'data.csv').exists()
+        assert (docs_path / 'example.jsonl').exists()
+        assert (docs_path / 'Working with Manifests.nbconvert.ipynb').exists()
+
+    def test_editing_a_package_notebook(self):
+        docs_path = Path(*self.quilt_path) / 'docs/Walkthrough/'
+        notebook = docs_path / 'Editing a Package.ipynb'
+        execute_notebook(notebook)
+
+        assert (docs_path / 'data.csv').exists()
+        assert (docs_path / 'data/').is_dir()
+        assert (docs_path / 'Editing a Package.nbconvert.ipynb').exists()
+
+    def test_getting_data_from_a_package_notebook(self):
+        docs_path = Path(*self.quilt_path) / 'docs/Walkthrough/'
+        notebook = docs_path / 'Getting Data from a Package.ipynb'
+        execute_notebook(notebook)
+
+        assert (docs_path / 'QuickStart.ipynb').exists()
+        assert (docs_path / '.quiltignore').exists()
+        assert (docs_path / 'quilt_summarize.json').exists()
+        assert (docs_path / 'scripts').is_dir()
+        assert (docs_path / 'notebooks').is_dir()
+        assert (docs_path / 'references').is_dir()
+        assert (docs_path / 'Getting Data from a Package.nbconvert.ipynb').exists()
+
+    def test_installing_a_package_notebook(self):
+        docs_path = Path(*self.quilt_path) / 'docs/Walkthrough/'
+        notebook = docs_path / 'Installing a Package.ipynb'
+        execute_notebook(notebook)
+
+        assert (docs_path / 'quilt_summarize.json').exists()
+        assert (docs_path / 'notebooks').is_dir()
+        assert (docs_path / 'scripts').is_dir()
+        assert (docs_path / 'data').is_dir()
+        assert (docs_path / 'Installing a Package.nbconvert.ipynb').exists()
+
+    def test_uploading_a_package_notebook(self):
+        docs_path = Path(*self.quilt_path) / 'docs/Walkthrough/'
+        notebook = docs_path / 'Uploading a Package.ipynb'
+        execute_notebook(notebook)
+
+        assert (docs_path / 'Uploading a Package.nbconvert.ipynb').exists()
+
+    def test_working_with_a_bucket_notebook(self):
+        docs_path = Path(*self.quilt_path) / 'docs/Walkthrough/'
+        notebook = docs_path / 'Working with a Bucket.ipynb'
+        execute_notebook(notebook)
+
+        assert (docs_path / 'read.md').exists()
+        assert (docs_path / 'aleksey').is_dir()
+        assert (docs_path / 'aleksey/quilt_summarize.json').exists()
+        assert (docs_path / 'aleksey/data').is_dir()
+        assert (docs_path / 'aleksey/notebooks').is_dir()
+        assert (docs_path / 'aleksey/scripts').is_dir()
+        assert (docs_path / 'Working with a Bucket.nbconvert.ipynb').exists()
