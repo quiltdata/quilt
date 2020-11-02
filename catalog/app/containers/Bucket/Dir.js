7820551dfbeca015c74b6073bec367e13d73b9de
nl_0 <nl.imbecile@gmail.com>
2020-11-02 23:12:52 +0500

    listing: apply filter explicitly by enter or btn

--- catalog/app/containers/Bucket/Dir.js
+++ catalog/app/containers/Bucket/Dir.js
@@ -4,7 +4,6 @@ import dedent from 'dedent'
 import * as R from 'ramda'
 import * as React from 'react'
 import { useHistory } from 'react-router-dom'
-import { useDebouncedCallback } from 'use-debounce'
 import * as M from '@material-ui/core'
 
 import { Crumb, copyWithoutSpaces, render as renderCrumbs } from 'components/BreadCrumbs'
@@ -14,7 +13,6 @@ import { useData } from 'utils/Data'
 import * as NamedRoutes from 'utils/NamedRoutes'
 import parseSearch from 'utils/parseSearch'
 import { getBreadCrumbs, ensureNoSlash, withoutPrefix, up, decode } from 'utils/s3paths'
-import usePrevious from 'utils/usePrevious'
 
 import Code from './Code'
 import { ListingItem, ListingWithPrefixFiltering } from './Listing'
@@ -123,31 +121,12 @@ export default function Dir({
     prefix,
   })
 
-  const [prefixValue, setPrefixValue] = React.useState(prefix || '')
-
   const setPrefix = React.useCallback(
     (newPrefix) => {
       history.push(urls.bucketDir(bucket, path, newPrefix))
     },
     [history, urls, bucket, path],
   )
-
-  const [setPrefixDebounced, , commitPrefixChange] = useDebouncedCallback(setPrefix, 300)
-
-  const changePrefixValue = React.useCallback(
-    (newPrefix) => {
-      setPrefixValue(newPrefix)
-      setPrefixDebounced(newPrefix)
-    },
-    [setPrefixValue, setPrefixDebounced],
-  )
-
-  // sync prefix from querystring to input value
-  usePrevious({ prefix }, (prev) => {
-    if (prev && prev.prefix !== prefix) {
-      setPrefixValue(prefix || '')
-    }
-  })
 
   return (
     <M.Box pt={2} pb={4}>
@@ -190,9 +169,9 @@ export default function Dir({
                 locked={locked}
                 truncated={res.truncated}
                 prefix={res.prefix}
-                prefixValue={prefixValue}
-                changePrefixValue={changePrefixValue}
-                commitPrefixChange={commitPrefixChange}
+                setPrefix={setPrefix}
+                bucket={res.bucket}
+                path={res.path}
               />
               <Summary files={res.files} />
             </>
