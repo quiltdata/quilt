74db24349236e2f881eccd758bc3c3c062ae8729
Dima Ryazanov <dima@gmail.com>
2020-11-03 23:48:43 -0800

    Don't rely on the registry DNS name

--- s3-proxy/run-nginx.sh
+++ s3-proxy/run-nginx.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-if [ -z "$REGISTRY_URL" ]
+if [ -z "$REGISTRY_HOST" ]
 then
-    echo "REGISTRY_URL not set"
+    echo "REGISTRY_HOST not set"
     exit 1
 fi
 
-REGISTRY_URL=${REGISTRY_URL%%/} # Remove a trailing slash.
+if [ -z "$INTERNAL_REGISTRY_URL" ]
+then
+    echo "INTERNAL_REGISTRY_URL not set"
+    exit 1
+fi
+
+INTERNAL_REGISTRY_URL=${INTERNAL_REGISTRY_URL%%/} # Remove a trailing slash.
 
 # Get the DNS server from /etc/resolv.conf
 nameserver=$(awk '{if ($1 == "nameserver") { print $2; exit;}}' < /etc/resolv.conf)
@@ -19,6 +25,6 @@ fi
 
 export NAMESERVER=$nameserver
 
-envsubst '$REGISTRY_URL $NAMESERVER' < /root/nginx.conf.tmpl > /etc/nginx/nginx.conf
+envsubst '$REGISTRY_HOST $INTERNAL_REGISTRY_URL $NAMESERVER' < /root/nginx.conf.tmpl > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
