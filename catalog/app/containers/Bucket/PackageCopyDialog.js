d5862962cfc335346c0b6299f99ade0aed285b72
Maxim Chervonny <fiskus@chervonny.ru>
2020-11-23 18:27:13 +0300

    wip

--- catalog/app/containers/Bucket/PackageCopyDialog.js
+++ catalog/app/containers/Bucket/PackageCopyDialog.js
@@ -1,18 +1,26 @@
+import cx from 'classnames'
 import * as R from 'ramda'
+import { FORM_ERROR } from 'final-form'
 import * as React from 'react'
+import { useDropzone } from 'react-dropzone'
 import * as RF from 'react-final-form'
 import * as M from '@material-ui/core'
 import { fade } from '@material-ui/core/styles'
 
+import AsyncResult from 'utils/AsyncResult'
 import * as AWS from 'utils/AWS'
+import * as Data from 'utils/Data'
+import Delay from 'utils/Delay'
+import { getBasename } from 'utils/s3paths'
 import { readableBytes } from 'utils/string'
 import tagged from 'utils/tagged'
+import * as validators from 'utils/validators'
 
-import * as PD from './PackageDialog'
 import * as ERRORS from './errors'
+import * as PD from './PackageDialog'
+import * as requests from './requests'
 
 function DialogWrapper({ exited, ...props }) {
-  console.log('DialogWrapper')
   const ref = React.useRef()
   ref.current = { exited, onExited: props.onExited }
   React.useEffect(
@@ -25,10 +33,491 @@ function DialogWrapper({ exited, ...props }) {
   return <M.Dialog {...props} />
 }
 
-function DialogForm() {
+const getTotalProgress = R.pipe(
+  R.values,
+  R.reduce(
+    (acc, { progress: p = {} }) => ({
+      total: acc.total + (p.total || 0),
+      loaded: acc.loaded + (p.loaded || 0),
+    }),
+    { total: 0, loaded: 0 },
+  ),
+  (p) => ({
+    ...p,
+    percent: p.total ? Math.floor((p.loaded / p.total) * 100) : 100,
+  }),
+)
+
+const useFilesInputStyles = M.makeStyles((t) => ({
+  root: {
+    marginTop: t.spacing(3),
+  },
+  header: {
+    alignItems: 'center',
+    display: 'flex',
+    height: 24,
+  },
+  headerFiles: {
+    ...t.typography.body1,
+    display: 'flex',
+  },
+  headerFilesDisabled: {
+    color: t.palette.text.secondary,
+  },
+  headerFilesError: {
+    color: t.palette.error.main,
+  },
+  headerFilesWarn: {
+    color: t.palette.warning.dark,
+  },
+  dropzoneContainer: {
+    marginTop: t.spacing(2),
+    position: 'relative',
+
+    '&::after': {
+      bottom: 0,
+      content: '""',
+      left: 0,
+      position: 'absolute',
+      right: 0,
+      top: 0,
+      background: 'rgba(255, 0, 0, 0.3)',
+      zIndex: 1,
+    },
+  },
+  dropzone: {
+    background: t.palette.action.hover,
+    border: `1px solid ${t.palette.action.disabled}`,
+    borderRadius: t.shape.borderRadius,
+    cursor: 'pointer',
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: 140,
+    outline: 'none',
+    overflow: 'hidden',
+  },
+  dropzoneErr: {
+    borderColor: t.palette.error.main,
+  },
+  dropzoneWarn: {
+    borderColor: t.palette.warning.dark,
+  },
+  active: {
+    background: t.palette.action.selected,
+  },
+  dropMsg: {
+    ...t.typography.body2,
+    alignItems: 'center',
+    display: 'flex',
+    flexGrow: 1,
+    justifyContent: 'center',
+    paddingBottom: t.spacing(1),
+    paddingTop: t.spacing(1),
+    textAlign: 'center',
+  },
+  dropMsgErr: {
+    color: t.palette.error.main,
+  },
+  dropMsgWarn: {
+    color: t.palette.warning.dark,
+  },
+  filesContainer: {
+    borderBottom: `1px solid ${t.palette.action.disabled}`,
+    maxHeight: 200,
+    overflowX: 'hidden',
+    overflowY: 'auto',
+  },
+  filesContainerErr: {
+    borderColor: t.palette.error.main,
+  },
+  filesContainerWarn: {
+    borderColor: t.palette.warning.dark,
+  },
+  fileEntry: {
+    alignItems: 'center',
+    background: t.palette.background.paper,
+    display: 'flex',
+    '&:not(:last-child)': {
+      borderBottomStyle: 'solid',
+      borderBottomWidth: '1px',
+      borderColor: 'inherit',
+    },
+  },
+  filePath: {
+    ...t.typography.body2,
+    flexGrow: 1,
+    marginRight: t.spacing(1),
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  },
+  fileSize: {
+    ...t.typography.body2,
+    color: t.palette.text.secondary,
+    marginRight: t.spacing(0.5),
+  },
+  lock: {
+    alignItems: 'center',
+    background: 'rgba(255,255,255,0.9)',
+    border: `1px solid ${t.palette.action.disabled}`,
+    borderRadius: t.shape.borderRadius,
+    bottom: 0,
+    cursor: 'not-allowed',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  progressContainer: {
+    display: 'flex',
+    position: 'relative',
+  },
+  progressPercent: {
+    ...t.typography.h5,
+    alignItems: 'center',
+    bottom: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  progressSize: {
+    ...t.typography.body2,
+    color: t.palette.text.secondary,
+    marginTop: t.spacing(1),
+  },
+}))
+
+export function FilesInput({
+  input: { value: inputValue, onChange: onInputChange },
+  meta,
+  uploads,
+  errors = {},
+}) {
+  const classes = useFilesInputStyles()
+
+  const value = inputValue || []
+  const disabled = meta.submitting || meta.submitSucceeded
+  const error = meta.submitFailed && meta.error
+
+  const totalSize = React.useMemo(() => value.reduce((sum, f) => sum + f.file.size, 0), [
+    value,
+  ])
+
+  const warn = totalSize > PD.MAX_SIZE
+
+  // eslint-disable-next-line no-nested-ternary
+  const label = error ? (
+    errors[error] || error
+  ) : warn ? (
+    <>Total file size exceeds recommended maximum of {readableBytes(PD.MAX_SIZE)}</>
+  ) : (
+    'Drop files here or click to browse'
+  )
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const onDrop = React.useCallback(
+    disabled
+      ? () => {}
+      : R.pipe(
+          R.reduce((entries, file) => {
+            const path = PD.getNormalizedPath(file)
+            const idx = entries.findIndex(R.propEq('path', path))
+            const put = idx === -1 ? R.append : R.update(idx)
+            return put({ path, file }, entries)
+          }, value),
+          R.sortBy(R.prop('path')),
+          onInputChange,
+        ),
+    [disabled, value, onInputChange],
+  )
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  // FIXME: we don't upload files
+  const totalProgress = React.useMemo(() => getTotalProgress(uploads), [uploads])
   return (
-    <RF.Form>
-      <h1>It works</h1>
+    <div className={classes.root}>
+      <div className={classes.header}>
+        <div
+          className={cx(
+            classes.headerFiles,
+            disabled // eslint-disable-line no-nested-ternary
+              ? classes.headerFilesDisabled
+              : error // eslint-disable-line no-nested-ternary
+              ? classes.headerFilesError
+              : warn
+              ? classes.headerFilesWarn
+              : undefined,
+          )}
+        >
+          Files
+          {!!value.length && (
+            <>
+              : {value.length} ({readableBytes(totalSize)})
+              {warn && <M.Icon style={{ marginLeft: 4 }}>error_outline</M.Icon>}
+            </>
+          )}
+        </div>
+      </div>
+
+      <div className={classes.dropzoneContainer}>
+        <div
+          {...getRootProps({
+            className: cx(
+              classes.dropzone,
+              isDragActive && !disabled && classes.active,
+              !!error && classes.dropzoneErr,
+              !error && warn && classes.dropzoneWarn,
+            ),
+          })}
+        >
+          <input {...getInputProps()} disabled />
+
+          {!!value.length && (
+            <div
+              className={cx(
+                classes.filesContainer,
+                !!error && classes.filesContainerErr,
+                !error && warn && classes.filesContainerWarn,
+              )}
+            >
+              {value.map(({ file }) => (
+                <div key={file.physicalKey} className={classes.fileEntry}>
+                  <M.Icon fontSize="small">attach_file</M.Icon>
+                  <div className={classes.filePath} title={file.physicalKey}>
+                    {getBasename(decodeURIComponent(file.physicalKey))}
+                  </div>
+                  <div className={classes.fileSize}>{readableBytes(file.size)}</div>
+                </div>
+              ))}
+            </div>
+          )}
+
+          <div
+            className={cx(
+              classes.dropMsg,
+              !!error && classes.dropMsgErr,
+              !error && warn && classes.dropMsgWarn,
+            )}
+          >
+            {label}
+          </div>
+        </div>
+        {disabled && (
+          <div className={classes.lock}>
+            <div className={classes.progressContainer}>
+              <M.CircularProgress
+                size={80}
+                value={totalProgress.percent}
+                variant="determinate"
+              />
+              <div className={classes.progressPercent}>{totalProgress.percent}%</div>
+            </div>
+            <div className={classes.progressSize}>
+              {readableBytes(totalProgress.loaded)} / {readableBytes(totalProgress.total)}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+function DialogForm({
+  bucket,
+  name: initialName,
+  close,
+  setSubmitting,
+  onSuccess,
+  manifest,
+  workflowsConfig,
+}) {
+  const [uploads, setUploads] = React.useState({})
+
+  const nameValidator = PD.useNameValidator()
+
+  const initialMeta = React.useMemo(
+    () => ({
+      mode: 'kv',
+      text: JSON.stringify(manifest.meta || {}),
+    }),
+    [manifest.meta],
+  )
+
+  const initialFiles = Object.values(manifest.entries).map((file) => ({
+    file,
+  }))
+
+  const onSubmit = async ({ name }) => {
+    onSuccess({ name })
+    return { [FORM_ERROR]: 'Error creating manifest' }
+  }
+
+  const onSubmitWrapped = async (...args) => {
+    setSubmitting(true)
+    try {
+      return await onSubmit(...args)
+    } finally {
+      setSubmitting(false)
+    }
+  }
+
+  const totalProgress = React.useMemo(() => getTotalProgress(uploads), [uploads])
+
+  return (
+    <RF.Form onSubmit={onSubmitWrapped}>
+      {({
+        handleSubmit,
+        submitting,
+        submitFailed,
+        error,
+        submitError,
+        hasValidationErrors,
+        form,
+        values,
+      }) => (
+        <>
+          <M.DialogTitle>Push package revision</M.DialogTitle>
+          <M.DialogContent style={{ paddingTop: 0 }}>
+            <form onSubmit={handleSubmit}>
+              <RF.Field
+                component={PD.Field}
+                name="name"
+                label="Name"
+                placeholder="Enter a package name"
+                validate={validators.composeAsync(
+                  validators.required,
+                  nameValidator.validate,
+                )}
+                validateFields={['name']}
+                errors={{
+                  required: 'Enter a package name',
+                  invalid: 'Invalid package name',
+                }}
+                margin="normal"
+                fullWidth
+                initialValue={initialName}
+              />
+
+              <RF.Field
+                component={PD.Field}
+                name="msg"
+                label="Commit message"
+                placeholder="Enter a commit message"
+                validate={validators.required}
+                validateFields={['msg']}
+                errors={{
+                  required: 'Enter a commit message',
+                }}
+                fullWidth
+                margin="normal"
+              />
+
+              <RF.Field
+                component={FilesInput}
+                name="files"
+                validate={validators.nonEmpty}
+                validateFields={['files']}
+                errors={{
+                  nonEmpty: 'Add files to create a package',
+                }}
+                uploads={uploads}
+                setUploads={setUploads}
+                isEqual={R.equals}
+                initialValue={initialFiles}
+              />
+
+              <PD.SchemaFetcher
+                schemaUrl={R.pathOr('', ['schema', 'url'], values.workflow)}
+              >
+                {AsyncResult.case({
+                  Ok: ({ responseError, schema, validate }) => (
+                    <RF.Field
+                      component={PD.MetaInput}
+                      name="meta"
+                      bucket={bucket}
+                      schema={schema}
+                      schemaError={responseError}
+                      validate={validate}
+                      validateFields={['meta']}
+                      isEqual={R.equals}
+                      initialValue={initialMeta}
+                    />
+                  ),
+                  _: () => <PD.MetaInputSkeleton />,
+                })}
+              </PD.SchemaFetcher>
+
+              <RF.Field
+                component={PD.WorkflowInput}
+                name="workflow"
+                workflowsConfig={workflowsConfig}
+                initialValue={PD.defaultWorkflowFromConfig(workflowsConfig)}
+                validateFields={['meta', 'workflow']}
+              />
+
+              <input type="submit" style={{ display: 'none' }} />
+            </form>
+          </M.DialogContent>
+          <M.DialogActions>
+            {submitting && (
+              <Delay ms={200} alwaysRender>
+                {(ready) => (
+                  <M.Fade in={ready}>
+                    <M.Box flexGrow={1} display="flex" alignItems="center" pl={2}>
+                      <M.CircularProgress
+                        size={24}
+                        variant={
+                          totalProgress.percent < 100 ? 'determinate' : 'indeterminate'
+                        }
+                        value={
+                          totalProgress.percent < 100
+                            ? totalProgress.percent * 0.9
+                            : undefined
+                        }
+                      />
+                      <M.Box pl={1} />
+                      <M.Typography variant="body2" color="textSecondary">
+                        {totalProgress.percent < 100
+                          ? 'Uploading files'
+                          : 'Writing manifest'}
+                      </M.Typography>
+                    </M.Box>
+                  </M.Fade>
+                )}
+              </Delay>
+            )}
+
+            {!submitting && (!!error || !!submitError) && (
+              <M.Box flexGrow={1} display="flex" alignItems="center" pl={2}>
+                <M.Icon color="error">error_outline</M.Icon>
+                <M.Box pl={1} />
+                <M.Typography variant="body2" color="error">
+                  {error || submitError}
+                </M.Typography>
+              </M.Box>
+            )}
+
+            <M.Button onClick={close} disabled={submitting}>
+              Cancel
+            </M.Button>
+            <M.Button
+              onClick={handleSubmit}
+              variant="contained"
+              color="primary"
+              disabled={submitting || (submitFailed && hasValidationErrors)}
+            >
+              Push
+            </M.Button>
+          </M.DialogActions>
+        </>
+      )}
     </RF.Form>
   )
 }
@@ -115,7 +604,7 @@ function DialogPlaceholder({ close }) {
   )
 }
 
-function DialogSuccess({ bucket, name, revision, close }) {
+function DialogSuccess() {
   return <h1>DialogSuccess</h1>
 }
 
@@ -139,7 +628,6 @@ export function usePackageCopyDialog({ bucket, name, revision, onExited }) {
   const [key, setKey] = React.useState(1)
 
   const open = React.useCallback(() => {
-    console.log('OPEN')
     setOpen(true)
     setWasOpened(true)
     setExited(false)
@@ -166,11 +654,28 @@ export function usePackageCopyDialog({ bucket, name, revision, onExited }) {
     }
   }, [setExited, setSuccess, setExitValue, onExited, exitValue, refreshManifest])
 
+  const manifestData = Data.use(
+    requests.loadManifest,
+    { s3, bucket, name, revision, key },
+    { noAutoFetch: !wasOpened },
+  )
+
+  const workflowsData = Data.use(requests.workflowsList, { s3, bucket })
+
   const state = React.useMemo(() => {
     if (exited) return DialogState.Closed()
     if (success) return DialogState.Success(success)
-    return DialogState.Loading()
-  }, [exited, success])
+    return workflowsData.case({
+      Ok: (workflowsConfig) =>
+        manifestData.case({
+          Ok: (manifest) => DialogState.Form({ manifest, workflowsConfig }),
+          Err: DialogState.Error,
+          _: DialogState.Loading,
+        }),
+      Err: DialogState.Error,
+      _: DialogState.Loading,
+    })
+  }, [exited, success, workflowsData, manifestData])
 
   const stateCase = React.useCallback((cases) => DialogState.case(cases, state), [state])
 
@@ -188,7 +693,18 @@ export function usePackageCopyDialog({ bucket, name, revision, onExited }) {
           Closed: () => null,
           Loading: () => <DialogPlaceholder close={close} />,
           Error: (e) => <DialogError close={close} error={e} />,
-          Form: (props) => <DialogForm />,
+          Form: (props) => (
+            <DialogForm
+              {...{
+                bucket,
+                close,
+                name,
+                onSuccess: setSuccess,
+                setSubmitting,
+                ...props,
+              }}
+            />
+          ),
           Success: (props) => <DialogSuccess {...{ bucket, close, ...props }} />,
         })}
       </DialogWrapper>
