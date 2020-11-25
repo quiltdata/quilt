1843051d9af9f562cd81e8303090fc83b896ee8f
nl_0 <nl.imbecile@gmail.com>
2020-11-25 18:29:34 +0500

    utils/PackageUri module

---
+++ catalog/app/utils/PackageUri.js
@@ -0,0 +1,84 @@
+import { parse as parseQs } from 'querystring'
+import { parse as parseUrl } from 'url'
+
+import * as R from 'ramda'
+
+import { BaseError } from 'utils/error'
+
+export class PackageUriError extends BaseError {
+  // eslint-disable-next-line react/static-property-placement
+  static displayName = 'PackageUriError'
+
+  constructor(msg, uri) {
+    super(`Invalid package URI (${uri}): ${msg}`, { uri })
+  }
+}
+
+function parsePackageSpec(spec, uri) {
+  if (spec.includes(':')) {
+    const [name, tag, ...rest] = spec.split(':')
+    if (!name) {
+      throw new PackageUriError(
+        '"package=" part must contain non-empty package name.',
+        uri,
+      )
+    }
+    if (!tag) {
+      throw new PackageUriError('"package=" part: tag must not be empty.', uri)
+    }
+    if (rest.length) {
+      throw new PackageUriError('"package=" part may contain only one ":".', uri)
+    }
+    return { name, tag }
+  }
+  if (spec.includes('@')) {
+    const [name, hash, ...rest] = spec.split('@')
+    if (!name) {
+      throw new PackageUriError(
+        '"package=" part must contain non-empty package name.',
+        uri,
+      )
+    }
+    if (!hash) {
+      throw new PackageUriError('"package=" part: hash must not be empty.', uri)
+    }
+    if (rest.length) {
+      throw new PackageUriError('"package=" part may contain only one "@".', uri)
+    }
+    return { name, hash }
+  }
+  return { name: spec }
+}
+
+// TODO: do we need strict parsing here (throw on extra parameters)?
+// TODO: do we need extra validation for each part (package name, path, registry, etc)?
+export function parse(uri) {
+  const url = parseUrl(uri)
+  if (url.protocol !== 'quilt+s3:') {
+    throw new PackageUriError(
+      `unsupported protocol "${url.protocol}". "quilt+s3:" is currently the only supported protocol.`,
+      uri,
+    )
+  }
+  if (!url.slashes) {
+    throw new PackageUriError('missing slashes between protocol and registry.', uri)
+  }
+  const registry = `s3://${url.host}${url.path || ''}`
+  const params = parseQs(url.hash.replace('#', ''))
+  if (!params.package) {
+    throw new PackageUriError('missing "package=" part.', uri)
+  }
+  const { name, hash, tag } = parsePackageSpec(params.package, uri)
+  return R.reject(R.isNil, { registry, name, hash, tag, path: params.path || null })
+}
+
+export function stringify(parsed) {
+  const pathPart = parsed.path ? `&path=${parsed.path}` : ''
+  let pkgSpec = parsed.name
+  if (parsed.hash) {
+    pkgSpec += `@${parsed.hash}`
+  } else if (parsed.tag) {
+    pkgSpec += `:${parsed.tag}`
+  }
+  return `quilt+${parsed.registry}#package=${pkgSpec}${pathPart}`
+}
---
+++ catalog/app/utils/PackageUri.spec.js
@@ -0,0 +1,32 @@
+import * as PackageUri from './PackageUri'
+
+// TODO: more coverage
+describe('utils/PackageUri', () => {
+  describe('parse', () => {
+    it('should work', () => {
+      expect(
+        PackageUri.parse(
+          'quilt+s3://bucket-name#package=quilt/test:latest&path=sub/path',
+        ),
+      ).toEqual({
+        registry: 's3://bucket-name',
+        name: 'quilt/test',
+        tag: 'latest',
+        path: 'sub/path',
+      })
+    })
+  })
+
+  describe('stringify', () => {
+    it('should work', () => {
+      expect(
+        PackageUri.stringify({
+          registry: 's3://bucket-name',
+          name: 'quilt/test',
+          tag: 'latest',
+          path: 'sub/path',
+        }),
+      ).toBe('quilt+s3://bucket-name#package=quilt/test:latest&path=sub/path')
+    })
+  })
+})
