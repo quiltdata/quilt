cdaaa1c470b994db99575e11ba7392b5891f3800
Maxim Chervonny <fiskus@chervonny.ru>
2020-11-26 13:57:34 +0300

    JSON Editor unit tests (#1920)

---
+++ catalog/app/components/JsonEditor/mocks/compound.js
@@ -0,0 +1,38 @@
+export const schemaAnyOf = {
+  id: 's3://fiskus-sandbox-dev/.quilt/workflows/schema-any-of.json',
+  type: 'object',
+  properties: {
+    numOrString: {
+      anyOf: [
+        {
+          type: 'number',
+        },
+        {
+          type: 'string',
+        },
+      ],
+    },
+    intOrNonNumberOrLess3: {
+      oneOf: [{ maximum: 3 }, { type: 'integer' }],
+    },
+    intLessThan3: {
+      allOf: [{ maximum: 3 }, { type: 'integer' }],
+    },
+  },
+}
+
+export const schemaTypeArray = {
+  id: 's3://fiskus-sandbox-dev/.quilt/workflows/schema-type-array.json',
+  type: 'object',
+  properties: {
+    strOrNum: {
+      type: ['string', 'number'],
+    },
+    strOrNumList: {
+      type: 'array',
+      items: {
+        type: ['string', 'number'],
+      },
+    },
+  },
+}
