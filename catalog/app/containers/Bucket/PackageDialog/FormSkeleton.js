2c1fe1a29f406793d7b6004e9e1f7167cc1833de
Maxim Chervonny <fiskus@chervonny.ru>
2021-01-14 16:53:56 +0300

    move metadata skeleton to separate component

--- catalog/app/containers/Bucket/PackageDialog/FormSkeleton.js
+++ catalog/app/containers/Bucket/PackageDialog/FormSkeleton.js
@@ -3,30 +3,47 @@ import * as M from '@material-ui/core'
 
 import Skeleton from 'components/Skeleton'
 
-export default function FormSkeleton({ animate }) {
+export function MetaInputSkeleton({ animate, className }) {
+  return (
+    <M.Box className={className}>
+      <M.Box display="flex" mb={2}>
+        <Skeleton {...{ height: 24, width: 64, animate }} />
+        <M.Box flexGrow={1} />
+        <Skeleton {...{ height: 24, width: 64, animate }} />
+      </M.Box>
+      <M.Box display="flex">
+        <Skeleton {...{ height: 32, width: 200, animate }} />
+        <Skeleton {...{ height: 32, ml: 0.5, flexGrow: 1, animate }} />
+      </M.Box>
+      <M.Box display="flex" mt={0.5}>
+        <Skeleton {...{ height: 32, width: 200, animate }} />
+        <Skeleton {...{ height: 32, ml: 0.5, flexGrow: 1, animate }} />
+      </M.Box>
+    </M.Box>
+  )
+}
+
+const useFormSkeletonStyles = M.makeStyles((t) => ({
+  meta: {
+    marginTop: t.spacing(3),
+  },
+}))
+
+export function FormSkeleton({ animate }) {
+  const classes = useFormSkeletonStyles()
+
   return (
     <>
       <Skeleton {...{ height: 48, mt: 2, animate }} />
       <Skeleton {...{ height: 48, mt: 3, animate }} />
+
       <M.Box mt={3}>
         <Skeleton {...{ height: 24, width: 64, animate }} />
         <Skeleton {...{ height: 140, mt: 2, animate }} />
       </M.Box>
-      <M.Box mt={3}>
-        <M.Box display="flex" mb={2}>
-          <Skeleton {...{ height: 24, width: 64, animate }} />
-          <M.Box flexGrow={1} />
-          <Skeleton {...{ height: 24, width: 64, animate }} />
-        </M.Box>
-        <M.Box display="flex">
-          <Skeleton {...{ height: 32, width: 200, animate }} />
-          <Skeleton {...{ height: 32, ml: 0.5, flexGrow: 1, animate }} />
-        </M.Box>
-        <M.Box display="flex" mt={0.5}>
-          <Skeleton {...{ height: 32, width: 200, animate }} />
-          <Skeleton {...{ height: 32, ml: 0.5, flexGrow: 1, animate }} />
-        </M.Box>
-      </M.Box>
+
+      <MetaInputSkeleton className={classes.meta} animate />
+
       <Skeleton {...{ height: 80, mt: 3, mb: 3, animate }} />
     </>
   )
