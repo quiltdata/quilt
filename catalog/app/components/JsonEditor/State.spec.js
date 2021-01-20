92f4a3aebb98dcb63dbbc26defef40535fcd5d64
Maxim Chervonny <fiskus@chervonny.ru>
2021-01-20 16:11:27 +0300

    fix tests

--- catalog/app/components/JsonEditor/State.spec.js
+++ catalog/app/components/JsonEditor/State.spec.js
@@ -40,50 +40,70 @@ describe('components/JsonEditor/State', () => {
 
   describe('iterateSchema', () => {
     it('should return empty object for no Schema', () => {
-      const jsonDict = iterateSchema({}, { current: 0 }, [], {})
+      const jsonDict = iterateSchema({}, { current: { counter: 0 } }, [], {})
       expect(jsonDict).toEqual({})
     })
 
     it('should return values for a flat Schema', () => {
-      const jsonDict = iterateSchema(booleansNulls.schema, { current: 0 }, [], {})
+      const jsonDict = iterateSchema(
+        booleansNulls.schema,
+        { current: { counter: 0 } },
+        [],
+        {},
+      )
       expect(jsonDict).toEqual(booleansNulls.jsonDict)
     })
 
     it('should return values for every nesting level of Schema, when type is `object`', () => {
-      const jsonDict = iterateSchema(deeplyNestedObject.schema, { current: 0 }, [], {})
+      const jsonDict = iterateSchema(
+        deeplyNestedObject.schema,
+        { current: { counter: 0 } },
+        [],
+        {},
+      )
       expect(jsonDict).toEqual(deeplyNestedObject.jsonDict)
     })
 
     it('should return first value only for deep nesting level of Schema, when type is `array`', () => {
-      const jsonDict = iterateSchema(deeplyNestedArray.schema, { current: 0 }, [], {})
+      const jsonDict = iterateSchema(
+        deeplyNestedArray.schema,
+        { current: { counter: 0 } },
+        [],
+        {},
+      )
       expect(jsonDict).toEqual(deeplyNestedArray.jsonDict)
     })
   })
 
   describe('iterateJsonDict', () => {
     it('should return one empty state object, when input is an empty object', () => {
-      const jsonDict = iterateSchema({}, { current: 0 }, [], {})
+      const jsonDict = iterateSchema({}, { current: { counter: 0 } }, [], {})
       const rootKeys = mergeSchemaAndObjRootKeys({}, {})
       const columns = iterateJsonDict(jsonDict, {}, [], rootKeys)
       expect(columns).toEqual([{ parent: {}, items: [] }])
     })
 
     it('should return one state object utilizing Schema keys, when input is an empty object', () => {
-      const jsonDict = iterateSchema(regular.schema, { current: 0 }, [], {})
+      const jsonDict = iterateSchema(regular.schema, { current: { counter: 0 } }, [], {})
       const rootKeys = mergeSchemaAndObjRootKeys(regular.schema, {})
       const columns = iterateJsonDict(jsonDict, {}, [], rootKeys)
       expect(columns).toEqual(regular.columnsSchemaOnly)
     })
 
     it('should return one state object utilizing Schema keys and object keys, when input is a flat object', () => {
-      const jsonDict = iterateSchema(regular.schema, { current: 0 }, [], {})
+      const jsonDict = iterateSchema(regular.schema, { current: { counter: 0 } }, [], {})
       const rootKeys = mergeSchemaAndObjRootKeys(regular.schema, regular.object1)
       const columns = iterateJsonDict(jsonDict, regular.object1, [], rootKeys)
       expect(columns).toEqual(regular.columnsSchemaAndObject1)
     })
 
     it('should return three state objects, when input is an empty object and the path is provided', () => {
-      const jsonDict = iterateSchema(deeplyNestedObject.schema, { current: 0 }, [], {})
+      const jsonDict = iterateSchema(
+        deeplyNestedObject.schema,
+        { current: { counter: 0 } },
+        [],
+        {},
+      )
       const rootKeys = mergeSchemaAndObjRootKeys(deeplyNestedObject.schema, {})
       const columns = iterateJsonDict(
         jsonDict,
@@ -95,7 +115,7 @@ describe('components/JsonEditor/State', () => {
     })
 
     it('should return three state objects, when input is an object and path is provided', () => {
-      const jsonDict = iterateSchema({}, { current: 0 }, [], {})
+      const jsonDict = iterateSchema({}, { current: { counter: 0 } }, [], {})
       const rootKeys = mergeSchemaAndObjRootKeys({}, deeplyNestedObject.object1)
       const columns = iterateJsonDict(
         jsonDict,
