d9f50e3df1c3d321e419f94cda931063dc35b504
Maxim Chervonny <fiskus@chervonny.ru>
2021-01-20 16:25:07 +0300

    fix tests

--- catalog/app/components/JsonEditor/State.spec.js
+++ catalog/app/components/JsonEditor/State.spec.js
@@ -77,51 +77,53 @@ describe('components/JsonEditor/State', () => {
 
   describe('iterateJsonDict', () => {
     it('should return one empty state object, when input is an empty object', () => {
-      const jsonDict = iterateSchema({}, { current: { counter: 0 } }, [], {})
+      const sortOrder = { current: { counter: 0, dict: {} } }
+      const jsonDict = iterateSchema({}, sortOrder, [], {})
       const rootKeys = mergeSchemaAndObjRootKeys({}, {})
-      const columns = iterateJsonDict(jsonDict, {}, [], rootKeys)
+      const columns = iterateJsonDict(jsonDict, {}, [], rootKeys, sortOrder)
       expect(columns).toEqual([{ parent: {}, items: [] }])
     })
 
     it('should return one state object utilizing Schema keys, when input is an empty object', () => {
-      const jsonDict = iterateSchema(regular.schema, { current: { counter: 0 } }, [], {})
+      const sortOrder = { current: { counter: 0, dict: {} } }
+      const jsonDict = iterateSchema(regular.schema, sortOrder, [], {})
       const rootKeys = mergeSchemaAndObjRootKeys(regular.schema, {})
-      const columns = iterateJsonDict(jsonDict, {}, [], rootKeys)
+      const columns = iterateJsonDict(jsonDict, {}, [], rootKeys, sortOrder)
       expect(columns).toEqual(regular.columnsSchemaOnly)
     })
 
     it('should return one state object utilizing Schema keys and object keys, when input is a flat object', () => {
-      const jsonDict = iterateSchema(regular.schema, { current: { counter: 0 } }, [], {})
+      const sortOrder = { current: { counter: 0, dict: {} } }
+      const jsonDict = iterateSchema(regular.schema, sortOrder, [], {})
       const rootKeys = mergeSchemaAndObjRootKeys(regular.schema, regular.object1)
-      const columns = iterateJsonDict(jsonDict, regular.object1, [], rootKeys)
+      const columns = iterateJsonDict(jsonDict, regular.object1, [], rootKeys, sortOrder)
       expect(columns).toEqual(regular.columnsSchemaAndObject1)
     })
 
     it('should return three state objects, when input is an empty object and the path is provided', () => {
-      const jsonDict = iterateSchema(
-        deeplyNestedObject.schema,
-        { current: { counter: 0 } },
-        [],
-        {},
-      )
+      const sortOrder = { current: { counter: 0, dict: {} } }
+      const jsonDict = iterateSchema(deeplyNestedObject.schema, sortOrder, [], {})
       const rootKeys = mergeSchemaAndObjRootKeys(deeplyNestedObject.schema, {})
       const columns = iterateJsonDict(
         jsonDict,
         {},
         deeplyNestedObject.fieldPathNested,
         rootKeys,
+        sortOrder,
       )
       expect(columns).toEqual(deeplyNestedObject.columnsNested)
     })
 
     it('should return three state objects, when input is an object and path is provided', () => {
-      const jsonDict = iterateSchema({}, { current: { counter: 0 } }, [], {})
+      const sortOrder = { current: { counter: 0, dict: {} } }
+      const jsonDict = iterateSchema({}, sortOrder, [], {})
       const rootKeys = mergeSchemaAndObjRootKeys({}, deeplyNestedObject.object1)
       const columns = iterateJsonDict(
         jsonDict,
         deeplyNestedObject.object1,
         deeplyNestedObject.fieldPath1,
         rootKeys,
+        sortOrder,
       )
       expect(columns).toEqual(deeplyNestedObject.columns1)
     })
