7b50c1439a392eb8c1b9496ae10dd4e12bbbf643
Maxim Chervonny <fiskus@chervonny.ru>
2021-01-26 18:12:00 +0300

    add DialogForm

--- catalog/app/containers/Bucket/PackageDirectoryDialog.js
+++ catalog/app/containers/Bucket/PackageDirectoryDialog.js
@@ -1,17 +1,248 @@
+import * as R from 'ramda'
 import * as React from 'react'
-// import * as RF from 'react-final-form'
+import * as RF from 'react-final-form'
 import * as M from '@material-ui/core'
 
+import AsyncResult from 'utils/AsyncResult'
 import * as AWS from 'utils/AWS'
 import * as Data from 'utils/Data'
+import Delay from 'utils/Delay'
 import * as NamedRoutes from 'utils/NamedRoutes'
 import StyledLink from 'utils/StyledLink'
+import * as validators from 'utils/validators'
 
 import * as PD from './PackageDialog'
 import * as requests from './requests'
 
-function DialogForm() {
-  return <h1>It works</h1>
+const useFormSkeletonStyles = M.makeStyles((t) => ({
+  meta: {
+    marginTop: t.spacing(3),
+  },
+}))
+
+function FormSkeleton({ animate }) {
+  const classes = useFormSkeletonStyles()
+
+  return (
+    <>
+      <PD.TextFieldSkeleton animate={animate} />
+      <PD.TextFieldSkeleton animate={animate} />
+
+      <PD.MetaInputSkeleton className={classes.meta} animate={animate} />
+      <PD.WorkflowsInputSkeleton animate={animate} />
+    </>
+  )
+}
+
+function DialogTitle({ bucket }) {
+  const { urls } = NamedRoutes.use()
+
+  // FIXME: edit text
+  return (
+    <M.DialogTitle>
+      Push package to{' '}
+      <StyledLink target="_blank" to={urls.bucketOverview(bucket)}>
+        {bucket}
+      </StyledLink>{' '}
+      bucket
+    </M.DialogTitle>
+  )
+}
+
+const defaultNameWarning = ' ' // Reserve space for warning
+
+const useStyles = M.makeStyles((t) => ({
+  meta: {
+    marginTop: t.spacing(3),
+  },
+}))
+
+function DialogForm({ close, successor, onSubmitStart, onSubmitEnd, workflowsConfig }) {
+  const nameValidator = PD.useNameValidator()
+  const nameExistence = PD.useNameExistence(successor.slug)
+  const [nameWarning, setNameWarning] = React.useState('')
+  const classes = useStyles()
+
+  const onSubmit = React.useCallback(async () => {
+    onSubmitStart()
+    onSubmitEnd()
+  }, [onSubmitStart, onSubmitEnd])
+
+  const onFormChange = React.useCallback(
+    async ({ values }) => {
+      const { name } = values
+      const fullName = `${successor.slug}/${name}`
+
+      let warning = defaultNameWarning
+
+      const nameExists = await nameExistence.validate(name)
+      if (nameExists) {
+        warning = `Package "${fullName}" exists. Submitting will revise it`
+      } else if (name) {
+        warning = `Package "${fullName}" will be created`
+      }
+
+      if (warning !== nameWarning) {
+        setNameWarning(warning)
+      }
+    },
+    [successor, nameExistence, nameWarning],
+  )
+
+  const initialName = React.useMemo(() => '', [])
+
+  const initialWorkflow = React.useMemo(
+    () => PD.defaultWorkflowFromConfig(workflowsConfig),
+    [workflowsConfig],
+  )
+
+  const [workflow, setWorkflow] = React.useState(initialWorkflow)
+
+  return (
+    <RF.Form
+      onSubmit={onSubmit}
+      subscription={{
+        handleSubmit: true,
+        submitting: true,
+        submitFailed: true,
+        error: true,
+        submitError: true,
+        hasValidationErrors: true,
+        form: true,
+      }}
+    >
+      {({
+        handleSubmit,
+        submitting,
+        submitFailed,
+        error,
+        submitError,
+        hasValidationErrors,
+      }) => (
+        <>
+          <DialogTitle bucket={successor.slug} />
+          <M.DialogContent style={{ paddingTop: 0 }}>
+            <form onSubmit={handleSubmit}>
+              <RF.FormSpy
+                subscription={{ dirtyFields: true, values: true }}
+                onChange={onFormChange}
+              />
+
+              <RF.FormSpy
+                subscription={{ modified: true, values: true }}
+                onChange={({ modified, values }) => {
+                  if (modified.workflow && values.workflow !== workflow) {
+                    setWorkflow(values.workflow)
+                  }
+                }}
+              />
+
+              <RF.Field
+                component={PD.PackageNameInput}
+                name="name"
+                validate={validators.composeAsync(
+                  validators.required,
+                  nameValidator.validate,
+                )}
+                validateFields={['name']}
+                errors={{
+                  required: 'Enter a package name',
+                  invalid: 'Invalid package name',
+                }}
+                helperText={nameWarning}
+                initialValue={initialName}
+              />
+
+              <RF.Field
+                component={PD.CommitMessageInput}
+                name="commitMessage"
+                validate={validators.required}
+                validateFields={['commitMessage']}
+                errors={{
+                  required: 'Enter a commit message',
+                }}
+              />
+
+              <PD.SchemaFetcher schemaUrl={R.pathOr('', ['schema', 'url'], workflow)}>
+                {AsyncResult.case({
+                  Ok: ({ responseError, schema, validate }) => (
+                    <RF.Field
+                      className={classes.meta}
+                      component={PD.MetaInput}
+                      name="meta"
+                      bucket={successor.slug}
+                      schema={schema}
+                      schemaError={responseError}
+                      validate={validate}
+                      validateFields={['meta']}
+                      isEqual={R.equals}
+                      initialValue={PD.EMPTY_META_VALUE}
+                    />
+                  ),
+                  _: () => <PD.MetaInputSkeleton className={classes.meta} />,
+                })}
+              </PD.SchemaFetcher>
+
+              <RF.Field
+                component={PD.WorkflowInput}
+                name="workflow"
+                workflowsConfig={workflowsConfig}
+                initialValue={initialWorkflow}
+                validate={validators.required}
+                validateFields={['meta', 'workflow']}
+                errors={{
+                  required: 'Workflow is required for this bucket.',
+                }}
+              />
+
+              <input type="submit" style={{ display: 'none' }} />
+            </form>
+          </M.DialogContent>
+          <M.DialogActions>
+            {submitting && (
+              <Delay ms={200} alwaysRender>
+                {(ready) => (
+                  <M.Fade in={ready}>
+                    <M.Box flexGrow={1} display="flex" alignItems="center" pl={2}>
+                      <M.CircularProgress size={24} variant="indeterminate" />
+                      <M.Box pl={1} />
+                      <M.Typography variant="body2" color="textSecondary">
+                        {successor.copyData
+                          ? 'Copying files and writing manifest'
+                          : 'Writing manifest'}
+                      </M.Typography>
+                    </M.Box>
+                  </M.Fade>
+                )}
+              </Delay>
+            )}
+
+            {!submitting && (!!error || !!submitError) && (
+              <M.Box flexGrow={1} display="flex" alignItems="center" pl={2}>
+                <M.Icon color="error">error_outline</M.Icon>
+                <M.Box pl={1} />
+                <M.Typography variant="body2" color="error">
+                  {error || submitError}
+                </M.Typography>
+              </M.Box>
+            )}
+
+            <M.Button onClick={close} disabled={submitting}>
+              Cancel
+            </M.Button>
+            <M.Button
+              onClick={handleSubmit}
+              variant="contained"
+              color="primary"
+              disabled={submitting || (submitFailed && hasValidationErrors)}
+            >
+              Push
+            </M.Button>
+          </M.DialogActions>
+        </>
+      )}
+    </RF.Form>
+  )
 }
 
 function DialogError({ bucket, error, onCancel }) {
@@ -21,7 +252,7 @@ function DialogError({ bucket, error, onCancel }) {
   return (
     <PD.DialogError
       error={error}
-      skeletonElement={<PD.FormSkeleton animate={false} />}
+      skeletonElement={<FormSkeleton animate={false} />}
       title={
         <>
           Push package to{' '}
@@ -42,7 +273,7 @@ function DialogLoading({ bucket, onCancel }) {
   // FIXME: edit text
   return (
     <PD.DialogLoading
-      skeletonElement={<PD.FormSkeleton />}
+      skeletonElement={<FormSkeleton />}
       title={
         <>
           Push package to{' '}
@@ -75,6 +306,13 @@ export default function PackageDirectoryDialog({
     { noAutoFetch: !successor || !open },
   )
 
+  const handleSuccess = React.useCallback(
+    (successData) => {
+      setSuccess(successData)
+    },
+    [setSuccess],
+  )
+
   const handleClose = React.useCallback(() => {
     if (submitting) return
 
@@ -85,21 +323,40 @@ export default function PackageDirectoryDialog({
     setSuccess(null)
   }, [submitting, success, setSuccess, onClose, onExited])
 
+  const handleExited = React.useCallback(() => {
+    if (submitting) return
+
+    onExited({
+      pushed: success,
+    })
+    if (onClose) onClose()
+    setSuccess(null)
+  }, [submitting, success, setSuccess, onClose, onExited])
+
   return (
-    <M.Dialog fullWidth open={open} scroll="body">
+    <M.Dialog
+      fullWidth
+      onClose={handleClose}
+      onExited={handleExited}
+      open={open}
+      scroll="body"
+    >
       {workflowsData.case({
         Err: (e) =>
           successor && (
             <DialogError bucket={successor.slug} onCancel={handleClose} error={e} />
           ),
-        Ok: (props) =>
+        Ok: (workflowsConfig) =>
           successor && (
             <DialogForm
               {...{
-                onSubmitStart: () => setSubmitting(true),
+                close: handleClose,
                 onSubmitEnd: () => setSubmitting(false),
+                onSubmitStart: () => setSubmitting(true),
+                onSuccess: handleSuccess,
                 path,
-                ...props,
+                successor,
+                workflowsConfig,
               }}
             />
           ),
