7ce1bf99230e337c08bc216759365d128b8d65c3
Maxim Chervonny <fiskus@chervonny.ru>
2021-01-28 11:25:24 +0300

    move FilesInput to component

--- catalog/app/containers/Bucket/PackageDialog/index.js
+++ catalog/app/containers/Bucket/PackageDialog/index.js
@@ -6,6 +6,8 @@ export { default as DialogLoading } from './DialogLoading'
 
 export { default as DialogSuccess } from './DialogSuccess'
 
+export { default as FilesInput } from './FilesInput'
+
 export * from './Skeleton'
 
 export * from './Layout'
