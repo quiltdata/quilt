7023ba87a0dc079b01c499ece7801558b1c20336
nl_0 <nl.imbecile@gmail.com>
2021-01-28 20:08:36 +0500

    tweak webpack config, use more defaults

--- catalog/internals/webpack/webpack.prod.babel.js
+++ catalog/internals/webpack/webpack.prod.babel.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
 
@@ -21,95 +20,17 @@ module.exports = require('./webpack.base.babel')({
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
   output: {
-    // TODO: use contenthash?
-    filename: '[name].[chunkhash].js',
-    chunkFilename: '[name].[chunkhash].chunk.js',
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].chunk.js',
   },
 
   optimization: {
-    minimize: true,
     minimizer: [new TerserPlugin({ parallel: 3 })],
-    nodeEnv: 'production',
-    sideEffects: true,
-    concatenateModules: true,
-    moduleIds: 'deterministic',
     runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: 10,
-      minSize: 0,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: (module) => {
-            const packageName = module.context.match(
-              /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
-            )[1]
-            return `npm.${packageName.replace('@', '')}`
-          },
-        },
-      },
-    },
+    splitChunks: { chunks: 'all' },
   },
 
   plugins: [
-    // Minify and optimize the index.html
-    new HtmlWebpackPlugin({
-      inject: true,
-      chunks: ['app'],
-      template: 'app/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-    }),
-
-    new HtmlWebpackPlugin({
-      inject: true,
-      chunks: ['embed'],
-      template: 'app/embed/index.html',
-      filename: 'embed.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-    }),
-
-    new HtmlWebpackPlugin({
-      inject: true,
-      chunks: ['embed-debug-harness'],
-      template: 'app/embed/debug-harness.html',
-      filename: 'embed-debug-harness.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-    }),
-
     new CompressionPlugin({
       algorithm: 'gzip',
       test: /\.js$|\.css$|\.html$/,
@@ -122,5 +43,21 @@ module.exports = require('./webpack.base.babel')({
 
   performance: {
     assetFilter: (assetFilename) => !/(\.map$)|(^(main\.|favicon\.))/.test(assetFilename),
+  },
+
+  // options passed to all instances of HtmlWebpackPlugin
+  html: {
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      keepClosingSlash: true,
+      minifyJS: true,
+      minifyCSS: true,
+      minifyURLs: true,
+    },
   },
 })
