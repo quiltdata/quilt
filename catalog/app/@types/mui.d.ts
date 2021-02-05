974173676ade262c0101e871ebd22e62e2e059f5
nl_0 <nl.imbecile@gmail.com>
2021-02-05 15:22:20 +0500

    lint app/@types/mui.d.ts

--- catalog/app/@types/mui.d.ts
+++ catalog/app/@types/mui.d.ts
@@ -1,7 +1,8 @@
-// workaround for MUI@4 not having ref in BoxProps
-// https://github.com/mui-org/material-ui/issues/17010
-import type { BoxProps } from '@material-ui/core'
+import '@material-ui/core'
+
 declare module '@material-ui/core' {
+  // workaround for MUI@4 not having ref in BoxProps
+  // https://github.com/mui-org/material-ui/issues/17010
   interface BoxProps {
     ref?: React.Ref<any>
   }
