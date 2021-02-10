4dec70fb2db88143743c8f507f9326db55cff06a
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2021-02-10 17:18:43 +0500

    Fix FCS indexing

--- lambdas/es/indexer/index.py
+++ lambdas/es/indexer/index.py
@@ -415,7 +415,7 @@ def maybe_get_contents(bucket, key, ext, *, etag, version_id, s3_client, size):
             body, info = extract_fcs(get_bytes(obj["Body"], compression), as_html=False)
             # be smart and just send column names to ES (instead of bloated full schema)
             # if this is not an HTML/catalog preview
-            content = trim_to_bytes(f"{body}\n{info}", ELASTIC_LIMIT_BYTES)
+            return trim_to_bytes(f"{body}\n{info}", ELASTIC_LIMIT_BYTES)
         if inferred_ext == ".ipynb":
             content = trim_to_bytes(
                 # we have no choice but to fetch the entire notebook, because we
