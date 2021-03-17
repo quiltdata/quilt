9c76691af62467ab45f3315fe83a243a3059dcdd
Maxim Chervonny <fiskus@chervonny.ru>
2021-03-17 21:38:57 +0300

    unsubscribe

--- catalog/app/utils/dragging.ts
+++ catalog/app/utils/dragging.ts
@@ -3,17 +3,20 @@ import * as React from 'react'
 export default function useDragging() {
   const [isDragActive, setDragActive] = React.useState(false)
 
+  const setActive = React.useCallback(() => setDragActive(true), [])
+  const setInactive = React.useCallback(() => setDragActive(false), [])
+
   React.useEffect(() => {
-    document.addEventListener('dragover', () => {
-      setDragActive(true)
-    })
-    document.addEventListener('dragleave', () => {
-      setDragActive(false)
-    })
-    document.addEventListener('drop', () => {
-      setDragActive(false)
-    })
-  }, [])
+    document.addEventListener('dragover', setActive)
+    document.addEventListener('dragleave', setInactive)
+    document.addEventListener('drop', setInactive)
+
+    return () => {
+      document.removeEventListener('dragover', setActive)
+      document.removeEventListener('dragleave', setInactive)
+      document.removeEventListener('drop', setInactive)
+    }
+  }, [setActive, setInactive])
 
   return isDragActive
 }
