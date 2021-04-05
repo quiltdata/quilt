16d60d4e8d9a9d2d13966b5425dd71f326a0bb05
Maxim Chervonny <fiskus@chervonny.ru>
2021-04-05 12:13:19 +0300

    stricter type

--- catalog/app/@types/react-table.d.ts
+++ catalog/app/@types/react-table.d.ts
@@ -2,6 +2,6 @@ import type * as RTable from 'react-table'
 
 declare module 'react-table' {
   export interface TableOptions extends RTable.TableOptions {
-    updateMyData: (path: any, id: any, value: any) => void
+    updateMyData: (path: string[], id: any, value: any) => void
   }
 }
