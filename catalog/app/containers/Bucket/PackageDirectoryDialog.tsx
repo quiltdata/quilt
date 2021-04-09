4eda6cacc63e2a19ea6eab7b53abf87cc41cfef1
Maxim Chervonny <fiskus@chervonny.ru>
2021-04-09 12:50:40 +0300

    Simplify warning messages for package name (#2134)

--- catalog/app/containers/Bucket/PackageDirectoryDialog.tsx
+++ catalog/app/containers/Bucket/PackageDirectoryDialog.tsx
@@ -6,7 +6,6 @@ import * as RF from 'react-final-form'
 import * as redux from 'react-redux'
 import * as M from '@material-ui/core'
 
-import Code from 'components/Code'
 import * as authSelectors from 'containers/Auth/selectors'
 import AsyncResult from 'utils/AsyncResult'
 import * as AWS from 'utils/AWS'
@@ -178,30 +177,14 @@ function DialogForm({
 
   const handleNameChange = React.useCallback(
     async (name) => {
-      const fullName = `${successor.slug}/${name}`
-
-      let warning: React.ReactNode = ''
-
       const nameExists = await nameExistence.validate(name)
-      if (nameExists) {
-        warning = (
-          <>
-            <Code>{fullName}</Code> already exists. Click Push to create a new revision.
-          </>
-        )
-      } else if (name) {
-        warning = (
-          <>
-            <Code>{fullName}</Code> is a new package
-          </>
-        )
-      }
+      const warning = <PD.PackageNameWarning exists={!!nameExists} />
 
       if (warning !== nameWarning) {
         setNameWarning(warning)
       }
     },
-    [nameWarning, nameExistence, successor],
+    [nameWarning, nameExistence],
   )
 
   const [editorElement, setEditorElement] = React.useState<HTMLElement | null>(null)
