31356957caf588eb3b595a1f10f22e4e5d7e50f5
Maxim Chervonny <fiskus@chervonny.ru>
2021-04-12 18:23:30 +0300

    progress state

--- catalog/app/components/Preview/loaders/Notebook.js
+++ catalog/app/components/Preview/loaders/Notebook.js
@@ -1,7 +1,9 @@
 import * as R from 'ramda'
 import * as React from 'react'
 
-import * as Html from './Html'
+import AsyncResult from 'utils/AsyncResult'
+import { handleToHttpsUri } from 'utils/s3paths'
+
 import { PreviewData } from '../types'
 import * as utils from './utils'
 
@@ -24,7 +26,20 @@ function VoilaLoader({ handle, children }) {
   // FIXME: make a Voila service request and preview html as iframe
   //        <iframe src="https://api/voila.html" />
   const voilaHandle = R.assoc('key', `${handle.key}.html`, handle)
-  return <Html.Loader {...{ handle: voilaHandle, children }} />
+  const src = handleToHttpsUri(voilaHandle)
+  const [loading, setLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    const link = document.createElement('iframe')
+    link.addEventListener('load', () => setLoading(false))
+    link.src = src
+    link.style.display = 'none'
+    document.body.appendChild(link)
+  }, [src, setLoading])
+
+  return children(
+    loading ? AsyncResult.Pending() : AsyncResult.Ok(PreviewData.IFrame({ src })),
+  )
 }
 
 export const Loader = function WrappedNotebookLoader({ handle, children }) {
