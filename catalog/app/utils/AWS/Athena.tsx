965b6bc36835218b96b5f330994cba3c123e279f
Maxim Chervonny <fiskus@chervonny.ru>
2021-04-29 11:31:53 +0300

    add region

--- catalog/app/utils/AWS/Athena.tsx
+++ catalog/app/utils/AWS/Athena.tsx
@@ -1,10 +1,18 @@
 import Athena from 'aws-sdk/clients/athena'
+import * as R from 'ramda'
 import * as React from 'react'
 
+import * as CatalogConfig from 'utils/Config'
 import useMemoEqLazy from 'utils/useMemoEqLazy'
 
 import * as Config from './Config'
 import * as Credentials from './Credentials'
+
+const getRegion: (input: string) => string = R.pipe(
+  R.match(/\.([a-z]{2}-[a-z]+-\d)\.amazonaws\.com$/),
+  R.nth(1),
+  R.defaultTo('us-east-1'),
+)
 
 const AthenaContext = React.createContext<() => Athena | null>(() => null)
 
@@ -14,11 +22,17 @@ export const Provider = function AthenaProvider({
   children,
   ...overrides
 }: AthenaProviderProps) {
-  const cfg = Config.use()
+  const awsConfig = Config.use()
+  const catalogConfig = CatalogConfig.use()
+
+  const region = React.useMemo(() => getRegion(catalogConfig.apiGatewayEndpoint), [
+    catalogConfig,
+  ])
 
   const client: () => Athena = useMemoEqLazy(
     {
-      ...cfg,
+      ...awsConfig,
+      region,
       ...overrides,
     },
     (opts: Athena.ClientConfiguration) => new Athena(opts),
