9e93808349c5a991dd33ed0f8149213f20114a30
Maxim Chervonny <fiskus@chervonny.ru>
2021-05-05 14:00:26 +0300

    add generic HtmlMeta and unit test for it

---
+++ catalog/app/utils/HtmlMeta.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
+
+const BASE = 'Quilt is a versioned data portal for AWS'
+
+const DIVIDER = ' • '
+
+interface MetaTitleProps {
+  children?: string | string[]
+  base?: string
+}
+
+export function getTitleSegments(base: string, children?: string | string[]) {
+  if (!children) return [base]
+
+  if (Array.isArray(children)) return [...children, base]
+
+  return [children, base]
+}
+
+export function getTitle(base: string, children?: string | string[]) {
+  return getTitleSegments(base, children).join(DIVIDER)
+}
+
+export default function MetaTitle({ children, base = BASE }: MetaTitleProps) {
+  return (
+    <Helmet>
+      <title>{getTitle(base, children)}</title>
+    </Helmet>
+  )
+}
