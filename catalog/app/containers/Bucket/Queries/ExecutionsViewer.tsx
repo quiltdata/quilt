ac432e38093df6f7438ec8a2f3b96b87e3d57395
Maxim Chervonny <fiskus@chervonny.ru>
2021-05-07 12:17:50 +0300

    polish UI

--- catalog/app/containers/Bucket/Queries/ExecutionsViewer.tsx
+++ catalog/app/containers/Bucket/Queries/ExecutionsViewer.tsx
@@ -4,67 +4,100 @@ import * as M from '@material-ui/core'
 
 import * as requests from './requests'
 
+const useExecutionStyles = M.makeStyles((t) => ({
+  date: {
+    whiteSpace: 'nowrap',
+  },
+  expandedCell: {
+    paddingBottom: 0,
+    paddingTop: 0,
+  },
+  expandedQuery: {
+    maxHeight: t.spacing(30),
+    maxWidth: '100%',
+    overflow: 'auto',
+    padding: t.spacing(1),
+  },
+}))
+
 interface ExecutionProps {
   queryExecution: requests.QueryExecution
 }
 
 function Execution({ queryExecution }: ExecutionProps) {
+  const classes = useExecutionStyles()
+
   const [expanded, setExpanded] = React.useState(false)
 
   const onToggle = React.useCallback(() => setExpanded(!expanded), [expanded])
+  const trimmedQuery = React.useMemo(
+    () =>
+      !queryExecution.query || queryExecution.query.length <= 30
+        ? queryExecution.query
+        : `${queryExecution.query?.substring(0, 30)} … ${queryExecution.query?.substr(
+            -20,
+          )}`,
+    [queryExecution],
+  )
 
   return (
     <>
       <M.TableRow>
-        <M.TableCell onClick={onToggle}>
-          {queryExecution.query?.length <= 30
-            ? queryExecution.query
-            : `${queryExecution.query?.substring(0, 20)} … ${queryExecution.query?.substr(
-                -10,
-              )}`}
+        <M.TableCell style={{ width: '50%' }} onClick={onToggle}>
+          {trimmedQuery}
         </M.TableCell>
-        <M.TableCell>
+        <M.TableCell align="right">
           <abbr title={queryExecution.id}>{queryExecution.status}</abbr>
         </M.TableCell>
-        <M.TableCell style={{ whiteSpace: 'nowrap' }}>
+        <M.TableCell align="right" className={classes.date}>
           {queryExecution.created
             ? dateFns.format(queryExecution.created, 'MMM do, HH:mm:ss')
             : null}
         </M.TableCell>
-        <M.TableCell style={{ whiteSpace: 'nowrap' }}>
+        <M.TableCell align="right" className={classes.date}>
           {queryExecution.completed
             ? dateFns.format(queryExecution.completed, 'MMM do, HH:mm:ss')
             : null}
         </M.TableCell>
       </M.TableRow>
       <M.TableRow>
-        <M.Collapse in={expanded}>
-          <M.TableCell>{queryExecution.query}</M.TableCell>
-        </M.Collapse>
+        <M.TableCell colSpan={4} className={classes.expandedCell}>
+          <M.Collapse in={expanded}>
+            <pre className={classes.expandedQuery}>{queryExecution.query}</pre>
+          </M.Collapse>
+        </M.TableCell>
       </M.TableRow>
     </>
   )
 }
+
+const useStyles = M.makeStyles({
+  table: {
+    tableLayout: 'fixed',
+  },
+})
 
 interface ExecutionsViewerProps {
   executions: requests.QueryExecution[]
 }
 
 export default function ExecutionsViewer({ executions }: ExecutionsViewerProps) {
+  const classes = useStyles()
+
   return (
     <M.TableContainer component={M.Paper}>
-      <M.Table size="small">
+      <M.Table size="small" className={classes.table}>
         <M.TableHead>
           <M.TableRow>
-            <M.TableCell>Query</M.TableCell>
-            <M.TableCell>Status</M.TableCell>
-            <M.TableCell>Date created</M.TableCell>
-            <M.TableCell>Date completed</M.TableCell>
+            <M.TableCell style={{ width: '50%' }}>Query</M.TableCell>
+            <M.TableCell align="right">Status</M.TableCell>
+            <M.TableCell align="right">Date created</M.TableCell>
+            <M.TableCell align="right">Date completed</M.TableCell>
           </M.TableRow>
         </M.TableHead>
         <M.TableBody>
           {executions.map((queryExecution) => (
-            <Execution queryExecution={queryExecution} />
+            <Execution queryExecution={queryExecution} key={queryExecution.id} />
           ))}
         </M.TableBody>
       </M.Table>
