eda8281630521d77d313a4677447f992d8c2e6ba
Maxim Chervonny <fiskus@chervonny.ru>
2021-05-28 14:50:09 +0300

    add force flag

--- catalog/app/components/Logo/index.js
+++ catalog/app/components/Logo/index.js
@@ -7,10 +7,12 @@ import quilt from './quilt.png'
 const HEIGHT_Q = 27
 const HEIGHT_QUILT = 29
 
-export default ({ responsive = false, ...props }) => {
+export default ({ responsive = false, forcedShort = false, ...props }) => {
   const t = M.useTheme()
   const xs = M.useMediaQuery(t.breakpoints.down('xs'))
-  const img =
-    responsive && xs ? { height: HEIGHT_Q, src: q } : { height: HEIGHT_QUILT, src: quilt }
-  return <M.Box component="img" alt="Quilt" {...img} {...props} />
+  const short = forcedShort || (responsive && xs)
+  const imgProps = short
+    ? { height: HEIGHT_Q, src: q }
+    : { height: HEIGHT_QUILT, src: quilt }
+  return <M.Box component="img" alt="Quilt" {...imgProps} {...props} />
 }
