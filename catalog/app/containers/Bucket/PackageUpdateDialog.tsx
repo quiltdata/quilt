cc83a81c0cdad69b735c2fea13082d42b7d8cbf7
Maxim Chervonny <fiskus@chervonny.ru>
2021-06-01 13:41:33 +0300

    Disable S3 filepicker when no `ui.sourceBuckets` and set default source bucket (#2215)

--- catalog/app/containers/Bucket/PackageUpdateDialog.tsx
+++ catalog/app/containers/Bucket/PackageUpdateDialog.tsx
@@ -116,7 +116,7 @@ interface DialogFormProps {
   setWorkflow: (workflow: workflows.Workflow) => void
   validate: FF.FieldValidator<any>
   workflowsConfig: workflows.WorkflowsConfig
-  sourceBuckets: string[]
+  sourceBuckets: BucketPreferences.SourceBuckets
 }
 
 function DialogForm({
@@ -145,12 +145,7 @@ function DialogForm({
   const dialogContentClasses = PD.useContentStyles({ metaHeight })
   const validateWorkflow = PD.useWorkflowValidator(workflowsConfig)
 
-  const buckets = React.useMemo(() => R.uniq([bucket, ...sourceBuckets]), [
-    bucket,
-    sourceBuckets,
-  ])
-
-  const [selectedBucket, selectBucket] = React.useState(buckets[0])
+  const [selectedBucket, selectBucket] = React.useState(sourceBuckets.getDefault)
 
   const initialFiles: PD.FilesState = React.useMemo(
     () => ({ existing: manifest.entries, added: {}, deleted: {} }),
@@ -495,7 +490,7 @@ function DialogForm({
                     isEqual={R.equals}
                     initialValue={initialFiles}
                     bucket={selectedBucket}
-                    buckets={buckets}
+                    buckets={sourceBuckets.list}
                     selectBucket={selectBucket}
                   />
                 </PD.RightColumn>
@@ -651,7 +646,7 @@ const DialogState = tagged.create(
     Form: (v: {
       manifest: Manifest
       workflowsConfig: workflows.WorkflowsConfig
-      sourceBuckets: string[]
+      sourceBuckets: BucketPreferences.SourceBuckets
     }) => v,
     Success: (v: { name: string; hash: string }) => v,
   },
