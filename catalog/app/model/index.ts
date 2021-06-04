5ca0d5249a110a06b630eb945659c00e82de1e4c
nl_0 <nl.imbecile@gmail.com>
2021-06-04 19:25:08 +0500

    add Model type definitions and utils

---
+++ catalog/app/model/index.ts
@@ -0,0 +1,5 @@
+export * as GQLTypes from './graphql/types.generated'
+
+// default bucket icon as returned by the registry
+export const DEFAULT_BUCKET_ICON =
+  'https://d1zvn9rasera71.cloudfront.net/q-128-square.png'
