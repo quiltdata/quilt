4580573fb55ff8b6427f92f3fef6a821ff66cff2
Maxim Chervonny <fiskus@chervonny.ru>
2021-06-04 17:59:30 +0300

    add convertors frontend/backend data

---
+++ catalog/app/containers/Admin/requests.spec.ts
@@ -0,0 +1,36 @@
+import * as requests from './requests'
+
+const backendPermissions: requests.BackendPermissionsData = {
+  's3://A': 'READ_WRITE',
+  's3://B-B': 'READ',
+  's3://Cc': null,
+}
+
+const frontendPermissions: requests.BucketPermissionData[] = [
+  {
+    bucket: 's3://A',
+    permission: 'ReadWrite',
+  },
+  {
+    bucket: 's3://B-B',
+    permission: 'Read',
+  },
+  {
+    bucket: 's3://Cc',
+    permission: 'None',
+  },
+]
+
+describe('Admin/requests', () => {
+  test('convertToBackendPermissions', () => {
+    expect(requests.convertToBackendPermissions(frontendPermissions)).toEqual(
+      backendPermissions,
+    )
+  })
+
+  test('convertToFrontendPermissions', () => {
+    expect(requests.convertToFrontendPermissions(backendPermissions)).toEqual(
+      frontendPermissions,
+    )
+  })
+})
