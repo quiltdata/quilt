45cc0e29e1f43a517c0c7fd225f5c768bceb1547
Maxim Chervonny <fiskus@chervonny.ru>
2021-06-25 18:42:24 +0300

    increase coverage to 97%

--- catalog/app/containers/Bucket/viewModes.spec.ts
+++ catalog/app/containers/Bucket/viewModes.spec.ts
@@ -1,6 +1,9 @@
 import { mocked } from 'ts-jest/utils'
 import { renderHook } from '@testing-library/react-hooks'
 
+// NOTE: module imported selectively because Preview's deps break unit-tests
+import { PreviewData } from 'components/Preview/types'
+import AsyncResult from 'utils/AsyncResult'
 import global from 'utils/global'
 
 import useViewModes from './viewModes'
@@ -27,13 +30,25 @@ function fetchNotOk(): Promise<Response> {
   )
 }
 
-function fetchThrow(): Promise<Response> {
-  return new Promise((resolve, reject) =>
-    setTimeout(() => {
-      reject(new Error('Expected'))
-    }, 100),
-  )
-}
+const jsonResult = AsyncResult.Ok(
+  PreviewData.Json({
+    rendered: {
+      $schema: 'https://vega.github.io/schema/a/b.json',
+    },
+  }),
+)
+
+const vegaResult = AsyncResult.Ok(
+  PreviewData.Vega({
+    spec: {
+      $schema: 'https://vega.github.io/schema/a/b.json',
+    },
+  }),
+)
+
+const imgResult = AsyncResult.Ok(PreviewData.Image())
+
+const pendingResult = AsyncResult.Pending()
 
 describe('containers/Bucket/viewModes', () => {
   describe('useViewModes', () => {
@@ -74,16 +89,45 @@ describe('containers/Bucket/viewModes', () => {
       ])
     })
 
-    it('returns Notebooks modes for .ipynb when request throws error', async () => {
-      mocked(global.fetch).mockImplementation(fetchThrow)
-
+    it('returns no modes for .json when no Vega mode', async () => {
       const { result } = renderHook(() =>
-        useViewModes('https://registry.example', 'test.ipynb'),
+        useViewModes('https://registry.example', 'test.json'),
+      )
+      expect(result.current).toMatchObject([])
+    })
+
+    it('returns no modes for .json when result is pending', async () => {
+      const { result } = renderHook(() =>
+        useViewModes('https://registry.example', 'test.json', pendingResult),
+      )
+      expect(result.current).toMatchObject([])
+    })
+
+    it('returns Vega/JSON modes for .json when Vega mode selected', async () => {
+      const { result } = renderHook(() =>
+        useViewModes('https://registry.example', 'test.json', vegaResult),
       )
       expect(result.current).toMatchObject([
-        { key: 'jupyter', label: 'Jupyter' },
+        { key: 'vega', label: 'Vega' },
         { key: 'json', label: 'JSON' },
       ])
+    })
+
+    it('returns Vega/JSON modes for .json when JSON mode selected', async () => {
+      const { result } = renderHook(() =>
+        useViewModes('https://registry.example', 'test.json', jsonResult),
+      )
+      expect(result.current).toMatchObject([
+        { key: 'vega', label: 'Vega' },
+        { key: 'json', label: 'JSON' },
+      ])
+    })
+
+    it('returns no modes for .json when result is different Preview', async () => {
+      const { result } = renderHook(() =>
+        useViewModes('https://registry.example', 'test.json', imgResult),
+      )
+      expect(result.current).toMatchObject([])
     })
   })
 })
