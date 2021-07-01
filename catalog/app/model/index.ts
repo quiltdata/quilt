081f7ea7286d7c5d0af160c30f3a41eb08dc6295
nl_0 <nl.imbecile@gmail.com>
2021-07-01 12:42:25 +0500

    model: helper runtime types for permissions

--- catalog/app/model/index.ts
+++ catalog/app/model/index.ts
@@ -1,5 +1,40 @@
+import * as IO from 'io-ts'
+
+import * as Types from 'utils/types'
+
+import * as GQLTypes from './graphql/types.generated'
+
 export * as GQLTypes from './graphql/types.generated'
 
 // default bucket icon as returned by the registry
 export const DEFAULT_BUCKET_ICON =
   'https://d1zvn9rasera71.cloudfront.net/q-128-square.png'
+
+export const BucketPermissionLevel = Types.enum(
+  GQLTypes.BucketPermissionLevel,
+  'BucketPermissionLevel',
+)
+
+export const BucketPermissionLevelStrings = ['None', 'Read', 'ReadWrite'] as const
+
+export type BucketPermissionLevelString = typeof BucketPermissionLevelStrings[number]
+
+export const NullableBucketPermissionLevelFromString = new IO.Type<
+  GQLTypes.BucketPermissionLevel | null,
+  BucketPermissionLevelString
+>(
+  'NullableBucketPermissionLevelFromString',
+  (u): u is GQLTypes.BucketPermissionLevel | null =>
+    u === null || BucketPermissionLevel.is(u),
+  (u, c) => {
+    if (u === 'None') return IO.success(null)
+    if (u === 'Read') return IO.success(GQLTypes.BucketPermissionLevel.READ)
+    if (u === 'ReadWrite') return IO.success(GQLTypes.BucketPermissionLevel.READ_WRITE)
+    return IO.failure(u, c)
+  },
+  (a) => {
+    if (a === GQLTypes.BucketPermissionLevel.READ) return 'Read' as const
+    if (a === GQLTypes.BucketPermissionLevel.READ_WRITE) return 'ReadWrite' as const
+    return 'None' as const
+  },
+)
