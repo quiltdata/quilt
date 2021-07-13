fea619d385a87321c53e164c38fbb953c509a0e1
Maxim Chervonny <fiskus@chervonny.ru>
2021-07-13 21:04:16 +0300

    replace link icon with lauch (#2269)

--- catalog/app/containers/Admin/Roles.tsx
+++ catalog/app/containers/Admin/Roles.tsx
@@ -583,7 +583,7 @@ export default function Roles() {
     role.arn
       ? {
           title: 'Open IAM',
-          icon: <M.Icon>link</M.Icon>,
+          icon: <M.Icon>launch</M.Icon>,
           href: getARNLink(role.arn),
         }
       : null,
