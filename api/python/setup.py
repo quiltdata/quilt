f6d7d5ea4a3247ff966dc843c7f83cdb2eae7c12
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2021-10-12 14:02:10 +0500

    Add Python 3.10 to CircleCI test jobs

--- api/python/setup.py
+++ api/python/setup.py
@@ -50,6 +50,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     author='quiltdata',
     author_email='contact@quiltdata.io',
