9609f4a5579d6a045e973e43869398eddd54fbef
Maxim Chervonny <fiskus@chervonny.ru>
2021-10-20 15:33:06 +0300

    add dirty ECharts renderer

---
+++ catalog/app/components/Preview/renderers/ECharts.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import * as M from '@material-ui/core'
+import * as echarts from 'echarts'
+
+const usetyles = M.makeStyles({
+  root: {
+    height: '200px',
+  },
+})
+
+interface EChartsEssential {
+  dataset: echarts.EChartsOption
+}
+
+interface EChartsProps extends React.HTMLProps<HTMLDivElement> {
+  dataset: echarts.EChartsOption
+}
+
+function ECharts({ dataset, ...props }: EChartsProps) {
+  const containerRef = React.useRef<HTMLDivElement | null>(null)
+
+  const classes = usetyles()
+
+  React.useEffect(() => {
+    if (!containerRef.current) return
+    const chart = echarts.init(containerRef.current)
+    chart.setOption(dataset)
+  }, [containerRef, dataset])
+
+  return <div ref={containerRef} className={classes.root} {...props} />
+}
+
+export default (
+  { dataset }: EChartsEssential,
+  props: React.HTMLProps<HTMLDivElement>,
+) => <ECharts dataset={dataset} {...props} />
