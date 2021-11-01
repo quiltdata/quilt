271c2ef7dc1bcb1a29a412dc3ede7bb0522e6267
Maxim Chervonny <fiskus@chervonny.ru>
2021-11-01 15:40:14 +0300

    add types

--- catalog/app/components/Preview/renderers/Voila.tsx
+++ catalog/app/components/Preview/renderers/Voila.tsx
@@ -1,10 +1,11 @@
+import type * as React from 'react'
 import * as R from 'ramda'
 
 import Iframe from './IFrame'
 
 const DEFAULT_IFRAME_HEIGHT = '1024px'
 
-const assocDefaultHeight = R.over(
+const assocDefaultHeight = R.over<{ style?: React.CSSProperties }, string | number>(
   R.lensPath(['style', 'height']),
   R.defaultTo(DEFAULT_IFRAME_HEIGHT),
 )
