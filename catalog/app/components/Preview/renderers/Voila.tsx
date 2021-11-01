5b8f72a854149997130f41526f8960e3c2a53c04
Maxim Chervonny <fiskus@chervonny.ru>
2021-11-01 15:39:28 +0300

    move setting default height to curried function

--- catalog/app/components/Preview/renderers/Voila.tsx
+++ catalog/app/components/Preview/renderers/Voila.tsx
@@ -4,11 +4,12 @@ import Iframe from './IFrame'
 
 const DEFAULT_IFRAME_HEIGHT = '1024px'
 
+const assocDefaultHeight = R.over(
+  R.lensPath(['style', 'height']),
+  R.defaultTo(DEFAULT_IFRAME_HEIGHT),
+)
+
 export default (
   ifr: React.HTMLProps<HTMLIFrameElement>,
   props: React.HTMLProps<HTMLIFrameElement>,
-) => {
-  const heightLens = R.lensPath(['style', 'height'])
-  const defaultHeight = R.defaultTo(DEFAULT_IFRAME_HEIGHT)
-  return Iframe(ifr, R.over(heightLens, defaultHeight, props))
-}
+) => Iframe(ifr, assocDefaultHeight(props))
