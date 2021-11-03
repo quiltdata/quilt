f2d9ea1e65fe60806064e3dcb563fe6d63dcb82b
Maxim Chervonny <fiskus@chervonny.ru>
2021-11-03 15:57:39 +0300

    pass schema to metadata editor

--- catalog/app/components/MetadataEditor/MetadataEditor.tsx
+++ catalog/app/components/MetadataEditor/MetadataEditor.tsx
@@ -1,12 +1,12 @@
 import Ajv from 'ajv'
 import brace from 'brace'
+import { JsonEditor as ReactJsonEditor } from 'jsoneditor-react'
 import * as React from 'react'
-import * as M from '@material-ui/core'
 import * as Lab from '@material-ui/lab'
 
-import { JsonEditor as ReactJsonEditor } from 'jsoneditor-react'
 import JsonEditor from 'components/JsonEditor'
 import { JsonValue } from 'components/JsonEditor/constants'
+import { JsonSchema } from 'utils/json-schema'
 
 import 'brace/mode/json'
 import 'brace/theme/eclipse'
@@ -14,22 +14,25 @@ import 'jsoneditor-react/es/editor.min.css'
 
 const ajv = new Ajv({ allErrors: true, verbose: true })
 
-interface MetadataEditorProps {
-  isRaw: boolean
-  isMultiColumned: boolean
-  value: JsonValue
-  onChange: (value: JsonValue) => void
+const editorHtmlProps = {
+  style: { height: '100%' },
 }
 
-export default function MetadataEditor({ isRaw, value, onChange }: MetadataEditorProps) {
+interface MetadataEditorProps {
+  isMultiColumned: boolean
+  isRaw: boolean
+  onChange: (value: JsonValue) => void
+  schema?: JsonSchema
+  value: JsonValue
+}
+
+export default function MetadataEditor({
+  isRaw,
+  onChange,
+  schema,
+  value,
+}: MetadataEditorProps) {
   const [errors, setErrors] = React.useState<Error[]>([])
-  const t = M.useTheme()
-  const editorHtmlProps = React.useMemo(
-    () => ({
-      style: { height: t.spacing(30) },
-    }),
-    [t],
-  )
   return (
     <>
       {isRaw ? (
@@ -43,15 +46,16 @@ export default function MetadataEditor({ isRaw, value, onChange }: MetadataEdito
           onChange={onChange}
           onError={(e) => setErrors([e])}
           onValidationError={setErrors}
-          //   schema={schema}
+          schema={schema}
           search={false}
           statusBar={false}
           theme="ace/theme/eclipse"
           value={value || {}}
         />
       ) : (
-        <JsonEditor isMultiColumned value={value} onChange={onChange} />
+        <JsonEditor isMultiColumned value={value} onChange={onChange} schema={schema} />
       )}
+
       {!!errors.length &&
         errors.map((error) => (
           <Lab.Alert key={error.message} severity="error">
