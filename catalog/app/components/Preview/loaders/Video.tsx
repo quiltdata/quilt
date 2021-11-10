304bb83f2ea3164b3f00a5e8e157c78efc637f3d
Maxim Chervonny <fiskus@chervonny.ru>
2021-11-10 18:27:43 +0300

    prepare to use video lambda

--- catalog/app/components/Preview/loaders/Video.tsx
+++ catalog/app/components/Preview/loaders/Video.tsx
@@ -1,3 +1,8 @@
+// import { extname } from 'path'
+
+import * as React from 'react'
+
+import * as AWS from 'utils/AWS'
 import AsyncResult from 'utils/AsyncResult'
 import type { S3HandleBase } from 'utils/s3paths'
 
@@ -5,13 +10,24 @@ import { PreviewData } from '../types'
 
 import * as utils from './utils'
 
-export const detect = utils.extIn(['.mp4'])
+export const detect = utils.extIn(['.mp4', '.webm'])
 
 interface VideoLoaderProps {
-  handle: S3HandleBase
   children: (result: $TSFixMe) => React.ReactNode
+  handle: S3HandleBase
+}
+
+function useVideoSrc(handle: S3HandleBase): string {
+  const sign = AWS.Signer.useS3Signer()
+  const url = React.useMemo(() => sign(handle), [handle, sign])
+  // const requestBody = {
+  //   url: url
+  //   format: extname(handle.key)
+  // }
+  return url
 }
 
 export const Loader = function VideoLoader({ handle, children }: VideoLoaderProps) {
-  return children(AsyncResult.Ok(PreviewData.Video({ handle })))
+  const src = useVideoSrc(handle)
+  return children(AsyncResult.Ok(PreviewData.Video({ src })))
 }
--- catalog/app/components/Preview/renderers/Video.tsx
+++ catalog/app/components/Preview/renderers/Video.tsx
@@ -2,12 +2,10 @@ import cx from 'classnames'
 import * as React from 'react'
 import * as M from '@material-ui/core'
 
-import * as AWS from 'utils/AWS'
-import { S3HandleBase } from 'utils/s3paths'
-
 const useStyles = M.makeStyles({
   root: {
     display: 'block',
+    height: '240px',
     margin: 'auto',
     maxWidth: '100%',
   },
@@ -15,15 +13,13 @@ const useStyles = M.makeStyles({
 
 interface VideoProps extends React.HTMLAttributes<HTMLVideoElement> {
   className?: string
-  handle: S3HandleBase
+  src: string
 }
 
-function Video({ handle, className, ...props }: VideoProps) {
-  const sign = AWS.Signer.useS3Signer()
+function Video({ src, className, ...props }: VideoProps) {
   const classes = useStyles()
-  const url = React.useMemo(() => sign(handle), [handle, sign])
   return (
-    <video controls muted src={url} className={cx(className, classes.root)} {...props} />
+    <video controls muted src={src} className={cx(className, classes.root)} {...props} />
   )
 }
 
