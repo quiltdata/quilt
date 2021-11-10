b071c79c38be4c5af3771aedcb7c5a5fc9d1cd2f
Maxim Chervonny <fiskus@chervonny.ru>
2021-11-10 16:51:16 +0300

    move function to top

--- catalog/app/components/Preview/loaders/Json.spec.js
+++ catalog/app/components/Preview/loaders/Json.spec.js
@@ -2,16 +2,16 @@ import { traverseUrls } from './Json'
 
 describe('components/Preview/loaders/Json', () => {
   describe('traverseUrls', () => {
+    const transformFn = (url) => `A${url}B`
+
     it('traverse urls in single data', () => {
       const spec = { data: { url: 'one.json' } }
-      const transformFn = (url) => `A${url}B`
       const expectedResult = { data: { url: 'Aone.jsonB' } }
       expect(traverseUrls(transformFn, spec)).toEqual(expectedResult)
     })
 
     it('traverse urls in data array', () => {
       const spec = { data: [{ url: 'one.json' }, { url: 'two.json' }] }
-      const transformFn = (url) => `A${url}B`
       const expectedResult = {
         data: [{ url: 'Aone.jsonB' }, { url: 'Atwo.jsonB' }],
       }
@@ -22,7 +22,6 @@ describe('components/Preview/loaders/Json', () => {
       const spec = {
         layer: [{ data: { url: 'one.json' } }, { data: { url: 'two.json' } }],
       }
-      const transformFn = (url) => `A${url}B`
       const expectedResult = {
         layer: [{ data: { url: 'Aone.jsonB' } }, { data: { url: 'Atwo.jsonB' } }],
       }
