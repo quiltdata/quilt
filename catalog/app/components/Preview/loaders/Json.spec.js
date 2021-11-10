f3140ad0fa7b9fcc91f5c39b24f916b6734d42db
Maxim Chervonny <fiskus@chervonny.ru>
2021-11-10 16:49:56 +0300

    shorter names

--- catalog/app/components/Preview/loaders/Json.spec.js
+++ catalog/app/components/Preview/loaders/Json.spec.js
@@ -3,31 +3,28 @@ import { traverseUrls } from './Json'
 describe('components/Preview/loaders/Json', () => {
   describe('traverseUrls', () => {
     it('traverse urls in single data', () => {
-      const spec = { data: { url: 'relative.json' } }
+      const spec = { data: { url: 'one.json' } }
       const transformFn = (url) => `A${url}B`
-      const expectedResult = { data: { url: 'Arelative.jsonB' } }
+      const expectedResult = { data: { url: 'Aone.jsonB' } }
       expect(traverseUrls(transformFn, spec)).toEqual(expectedResult)
     })
 
     it('traverse urls in data array', () => {
-      const spec = { data: [{ url: 'relative.json' }, { url: 'another.json' }] }
+      const spec = { data: [{ url: 'one.json' }, { url: 'two.json' }] }
       const transformFn = (url) => `A${url}B`
       const expectedResult = {
-        data: [{ url: 'Arelative.jsonB' }, { url: 'Aanother.jsonB' }],
+        data: [{ url: 'Aone.jsonB' }, { url: 'Atwo.jsonB' }],
       }
       expect(traverseUrls(transformFn, spec)).toEqual(expectedResult)
     })
 
     it('traverse urls in layers', () => {
       const spec = {
-        layer: [{ data: { url: 'relative.json' } }, { data: { url: 'another.json' } }],
+        layer: [{ data: { url: 'one.json' } }, { data: { url: 'two.json' } }],
       }
       const transformFn = (url) => `A${url}B`
       const expectedResult = {
-        layer: [
-          { data: { url: 'Arelative.jsonB' } },
-          { data: { url: 'Aanother.jsonB' } },
-        ],
+        layer: [{ data: { url: 'Aone.jsonB' } }, { data: { url: 'Atwo.jsonB' } }],
       }
       expect(traverseUrls(transformFn, spec)).toEqual(expectedResult)
     })
