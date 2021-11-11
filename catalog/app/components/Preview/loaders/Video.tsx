90b0dc5ef7d9a7f3c529e7d2b14f74436b18812a
Maxim Chervonny <fiskus@chervonny.ru>
2021-11-11 17:58:39 +0300

    use real API

--- catalog/app/components/Preview/loaders/Video.tsx
+++ catalog/app/components/Preview/loaders/Video.tsx
@@ -1,4 +1,4 @@
-// import { extname } from 'path'
+import { extname } from 'path'
 
 import * as React from 'react'
 
@@ -20,11 +20,12 @@ interface VideoLoaderProps {
 function useVideoSrc(handle: S3HandleBase): string {
   const sign = AWS.Signer.useS3Signer()
   const url = React.useMemo(() => sign(handle), [handle, sign])
-  // const requestBody = {
-  //   url: url
-  //   format: extname(handle.key)
-  // }
-  return url
+  const query = new URLSearchParams({
+    duration: '1',
+    format: extname(handle.key).replace(/^\./, ''),
+    url,
+  })
+  return `https://zg07p3bs9g.execute-api.us-east-1.amazonaws.com/prod/transcode?${query.toString()}`
 }
 
 export const Loader = function VideoLoader({ handle, children }: VideoLoaderProps) {
