7d6e411779a75d1c5644ab616160e0249bb06794
Dima Ryazanov <dima@quiltdata.io>
2021-11-22 19:13:35 -0800

    Add audio transcoding: mp3 and ogg (#2542)

--- lambdas/transcode/index.py
+++ lambdas/transcode/index.py
@@ -3,9 +3,21 @@ Generate video previews for videos in S3.
 """
 import subprocess
 import tempfile
+from urllib.parse import urlparse
 
 from t4_lambda_shared.decorator import api, validate
 from t4_lambda_shared.utils import get_default_origins, make_json_response
+
+# Map of supported content types and corresponding FFMPEG formats
+FORMATS = {
+    'video/mp4': 'mp4',
+    'video/webm': 'webm',
+    'audio/mpeg': 'mp3',
+    'audio/ogg': 'ogg',
+}
+
+# TODO: Remove this.
+LEGACY_FORMATS = {v: k for k, v in FORMATS.items()}
 
 SCHEMA = {
     'type': 'object',
@@ -14,7 +26,7 @@ SCHEMA = {
             'type': 'string'
         },
         'format': {
-            'enum': ['mp4', 'webm']
+            'enum': list(FORMATS) + list(LEGACY_FORMATS)
         },
         'width': {
             'type': 'string'
@@ -25,8 +37,14 @@ SCHEMA = {
         'duration': {
             'type': 'string'
         },
+        'audio_bitrate': {
+            'type': 'string',
+        },
+        'file_size': {
+            'type': 'string'
+        }
     },
-    'required': ['url'],
+    'required': ['url', 'format'],
     'additionalProperties': False
 }
 
@@ -34,12 +52,7 @@ FFMPEG = '/opt/bin/ffmpeg'
 
 # Lambda has a 6MB limit for request and response, however, base64 adds 33% overhead.
 # Also, leave a few KB for the headers.
-MAX_VIDEO_SIZE = 6 * 1024 * 1024 * 3 // 4 - 4096
-
-MIN_WIDTH = 10
-MAX_WIDTH = 640
-MIN_HEIGHT = 10
-MAX_HEIGHT = 480
+MAX_FILE_SIZE = 6 * 1024 * 1024 * 3 // 4 - 4096
 
 
 @api(cors_origins=get_default_origins())
@@ -49,44 +62,56 @@ def lambda_handler(request):
     Generate previews for videos in S3
     """
     url = request.args['url']
-    format = request.args.get('format', 'mp4')
-    width_str = request.args.get('width', '320')
-    height_str = request.args.get('height', '240')
-    duration_str = request.args.get('duration', '5')
+    format = request.args['format']
+
+    def _parse_param(name, default_value, min_value, max_value):
+        value_str = request.args.get(name)
+        if value_str is None:
+            return default_value
+
+        try:
+            value = type(default_value)(value_str)
+            if not min_value <= value <= max_value:
+                raise ValueError
+            return value
+        except ValueError:
+            raise ValueError(f"Invalid {name!r}; must be between {min_value} and {max_value} inclusive")
 
     try:
-        width = int(width_str)
-        if not MIN_WIDTH <= width <= MAX_WIDTH:
-            raise ValueError
-    except ValueError:
-        return make_json_response(400, {'error': f"Invalid 'width'; must be between {MIN_WIDTH} and {MAX_WIDTH}"})
+        width = _parse_param('width', 320, 10, 640)
+        height = _parse_param('height', 240, 10, 480)
+        duration = _parse_param('duration', 5.0, 0.1, 10)
+        audio_bitrate = _parse_param('audio_bitrate', 128, 64, 320)
+        file_size = _parse_param('file_size', MAX_FILE_SIZE, 1024, MAX_FILE_SIZE)
+    except ValueError as ex:
+        return make_json_response(400, {'error': str(ex)})
 
-    try:
-        height = int(height_str)
-        if not MIN_HEIGHT <= height <= MAX_HEIGHT:
-            raise ValueError
-    except ValueError:
-        return make_json_response(400, {'error': f"Invalid 'height'; must be between {MIN_HEIGHT} and {MAX_HEIGHT}"})
+    format = LEGACY_FORMATS.get(format, format)
+    category = format.split('/')[0]
 
-    try:
-        duration = float(duration_str)
-        if not 0 < duration <= 10:
-            raise ValueError
-    except ValueError:
-        return make_json_response(400, {'error': "Invalid 'duration'"})
+    format_params = []
+    if category == 'audio':
+        format_params.extend([
+            '-b:a', f'{audio_bitrate}k',
+            '-vn',  # Drop the video stream
+        ])
+    elif category == 'video':
+        format_params.extend([
+            "-vf", ','.join([
+                f"scale=w={width}:h={height}:force_original_aspect_ratio=decrease",
+                "crop='iw-mod(iw\\,2)':'ih-mod(ih\\,2)'",
+            ]),
+        ])
 
     with tempfile.NamedTemporaryFile() as output_file:
         p = subprocess.run([
             FFMPEG,
             "-t", str(duration),
             "-i", url,
-            "-f", format,
-            "-vf", ','.join([
-                f"scale=w={width}:h={height}:force_original_aspect_ratio=decrease",
-                "crop='iw-mod(iw\\,2)':'ih-mod(ih\\,2)'",
-            ]),
+            "-f", FORMATS[format],
+            *format_params,
             "-timelimit", str(request.context.get_remaining_time_in_millis() // 1000 - 2),  # 2 seconds for padding
-            "-fs", str(MAX_VIDEO_SIZE),
+            "-fs", str(file_size),
             "-y",  # Overwrite output file
             "-v", "error",  # Only print errors
             output_file.name
@@ -97,4 +122,13 @@ def lambda_handler(request):
 
         data = output_file.read()
 
-    return 200, data, {'Content-Type': f'video/{format}'}
+    parsed = urlparse(url)
+    filename = parsed.path.rpartition('/')[-1]
+
+    headers = {
+        'Content-Type': format,
+        'Title': f"Preview of {filename}",
+        'Content-Disposition': f'inline; filename="{filename}"',
+    }
+
+    return 200, data, headers
