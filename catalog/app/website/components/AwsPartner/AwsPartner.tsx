8555337fc2730b72c39fcd9220ce4fb0bf247c30
Maxim Chervonny <fiskus@chervonny.ru>
2021-11-29 11:26:39 +0300

    Aws partner logo as inline component

--- catalog/app/website/components/AwsPartner/AwsPartner.tsx
+++ catalog/app/website/components/AwsPartner/AwsPartner.tsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 
 import img2x from 'utils/img2x'
 
-import partner from 'website/pages/Landing/Platform/partner.png'
-import partner2x from 'website/pages/Landing/Platform/partner@2x.png'
+import partner from './partner.png'
+import partner2x from './partner@2x.png'
 
 export default function AwsPartner(props: React.HTMLAttributes<HTMLImageElement>) {
   return (
