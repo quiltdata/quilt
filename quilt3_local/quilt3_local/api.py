28875ac3c0effd9e94067a956c65e322c1ab3f11
nl_0 <nl.imbecile@gmail.com>
2021-12-02 22:18:34 +0500

    support session credentials

--- quilt3_local/quilt3_local/api.py
+++ quilt3_local/quilt3_local/api.py
@@ -17,6 +17,8 @@ class ApiException(Exception):
 
 
 sts_client = boto3.client("sts")
+session_cred = boto3._get_default_session().get_credentials()
+
 
 api = fastapi.FastAPI()
 
@@ -28,16 +30,22 @@ def get_credentials():
 
     Returns a JSON object with three keys:
         AccessKeyId(string): access key ID
-        SecretKey(string): secret key
+        SecretAccessKey(string): secret key
         SessionToken(string): session token
+        Expiration(ISO date string)
     """
     try:
-        creds = sts_client.get_session_token()
+        if session_cred.token:
+            return {
+                "AccessKeyId": session_cred.access_key,
+                "SecretAccessKey": session_cred.secret_key,
+                "SessionToken": session_cred.token,
+                "Expiration": getattr(session_cred, "expiry_time", None),
+            }
+        return sts_client.get_session_token()["Credentials"]
     except ClientError as ex:
         print(ex)
         raise ApiException(500, "Failed to get credentials for your AWS Account.")
-
-    return creds["Credentials"]
 
 
 api.mount("/graphql", ariadne.asgi.GraphQL(graphql_schema), "GraphQL")
