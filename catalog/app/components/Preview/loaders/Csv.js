7b52a98cc6c21f2de8007a5731e72200f38bccb2
Maxim Chervonny <fiskus@chervonny.ru>
2021-12-06 07:30:43 +0300

    use perspective for table view

--- catalog/app/components/Preview/loaders/Csv.js
+++ catalog/app/components/Preview/loaders/Csv.js
@@ -1,24 +1,47 @@
+import * as Papa from 'papaparse'
 import * as R from 'ramda'
 
 import { PreviewData } from '../types'
 import * as utils from './utils'
 
+export const MAX_BYTES = 10 * 1024
+
 export const detect = R.pipe(utils.stripCompression, utils.extIn(['.csv', '.tsv']))
 
-const isTsv = R.pipe(utils.stripCompression, utils.extIs('.tsv'))
+// [[...row1], [...row2]] → { a: [...rowA], b: [...rowB]}
+function makeHeadedTable(parsed) {
+  return parsed.reduce((memo, row, index) => {
+    if (index === 0) {
+      return row.reduce((memoHead, title) => {
+        memoHead[title] = []
+        return memoHead
+      }, memo)
+    }
+
+    return row.reduce((memoCells, value, cellIndex) => {
+      const title = parsed[0][cellIndex]
+      memoCells[title].push(value)
+      return memoCells
+    }, memo)
+  }, {})
+}
 
 export const Loader = function CsvLoader({ handle, children }) {
-  const data = utils.usePreview({
-    type: 'csv',
+  const asyncData = utils.usePreview({
+    type: 'txt',
     handle,
-    query: isTsv(handle.key) ? { sep: '\t' } : undefined,
+    query: { max_bytes: MAX_BYTES },
   })
-  const processed = utils.useProcessing(data.result, (json) =>
-    PreviewData.DataFrame({
-      preview: json.html,
-      note: json.info.note,
-      warnings: json.info.warnings,
-    }),
+  const processed = utils.useProcessing(
+    asyncData.result,
+    ({ info: { data, note, warnings } }) => {
+      const csv = data.head.join('\n')
+      return PreviewData.DataFrame({
+        data: makeHeadedTable(Papa.parse(csv).data),
+        note,
+        warnings,
+      })
+    },
   )
-  return children(utils.useErrorHandling(processed, { handle, retry: data.fetch }))
+  return children(utils.useErrorHandling(processed, { handle, retry: asyncData.fetch }))
 }
--- catalog/app/components/Preview/renderers/DataFrame.js
+++ catalog/app/components/Preview/renderers/DataFrame.js
@@ -1,57 +1,65 @@
 import cx from 'classnames'
 import * as React from 'react'
 import * as M from '@material-ui/core'
+import perspective from '@finos/perspective'
+
+import 'utils/perspective-pollution'
 
 import { renderWarnings } from './util'
+
+const worker = perspective.worker()
 
 const useStyles = M.makeStyles((t) => ({
   root: {
     width: '100%',
   },
-  wrapper: {
-    overflow: 'auto',
-
-    '& table.dataframe': {
-      border: 'none',
-      minWidth: '100%',
-      width: 'auto',
-
-      '& tr:nth-child(even)': {
-        backgroundColor: t.palette.grey[100],
-      },
-
-      '& th, & td': {
-        border: 'none',
-        fontSize: 'small',
-        height: t.spacing(3),
-        paddingLeft: t.spacing(1),
-        paddingRight: t.spacing(1),
-      },
-
-      '& td': {
-        whiteSpace: 'nowrap',
-      },
-    },
+  viewer: {
+    height: t.spacing(40),
   },
 }))
 
-function DataFrame({ children, className, note, warnings, ...props } = {}) {
+function renderPerspectiveViewer(parentNode, className) {
+  const element = document.createElement('perspective-viewer')
+  element.className = className
+  parentNode.appendChild(element)
+  return element
+}
+
+async function renderTable(data, viewer) {
+  const table = await worker.table(data)
+  viewer.load(table)
+  return table
+}
+
+function DataFrame({ children, className, data, note, warnings, ...props } = {}) {
   const classes = useStyles()
+
+  const [root, setRoot] = React.useState(null)
+
+  React.useEffect(() => {
+    let table, viewer
+
+    async function fetchData() {
+      if (!root) return
+
+      viewer = renderPerspectiveViewer(root, classes.viewer)
+      table = await renderTable(data, viewer)
+    }
+    fetchData()
+
+    return () => {
+      table?.delete()
+      viewer?.parentNode?.removeChild(viewer)
+    }
+  }, [classes.viewer, data, root])
+
   return (
-    <div className={cx(className, classes.root)} {...props}>
+    <div className={cx(className, classes.root)} ref={setRoot} title={note} {...props}>
       {renderWarnings(warnings)}
-      <div
-        title={note}
-        className={classes.wrapper}
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: children }}
-      />
     </div>
   )
 }
 
-export default ({ preview, note, warnings }, props) => (
-  <DataFrame {...{ note, warnings }} {...props}>
-    {preview}
-  </DataFrame>
+export default ({ data, note, warnings }, props) => (
+  <DataFrame {...{ data, note, warnings }} {...props} />
 )
