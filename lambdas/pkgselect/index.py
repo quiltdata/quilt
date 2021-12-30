a1e4826a1e7cd127edcf11e3a511addcf6ed09eb
Alexei Mochalov <nl.imbecile@gmail.com>
2021-12-30 18:21:30 +0500

    pkgselect: make directly invocable, always use execution role (#2560)

--- lambdas/pkgselect/index.py
+++ lambdas/pkgselect/index.py
@@ -2,62 +2,52 @@
 Provide a virtual-file-system view of a package's logical keys.
 """
 
+import asyncio
+import dataclasses
+import functools
 import json
-import os
+import typing as T
 
 import boto3
-import botocore
 import pandas as pd
 
-from t4_lambda_shared.decorator import api, validate
-from t4_lambda_shared.utils import (
-    get_default_origins,
-    make_json_response,
-    query_manifest_content,
-    sql_escape,
-)
+from t4_lambda_shared.utils import query_manifest_content, sql_escape
 
-SCHEMA = {
-    'type': 'object',
-    'properties': {
-        'bucket': {
-            'type': 'string'
-        },
-        'manifest': {
-            'type': 'string'
-        },
-        'access_key': {
-            'type': 'string'
-        },
-        'secret_key': {
-            'type': 'string'
-        },
-        'session_token': {
-            'type': 'string'
-        },
-        'prefix': {
-            'type': 'string'
-        },
-        'logical_key': {
-            'type': 'string'
-        },
-        'offset': {
-            'type': 'integer'
-        },
-        'limit': {
-            'type': 'integer'
-        }
-    },
-    'required': ['bucket', 'manifest'],
-    'additionalProperties': False
-}
+
+async def run_async(fn, executor=None, loop=None):
+    if loop is None:
+        loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(executor, fn)
+
+
+class PkgselectException(Exception):
+    def __str__(self):
+        s = self.__class__.__name__
+        if self.args:
+            s = f"{s}: {self.args[0]}"
+        return s
+
+
+class BadInputParameters(PkgselectException):
+    pass
+
+
+class AccessDenied(PkgselectException):
+    pass
+
+
+class NotFound(PkgselectException):
+    pass
+
+
+def validate(condition: T.Any, message: str):
+    if not condition:
+        raise BadInputParameters(message)
 
 
 def file_list_to_folder(df: pd.DataFrame, limit: int, offset: int) -> dict:
     """
-    Post process a set of logical keys to return only the
-    top-level folder view (a special case of the s3-select
-    lambda).
+    Post process a set of logical keys to return only the top-level folder view.
     """
     if {'physical_key', 'logical_key', 'size'}.issubset(df.columns):
         groups = df.groupby(df.logical_key.str.extract('([^/]+/?).*')[0], dropna=True)
@@ -82,180 +72,199 @@ def file_list_to_folder(df: pd.DataFrame, limit: int, offset: int) -> dict:
         ).to_dict(orient='records')
         objects = folder[~folder.logical_key.str.contains('/')].to_dict(orient='records')
     else:
-        # df might not have the expected columns if either: (1) the
-        # package is empty (has zero package entries) or, (2) zero
-        # package entries match the prefix filter. In either case,
-        # the folder view is empty.
+        # df might not have the expected columns if either:
+        # (1) the package is empty (has zero package entries) or,
+        # (2) zero package entries match the prefix filter.
+        # In either case, the folder view is empty.
         prefixes = []
         objects = []
         total_results = 0
 
-    returned_results = len(prefixes) + len(objects)
     return dict(
         total=total_results,
-        returned=returned_results,
         prefixes=prefixes,
-        objects=objects
+        objects=objects,
     )
 
 
-def create_s3_client(
-    *,
-    aws_access_key_id: str,
-    aws_secret_access_key: str,
-    aws_session_token: str
-):
+@functools.lru_cache(maxsize=None)
+def get_s3_client():
+    return boto3.client("s3")
+
+
+async def select(bucket: str, key: str, stmt: str):
+    s3 = get_s3_client()
+    try:
+        return await run_async(functools.partial(
+            query_manifest_content,
+            s3,
+            bucket=bucket,
+            key=key,
+            sql_stmt=stmt,
+        ))
+    except (s3.exceptions.NoSuchKey, s3.exceptions.NoSuchBucket):
+        raise NotFound
+    except s3.exceptions.ClientError as ex:
+        if ex.response.get("Error", {}).get("Code") == "AccessDenied":
+            raise AccessDenied
+        raise ex
+
+
+async def select_meta(bucket: str, manifest: str, path: T.Optional[str] = None) -> dict:
     """
-    Create an S3 Client using caller-provided credentials.
+    Fetch package-level, directory-level or object-level metadata
     """
-    assert aws_access_key_id and aws_secret_access_key and aws_session_token
-    session = boto3.Session(
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        aws_session_token=aws_session_token
+    if path:
+        sql_stmt = f"SELECT s.meta FROM s3object s WHERE s.logical_key = '{sql_escape(path)}' LIMIT 1"
+    else:
+        sql_stmt = "SELECT s.* FROM s3object s WHERE s.logical_key is NULL LIMIT 1"
+
+    result = await select(bucket, manifest, sql_stmt)
+    return json.load(result) if result else {}
+
+
+@dataclasses.dataclass
+class FileView:
+    physical_key: str
+    size: int
+    hash: str
+    meta: T.Optional[dict]
+
+
+async def file_view(bucket: str, manifest: str, path: str) -> T.Optional[FileView]:
+    """
+    Get details of a single file in the package.
+    """
+    validate(
+        isinstance(bucket, str) and bucket,
+        f"file_view: bucket must be a non-empty string (given: {bucket!r})",
     )
-    return session.client('s3')
+    validate(
+        isinstance(manifest, str) and manifest,
+        f"file_view: manifest must be a non-empty string (given: {manifest!r})",
+    )
+    validate(
+        isinstance(path, str) and path,
+        f"file_view: path must be a non-empty string (given: {path!r})",
+    )
+
+    details = await select(
+        bucket,
+        manifest,
+        f"""
+        SELECT s.physical_keys[0] as physical_key, s."size", s.hash."value" as hash, s.meta
+        FROM s3object s
+        WHERE s.logical_key = '{sql_escape(path)}'
+        LIMIT 1
+        """,
+    )
+    return FileView(**json.load(details)) if details is not None else None
 
 
-@api(cors_origins=get_default_origins())
-@validate(SCHEMA)
-def lambda_handler(request):
+@dataclasses.dataclass
+class DirView:
+    total: int
+    prefixes: T.List[dict]  # {logical_key: str, size: float}
+    objects: T.List[dict]  # {logical_key: str, size: float, physical_key: str}
+    meta: dict
+
+
+async def dir_view(
+    bucket: str,
+    manifest: str,
+    path: T.Optional[str],
+    limit: T.Optional[int] = None,
+    offset: T.Optional[int] = None,
+) -> DirView:
+    validate(
+        isinstance(bucket, str) and bucket,
+        f"dir_view: bucket must be a non-empty string (given: {bucket!r})",
+    )
+    validate(
+        isinstance(manifest, str) and manifest,
+        f"dir_view: manifest must be a non-empty string (given: {manifest!r})",
+    )
+    validate(
+        path is None or isinstance(path, str),
+        f"dir_view: path must be a string if provided (given: {path!r})",
+    )
+    validate(
+        limit is None or isinstance(limit, int) and limit > 0,
+        f"dir_view: limit must be a positive int if provided (given: {limit!r})",
+    )
+    validate(
+        offset is None or isinstance(offset, int) and offset >= 0,
+        f"dir_view: offset must be a non-negative int if provided (given: {offset!r})",
+    )
+
+    if limit is None:
+        limit = 1000
+    if offset is None:
+        offset = 0
+
+    meta = asyncio.create_task(select_meta(bucket, manifest, path))
+
+    # Call s3 select to fetch only logical keys matching the desired prefix (folder path)
+    prefix_length = len(path) if path is not None else 0
+    sql_stmt = \
+        f"""
+        SELECT
+            SUBSTRING(s.logical_key, {prefix_length + 1}) as logical_key,
+            s."size",
+            s.physical_keys[0] as physical_key
+        FROM s3object s
+        """
+
+    if path:
+        sql_stmt += f" WHERE SUBSTRING(s.logical_key, 1, {prefix_length}) = '{sql_escape(path)}'"
+
+    result = await select(bucket, manifest, sql_stmt)
+
+    # Parse the response into a logical folder view
+    if result is not None:
+        df = pd.read_json(
+            result,
+            lines=True,
+            dtype=dict(logical_key="string", physical_key="string"),
+        )
+    else:
+        df = pd.DataFrame()
+
+    return DirView(
+        **file_list_to_folder(df, limit, offset),
+        meta=await meta,
+    )
+
+
+actions = {
+    "file": file_view,
+    "dir": dir_view,
+}
+
+
+def lambda_handler(evt, _ctx):
     """
     Parse a manifest to return a folder-like view of its contents (logical keys).
-
-    Returns:
-        JSON response
+    Payload format:
+        bucket: str
+        manifest: str
+        action: see actions mapping
+        params: see *_view functions
+    Returns: {result} or {error} (see *_view functions for result format)
     """
-    bucket = request.args['bucket']
-    key = request.args['manifest']
-    prefix = request.args.get('prefix')
-    logical_key = request.args.get('logical_key')
-    access_key = request.args.get('access_key')
-    secret_key = request.args.get('secret_key')
-    session_token = request.args.get('session_token')
-    limit = request.args.get('limit', 1000)
-    offset = request.args.get('offset', 0)
-    allow_anonymous_access = bool(os.getenv('ALLOW_ANONYMOUS_ACCESS'))
-
-    # If credentials are passed in, use them
-    # for the client. If no credentials are supplied, test that
-    # the manifest object is publicly accessible. If so, create
-    # an s3 client using the underlying IAM role's permissions.
-
-    if access_key and secret_key and session_token:
-        s3_client = create_s3_client(
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key,
-            aws_session_token=session_token
-        )
-    elif (
-        allow_anonymous_access and
-        access_key is None and
-        secret_key is None and
-        session_token is None
-    ):
-        # Test to see if the target key is publicly accessible. If not, the call
-        # below will raise and exception and return a 403 response
-        anons3 = boto3.client(
-            's3',
-            config=botocore.client.Config(signature_version=botocore.UNSIGNED)
-        )
-        try:
-            anons3.head_object(Bucket=bucket, Key=key)
-        except botocore.exceptions.ClientError as error:
-            if error.response.get('Error'):
-                code = error.response['Error']['Code']
-                if code == '403':
-                    return make_json_response(
-                        403,
-                        {
-                            'title': 'Access Denied',
-                            'detail': f"Access denied reading manifest: {key}"
-                        }
-                    )
-            raise error
-
-        # Use the default S3 client configuration
-        s3_client = boto3.client('s3')
-    else:
-        return make_json_response(
-            401,
-            {
-                'title': 'Incomplete credentials',
-                'detail': "access_key, secret_key and session_token are required"
-            }
-        )
-    assert s3_client
-
-    # Get details of a single file in the package
-    if logical_key is not None:
-        sql_stmt = f"SELECT s.* FROM s3object s WHERE s.logical_key = '{sql_escape(logical_key)}' LIMIT 1"
-        details = query_manifest_content(
-            s3_client,
-            bucket=bucket,
-            key=key,
-            sql_stmt=sql_stmt
-        )
-        if details is not None:
-            response_data = json.load(details)
-        else:
-            return make_json_response(
-                404,
-                {
-                    'title': 'Key Not Found',
-                    'detail': f"key, {logical_key} not found in package"
-                }
-            )
-    else:
-        # Call s3 select to fetch only logical keys matching the
-        # desired prefix (folder path)
-        prefix_length = len(prefix) if prefix is not None else 0
-        sql_stmt = (
-            f"SELECT SUBSTRING(s.logical_key, {prefix_length + 1}) AS logical_key"
-            ", s.\"size\", s.physical_keys[0] as physical_key FROM s3object s"
-        )
-        if prefix:
-            sql_stmt += f" WHERE SUBSTRING(s.logical_key, 1, {prefix_length}) = '{sql_escape(prefix)}'"
-        result = query_manifest_content(
-            s3_client,
-            bucket=bucket,
-            key=key,
-            sql_stmt=sql_stmt
+    try:
+        action = evt.get("action")
+        validate(
+            action in actions,
+            f"action must be one of: {', '.join(actions)} (given: {action!r})",
         )
 
-        # Parse the response into a logical folder view
-        if result is not None:
-            df = pd.read_json(
-                result,
-                lines=True,
-                dtype=dict(
-                    logical_key='string',
-                    physical_key='string'
-                )
-            )
-        else:
-            df = pd.DataFrame()
-        response_data = file_list_to_folder(df, limit, offset)
+        result = asyncio.run(actions[action](
+            evt.get("bucket"),
+            evt.get("manifest"),
+            **evt.get("params", {}),
+        ))
+        return {"result": dataclasses.asdict(result) if result is not None else None}
 
-        # Fetch package-level or directory-level metadata
-        if prefix:
-            sql_stmt = f"SELECT s.meta FROM s3object s WHERE s.logical_key = '{sql_escape(prefix)}'"
-        else:
-            sql_stmt = "SELECT s.* FROM s3object s WHERE s.logical_key is NULL"
-        result = query_manifest_content(
-            s3_client,
-            bucket=bucket,
-            key=key,
-            sql_stmt=sql_stmt
-        )
-        meta = json.load(result) if result else {}
-        response_data.update(dict(meta=meta))
-
-    ret_val = make_json_response(
-        200,
-        {
-            'contents': response_data
-        }
-    )
-
-    return ret_val
+    except PkgselectException as ex:
+        return {"error": str(ex)}
