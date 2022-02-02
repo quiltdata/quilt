b27b539a35bd049bd0816d1c63e46044dc09cbaf
Maxim Chervonny <fiskus@chervonny.ru>
2022-02-02 16:49:52 +0300

    add desktop mode, it loads config as js object

--- catalog/app/utils/Config.js
+++ catalog/app/utils/Config.js
@@ -33,8 +33,10 @@ const parseJSON = (msg = 'invalid JSON') =>
     throw new ConfigError(`${msg}:\n${src}`, { src, originalError: e })
   })
 
-const fetchConfig = async (path) => {
+const fetchConfig = async ({ path, opts = {} }) => {
   try {
+    if (opts.desktop) return opts
+
     const res = await fetch(path)
     const text = await res.text()
     if (!res.ok) {
@@ -46,6 +48,7 @@ const fetchConfig = async (path) => {
     return R.pipe(
       parseJSON(`invalid config JSON at "${path}"`),
       validate('Config', `invalid config format at "${path}"`),
+      R.merge(opts),
     )(text)
   } catch (e) {
     if (!(e instanceof ConfigError)) {
@@ -82,8 +85,8 @@ const ConfigResource = Cache.createResource({
 
 const Ctx = React.createContext()
 
-export function ConfigProvider({ path, children }) {
-  return <Ctx.Provider value={path}>{children}</Ctx.Provider>
+export function ConfigProvider({ path, opts, children }) {
+  return <Ctx.Provider value={{ path, opts }}>{children}</Ctx.Provider>
 }
 
 export function useConfig({ suspend = true } = {}) {
