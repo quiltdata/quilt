9f33a7c6ccd1c0ab5fee8e0a54fac3b602aff87a
Maxim Chervonny <fiskus@chervonny.ru>
2022-02-09 16:12:09 +0300

    use Tabular loader for other tabular types

--- catalog/app/components/Preview/loaders/Tabular.js
+++ catalog/app/components/Preview/loaders/Tabular.js
@@ -1,17 +1,39 @@
 import * as Papa from 'papaparse'
 import * as R from 'ramda'
+import * as React from 'react'
 
+import { HTTPError } from 'utils/APIConnector'
 import * as AWS from 'utils/AWS'
 import * as Config from 'utils/Config'
 import * as Data from 'utils/Data'
 import { mkSearch } from 'utils/NamedRoutes'
 
+import * as Excel from './Excel'
+import * as Fcs from './Fcs'
+import * as Parquet from './Parquet'
+import * as Vcf from './Vcf'
 import { PreviewData } from '../types'
 import * as utils from './utils'
 
 export const MAX_BYTES = 10 * 1024
 
-export const detect = R.pipe(utils.stripCompression, utils.extIn(['.csv', '.tsv']))
+export const detect = R.anyPass([
+  Excel.detect,
+  Fcs.detect,
+  Parquet.detect,
+  Vcf.detect,
+  R.pipe(utils.stripCompression, utils.extIn(['.csv', '.tsv'])),
+])
+
+// TODO: ['ipynb', 'bed']
+const detectTabularType = R.cond([
+  [Excel.detect, R.always('excel')],
+  [Fcs.detect, R.always('fcs')],
+  [Parquet.detect, R.always('parquet')],
+  [Vcf.detect, R.always('vcf')],
+  [R.pipe(utils.stripCompression, utils.extIn(['.csv', '.tsv'])), R.always('csv')],
+  [R.T, R.always('txt')],
+])
 
 // [[...row1], [...row2]] → { a: [...rowA], b: [...rowB]}
 function makeHeadedTable(parsed) {
@@ -49,10 +71,11 @@ const loadTabularData = async ({ endpoint, handle, sign, type }) => {
   }
 }
 
-export const Loader = function CsvLoader({ handle, children }) {
+export const Loader = function TabularLoader({ handle, children }) {
   const endpoint = Config.use().binaryApiGatewayEndpoint
   const sign = AWS.Signer.useS3Signer()
-  const data = Data.use(loadTabularData, { endpoint, handle, sign, type: 'csv' })
+  const type = React.useMemo(() => detectTabularType(handle.key), [handle.key])
+  const data = Data.use(loadTabularData, { endpoint, handle, sign, type })
   const processed = utils.useProcessing(data.result, (csv) =>
     PreviewData.Perspective({
       data: makeHeadedTable(Papa.parse(csv).data),
