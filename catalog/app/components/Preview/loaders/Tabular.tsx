8848368a1cec344cc6c4976b3bc71adda87d6b76
Maxim Chervonny <fiskus@chervonny.ru>
2022-02-09 16:37:39 +0300

    convert Tabular loader to typescript

--- catalog/app/components/Preview/loaders/Tabular.tsx
+++ catalog/app/components/Preview/loaders/Tabular.tsx
@@ -7,6 +7,7 @@ import * as AWS from 'utils/AWS'
 import * as Config from 'utils/Config'
 import * as Data from 'utils/Data'
 import { mkSearch } from 'utils/NamedRoutes'
+import type { S3HandleBase } from 'utils/s3paths'
 
 import * as Csv from './Csv'
 import * as Excel from './Excel'
@@ -26,8 +27,9 @@ export const detect = R.anyPass([
   Vcf.detect,
 ])
 
+type TabularType = 'csv' | 'excel' | 'fcs' | 'parquet' | 'vcf' | 'txt'
 // TODO: ['ipynb', 'bed']
-const detectTabularType = R.cond([
+const detectTabularType: (type: string) => TabularType = R.cond([
   [Csv.detect, R.always('csv')],
   [Excel.detect, R.always('excel')],
   [Fcs.detect, R.always('fcs')],
@@ -36,8 +38,11 @@ const detectTabularType = R.cond([
   [R.T, R.always('txt')],
 ])
 
+type CellType = string | number
+
 // [[...row1], [...row2]] → { a: [...rowA], b: [...rowB]}
-function makeHeadedTable(parsed) {
+function makeHeadedTable(parsed: CellType[][]) {
+  const perpectiveInput: Record<string, CellType[]> = {}
   return parsed.reduce((memo, row, index) => {
     if (index === 0) {
       return row.reduce((memoHead, title) => {
@@ -51,10 +56,17 @@ function makeHeadedTable(parsed) {
       memoCells[title].push(value)
       return memoCells
     }, memo)
-  }, {})
+  }, perpectiveInput)
 }
 
-const loadTabularData = async ({ endpoint, handle, sign, type }) => {
+interface LoadTabularDataArgs {
+  endpoint: string
+  handle: S3HandleBase
+  sign: (h: S3HandleBase) => string
+  type: TabularType
+}
+
+const loadTabularData = async ({ endpoint, handle, sign, type }: LoadTabularDataArgs) => {
   const url = sign(handle)
   const r = await fetch(`${endpoint}/tabular-preview${mkSearch({ url, input: type })}`)
   try {
@@ -65,21 +77,26 @@ const loadTabularData = async ({ endpoint, handle, sign, type }) => {
     return text
   } catch (e) {
     // eslint-disable-next-line no-console
-    console.warn('Error loading tabular preview', { ...e })
+    console.warn('Error loading tabular preview', e)
     // eslint-disable-next-line no-console
     console.error(e)
     throw e
   }
 }
 
-export const Loader = function TabularLoader({ handle, children }) {
+interface TabularLoaderProps {
+  children: (result: $TSFixMe) => React.ReactNode
+  handle: S3HandleBase
+}
+
+export const Loader = function TabularLoader({ handle, children }: TabularLoaderProps) {
   const endpoint = Config.use().binaryApiGatewayEndpoint
   const sign = AWS.Signer.useS3Signer()
   const type = React.useMemo(() => detectTabularType(handle.key), [handle.key])
   const data = Data.use(loadTabularData, { endpoint, handle, sign, type })
-  const processed = utils.useProcessing(data.result, (csv) =>
+  const processed = utils.useProcessing(data.result, (csv: string) =>
     PreviewData.Perspective({
-      data: makeHeadedTable(Papa.parse(csv).data),
+      data: makeHeadedTable(Papa.parse<CellType[]>(csv).data),
     }),
   )
   return children(utils.useErrorHandling(processed, { handle, retry: data.fetch }))
