01022552769263d6a519fc5c9b0fd797ce15e04e
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2022-02-11 19:48:34 +0500

    some polishing

--- lambdas/tabular_preview/src/t4_lambda_tabular_preview/__init__.py
+++ lambdas/tabular_preview/src/t4_lambda_tabular_preview/__init__.py
@@ -1,106 +1,61 @@
-import csv
 import gzip
 import io
 import json
-import os
 import urllib.request
-from contextlib import redirect_stderr
 from urllib.parse import urlparse
 
 import fsspec
-import pandas
 import pyarrow
 import pyarrow.csv
 import pyarrow.json
 import pyarrow.parquet
 
 from t4_lambda_shared.decorator import api, validate, QUILT_INFO_HEADER
-from t4_lambda_shared.preview import (
-    CATALOG_LIMIT_BYTES,
-    CATALOG_LIMIT_LINES,
-    TRUNCATED,
-    extract_excel,
-    extract_fcs,
-    extract_parquet,
-    get_bytes,
-    get_preview_lines,
-    remove_pandas_footer,
-)
 from t4_lambda_shared.utils import get_default_origins, make_json_response
 
-# Number of bytes for read routines like decompress() and
-# response.content.iter_content()
-CHUNK = 1024*8
 # We can pump a max of 6MB out of Lambda
 LAMBDA_MAX_OUT = 6_000_000
-MIN_VCF_COLS = 8  # per 4.2 spec on header and data lines
-
-S3_DOMAIN_SUFFIX = '.amazonaws.com'
-
-FILE_EXTENSIONS = ["csv", "excel", "fcs", "ipynb", "parquet", "vcf"]
-# BED https://genome.ucsc.edu/FAQ/FAQformat.html#format1
-TEXT_TYPES = ["bed", "txt"]
-FILE_EXTENSIONS.extend(TEXT_TYPES)
-
-EXTRACT_PARQUET_MAX_BYTES = 10_000
-
 LAMBDA_MAX_OUT_BINARY = 4_000_000
+
+# TODO: comment about batch_size.
+CSV_OUT_BATCH_SIZE = 100
+PYARROW_CSV_WRITE_OPTIONS = pyarrow.csv.WriteOptions(batch_size=CSV_OUT_BATCH_SIZE)
+
+S3_DOMAIN_SUFFIX = ".amazonaws.com"
+
 OUTPUT_SIZES = {
-    "small": 1_000_000,
-    "medium": 5_000_000,
-    "large": 150_000_000,
+    "small": 100_000,
+    "medium": 500_000,
+    "large": None,
 }
 
 SCHEMA = {
-    'type': 'object',
-    'properties': {
-        'url': {
-            'type': 'string'
+    "type": "object",
+    "properties": {
+        "url": {
+            "type": "string"
         },
-        # separator for CSV files
-        'sep': {
-            'minLength': 1,
-            'maxLength': 1
+        # # separator for CSV files
+        # 'sep': {
+        #     'minLength': 1,
+        #     'maxLength': 1
+        # },
+        # 'max_bytes': {
+        #     'type': 'string',
+        # },
+        "input": {
+            "enum": ["csv", "excel", "parquet"]
         },
-        'max_bytes': {
-            'type': 'string',
-        },
-        # line_count used to be an integer with a max and min, which is more correct
-        # nevertheless, request.args has it as a string, even if
-        # the request specifies it as an integer
-        'line_count': {
-            'type': 'string',
-        },
-        'input': {
-            'enum': FILE_EXTENSIONS
-        },
-        'compression': {
-            'enum': ['gz']
+        "compression": {
+            "enum": ["gz"]
         },
         "size": {
             "enum": list(OUTPUT_SIZES),
         },
     },
-    'required': ['url', 'input'],
-    'additionalProperties': False
+    "required": ["url", "input"],
+    "additionalProperties": False
 }
-
-
-# TODO: remove
-class CSVFormatter:
-    """
-    `csv` module doesn't provide anything like that ¯\_(ツ)_/¯.
-    """
-    def __init__(self, **fmtparams):
-        self._buf = io.StringIO()
-        self._writer = csv.writer(self._buf, **fmtparams)
-
-    def format(self, row):
-        self._writer.writerow(row)
-        formatted = self._buf.getvalue().encode()
-        self._buf.seek(0)
-        self._buf.truncate()
-        return formatted
 
 
 def urlopen_seekable(url):
@@ -111,7 +66,7 @@ class GzipOutputBuffer(gzip.GzipFile):
     class Full(Exception):
         pass
 
-    def __init__(self, compressed_max_size: int, max_size:int, *args, **kwargs):
+    def __init__(self, compressed_max_size: int, max_size: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.compressed_max_size = compressed_max_size
         self.max_size = max_size
@@ -130,38 +85,21 @@ class GzipOutputBuffer(gzip.GzipFile):
         return super().write(data)
 
 
-# TODO: remove
-def write_csv(rows):
-    # TODO: fmt paramste
-    formatter = CSVFormatter()
-    buf = io.BytesIO()
-    with gzip.open(buf, 'wb') as out:
-        for row in map(formatter.format, rows):
-            # TODO: left a comment
-            if buf.tell() + len(row) > LAMBDA_MAX_OUT_BINARY:
-                break
-            out.write(row)
-
-    return buf.getvalue()
-
-
-PYARROW_WRITE_OPTIONS = pyarrow.csv.WriteOptions(batch_size=100)
-
-
 def preview_csv(src, out):
     truncated = False
+    # TODO: rework this `+ 1_000_000` hack needed for `truncated` flag.
+    max_input_size = out.max_size + 1_000_000 if out.max_size else 150_000_000
     with src:
         # TODO: comment about max_size.
         reader = pyarrow.csv.open_csv(
             src,
-            # TODO: rework this `+ 1_000_000` hack needed for `truncated` flag.
-            read_options=pyarrow.csv.ReadOptions(block_size=out.max_size + 1_000_000)
+            read_options=pyarrow.csv.ReadOptions(block_size=max_input_size)
         )
         batch = next(iter(reader), None)
     with out:
         if batch:
             try:
-                pyarrow.csv.write_csv(batch, out, write_options=PYARROW_WRITE_OPTIONS)
+                pyarrow.csv.write_csv(batch, out, write_options=PYARROW_CSV_WRITE_OPTIONS)
             except out.Full:
                 truncated = True
 
@@ -171,12 +109,17 @@ def preview_csv(src, out):
 
 
 def preview_excel(src, out):
+    # Importing pandas here, because it's quite slow (150-200 msec when benchmarked locally).
+    import pandas
+
     truncated = False
     with src:
         df = pandas.read_excel(io.BytesIO(src.read()), sheet_name=0)
     with out:
         try:
-            df.to_csv(out)
+            # pyarrow.csv.write_csv(pyarrow.Table.from_pandas(df), ...) works faster,
+            # but conversion to pyarrow.Table fails for some files.
+            df.to_csv(out, chunksize=CSV_OUT_BATCH_SIZE)
         except out.Full:
             truncated = True
 
@@ -191,11 +134,12 @@ def preview_parquet(src, out):
         parquet_file = pyarrow.parquet.ParquetFile(src)
         meta = parquet_file.metadata
         with out:
-            for batch in parquet_file.iter_batches():
-                try:
-                    pyarrow.csv.write_csv(batch, out, write_options=PYARROW_WRITE_OPTIONS)
-                except out.Full:
-                    truncated = True
+            with pyarrow.csv.CSVWriter(out, parquet_file.schema_arrow, write_options=PYARROW_CSV_WRITE_OPTIONS) as w:
+                for batch in parquet_file.iter_batches():
+                    try:
+                        w.write_batch(batch)
+                    except out.Full:
+                        truncated = True
 
     return {
         "truncated": truncated,
@@ -264,60 +208,3 @@ def lambda_handler(request):
         "Content-Encoding": "gzip",
         QUILT_INFO_HEADER: json.dumps(info),
     }
-
-
-def extract_vcf(head):
-    """
-    Pull summary info from VCF: meta-information, header line, and data lines
-    VCF file format: https://github.com/samtools/hts-specs/blob/master/VCFv4.3.pdf
-
-    Args:
-        array of first few lines of file
-    Returns:
-        dict
-    """
-    meta = []
-    header = None
-    data = []
-    variants = []
-    limit = MIN_VCF_COLS + 1  # +1 to get the FORMAT column
-    for line in head:
-        if line.startswith('##'):
-            meta.append(line)
-        elif line.startswith('#'):
-            if header:
-                print('Unexpected multiple headers:', header)
-            header = line
-            columns = header.split()  # VCF is tab-delimited
-            # only grab first "limit"-many rows
-            header = columns[:limit]
-            variants = columns[limit:]
-        elif line:
-            columns = line.split()[:limit]
-            data.append(columns)
-    info = {
-        'data': {
-            'meta': meta,
-            'header': header,
-            'data': data
-        },
-        'metadata': {
-            'variants': variants,
-            'variant_count': len(variants)
-        }
-    }
-
-    return '', info
-
-
-def _str_to_line_count(int_string, lower=1, upper=CATALOG_LIMIT_LINES):
-    """
-    validates an integer string
-
-    Raises: ValueError
-    """
-    integer = int(int_string)
-    if integer < lower or integer > upper:
-        raise ValueError(f'{integer} out of range: [{lower}, {upper}]')
-
-    return integer
