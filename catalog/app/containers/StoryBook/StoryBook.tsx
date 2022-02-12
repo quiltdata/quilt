eda46e6f4f2bec7c726a25c89cf95986d7877b45
Maxim Chervonny <fiskus@chervonny.ru>
2022-02-12 14:04:51 +0300

    play with ngl

--- catalog/app/containers/StoryBook/StoryBook.tsx
+++ catalog/app/containers/StoryBook/StoryBook.tsx
@@ -2,8 +2,20 @@ import * as React from 'react'
 
 import Layout from 'components/Layout'
 
-import JsonEditorBook from './JsonEditor'
+// import JsonEditorBook from './JsonEditor'
+import NglBook from './Ngl'
 
 export default function StoryBook() {
-  return <Layout pre={<JsonEditorBook />} />
+  return (
+    <Layout
+      pre={
+        <>
+          <NglBook url="s3://fiskus-sandbox-dev/structure.pdb" />
+          <NglBook url="s3://fiskus-sandbox-dev/1crn.cif" />
+          <NglBook url="s3://fiskus-sandbox-dev/1crn.pdb" />
+        </>
+      }
+    />
+  )
+  // return <Layout pre={<JsonEditorBook />} />
 }
