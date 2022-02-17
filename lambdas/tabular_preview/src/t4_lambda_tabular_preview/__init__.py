7006e82139ccec3160b79b9b6e501e4c32e167d9
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2022-02-17 19:14:54 +0500

    rework

--- lambdas/tabular_preview/src/t4_lambda_tabular_preview/__init__.py
+++ lambdas/tabular_preview/src/t4_lambda_tabular_preview/__init__.py
@@ -5,6 +5,7 @@ import urllib.request
 from urllib.parse import urlparse
 
 import fsspec
+import pandas
 import pyarrow
 import pyarrow.csv
 import pyarrow.json
@@ -13,14 +14,13 @@ import pyarrow.parquet
 from t4_lambda_shared.decorator import api, validate, QUILT_INFO_HEADER
 from t4_lambda_shared.utils import get_default_origins, make_json_response
 
-# We can pump a max of 6MB out of Lambda
-LAMBDA_MAX_OUT = 6_000_000
+
 LAMBDA_MAX_OUT_BINARY = 4_000_000
+
+MAX_INPUT_CSV = 150_000_000
 
 # TODO: comment about batch_size.
 OUT_BATCH_SIZE = 100
-CSV_OUT_BATCH_SIZE = 100
-PYARROW_CSV_WRITE_OPTIONS = pyarrow.csv.WriteOptions(batch_size=CSV_OUT_BATCH_SIZE)
 
 S3_DOMAIN_SUFFIX = ".amazonaws.com"
 
@@ -30,8 +30,27 @@ OUTPUT_SIZES = {
     "large": None,
 }
 
-def urlopen_seekable(url):
-    return fsspec.open(url).open()
+
+def urlopen(url: str, *, compression: str, seekable: bool = False):
+    # urllib's urlopen() works faster than fsspec, but is not seekable.
+    raw = fsspec.open(url).open() if seekable else urllib.request.urlopen(url)
+    if compression is None:
+        uncompressed = raw
+    elif compression == "gz":
+        uncompressed = pyarrow.CompressedInputStream(raw, "gzip")
+    else:
+        assert False
+
+    return uncompressed
+
+
+def read_lines(src, max_bytes):
+    data = src.read(max_bytes)
+    next_byte = src.read(1)
+    if not next_byte:
+        return data, False
+    data = data.rpartition(b'\n')[0]
+    return data, True
 
 
 class GzipOutputBuffer(gzip.GzipFile):
@@ -59,7 +78,10 @@ class GzipOutputBuffer(gzip.GzipFile):
 
 def write_data_as_arrow(data, schema, max_size):
     if isinstance(data, pyarrow.Table):
+        print(data.num_rows)
         data = data.to_batches(OUT_BATCH_SIZE)
+
+    print([pyarrow.ipc.get_record_batch_size(batch) for batch in data])
 
     truncated = False
     buf = pyarrow.BufferOutputStream()
@@ -67,6 +89,8 @@ def write_data_as_arrow(data, schema, max_size):
         with pyarrow.ipc.new_file(sink, schema) as writer:
             for batch in data:
                 batch_size = pyarrow.ipc.get_record_batch_size(batch)
+                print(batch_size)
+                print(sink.tell())
                 if (
                     (max_size is not None and sink.tell() + batch_size > max_size) or
                     # TODO: comment
@@ -79,110 +103,115 @@ def write_data_as_arrow(data, schema, max_size):
     return memoryview(buf.getvalue()), truncated
 
 
-def write_pandas_as_csv(df, out):
-    with out:
+def write_pandas_as_csv(df, max_size):
+    truncated = False
+    buf = io.BytesIO()
+    with GzipOutputBuffer(
+        compressed_max_size=LAMBDA_MAX_OUT_BINARY,
+        max_size=max_size,
+        fileobj=buf,
+        mode="wb",
+    ) as out:
         try:
             # pyarrow.csv.write_csv(pyarrow.Table.from_pandas(df), ...) works faster,
             # but conversion to pyarrow.Table fails for some files.
-            df.to_csv(out, chunksize=CSV_OUT_BATCH_SIZE, index=False)
+            df.to_csv(out, chunksize=OUT_BATCH_SIZE, index=False)
         except out.Full:
             truncated = True
 
-    return truncated
+    return buf.getvalue(), truncated
 
 
-def preview_csv(src, out):
-    truncated = False
-    # TODO: rework this `+ 1_000_000` hack needed for `truncated` flag.
-    max_input_size = out.max_size + 1_000_000 if out.max_size else 150_000_000
-    with src:
-        # TODO: comment about max_size.
-        reader = pyarrow.csv.open_csv(
-            src,
-            read_options=pyarrow.csv.ReadOptions(block_size=max_input_size)
-        )
-        batch = next(iter(reader), None)
+def preview_csv(url, compression, max_out_size):
+    with urlopen(url, compression=compression) as src:
+        max_input_size = max_out_size or MAX_INPUT_CSV
+        input_data, input_truncated = read_lines(src, max_input_size)
 
-    t = pyarrow.Table.from_batches((batch,))
-    data, truncated = write_data_as_arrow(t, t.schema, 10_000_000)
+    t = pyarrow.csv.read_csv(pyarrow.BufferReader(input_data))
+    output_data, output_truncated = write_data_as_arrow(t, t.schema, max_out_size)
 
-    return data, {
-        "truncated": truncated,
+    return 200, output_data, {
+        "Content-Type": "application/vnd.apache.arrow.file",
+        "Content-Encoding": "gzip",
+        QUILT_INFO_HEADER: json.dumps(
+            {
+                "truncated": input_truncated or output_truncated,
+            }
+        ),
     }
 
 
-def preview_excel(src, out):
-    # Importing pandas here, because it's quite slow (150-200 msec when benchmarked locally).
-    import pandas
+def preview_jsonl(url, compression, max_out_size):
+    with urlopen(url, compression=compression) as src:
+        # TODO: comment
+        max_input_size = int((max_out_size or MAX_INPUT_CSV) * 1.5)
+        input_data, input_truncated = read_lines(src, max_input_size)
+    df = pandas.read_json(io.BytesIO(input_data), lines=True)
+    output_data, output_truncated = write_pandas_as_csv(df, max_out_size)
 
-    with src:
+    return 200, output_data, {
+        "Content-Type": "text/csv",
+        "Content-Encoding": "gzip",
+        QUILT_INFO_HEADER: json.dumps(
+            {
+                "truncated": input_truncated or output_truncated,
+            }
+        ),
+    }
+
+
+def preview_excel(url, compression, max_out_size):
+    with urlopen(url, compression=compression) as src:
         data = src.read()
     df = pandas.read_excel(data)
-    truncated = write_pandas_as_csv(df, out)
+    output_data, output_truncated = write_pandas_as_csv(df, max_out_size)
 
-    return {
-        "truncated": truncated,
+    return 200, output_data, {
+        "Content-Type": "text/csv",
+        "Content-Encoding": "gzip",
+        QUILT_INFO_HEADER: json.dumps(
+            {
+                "truncated": output_truncated,
+            }
+        ),
     }
 
 
-def preview_parquet(src, out):
-    with src:
+def preview_parquet(url, compression, max_out_size):
+    with urlopen(url, compression=compression, seekable=True) as src:
         parquet_file = pyarrow.parquet.ParquetFile(src)
         meta = parquet_file.metadata
-        with out:
-            data, truncated = write_data_as_arrow(parquet_file.iter_batches(OUT_BATCH_SIZE), parquet_file.schema_arrow)
+        df = parquet_file.read().to_pandas()
 
-    return {
-        "truncated": truncated,
-        "meta": {
-            "created_by": meta.created_by,
-            "format_version": meta.format_version,
-            "num_row_groups": meta.num_row_groups,
-            "schema": {
-                "names": meta.schema.names
-            },
-            "serialized_size": meta.serialized_size,
-            "shape": (meta.num_rows, meta.num_columns),
-        }
-    }
+    output_data, output_truncated = write_pandas_as_csv(df, max_out_size)
 
-
-def read_lines(src, max_bytes):
-    data = src.read(max_bytes)
-    next_byte = src.read(1)
-    if not next_byte:
-        return data, False
-    data = data.rpartition(b'\n')[0]
-    return data, True
-
-
-def preview_jsonl(src, out):
-    import pandas
-
-    with src:
-        # TODO:
-        data, truncated = read_lines(src, out.max_size + out.max_size // 2)
-    df = pandas.read_json(io.BytesIO(data), lines=True)
-    truncated = truncated or write_pandas_as_csv(df, out)
-    return data, {
-        "truncated": truncated,
+    return 200, output_data, {
+        "Content-Type": "text/csv",
+        "Content-Encoding": "gzip",
+        QUILT_INFO_HEADER: json.dumps(
+            {
+                "truncated": output_truncated,
+                "meta": {
+                    "created_by": meta.created_by,
+                    "format_version": meta.format_version,
+                    "num_row_groups": meta.num_row_groups,
+                    "schema": {
+                        "names": meta.schema.names
+                    },
+                    "serialized_size": meta.serialized_size,
+                    "shape": (meta.num_rows, meta.num_columns),
+                }
+            }
+        ),
     }
 
 
 handlers = {
-    "csv": (urllib.request.urlopen, preview_csv),
-    "excel": (urllib.request.urlopen, preview_excel),
-    "parquet": (urlopen_seekable, preview_parquet),
-    "jsonl": (urllib.request.urlopen, preview_jsonl),
+    "csv": preview_csv,
+    "excel": preview_excel,
+    "parquet": preview_parquet,
+    "jsonl": preview_jsonl,
 }
-
-
-# handlers = {
-#     "csv": preview_csv,
-#     "excel": preview_excel,
-#     "parquet": preview_parquet,
-#     "jsonl": preview_jsonl,
-# }
 
 SCHEMA = {
     "type": "object",
@@ -205,6 +234,16 @@ SCHEMA = {
 }
 
 
+def is_s3_url(url: str) -> bool:
+    parsed_url = urlparse(url, allow_fragments=False)
+    return (
+        parsed_url.scheme == "https" and
+        parsed_url.netloc.endswith(S3_DOMAIN_SUFFIX) and
+        parsed_url.username is None and
+        parsed_url.password is None
+    )
+
+
 @api(cors_origins=get_default_origins())
 @validate(SCHEMA)
 def lambda_handler(request):
@@ -213,30 +252,10 @@ def lambda_handler(request):
     output_size = request.args.get("size", "small")
     compression = request.args.get("compression")
 
-    parsed_url = urlparse(url, allow_fragments=False)
-    if not (parsed_url.scheme == "https" and
-            parsed_url.netloc.endswith(S3_DOMAIN_SUFFIX) and
-            parsed_url.username is None and
-            parsed_url.password is None):
+    if not is_s3_url(url):
         return make_json_response(400, {
             "title": "Invalid url=. Expected S3 virtual-host URL."
         })
 
-    urlopener, handler = handlers[input_type]
-    src = urlopener(url)
-    if compression == "gz":
-        src = pyarrow.CompressedInputStream(src, "gzip")
-    buf = io.BytesIO()
-    out = GzipOutputBuffer(
-        compressed_max_size=LAMBDA_MAX_OUT_BINARY,
-        max_size=OUTPUT_SIZES[output_size],
-        fileobj=buf,
-        mode="wb",
-    )
-    data, info = handler(src, out)
-
-    return 200, data, {
-        "Content-Type": "application/vnd.apache.arrow.file",
-        "Content-Encoding": "gzip",
-        QUILT_INFO_HEADER: json.dumps(info),
-    }
+    handler = handlers[input_type]
+    return handler(url, compression, OUTPUT_SIZES[output_size])
