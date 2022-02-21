e833a261a5d2cd761d8637feb39201da123fac69
Maxim Chervonny <fiskus@chervonny.ru>
2022-02-21 16:47:51 +0300

    convert to ts

--- catalog/app/utils/perspective.ts
+++ catalog/app/utils/perspective.ts
@@ -3,25 +3,39 @@ import * as React from 'react'
 import 'utils/perspective-pollution'
 
 import perspective from '@finos/perspective'
+import type { Table } from '@finos/perspective'
+import type { HTMLPerspectiveViewerElement } from '@finos/perspective-viewer'
 
 const worker = perspective.worker()
 
-export function renderViewer(parentNode, { className }) {
+export function renderViewer(
+  parentNode: HTMLElement,
+  { className }: React.HTMLAttributes<HTMLDivElement>,
+): HTMLPerspectiveViewerElement {
   const element = document.createElement('perspective-viewer')
-  element.className = className
+  if (className) {
+    element.className = className
+  }
   parentNode.appendChild(element)
   return element
 }
 
-export async function renderTable(data, viewer) {
+export async function renderTable(
+  data: string | ArrayBuffer,
+  viewer: HTMLPerspectiveViewerElement,
+) {
   const table = await worker.table(data)
   viewer.load(table)
   return table
 }
 
-function usePerspective(container, data, attrs) {
+function usePerspective(
+  container: HTMLDivElement | null,
+  data: string | ArrayBuffer,
+  attrs: React.HTMLAttributes<HTMLDivElement>,
+) {
   React.useEffect(() => {
-    let table, viewer
+    let table: Table, viewer: HTMLPerspectiveViewerElement
 
     async function fetchData() {
       if (!container) return
@@ -31,10 +45,14 @@ function usePerspective(container, data, attrs) {
     }
     fetchData()
 
-    return async () => {
+    async function disposeTable() {
       viewer?.parentNode?.removeChild(viewer)
       await viewer?.delete()
       await table?.delete()
+    }
+
+    return () => {
+      disposeTable()
     }
   }, [attrs, container, data])
 }
