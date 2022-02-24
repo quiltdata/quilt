512ee2d08698992a59ef673be7c223a93a191c6a
Maxim Chervonny <fiskus@chervonny.ru>
2022-02-24 18:22:32 +0300

    restore Parquet loader and renderer

--- catalog/app/components/Preview/types.js
+++ catalog/app/components/Preview/types.js
@@ -1,6 +1,22 @@
 import tagged from 'utils/tagged'
 
 /*
+ParquetMeta: {
+  createdBy: string,
+  formatVersion: string,
+  metadata: object,
+  numRowGroups: number,
+  schema: Array({
+    path: string,
+    logicalType: string,
+    physicalType: string,
+    maxDefinitionLevel: string,
+    maxRepetitionLevel: string,
+  }),
+  serializedSize: number,
+  shape: { rows: number, columns: number },
+}
+
 PreviewStatus: {
   note: string?,
   warnings: string?,
@@ -17,6 +33,7 @@ export const PreviewData = tagged([
   'Json', // { rendered: object }
   'Markdown', // { rendered: string }
   'Notebook', // { preview: string, ...PreviewStatus }
+  'Parquet', // { preview: string, ...ParquetMeta, ...PreviewStatus }
   'Pdf', // { handle: object, pages: number, firstPageBlob: Blob, type: 'pdf' | 'pptx' }
   'Perspective', // { context: CONTEXT, data: string | ArrayBuffer, handle: S3Handle, onLoadMore: () => void, truncated: boolean }
   'Text', // { head: string, tail: string, lang: string, highlighted: { head: string, tail: string }, ...PreviewStatus }
