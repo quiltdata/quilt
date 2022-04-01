cbb9aef8255af4b204e917db593e6206e7fcccb1
Maxim Chervonny <fiskus@chervonny.ru>
2022-04-01 10:37:54 +0300

    revert change

--- catalog/app/containers/Bucket/requests/requestsUntyped.js
+++ catalog/app/containers/Bucket/requests/requestsUntyped.js
@@ -628,6 +628,8 @@ export const summarize = async ({ s3, handle: inputHandle, resolveLogicalKey }) 
     const manifest = JSON.parse(json)
     const configErrors = isValidManifest(manifest)
     if (configErrors.length) {
+      // eslint-disable-next-line no-console
+      console.error(configErrors[0])
       throw new Error(
         'Invalid manifest: must be a JSON array of files or arrays of files',
       )
