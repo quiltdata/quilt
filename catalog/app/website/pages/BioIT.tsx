94afa90e3707dbbd345276b644ae35cd53876992
Maxim Chervonny <fiskus@chervonny.ru>
2022-04-28 16:40:38 +0300

    update BioIT button text

--- catalog/app/website/pages/BioIT.tsx
+++ catalog/app/website/pages/BioIT.tsx
@@ -135,7 +135,7 @@ function BioIT() {
             </strong>
             <M.Box display="flex" flexDirection="column" pt={6}>
               <M.Button variant="contained" color="primary" onClick={talk}>
-                Book a demo
+                Meet Quilt at Bio IT World
               </M.Button>
             </M.Box>
             <M.Box pt={2} />
