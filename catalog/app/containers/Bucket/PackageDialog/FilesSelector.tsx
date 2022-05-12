d8106ea156cc6b7fe12a94956b3f725ffbe503c0
Maxim Chervonny <fiskus@chervonny.ru>
2022-05-12 18:11:07 +0300

    add s3 overlay for s3 file picker

--- catalog/app/containers/Bucket/PackageDialog/FilesSelector.tsx
+++ catalog/app/containers/Bucket/PackageDialog/FilesSelector.tsx
@@ -161,6 +161,7 @@ export function FilesSelector({
                     data-name={name}
                     faint={!sel}
                     actions={[]}
+                    type="s3"
                   />
                 ),
               )}
