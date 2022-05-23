8345b2132470201efc07d3066f3365f753d00eba
Maxim Chervonny <fiskus@chervonny.ru>
2022-05-23 13:36:43 +0300

    convert V3000 to v2000

--- catalog/app/components/Preview/loaders/Ngl.tsx
+++ catalog/app/components/Preview/loaders/Ngl.tsx
@@ -1,6 +1,7 @@
 import { extname } from 'path'
 
 import type { PromiseResult } from 'aws-sdk/lib/request'
+import { Molecule } from 'openchemlib/minimal'
 import * as R from 'ramda'
 import * as React from 'react'
 import { DecompressorRegistry } from 'ngl'
@@ -10,6 +11,24 @@ import type { S3HandleBase } from 'utils/s3paths'
 import { PreviewData } from '../types'
 
 import * as utils from './utils'
+
+function parseFile(
+  file: string | Uint8Array,
+  handle: S3HandleBase,
+): { file: string | Uint8Array; ext: string } {
+  const ext = extname(utils.stripCompression(handle.key)).substring(1)
+  if (ext !== 'sdf' && ext !== 'mol' && ext !== 'mol2')
+    return {
+      file,
+      ext,
+    }
+  const strFile = file.toString()
+  if (strFile.indexOf('V3000') === -1) return { file, ext }
+  return {
+    file: Molecule.fromMolfile(strFile).toMolfile(),
+    ext: 'mol',
+  }
+}
 
 export const detect = R.pipe(
   utils.stripCompression,
@@ -30,8 +49,8 @@ export const Loader = function NglLoader({ handle, children }: NglLoaderProps) {
     (r: PromiseResult<{ Body: Uint8Array | string }, null>) => {
       const compression = utils.getCompression(handle.key)
       const body = compression === 'gz' ? gzipDecompress(r.Body as string) : r.Body
-      const ext = extname(utils.stripCompression(handle.key)).substring(1)
-      return PreviewData.Ngl({ blob: new Blob([body]), ext })
+      const { file, ext } = parseFile(body, handle)
+      return PreviewData.Ngl({ blob: new Blob([file]), ext })
     },
   )
   const handled = utils.useErrorHandling(processed, { handle, retry: data.fetch })
