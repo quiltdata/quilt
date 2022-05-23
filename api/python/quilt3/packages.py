7f4e9553f7b30384299a6449b6ef53e7316096cd
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2022-05-23 20:54:43 +0500

    try to debug

--- api/python/quilt3/packages.py
+++ api/python/quilt3/packages.py
@@ -1467,7 +1467,7 @@ class Package:
         temp_file_logical_keys = [lk for lk, entry in self.walk() if physical_key_is_temp_file(entry.physical_key)]
         print('!!!')
         print(APP_DIR_TEMPFILE_DIR)
-        print([lk for lk, entry in self.walk()])
+        print(list(self.walk()))
         print(temp_file_logical_keys)
         if temp_file_logical_keys:
             temp_file_physical_keys = [self[lk].physical_key for lk in temp_file_logical_keys]
