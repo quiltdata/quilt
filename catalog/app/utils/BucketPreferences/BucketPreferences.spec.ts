b604e8b21624c4199d6a4688efd08aedcc208603
Maxim Chervonny <fiskus@chervonny.ru>
2022-05-30 16:47:10 +0300

    check overwrites

--- catalog/app/utils/BucketPreferences/BucketPreferences.spec.ts
+++ catalog/app/utils/BucketPreferences/BucketPreferences.spec.ts
@@ -1,3 +1,5 @@
+import dedent from 'dedent'
+
 import { parse } from './BucketPreferences'
 
 const sentryMock = () => {}
@@ -31,6 +33,30 @@ describe('utils/BucketPreferences', () => {
   describe('parse', () => {
     test('Empty config returns default preferences', () => {
       expect(parse('', sentryMock)).toMatchObject(expectedDefaults)
+    })
+
+    test('If one action is overwritten, others should be default', () => {
+      const config = dedent`
+            ui:
+                actions:
+                    copyPackage: False
+      `
+      const result = parse(config, sentryMock)
+      expect(result.ui.actions.copyPackage).toBe(false)
+      expect(result.ui.actions.createPackage).toBe(true)
+      expect(result.ui.actions.deleteRevision).toBe(false)
+    })
+
+    test('If one block is overwritten, others should be default', () => {
+      const config = dedent`
+            ui:
+                blocks:
+                    analytics: False
+      `
+      const result = parse(config, sentryMock)
+      expect(result.ui.blocks.analytics).toBe(false)
+      expect(result.ui.blocks.browser).toBe(true)
+      expect(result.ui.blocks.code).toBe(true)
     })
   })
 
