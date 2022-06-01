d5c574054d1180f1aef05e4d4ba0fe4cb74d0c38
Maxim Chervonny <fiskus@chervonny.ru>
2022-06-01 15:38:54 +0300

    Bucket preferences iteration (#2879)
    Co-authored-by: Alexei Mochalov <nl.imbecile@gmail.com>

---
+++ catalog/app/utils/BucketPreferences/LocalProvider.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+
+import * as Sentry from 'utils/Sentry'
+
+import { BucketPreferences, extendDefaults } from './BucketPreferences'
+
+const localModePreferences = {
+  ui: {
+    actions: {
+      copyPackage: false,
+      createPackage: false,
+      deleteRevision: false,
+      revisePackage: false,
+    },
+    nav: {
+      queries: false,
+    },
+  },
+}
+
+interface LocalProviderProps {
+  context: React.Context<BucketPreferences | null>
+  children: React.ReactNode
+}
+
+export default function LocalProvider({ context: Ctx, children }: LocalProviderProps) {
+  const sentry = Sentry.use()
+  const value = React.useMemo(
+    () => extendDefaults(localModePreferences, sentry),
+    [sentry],
+  )
+  return <Ctx.Provider value={value}>{children}</Ctx.Provider>
+}
