b36fab437720c952b8fa54dccac9d4f5bfd03f48
Maxim Chervonny <fiskus@chervonny.ru>
2022-06-23 18:51:34 +0300

    add parse dummy comment

--- catalog/app/utils/TeleportUri.ts
+++ catalog/app/utils/TeleportUri.ts
@@ -1,5 +1,8 @@
 import * as PackageUri from "./PackageUri";
 
+// TODO: add query
+// export functin parse() {}
+
 export function stringify(packageHandle: PackageUri.PackageUri): string {
   return PackageUri.stringify(packageHandle).replace("quilt+s3", "teleport");
 }
