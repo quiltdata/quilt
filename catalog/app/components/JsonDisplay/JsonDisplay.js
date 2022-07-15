32c5839ae9474ff41f0e2844d40cdd0fdd78f234
Maxim Chervonny <fiskus@chervonny.ru>
2022-07-15 12:41:49 +0300

    fix condition

--- catalog/app/components/JsonDisplay/JsonDisplay.js
+++ catalog/app/components/JsonDisplay/JsonDisplay.js
@@ -93,14 +93,16 @@ function S3UrlValue({ href, children }) {
 function StringValue({ value }) {
   const href = React.useMemo(() => getHref(value), [value])
   if (!href) return <div>"{value}"</div>
-  try {
-    if (s3paths.isS3Url(href)) return <S3UrlValue href={href}>{value}</S3UrlValue>
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log('Invalid url')
-    // eslint-disable-next-line no-console
-    console.error(error)
-    return <div>"{value}"</div>
+  if (s3paths.isS3Url(href)) {
+    try {
+      return <S3UrlValue href={href}>{value}</S3UrlValue>
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log('Invalid url')
+      // eslint-disable-next-line no-console
+      console.error(error)
+      return <div>"{value}"</div>
+    }
   }
   return (
     <div>
