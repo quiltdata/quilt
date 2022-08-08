3d11d56bbc73374e90e063fad0c2b09280ae20d0
Ernest Prabhakar <ernest@quiltdata.io>
2022-08-08 10:08:46 -0700

    Update clean.sh

--- testdocs/clean.sh
+++ testdocs/clean.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
-TEST_FILES=('data.csv' 'test_workflow'	'example.jsonl'	'vega.json')
-
+TEST_FILES=('data' 'notebooks' 'scripts' 'test_workflow' 'data.csv' 'example.jsonl' 'quilt_summarize.json' 'vega.json')
+echo "Removing files and directories created by tests..."
 for x in $TEST_FILES; do
-  echo $x
+  echo " - " $x
   rm -rf $x
 done
