84ef3800ca4168ed0eab94f347f95be615483394
Maxim Chervonny <fiskus@chervonny.ru>
2022-08-10 15:36:49 +0300

    IGV renderer (#2965)
    Co-authored-by: Aneesh Karve <akarve@users.noreply.github.com>

---
+++ catalog/app/components/Preview/renderers/Igv/Igv.tsx
@@ -0,0 +1,62 @@
+import igv from 'igv'
+import * as React from 'react'
+import * as M from '@material-ui/core'
+
+const useStyles = M.makeStyles(() => ({
+  root: {
+    position: 'relative',
+    zIndex: 1,
+  },
+}))
+
+interface IgvEssential {
+  options: igv.IgvBrowserOptions
+}
+
+export interface IgvProps extends React.HTMLProps<HTMLDivElement> {
+  options: igv.IgvBrowserOptions
+}
+
+// XXX: consider using components/EChartsChart (may require some adjustments)
+function Igv({ options, ...props }: IgvProps) {
+  const containerRef = React.useRef<HTMLDivElement | null>(null)
+  const classes = useStyles()
+
+  const [error, setError] = React.useState<Error | null>(null)
+
+  React.useEffect(() => {
+    let browser: igv.IgvBrowser
+    async function initIgv() {
+      if (!containerRef.current) return
+      try {
+        browser = await igv.createBrowser(containerRef.current, options)
+        return () => browser && igv.removeBrowser(browser)
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e)
+        if (e instanceof Error) setError(e)
+        // XXX: should NOT set state in dispose callback
+        return () => setError(null)
+      }
+    }
+    initIgv()
+  }, [containerRef, options])
+
+  if (error)
+    return (
+      <>
+        <M.Typography variant="h6" gutterBottom>
+          Unexpected Error
+        </M.Typography>
+        <M.Typography variant="body1" gutterBottom>
+          Something went wrong while loading preview
+        </M.Typography>
+      </>
+    )
+
+  return <div className={classes.root} ref={containerRef} {...props} />
+}
+
+export default ({ options }: IgvEssential, props: React.HTMLProps<HTMLDivElement>) => (
+  <Igv options={options} {...props} />
+)
