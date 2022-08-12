9cf89feeb0fcfe062ffeec5c048af87b57784d71
Philipp A <flying-sheep@web.de>
2022-08-12 15:33:41 +0200

    Add anndata to extras

--- api/python/setup.py
+++ api/python/setup.py
@@ -73,6 +73,7 @@ setup(
             'pandas>=0.19.2',
             'pyarrow>=0.14.1',              # as of 7/5/19: linux/circleci bugs on 0.14.0
         ],
+        'anndata': ['anndata>=0.8.0'],
         'tests': [
             'numpy>=1.14.0',                # required by pandas, but missing from its dependencies.
             'pandas>=0.19.2',
