22dcf500b81583bdb899b563c5d9879a23312381
Maxim Chervonny <fiskus@chervonny.ru>
2022-08-15 21:45:22 +0300

    wip: show list

--- catalog/app/containers/Bookmarks/Sidebar.tsx
+++ catalog/app/containers/Bookmarks/Sidebar.tsx
@@ -1,28 +1,45 @@
-// import * as R from 'ramda'
 import * as React from 'react'
 import * as M from '@material-ui/core'
 
-// import type * as Model from 'model'
 import type { S3HandleBase } from 'utils/s3paths'
 
 import { useBookmarks } from './Provider'
 
+const useSidebarStyles = M.makeStyles((t) => ({
+  root: {
+    padding: t.spacing(1, 2),
+  },
+}))
+
 export default function Sidebar() {
   const bookmarks = useBookmarks()
+  const classes = useSidebarStyles()
   const entries = bookmarks?.groups.bookmarks?.entries
   const list: S3HandleBase[] = React.useMemo(
     () => (entries ? Object.values(entries) : []),
     [entries],
   )
-  //   const isOpened = bookmarks?.isOpened || true
+  const handleSubmit = React.useCallback(() => {}, [])
   const isOpened = bookmarks?.isOpened
   return (
     <M.Drawer anchor="left" open={isOpened} onClose={bookmarks?.hide}>
-      <M.List>
-        {list.map((file) => (
-          <M.ListItem>{file.key}</M.ListItem>
-        ))}
-      </M.List>
+      <div className={classes.root}>
+        <M.List>
+          {list.map((file) => (
+            <M.ListItem>
+              <M.ListItemIcon>
+                <M.Icon>insert_drive_file</M.Icon>
+              </M.ListItemIcon>
+              <M.ListItemText>
+                s3://{file.bucket}/{file.key}
+              </M.ListItemText>
+            </M.ListItem>
+          ))}
+        </M.List>
+        <M.Button color="primary" variant="contained" onClick={handleSubmit}>
+          Create package
+        </M.Button>
+      </div>
     </M.Drawer>
   )
 }
