84523e04a11c25bafcdee04f534d61c1675c71c5
Maxim Chervonny <fiskus@chervonny.ru>
2022-09-13 16:14:49 +0300

    use M.* components

--- catalog/app/containers/Auth/wrapper.tsx
+++ catalog/app/containers/Auth/wrapper.tsx
@@ -4,8 +4,6 @@ import * as React from 'react'
 import * as redux from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { createStructuredSelector } from 'reselect'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
 import * as M from '@material-ui/core'
 
 import Error from 'components/Error'
@@ -29,14 +27,14 @@ const ErrorScreen = () => {
         detail={
           <span>
             Something went wrong. Try again.
-            <Button
+            <M.Button
               variant="contained"
               color="primary"
               style={{ marginLeft: '1em' }}
               onClick={retry}
             >
               Retry
-            </Button>
+            </M.Button>
           </span>
         }
       />
@@ -44,22 +42,22 @@ const ErrorScreen = () => {
   )
 }
 
-const useStyles = M.makeStyles((t) => ({
+const useNotAuthorizedStyles = M.makeStyles((t) => ({
   heading: {
     marginTop: t.spacing(10),
   },
 }))
 
 function NotAuthorized() {
-  const classes = useStyles()
+  const classes = useNotAuthorizedStyles()
   return (
     <Layout>
-      <Typography variant="h3" align="center" className={classes.heading} gutterBottom>
+      <M.Typography variant="h3" align="center" className={classes.heading} gutterBottom>
         Not Authorized
-      </Typography>
-      <Typography variant="body1" align="center">
+      </M.Typography>
+      <M.Typography variant="body1" align="center">
         Contact a Quilt admin to perform this task.
-      </Typography>
+      </M.Typography>
     </Layout>
   )
 }
