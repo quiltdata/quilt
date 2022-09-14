b673538d1b41f16e5127aec86b0803781b6e3569
Maxim Chervonny <fiskus@chervonny.ru>
2022-09-14 17:12:14 +0300

    cosmetic changes

--- catalog/app/containers/Bucket/Queries/QueryViewer.tsx
+++ catalog/app/containers/Bucket/Queries/QueryViewer.tsx
@@ -64,9 +64,7 @@ export default function QueryViewer({
   const [error, setError] = React.useState<Error | null>(null)
 
   const handleChange = React.useCallback(
-    (value: object) => {
-      onChange(value as requests.ElasticSearchQuery)
-    },
+    (value: object) => onChange(value as requests.ElasticSearchQuery),
     [onChange],
   )
 
