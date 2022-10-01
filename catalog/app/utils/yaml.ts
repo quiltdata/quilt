854ebf7efcf6151ae39195a3180731c802532039
Maksim Chervonnyi <fiskus@chervonny.ru>
2022-10-01 11:32:39 +0300

    yaml.js –> yaml.ts

--- catalog/app/utils/yaml.ts
+++ catalog/app/utils/yaml.ts
@@ -1,9 +1,10 @@
 import yaml from 'js-yaml'
 
 // eslint-disable-next-line consistent-return
-export default function parseYaml(inputStr) {
+export default function parseYaml(inputStr?: string) {
+  if (!inputStr) return undefined
   try {
-    return yaml.load(inputStr, 'utf8')
+    return yaml.load(inputStr)
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error)
