a32e3f4f18db3581d53d8d936fb1871f0638d842
Maksim Chervonnyi <fiskus@chervonny.ru>
2022-10-11 17:35:46 +0300

    linting test

--- catalog/app/containers/Bucket/Search.js
+++ catalog/app/containers/Bucket/Search.js
@@ -342,7 +342,8 @@ export default function BucketSearch({
 }) {
   const isInStack = BucketConfig.useIsInStack()
   const { q: query = '', p, mode, ...params } = parseSearch(l.search)
-  const { preferences: prefs, result } = BucketPreferences.use()
+  const { result } = BucketPreferences.use()
+  console.log(result)
   const page = p && parseInt(p, 10)
   const retry = (params.retry && parseInt(params.retry, 10)) || undefined
 
