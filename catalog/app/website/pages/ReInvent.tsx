6fed98e28d67d06664a2148f845a5791e7c78e0a
Simon Kohnstamm <simon@cloud.ceo>
2022-11-22 09:27:33 -0600

    changing arrangement of sections

--- catalog/app/website/pages/ReInvent.tsx
+++ catalog/app/website/pages/ReInvent.tsx
@@ -21,11 +21,10 @@ import logoNeumora from 'website/pages/Landing/Logos/logo-neumora.png'
 import logoObsidian from 'website/pages/Landing/Logos/logo-obsidian.png'
 import logoStemson from 'website/pages/Landing/Logos/logo-stemson.png'
 import logoVir from 'website/pages/Landing/Logos/logo-vir.png'
-import { Strong } from 'fp-ts/lib/Reader'
 
 const SALES_ADDRESS = 'mailto:sales@quiltdata.io'
 const DECK_URL =
-  'https://quilt-web-public.s3.amazonaws.com/deck/Quilt%E2%80%94the+data+hub+for+biotech.pdf'
+  'https://s3.amazonaws.com/quilt-sales-public/Quilt-Data_Mesh.pdf'
 
 const logos = [
   {
@@ -121,41 +120,94 @@ function ReInvent() {
         }
         detail={
           <>
-          <M.Box alignContent={"left"}><img src={imageArch} width="5%"></img></M.Box>
+          {/* SRK To:Do 
+          Add MailChimp Form to the right, adjust the Images to suit. 
+          */}
             Quilt platform combines a suite of interlocking capabilities which allow Life Sciences organizations to move fast. 
             Initially, it enables the creation of <strong>data packages</strong> which allows data to be 
             logically organized, enables it to flow into the cloud seamlessly, and makes 
             it searchable, version controlled, and beautiful. 
-            
+          <M.Box pt={4} />
             The <strong>Quilt Data Catalog</strong> allows users to view their organizations' data beautifully displayed, supported by rich 
             visualizations*.
-            <br />
-            Quilt stores, tags, manages, and integrates your data sources so that all of
+          <br />
+          <M.Box pt={4} />
+            
+          {/* SRK To:Do Note this section runs into the foot on the graphic to the right on my (SK) Browser. */}
+          <M.Box mr={10}>
+            {/* This does not fix it */}
+          Quilt runs privately and securely in your AWS account, as a 
+          CloudFormation stack.&nbsp;
+          <strong>
+            Quilt is powered by scalable and secure services like Amazon S3, Amazon
+            OpenSearch, and Amazon Athena.
+          </strong>
+          </M.Box>
+          <M.Box display="flex" flexDirection="column" pt={6}>
+            <M.Button variant="contained" color="primary" onClick={talk}>
+              Book a demo
+            </M.Button>
+          </M.Box>
+          <M.Box pt={2} />
+        
+            <ChevronLink href={DECK_URL}>
+            Deck: Quilt the Data Meshfor Life Sciences (PDF)
+          </ChevronLink>
+        
+            {/* SRK For Removal: Quilt stores, tags, manages, and integrates your data sources so that all of
             your data are findable, accessible, interoperable, and reusable (FAIR).
             Publicly traded companies in life sciences choose Quilt as their{' '}
             <strong>scientific data management system (SDMS) </strong>to leverage all of
             their organizational knowledge into credible decisions that bring drugs and
-            therapies to market faster.
-            <M.Box pt={4} />
-            Quilt runs privately and securely in your AWS account, as a CloudFormation
-            stack.&nbsp;
-            <strong>
-              Quilt is powered by scalable and secure services like Amazon S3, Amazon
-              OpenSearch, and Amazon Athena.
-            </strong>
-            <M.Box display="flex" flexDirection="column" pt={6}>
-              <M.Button variant="contained" color="primary" onClick={talk}>
-                Book a demo
-              </M.Button>
-            </M.Box>
-            <M.Box pt={2} />
-            <ChevronLink href={DECK_URL}>
-              Learn more about Quilt for life sciences (PDF)
-            </ChevronLink>
+            therapies to market faster. */}
           </>
         }
       />
-      <M.Box pb={{ xs: 0, md: 10 }} />
+
+      {/* Add Logo Carosel here */}
+      <LogosCarousel logos={logos} title="Life Sciences Organizations Run on Quilt" />
+      {/* Breakout boxes */}
+      <M.Box pb={{ xs: 0, md: 2 }} />
+      <Lede
+        heading={<>Data Management Democratized</>}
+        variant="left"
+        detail={
+          <>
+            Your company rapidly accumulates data from instruments, CROs, scientists, and
+            computational pipelines. But simply storing data brings cost without benefit.
+            Data without context (labels, documentation, links, and charts) quickly
+            becomes meaningless. With Quilt, your team builds your data schema from the bottom up, using Quilt’s 
+            rich metadata discovery and management tools. This way, everyone in the organization is empowered to 
+            cultivate, cleanse and enrich data into powerful Data Products which can be presented across domains, 
+            across the organization, or across the industry.
+
+            <M.Box pt={2} />
+            
+            {/* Decision quality suffers, experiments are needlessly
+            repeated, and teams waste months doing "data archaeology" to reconstruct past
+            results. */}
+          </>
+        }
+      />
+      <M.Box pb={{ xs: 0, md: 2 }} />
+      <Lede
+        heading={<>Rich Visualizations</>}
+        variant="center"
+        detail={
+          <>
+            Your company rapidly accumulates data from instruments, CROs, scientists, and
+            computational pipelines. But simply storing data brings cost without benefit.
+            Data without context (labels, documentation, links, and charts) quickly
+            becomes meaningless. With Quilt, your team builds your data schema from the bottom up, using Quilt’s 
+            rich metadata discovery and management tools. This way, everyone in the organization is empowered to 
+            cultivate, cleanse and enrich data into powerful Data Products which can be presented across domains, 
+            across the organization, or across the industry.
+              {/* Decision quality suffers, experiments are needlessly
+            repeated, and teams waste months doing "data archaeology" to reconstruct past
+            results. */}
+          </>
+        }
+      />
       <Section>
         <Tabs
           sections={[
