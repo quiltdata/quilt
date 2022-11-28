a6880ff43206213be871556fdbed04bf055d33bc
Maxim Chervonny <fiskus@chervonny.ru>
2022-11-28 12:40:59 +0200

    move onMessage to hook and remove unused code

--- catalog/app/components/Preview/loaders/IFrame.tsx
+++ catalog/app/components/Preview/loaders/IFrame.tsx
@@ -5,9 +5,7 @@ import * as React from 'react'
 
 import * as requests from 'containers/Bucket/requests'
 import * as AWS from 'utils/AWS'
-import AsyncResult from 'utils/AsyncResult'
 import * as Config from 'utils/Config'
-import { useData } from 'utils/Data'
 import { mkSearch } from 'utils/NamedRoutes'
 import * as s3paths from 'utils/s3paths'
 import type { PackageHandle } from 'utils/packageHandle'
@@ -41,35 +39,9 @@ function generateCsvUrl(handle: s3paths.S3HandleBase, endpoint: string, sign: Si
   )
 }
 
-function prepareFiles(
-  files: requests.BucketListingFile[],
-  config: Config.Config,
-  sign: Sign,
-) {
-  return files.map(({ bucket, key }) => {
-    const handle = { bucket, key }
-    if (utils.extIs('.csv')(key)) {
-      return {
-        handle,
-        url: generateCsvUrl(handle, config.binaryApiGatewayEndpoint, sign),
-      }
-    }
-    return {
-      handle,
-      url: generateJsonUrl(handle, config.apiGatewayEndpoint, sign),
-    }
-  })
-}
-
 interface Env {
   fileHandle: s3paths.S3HandleBase
-  package: {
-    files: {
-      handle: s3paths.S3HandleBase
-      url: string
-    }[]
-    handle: PackageHandle
-  }
+  packageHandle: PackageHandle
 }
 
 interface FileHandle extends s3paths.S3HandleBase {
@@ -131,56 +103,21 @@ function prepareSrcDoc(html: string, env: Env) {
 }
 
 function useContextEnv(handle: FileHandle): Env {
-  const s3 = AWS.S3.use()
-  const sign = AWS.Signer.useS3Signer()
-  const config = Config.use()
-
-  const { packageHandle, ...fileHandle } = handle
-  const { result, fetch } = useData(requests.bucketListing, {
-    s3,
-    bucket: handle.bucket,
-    path: s3paths.ensureSlash(dirname(handle.key)),
-  })
-  const processed = utils.useProcessing(
-    result,
-    ({ files }: requests.BucketListingResult) => ({
+  return React.useMemo(() => {
+    const { packageHandle, ...fileHandle } = handle
+    return {
       fileHandle,
-      package: {
-        files: prepareFiles(files, config, sign),
-        handle: packageHandle,
-      },
-    }),
-  )
-  return utils.useErrorHandling(processed, { handle, retry: fetch })
-}
-
-interface TextDataOutput {
-  info: {
-    data: {
-      head: string[]
-      tail: string[]
+      packageHandle,
     }
-    note: string
-    warnings: string
-  }
+  }, [handle])
 }
 
-interface IFrameLoaderProps {
-  children: (result: $TSFixMe) => React.ReactNode
-  env: Env
-  handle: FileHandle
-}
-
-function IFrameLoader({ env, handle, children }: IFrameLoaderProps) {
+function useMessageBus(handle: FileHandle) {
   const s3 = AWS.S3.use()
   const sign = AWS.Signer.useS3Signer()
   const { apiGatewayEndpoint, binaryApiGatewayEndpoint } = Config.use()
 
-  const src = React.useMemo(
-    () => sign(handle, { ResponseContentType: 'text/html' }),
-    [handle, sign],
-  )
-  const onMessage = React.useCallback(
+  return React.useCallback(
     async ({ name, payload }) => {
       switch (name) {
         case 'list-files': {
@@ -221,6 +158,34 @@ function IFrameLoader({ env, handle, children }: IFrameLoaderProps) {
     },
     [apiGatewayEndpoint, binaryApiGatewayEndpoint, handle, sign, s3],
   )
+}
+
+interface TextDataOutput {
+  info: {
+    data: {
+      head: string[]
+      tail: string[]
+    }
+    note: string
+    warnings: string
+  }
+}
+
+interface IFrameLoaderProps {
+  children: (result: $TSFixMe) => React.ReactNode
+  handle: FileHandle
+}
+
+export const Loader = function IFrameLoader({ handle, children }: IFrameLoaderProps) {
+  const env = useContextEnv(handle)
+
+  const sign = AWS.Signer.useS3Signer()
+  const onMessage = useMessageBus(handle)
+
+  const src = React.useMemo(
+    () => sign(handle, { ResponseContentType: 'text/html' }),
+    [handle, sign],
+  )
   const { result, fetch } = utils.usePreview({
     type: 'txt',
     handle,
@@ -236,20 +201,4 @@ function IFrameLoader({ env, handle, children }: IFrameLoaderProps) {
     },
   )
   return <>{children(utils.useErrorHandling(processed, { handle, retry: fetch }))}</>
-}
-
-interface IFrameEnvLoaderProps {
-  handle: FileHandle
-  children: (result: $TSFixMe) => React.ReactNode
-}
-
-export const Loader = function IFrameEnvLoader({
-  handle,
-  children,
-}: IFrameEnvLoaderProps) {
-  const envData = useContextEnv(handle)
-  return AsyncResult.case({
-    _: children,
-    Ok: (env: Env) => <IFrameLoader {...{ env, handle, children }} />,
-  })(envData)
 }
