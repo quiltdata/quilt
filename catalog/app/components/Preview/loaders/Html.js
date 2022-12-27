d76c0c7e90c9c2fa7e2e964c24af5b6740e3391a
Maksim Chervonnyi <mail@redmax.dev>
2022-12-27 14:04:21 +0300

    Refactor control to switch file types; Add "Plain Text" and "Echarts" file types (#3240)

--- catalog/app/components/Preview/loaders/Html.js
+++ catalog/app/components/Preview/loaders/Html.js
@@ -10,9 +10,12 @@ import useMemoEq from 'utils/useMemoEq'
 import { PreviewData } from '../types'
 
 import * as Text from './Text'
+import FileType from './fileType'
 import * as utils from './utils'
 
 export const detect = utils.extIn(['.htm', '.html'])
+
+export const FILE_TYPE = FileType.Html
 
 function IFrameLoader({ handle, children }) {
   const sign = AWS.Signer.useS3Signer()
@@ -20,7 +23,9 @@ function IFrameLoader({ handle, children }) {
     sign(handle, { ResponseContentType: 'text/html' }),
   )
   // TODO: issue a head request to ensure existence and get storage class
-  return children(AsyncResult.Ok(PreviewData.IFrame({ src })))
+  return children(
+    AsyncResult.Ok(PreviewData.IFrame({ src, modes: [FileType.Html, FileType.Text] })),
+  )
 }
 
 export const Loader = function HtmlLoader({ handle, children }) {
