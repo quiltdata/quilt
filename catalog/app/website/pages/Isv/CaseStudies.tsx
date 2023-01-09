b1a04561ed2aed613479ba2f2746ed7f5362712b
Maxim Chervonny <fiskus@chervonny.ru>
2023-01-09 22:31:20 +0200

    add readmore link

--- catalog/app/website/pages/Isv/CaseStudies.tsx
+++ catalog/app/website/pages/Isv/CaseStudies.tsx
@@ -26,16 +26,25 @@ const useCaseStudyStyles = M.makeStyles((t) => ({
     fontSize: '16px',
     lineHeight: '32px',
   },
+  readMore: {
+    marginTop: t.spacing(1),
+    fontSize: '16px',
+    lineHeight: '32px',
+  },
+  link: {
+    color: '#5471F1',
+  },
 }))
 
 interface CaseStudyProps {
   className?: string
-  title: string
   content: string
+  link: string
   logo: string
+  title: string
 }
 
-function CaseStudy({ className, title, content, logo }: CaseStudyProps) {
+function CaseStudy({ className, title, content, link, logo }: CaseStudyProps) {
   const classes = useCaseStudyStyles()
   const backgroundImageStyle = React.useMemo(
     () => ({
@@ -48,6 +57,11 @@ function CaseStudy({ className, title, content, logo }: CaseStudyProps) {
       <div className={classes.logo} style={backgroundImageStyle} />
       <M.Typography className={classes.title}>{title}</M.Typography>
       <M.Typography className={classes.content}>{content}</M.Typography>
+      <p className={classes.readMore}>
+        <a className={classes.link} href={link} target="_blank">
+          Read more
+        </a>
+      </p>
     </div>
   )
 }
@@ -87,16 +101,19 @@ export default function CaseStudies({ className }: CaseStudiesProps) {
           title="Improving Data Management with Quilt and NextFlow"
           content="Tessera Therapeutics is a pioneer in gene writing, using technology to insert messages into genomes to treat diseases. The company needed to manage and share more than 12 terabytes of scientific data across large teams of wet scientists and computational biologists. To address this, Tessera implemented Quilt Data and Nextflow to ensure that data was findable, accessible, interoperable, and reusable (FAIR) and to accelerate its gene-writing discoveries to market"
           logo={logoTessera}
+          link="https://quiltdata.com"
         />
         <CaseStudy
           title="Accelerating Data Access and Collaboration with DataSync"
           content="National Resilience, Inc. (Resilience) is a manufacturing and technology company that uses Quilt Data to manage and share data from its research sites, enabling scientists to access data faster and collaborate more effectively. By using Quilt's deep indexing and free-text search capabilities, Resilience can easily browse and access data from over 100 instruments and 10 million files in Amazon S3. The Quilt API also helps Resilience track data and intellectual property as it moves from research and analysis to downstream, GXP, and manufacturing processes."
           logo={logoResilience}
+          link="https://quiltdata.com"
         />
         <CaseStudy
           title="Managing and Accessing Data for Single Cell Genomics Research"
           content="Celsius Therapeutics is using Quilt Data to manage and access data for single cell genomics research in the areas of cancer and autoimmune disease. Quilt helps Celsius maintain full versioning of its data sets and track data down to specific attributes and characteristics. The company has used Quilt to scale from a startup to production, uniformly access all its data types, and build apps to meet the specific needs of scientists."
           logo={logoCelsius}
+          link="https://quiltdata.com"
         />
       </div>
     </div>
