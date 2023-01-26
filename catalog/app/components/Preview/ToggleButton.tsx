1dae03f9f84ad03ec43b25c1b40d47387fc8c1fe
Maxim Chervonny <fiskus@chervonny.ru>
2023-01-26 14:12:35 +0200

    remove extra styles

--- catalog/app/components/Preview/ToggleButton.tsx
+++ catalog/app/components/Preview/ToggleButton.tsx
@@ -1,17 +1,7 @@
-import cx from 'classnames'
 import * as React from 'react'
 import * as M from '@material-ui/core'
 
 import ShrinkingIconButton from 'components/Buttons/ShrinkingIconButton'
-
-const useStyles = M.makeStyles((t) => ({
-  root: {
-    alignItems: 'center',
-    display: 'flex',
-    height: t.spacing(4),
-    justifyContent: 'center',
-  },
-}))
 
 interface ToggleButtonProps extends M.BoxProps {
   className?: string
@@ -25,9 +15,8 @@ export default function ToggleButton({
   onToggle,
   ...props
 }: ToggleButtonProps) {
-  const classes = useStyles()
   return (
-    <M.Box className={cx(classes.root, className)} {...props}>
+    <M.Box className={className} {...props}>
       <ShrinkingIconButton
         label={expanded ? 'Collapse' : 'Expand'}
         icon={expanded ? 'unfold_less' : 'unfold_more'}
