613bf898e889bec584203d35d0689ad1a1d6700a
Maxim Chervonny <fiskus@chervonny.ru>
2023-01-26 13:56:37 +0200

    reuse shrinkingbutton for toggle button

--- catalog/app/components/Buttons/ShrinkingIconButton.tsx
+++ catalog/app/components/Buttons/ShrinkingIconButton.tsx
@@ -8,22 +8,35 @@ const useStyles = M.makeStyles({
     marginBottom: -3,
     marginTop: -3,
   },
+  icon: {
+    transition: 'ease transform .15s',
+  },
+  iconRotated: {
+    transform: `rotate(180deg)`,
+  },
 })
 
 interface ShrinkingIconButtonProps extends M.IconButtonProps {
   label: React.ReactNode
   icon: string
+  rotate?: boolean
 }
 
 export default function ShrinkingIconButton({
   className,
   label,
   icon,
+  rotate,
   ...props
 }: ShrinkingIconButtonProps) {
   const classes = useStyles()
   const t = M.useTheme()
   const sm = M.useMediaQuery(t.breakpoints.down('sm'))
+  const iconElement = (
+    <M.Icon className={cx(classes.icon, { [classes.iconRotated]: rotate })}>
+      {icon}
+    </M.Icon>
+  )
 
   return sm ? (
     <M.IconButton
@@ -32,14 +45,14 @@ export default function ShrinkingIconButton({
       size="small"
       {...props}
     >
-      <M.Icon>{icon}</M.Icon>
+      {iconElement}
     </M.IconButton>
   ) : (
     <M.Button
       className={cx(classes.root, className)}
-      variant="outlined"
       size="small"
-      startIcon={<M.Icon>{icon}</M.Icon>}
+      startIcon={iconElement}
+      variant="outlined"
       {...props}
     >
       {label}
--- catalog/app/components/Preview/ToggleButton.tsx
+++ catalog/app/components/Preview/ToggleButton.tsx
@@ -2,18 +2,14 @@ import cx from 'classnames'
 import * as React from 'react'
 import * as M from '@material-ui/core'
 
+import ShrinkingIconButton from 'components/Buttons/ShrinkingIconButton'
+
 const useStyles = M.makeStyles((t) => ({
   root: {
     alignItems: 'center',
     display: 'flex',
     height: t.spacing(4),
     justifyContent: 'center',
-  },
-  icon: {
-    transition: 'ease transform .15s',
-  },
-  iconExpanded: {
-    transform: `rotate(180deg)`,
   },
 }))
 
@@ -32,18 +28,12 @@ export default function ToggleButton({
   const classes = useStyles()
   return (
     <M.Box className={cx(classes.root, className)} {...props}>
-      <M.Button
+      <ShrinkingIconButton
+        label={expanded ? 'Collapse' : 'Expand'}
+        icon={expanded ? 'unfold_less' : 'unfold_more'}
+        rotate={expanded}
         onClick={onToggle}
-        startIcon={
-          <M.Icon className={cx(classes.icon, { [classes.iconExpanded]: expanded })}>
-            {expanded ? 'unfold_less' : 'unfold_more'}
-          </M.Icon>
-        }
-        variant="outlined"
-        size="small"
-      >
-        {expanded ? 'Collapse' : 'Expand'}
-      </M.Button>
+      />
     </M.Box>
   )
 }
