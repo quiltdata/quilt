65303ab59c72314e543dd5734f6546429ddfff11
Maxim Chervonny <fiskus@chervonny.ru>
2023-02-06 16:25:39 +0200

    wip: use Json editor

--- catalog/app/components/Preview/renderers/Perspective/Schema.tsx
+++ catalog/app/components/Preview/renderers/Perspective/Schema.tsx
@@ -1,10 +1,23 @@
-// import Ajv from 'ajv'
-// import brace from 'brace'
-// import { JsonEditor as ReactJsonEditor } from 'jsoneditor-react'
+import Ajv from 'ajv'
+import brace from 'brace'
+import { JsonEditor as ReactJsonEditor } from 'jsoneditor-react'
 import * as React from 'react'
 import * as M from '@material-ui/core'
+import * as Lab from '@material-ui/lab'
 
 import type { PerspectiveSchema } from 'utils/perspective'
+
+const ajv = new Ajv({ allErrors: true, verbose: true })
+
+const editorHtmlProps = { style: { height: '240px' } }
+
+const jsonSchemaOfPerspectiveSchema = {
+  type: 'object',
+  additionalProperties: {
+    type: 'string',
+    default: 'string',
+  },
+}
 
 interface SchemaEditorProps {
   initialValue: PerspectiveSchema
@@ -20,25 +33,46 @@ export default function SchemaEditor({
   open,
 }: SchemaEditorProps) {
   const [error, setError] = React.useState<Error | null>(null)
-  const [valueStr, setValueStr] = React.useState(JSON.stringify(initialValue, null, 2))
-  const handleChange = React.useCallback((e) => setValueStr(e.target.value), [])
+  const [value, setValue] = React.useState(initialValue)
+  const [submitted, setSubmitted] = React.useState(false)
+  const handleChange = React.useCallback((v) => {
+    setValue(v)
+    setSubmitted(false)
+  }, [])
   const handleSubmit = React.useCallback(() => {
+    setSubmitted(true)
     try {
-      onSubmit(JSON.parse(valueStr))
+      onSubmit(value)
     } catch (e) {
       if (e instanceof Error) setError(e)
     }
     onClose()
-  }, [onClose, onSubmit, valueStr])
+  }, [onClose, onSubmit, value])
   return (
     <M.Dialog open={open} onClose={onClose} fullWidth maxWidth="xs">
       <M.DialogTitle>Change table Schema</M.DialogTitle>
       <M.DialogContent>
-        <textarea
-          value={valueStr}
+        <ReactJsonEditor
+          ace={brace}
+          ajv={ajv}
+          htmlElementProps={editorHtmlProps}
+          mainMenuBar={false}
+          mode="code"
+          navigationBar={false}
           onChange={handleChange}
-          style={{ minHeight: '300px' }}
+          onError={setError}
+          onValidationError={(es) => setError(es[0])}
+          schema={jsonSchemaOfPerspectiveSchema}
+          search={false}
+          statusBar={false}
+          theme="ace/theme/eclipse"
+          value={initialValue}
         />
+        {!!error && submitted && (
+          <Lab.Alert key={error.message} severity="error">
+            {error.message}
+          </Lab.Alert>
+        )}
       </M.DialogContent>
       <M.DialogActions>
         <M.Button>Cancel</M.Button>
