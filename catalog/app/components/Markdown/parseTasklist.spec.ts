d054c09720a4fe9a57c5ca32c6aeb3cf283bb62f
Maxim Chervonny <fiskus@chervonny.ru>
2023-02-21 22:36:45 +0200

    simplify tests by reusing variables

--- catalog/app/components/Markdown/parseTasklist.spec.ts
+++ catalog/app/components/Markdown/parseTasklist.spec.ts
@@ -13,66 +13,45 @@ function createState(src: string) {
   } as Remarkable.StateInline
 }
 
+const uncheckedCheckbox = {
+  type: 'tasklist',
+  checked: false,
+  level: 0,
+}
+const checkedCheckbox = {
+  type: 'tasklist',
+  checked: true,
+  level: 0,
+}
+
 describe('components/Markdown/parseTasklist', () => {
   it('Detect empty checkbox', () => {
     const state = createState('[]')
     parseTasklist(state as Remarkable.StateInline)
-    expect(state.tokens).toMatchObject([
-      {
-        type: 'tasklist',
-        checked: false,
-        level: 0,
-      },
-    ])
+    expect(state.tokens).toMatchObject([uncheckedCheckbox])
   })
   it('Detect empty checkbox with space inside', () => {
-    const state = {
-      src: '[ ]',
-      pos: 0,
-      level: 0,
-      tokens: [] as Remarkable.ContentToken[],
-      push(x: Remarkable.ContentToken) {
-        this.tokens.push(x)
-      },
-    }
+    const state = createState('[ ]')
     parseTasklist(state as Remarkable.StateInline)
-    expect(state.tokens).toMatchObject([
-      {
-        type: 'tasklist',
-        checked: false,
-        level: 0,
-      },
-    ])
+    expect(state.tokens).toMatchObject([uncheckedCheckbox])
   })
   it('Detect checked checkbox', () => {
-    const state = {
-      src: '[x]',
-      pos: 0,
-      level: 0,
-      tokens: [] as Remarkable.ContentToken[],
-      push(x: Remarkable.ContentToken) {
-        this.tokens.push(x)
-      },
-    }
+    const state = createState('[x]')
     parseTasklist(state as Remarkable.StateInline)
-    expect(state.tokens).toMatchObject([
-      {
-        type: 'tasklist',
-        checked: true,
-        level: 0,
-      },
-    ])
+    expect(state.tokens).toMatchObject([checkedCheckbox])
   })
   it('Does not detect checkbox if no one', () => {
-    const state = {
-      src: 'foobar',
-      pos: 0,
-      level: 0,
-      tokens: [] as Remarkable.ContentToken[],
-      push(x: Remarkable.ContentToken) {
-        this.tokens.push(x)
-      },
-    }
+    const state = createState('foobar')
+    parseTasklist(state as Remarkable.StateInline)
+    expect(state.tokens).toMatchObject([])
+  })
+  it('Does not detect checkbox if checkbox has two spaces', () => {
+    const state = createState('[  ]')
+    parseTasklist(state as Remarkable.StateInline)
+    expect(state.tokens).toMatchObject([])
+  })
+  it('Does not detect checkbox if checkbox has tabulation', () => {
+    const state = createState(`[\t]`)
     parseTasklist(state as Remarkable.StateInline)
     expect(state.tokens).toMatchObject([])
   })
