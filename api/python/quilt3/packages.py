f5b400394a3bdd151ffbd1e9f29a478bb5d23afa
Maxim Chervonny <fiskus@chervonny.ru>
2023-02-21 15:11:11 +0200

    use visually similar colon

--- api/python/quilt3/packages.py
+++ api/python/quilt3/packages.py
@@ -1325,7 +1325,7 @@ class Package:
             name: name for package in registry
             dest: where to copy the objects in the package.
                 Must be either an S3 URI prefix in the registry bucket, or a callable that takes
-                logical_key, package_entry, and top_hash and returns S3 URI. S3 URIs format is `"s3://$bucket/$key"`.
+                logical_key, package_entry, and top_hash and returns S3 URI. S3 URIs format is s3꞉//$bucket/$key.
             registry: registry where to create the new package
             message: the commit message for the new package
             selector_fn: An optional function that determines which package entries should be copied to S3.
