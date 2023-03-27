86c088abd1aaf3fe39d8392f3320c7ae8795aece
Maxim Chervonny <fiskus@chervonny.ru>
2023-03-27 20:15:41 +0300

    prettify

--- catalog/app/containers/Admin/Buckets/Buckets.tsx
+++ catalog/app/containers/Admin/Buckets/Buckets.tsx
@@ -128,10 +128,10 @@ function PFSCheckbox({ input, meta }: Form.CheckboxProps & M.CheckboxProps) {
           <>
             Enable permissive HTML rendering
             <Hint>
-              This allows execution of any linked JavaScript code and fetching network resources
-              relative to the package. Be aware that the iframe with rendered HTML (and package
-              resources) can be shared publicly during the session lifespan. The session is active
-              while the page with rendered HTML is open.
+              This allows execution of any linked JavaScript code and fetching network
+              resources relative to the package. Be aware that the iframe with rendered
+              HTML (and package resources) can be shared publicly during the session
+              lifespan. The session is active while the page with rendered HTML is open.
               <br />
               Enable only on trusted AWS S3 buckets.
             </Hint>
