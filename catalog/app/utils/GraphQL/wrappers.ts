a96d62614959c36c3f14ca53817549448678686c
nl_0 <nl.imbecile@gmail.com>
2023-04-05 20:30:03 +0200

    add comments and explicit types

--- catalog/app/utils/GraphQL/wrappers.ts
+++ catalog/app/utils/GraphQL/wrappers.ts
@@ -10,7 +10,7 @@ import useMemoEq from 'utils/useMemoEq'
 export class OperationError extends BaseError {}
 
 export class QueryError<Data = any> extends OperationError {
-  constructor(result?: urql.UseQueryState<Data, any>) {
+  constructor(result?: ResultForData<Data>) {
     super('GraphQL query error')
     this.result = result
   }
@@ -23,75 +23,179 @@ export class MutationError<Data, Variables> extends OperationError {
   }
 }
 
-interface FoldOptions<Data, OnData, OnFecthing, OnError = never> {
-  data: (data: Data, result: urql.UseQueryState<Data, any>) => OnData
-  fetching: (result: urql.UseQueryState<Data, any>) => OnFecthing
-  error?: (
-    error: urql.CombinedError | QueryError<Data>,
-    result: urql.UseQueryState<Data, any>,
-  ) => OnError
+type ResultForData<Data> = urql.UseQueryState<Data, any>
+
+type ErrorForData<Data> = urql.CombinedError | QueryError<Data>
+
+type FoldOnData<Data, OnData> = (data: Data, result: ResultForData<Data>) => OnData
+
+type FoldOnFetching<Data, OnFetching> = (result: ResultForData<Data>) => OnFetching
+
+type FoldOnError<Data, OnError> = (
+  error: ErrorForData<Data>,
+  result: ResultForData<Data>,
+) => OnError
+
+interface FoldCases<Data, OnData, OnFecthing, OnError = never> {
+  data: FoldOnData<Data, OnData>
+  fetching: FoldOnFetching<Data, OnFecthing>
+  error?: FoldOnError<Data, OnError>
+}
+
+interface FoldOptions {
   partial?: boolean
   silent?: boolean
 }
 
-export const foldC =
-  <Data, OnData, OnFetching, OnError = never>({
-    data,
-    error,
-    fetching,
-    partial = false,
-    silent = false,
-  }: FoldOptions<Data, OnData, OnFetching, OnError>) =>
-  (result: urql.UseQueryState<Data, any>) => {
-    const isPartial = result.operation?.context?.meta?.cacheOutcome === 'partial'
-    if (!partial && isPartial) return fetching(result)
+interface FoldConfig<Data, OnData, OnFecthing, OnError>
+  extends FoldCases<Data, OnData, OnFecthing, OnError>,
+    FoldOptions {}
 
-    if (result.data) {
-      if (!silent && result.error) {
-        log.warn('Non-critical error while executing the query:', result.error)
-        Sentry.captureException(result.error, { extra: { result } })
-      }
-      return data(result.data, result)
+/**
+ * Fold / unwrap an {@link urql#UseQueryState | `urql` query result} into a value.
+ *
+ * @param result - The query result as returned by {@link urql#useQuery} (or our {@link useQuery} wrapper around it).
+ *
+ * @param config - Fold cases (for data, error, fetching states) and options (silent, partial).
+ * @param config.data - This is called when the query data is available.
+ * @param config.fetching - This is called when the query is fetching.
+ * @param config.error - This is called when the query has an error. Optional, defaults to throwing the error.
+ * @param config.partial - Whether to treat partial data (from cache) as complete or to wait for the full data. Optional, defaults to `false`.
+ * @param config.silent - Whether to disable logging errors and sending them to Sentry. Optional, defaults to `false` (error logging and capturing enabled).
+ *
+ * @returns The folded value.
+ *
+ * @example
+ * ```ts
+ * const result = useQuery(...)
+ * const value = fold(result, {
+ *   data: (data) => data,
+ *   fetching: () => 'fetching',
+ * })
+ * ```
+ */
+export function fold<Data, OnData, OnFetching, OnError = never>(
+  result: ResultForData<Data>,
+  config: FoldConfig<Data, OnData, OnFetching, OnError>,
+): OnData | OnFetching | OnError {
+  const isPartial = result.operation?.context?.meta?.cacheOutcome === 'partial'
+  if (!config.partial && isPartial) return config.fetching(result)
+
+  if (result.data) {
+    if (!config.silent && result.error) {
+      log.warn('Non-critical error while executing the query:', result.error)
+      Sentry.captureException(result.error, { extra: { result } })
     }
-
-    if (result.fetching) return fetching(result)
-
-    const err = result.error || new QueryError(result)
-    if (error) return error(err, result)
-    if (!silent) {
-      log.error('Critical error while executing the query:', err)
-      Sentry.captureException(err, { extra: { result } })
-    }
-    throw error
+    return config.data(result.data, result)
   }
 
-export const fold = <Data, OnData, OnFetching, OnError = never>(
-  result: urql.UseQueryState<Data, any>,
-  casesAndOpts: FoldOptions<Data, OnData, OnFetching, OnError>,
-) => foldC(casesAndOpts)(result)
+  if (result.fetching) return config.fetching(result)
 
+  const err = result.error || new QueryError(result)
+  if (config.error) return config.error(err, result)
+  if (!config.silent) {
+    log.error('Critical error while executing the query:', err)
+    Sentry.captureException(err, { extra: { result } })
+  }
+  throw err
+}
+
+/**
+ * {@link fold} curried (config first, result last).
+ *
+ * @example
+ * ```ts
+ * const result = useQuery(MY_QUERY_DOC)
+ * const foldResult = foldC({
+ *   // help TS infer the type of the data
+ *   data: (data: DataForDoc<typeof MY_QUERY_DOC>) => data,
+ *   fetching: () => 'fetching',
+ * })
+ * const value = foldResult(result)
+ * ```
+ */
+export const foldC =
+  <Data, OnData, OnFetching, OnError = never>(
+    opts: FoldConfig<Data, OnData, OnFetching, OnError>,
+  ) =>
+  (result: ResultForData<Data>): OnData | OnFetching | OnError =>
+    fold(result, opts)
+
+export type DataForDoc<Doc extends urql.TypedDocumentNode<any, any>> =
+  Doc extends urql.TypedDocumentNode<infer Data, any> ? Data : never
+
+export type VariablesForDoc<Doc extends urql.TypedDocumentNode<any, any>> =
+  Doc extends urql.TypedDocumentNode<any, infer Variables> ? Variables : never
+
+export type QueryResultForDoc<Doc extends urql.TypedDocumentNode<any, any>> =
+  UseQueryResult<DataForDoc<Doc>, VariablesForDoc<Doc>>
+
+type QueryDoc<Data, Variables> = urql.UseQueryArgs<Variables, Data>['query']
+
+type UseQueryOptions<Data, Variables> = Omit<
+  urql.UseQueryArgs<Variables, Data>,
+  'suspense' | 'query' | 'variables'
+>
+
+interface UseQueryResult<Data, Variables> extends urql.UseQueryState<Data, Variables> {
+  run: urql.UseQueryResponse<Data, Variables>[1]
+}
+
+/**
+ * An opinionated wrapper around {@link urql#useQuery} that:
+ * - disables suspense
+ * - memoizes the context to ensure it doesn't change on every render
+ * - puts the `run` function in the returned result object and memoizes it
+ *
+ * @param query - The query document.
+ * @param variables - The variables. Optional.
+ * @param options - The options ({@link urql#UseQueryArgs} minus `query`, `variables` and `suspense`). Optional.
+ * @param options.context - The context. Optional.
+ *
+ * @returns The query result as returned by {@link urql#useQuery} + the `run` function.
+ *
+ * @example
+ * ```ts
+ * const query = useQuery(MY_QUERY, variables)
+ * ```
+ */
 export function useQuery<Data, Variables = {}>(
-  query: urql.UseQueryArgs<Variables, Data>['query'],
-  variables?: urql.UseQueryArgs<Variables, Data>['variables'],
-  opts?: Omit<urql.UseQueryArgs<Variables, Data>, 'suspense' | 'query' | 'variables'>,
-) {
-  const { context: ctx, ...rest } = opts || {}
+  query: QueryDoc<Data, Variables>,
+  variables?: Variables,
+  options?: UseQueryOptions<Data, Variables>,
+): UseQueryResult<Data, Variables> {
+  const { context: ctx, ...rest } = options || {}
   const context = useMemoEq({ suspense: false, ...ctx }, R.identity)
   const [result, run] = urql.useQuery({ query, variables, context, ...rest })
   return useMemoEq({ ...result, run }, R.identity)
 }
 
-export type QueryResultForDoc<Doc extends urql.TypedDocumentNode<any, any>> =
-  Doc extends urql.TypedDocumentNode<infer Data, infer Variables>
-    ? ReturnType<typeof useQuery<Data, Variables>>
-    : never
-
+/**
+ * An opinionated wrapper around {@link urql#useQuery} that:
+ * - enables suspense
+ * - memoizes the context to ensure it doesn't change on every render
+ * - suspends and unwraps the data
+ *
+ * @param query - The query document.
+ * @param variables - The variables. Optional.
+ * @param options - The options ({@link urql#UseQueryArgs} minus `query`, `variables` and `suspense`). Optional.
+ * @param options.context - The context. Optional.
+ *
+ * @returns The data returned by the query.
+ *
+ * @throws An error if the query failed ({@link urql#CombinedError} if returned by the upstream, otherwise {@link QueryError}).
+ *
+ * @example
+ * ```ts
+ * const data = useQueryS(MY_QUERY, variables)
+ * ```
+ */
 export function useQueryS<Data, Variables>(
-  query: urql.UseQueryArgs<Variables, Data>['query'],
-  variables?: urql.UseQueryArgs<Variables, Data>['variables'],
-  opts?: Omit<urql.UseQueryArgs<Variables, Data>, 'suspense' | 'query' | 'variables'>,
+  query: QueryDoc<Data, Variables>,
+  variables?: Variables,
+  options?: UseQueryOptions<Data, Variables>,
 ): Data {
-  const { context, ...restOpts } = opts || {}
+  const { context, ...restOpts } = options || {}
   const ctxMemo = useMemoEq({ ...context, suspense: true }, R.identity)
   const [result] = urql.useQuery({ query, variables, context: ctxMemo, ...restOpts })
 
@@ -111,16 +215,36 @@ export function useQueryS<Data, Variables>(
   throw err
 }
 
-type QueryInput<Data, Variables> = Parameters<typeof urql.useMutation<Data, Variables>>[0]
+interface RunMutationContext extends Partial<urql.OperationContext> {
+  silent?: boolean
+}
 
-export function useMutation<Data, Variables>(query: QueryInput<Data, Variables>) {
+type RunMutation<Data, Variables> = (
+  variables?: Variables,
+  context?: RunMutationContext,
+) => Promise<Data>
+
+/**
+ * An opinionated wrapper around {@link urql#useMutation} that unwraps the data
+ * when invoked and logs / captures errors.
+ *
+ * @param query - The query document.
+ *
+ * @returns A function that runs the mutation.
+ *
+ * @example
+ * ```ts
+ * const mutate = useMutation(MY_MUTATION)
+ * const data = await mutate(variables)
+ * ```
+ */
+export function useMutation<Data, Variables>(
+  query: QueryDoc<Data, Variables>,
+): RunMutation<Data, Variables> {
   const [, execMutation] = urql.useMutation<Data, Variables>(query)
 
   return React.useCallback(
-    async (
-      variables?: Variables,
-      context?: Partial<urql.OperationContext> & { silent?: boolean },
-    ) => {
+    async (variables?: Variables, context?: RunMutationContext) => {
       const { silent = false, ...ctx } = context || {}
       const result = await execMutation(variables, ctx)
 
