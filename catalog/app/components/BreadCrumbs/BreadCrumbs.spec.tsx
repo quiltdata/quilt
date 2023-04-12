fc9c216a4b72e20e7c7c662e3a47881653f4cba7
Maxim Chervonny <fiskus@chervonny.ru>
2023-04-12 20:01:49 +0300

    add unit test

---
+++ catalog/app/components/BreadCrumbs/BreadCrumbs.spec.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react'
+import renderer from 'react-test-renderer'
+import { MemoryRouter } from 'react-router-dom'
+
+import * as BreadCrumbs from './BreadCrumbs'
+
+describe('components/BreadCrumbs', () => {
+  describe('Segment', () => {
+    it('should render EMPTY', () => {
+      const tree = renderer.create(<BreadCrumbs.Segment />).toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+    it('should render label', () => {
+      const tree = renderer.create(<BreadCrumbs.Segment label="Lorem ipsum" />).toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+    it('should render link', () => {
+      const tree = renderer
+        .create(
+          <MemoryRouter>
+            <BreadCrumbs.Segment label="A" to="/a" />
+          </MemoryRouter>,
+        )
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+  describe('render', () => {
+    it('basic breadcrumbs', () => {
+      const crumbs = BreadCrumbs.getCrumbs('aa a/bb-b/c/d_d', 'ROOT', (x) => x)
+      const tree = renderer
+        .create(<MemoryRouter>{BreadCrumbs.render(crumbs)}</MemoryRouter>)
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+    it('and transform links', () => {
+      const crumbs = BreadCrumbs.getCrumbs('a/b', 'ROOT', (x) => x)
+      const getLinkProps = ({ to }: { to?: string }) => ({
+        href: `https://quiltdata.com/${to || ''}`,
+      })
+      const tree = renderer
+        .create(
+          <MemoryRouter>{BreadCrumbs.render(crumbs, { getLinkProps })}</MemoryRouter>,
+        )
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+    it('and make links for every crumb', () => {
+      const crumbs = BreadCrumbs.getCrumbs('a/b', 'ROOT', (x) => x, { tailLink: true })
+      const getLinkProps = ({ to }: { to?: string }) => ({
+        href: `https://quiltdata.com/${to || ''}`,
+      })
+      const tree = renderer
+        .create(
+          <MemoryRouter>{BreadCrumbs.render(crumbs, { getLinkProps })}</MemoryRouter>,
+        )
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+    it('and end with separator', () => {
+      const crumbs = BreadCrumbs.getCrumbs('a/b', 'ROOT', (x) => x, {
+        tailLink: true,
+        tailSeparator: true,
+      })
+      const getLinkProps = ({ to }: { to?: string }) => ({
+        href: `https://quiltdata.com/${to || ''}`,
+      })
+      const tree = renderer
+        .create(
+          <MemoryRouter>{BreadCrumbs.render(crumbs, { getLinkProps })}</MemoryRouter>,
+        )
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+})
