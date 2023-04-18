c0a67f1e14de3aebd1ac89a2036b57218bf0c591
Maxim Chervonny <fiskus@chervonny.ru>
2023-04-18 12:03:59 +0300

    add Buttons.Skeleton

---
+++ catalog/app/components/Buttons/Skeleton.tsx
@@ -0,0 +1,36 @@
+import cx from 'classnames'
+import * as React from 'react'
+import * as M from '@material-ui/core'
+
+import Skeleton from 'components/Skeleton'
+
+const useStyles = M.makeStyles((t) => ({
+  root: {
+    display: 'inline-block',
+    boxShadow: t.shadows[4],
+    borderRadius: '4px',
+    width: t.spacing(15),
+  },
+  small: {
+    height: t.spacing(3.75),
+  },
+  medium: {
+    height: t.spacing(4.5),
+  },
+  large: {
+    height: t.spacing(5),
+  },
+}))
+
+interface ButtonSkeletonProps {
+  className?: string
+  size?: 'small' | 'medium' | 'large'
+}
+
+export default function ButtonSkeleton({
+  className,
+  size = 'medium',
+}: ButtonSkeletonProps) {
+  const classes = useStyles()
+  return <Skeleton className={cx(classes.root, classes[size], className)} />
+}
