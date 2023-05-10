78037ac983fbf05804edcf86dec5b13facf42420
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2023-05-10 16:23:05 +0400

    WIP Fix search in Python client

--- api/python/quilt3/search_util.py
+++ api/python/quilt3/search_util.py
@@ -3,14 +3,10 @@ search_util.py
 
 Contains search-related glue code
 """
-import re
-from urllib.parse import quote, urlencode, urlparse
-
-import requests
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 
+from . import session
 from .session import create_botocore_session
-from .util import QuiltException, get_from_config
 
 
 def search_credentials(host, region, service):
@@ -37,19 +33,8 @@ def search_api(query, index, limit=10):
     Sends a query to the search API (supports simple search
     queries only)
     """
-    api_gateway = get_from_config('apiGatewayEndpoint')
-    api_gateway_host = urlparse(api_gateway).hostname
-    match = re.match(r".*\.([a-z]{2}-[a-z]+-\d)\.amazonaws\.com$", api_gateway_host)
-    region = match.groups()[0]
-    auth = search_credentials(api_gateway_host, region, 'execute-api')
-    # Encode the parameters manually because AWS Auth requires spaces to be encoded as '%20' rather than '+'.
-    encoded_params = urlencode(dict(index=index, action='search', query=query), quote_via=quote)
-    response = requests.get(
-        f"{api_gateway}/search?{encoded_params}",
-        auth=auth
+    response = session.get_session().get(
+        f"{session.get_registry_url()}/api/search",
+        params=dict(index=index, action='search', query=query, size=limit),
     )
-
-    if not response.ok:
-        raise QuiltException(response.text)
-
     return response.json()
