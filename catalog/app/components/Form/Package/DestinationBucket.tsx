9c36027798c05e732449fc58b76857ac192b4830
Maxim Chervonny <fiskus@chervonny.ru>
2023-05-15 15:45:29 +0300

    add cool Skeleton

--- catalog/app/components/Form/Package/DestinationBucket.tsx
+++ catalog/app/components/Form/Package/DestinationBucket.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import * as Lab from '@material-ui/lab'
 import * as M from '@material-ui/core'
 
-import Skeleton from 'components/Skeleton'
+import Skel from 'components/Skeleton'
 import * as Model from 'model'
 
 import { L } from './types'
@@ -12,6 +12,36 @@ export type BucketConfig = Pick<
   Model.GQLTypes.BucketConfig,
   'name' | 'title' | 'description'
 >
+
+const useSkeletonStyles = M.makeStyles({
+  label: {
+    height: '12px',
+    width: '100px',
+  },
+  input: {
+    height: '32px',
+    margin: '6px 0',
+  },
+  helperText: {
+    height: '18px',
+    width: '50%',
+  },
+})
+
+interface SkeletonProps {
+  className?: string
+}
+
+function Skeleton({ className }: SkeletonProps) {
+  const classes = useSkeletonStyles()
+  return (
+    <div className={className}>
+      <Skel className={classes.label} animate />
+      <Skel className={classes.input} animate />
+      <Skel className={classes.helperText} animate />
+    </div>
+  )
+}
 
 const useStyles = M.makeStyles((t) => ({
   alert: {
@@ -43,7 +73,7 @@ export default function DestinationBucket({
     [onChange],
   )
   const errorMessage = errors.map(({ message }) => message).join('; ')
-  if (successors === L) return <Skeleton height={70} animate />
+  if (successors === L) return <Skeleton />
   if (successors instanceof Error)
     return (
       <Lab.Alert className={classes.alert} severity="error">
