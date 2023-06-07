quiltdata/quilt

commit d6d9bdc44a9458789d66a050b3001f0243bf9e7e

Pytest codeblocks (#2966)
* stub testdocs

* add dependencies

* README (markdown)

* Use single "`" for fixed-width style

* Minimize BASH failures

* skip bash
add modules

* skip workflow tests

* PASS ../docs/api-reference using https://open.quiltdata.com/b/allencell

* pass ../docs/Catalog

* filtering-a-package.md

* advanced-features

* editing-a-package.md

* edit package robustly

* ../docs/walkthrough/getting-data-from-a-package.md .                                                                  [100%]

* installing

* uploading

* working bucket

* ignore json test files

* remove test turds

* Update .gitignore

* Path(f'{TEST_DIR}/{SUB_DIR}')

* Update .gitignore

* linter error

* Fix CI/CD failures

* Restore editing-a-package.md

* Fix api, editing

* getting-data ipy

* getting-data

* installing-package

* uploading-package

* working-bucket

* rm test turds

* Fix parsing errors

* work-with-manifests

* fix api doc

* Update working-with-manifests.md

* working-bucket

* Add debugging

* revert js_yaml.safe_load

* Overwrite md from py

* Redo syntax fixes

* more syntax fixes

* linter wants blank lines

* --ignore-cr-at-eol

* remove trailing newline

* better git diff

* git diff | cat -A

* Add newlines back

* double newlines

* install and run testdoc

* Skip parsing "/usr/share/dict/words"

* poetry run pytest --codeblocks

* PR fixes

* Remove typo

* Update .github/workflows/py-ci.yml

Co-authored-by: Sergey Fedoseev <fedoseev.sergey@quiltdata.io>

* Update docs/advanced-features/workflows.md

Co-authored-by: Sergey Fedoseev <fedoseev.sergey@quiltdata.io>

* unused import

* subdir

* clean.sh

* Update docs/Catalog/VisualizationDashboards.md

* Match editing md to notebook

* remove unused module files

* Add scripts to .gitignore

* Update clean.sh

api/python/quilt3/api.py
@@ -91,17 +91,18 @@ def config(*catalog_url, **config_values):
 
     To retrieve the current config, call directly, without arguments:
 
-        >>> import quilt3
-        >>> quilt3.config()
+        import quilt3
+        quilt3.config()
 
     To trigger autoconfiguration, call with just the navigator URL:
 
-        >>> quilt3.config('https://example.com')
+        import quilt3
+        quilt3.config(navigator_url='https://example.com')
 
     To set config values, call with one or more key=value pairs:
 
-        >>> quilt3.config(navigator_url='http://example.com',
-        ...               elastic_search_url='http://example.com/queries')
+        import quilt3
+        quilt3.config(navigator_url='http://example.com', elastic_search_url='http://example.com/queries')
 
     Default config values can be found in `quilt3.util.CONFIG_TEMPLATE`.
 

.github/workflows/py-ci.yml
@@ -48,8 +48,26 @@ jobs:
           python -m pip install PyYAML~=5.4.1 api/python nbconvert git+https://github.com/quiltdata/pydoc-markdown.git@quilt
       - name: generate docs
         run: cd gendocs && python build.py
+      - name: show invisible changes via cat
+        run: git diff | cat -A
       - name: check there are no changes
         run: git diff --exit-code
+
+  test-testdocs:
+    runs-on: ubuntu-latest
+    env:
+      QUILT_DISABLE_USAGE_METRICS: true
+    steps:
+      - uses: actions/checkout@v3
+      - uses: actions/setup-python@v3
+        with:
+          python-version: '3.7'
+      - name: install poetry
+        run: python -m pip install poetry
+      - name: install deps
+        run: cd testdocs && poetry install
+      - name: test codeblocks
+        run: cd testdocs && poetry run pytest --codeblocks ../docs
 
   test-client:
     strategy:

.gitignore
@@ -39,3 +39,8 @@ registry/dpmd
 
 # Doc Build
 _build
+testdocs/*.json*
+testdocs/test_workflow
+testdocs/references
+testdocs/notebooks
+testdocs/scripts

api/python/quilt3/bucket.py
@@ -159,8 +159,8 @@ class Bucket:
             recursive (bool): show subdirectories and their contents as well
 
         Returns:
-            ``list``: Return value structure has not yet been permanently decided
-            Currently, it's a ``tuple`` of ``list`` objects, containing the
+            `list`: Return value structure has not yet been permanently decided
+            Currently, it's a `tuple` of `list` objects, containing the
             following: (directory info, file/object info, delete markers).
         """
         if path and not path.endswith('/'):

docs/advanced-features/working-with-manifests.ipynb
@@ -101,6 +101,7 @@
     }
    ],
    "source": [
+    "import quilt3\n",
     "p = quilt3.Package().set(\"data.csv\", \"data.csv\")\n",
     "p.set_meta({\"key\": \"value\"})\n",
     "\n",

docs/walkthrough/editing-a-package.ipynb
@@ -56,6 +56,7 @@
     }
    ],
    "source": [
+    "import quilt3\n",
     "quilt3.Package.install(\n",
     "    \"examples/hurdat\",\n",
     "    \"s3://quilt-example\",\n",
@@ -66,7 +67,8 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "Use `browse` to edit the package:"
+    "Use `browse` to edit the package:\n",
+    "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -119,6 +121,18 @@
     }
    ],
    "source": [
+    "# Create test directories\n",
+    "import quilt3\n",
+    "from pathlib import Path\n",
+    "from os import chdir\n",
+    "TEST_DIR = \"test_workflow\"\n",
+    "SUB_DIR = \"subdir\"\n",
+    "\n",
+    "# create test directories\n",
+    "Path(TEST_DIR).mkdir(exist_ok=True)\n",
+    "Path(TEST_DIR, SUB_DIR).mkdir(exist_ok=True)\n",
+    "chdir(TEST_DIR) # %cd TEST_DIR/ if in Jupyter\n",
+    "\n",
     "# add entries individually using `set`\n",
     "# ie p.set(\"foo.csv\", \"/local/path/foo.csv\"),\n",
     "# p.set(\"bar.csv\", \"s3://bucket/path/bar.csv\")\n",
@@ -135,10 +149,8 @@
     "# ie p.set_dir(\"stuff/\", \"/path/to/stuff/\"),\n",
     "# p.set_dir(\"things/\", \"s3://path/to/things/\")\n",
     "\n",
-    "# create test directory\n",
-    "import os\n",
-    "os.mkdir(\"data\")\n",
-    "p.set_dir(\"stuff/\", \"./data/\")\n",
+    "# create logical directory in package\n",
+    "p.set_dir(\"stuff/\", SUB_DIR)\n",
     "p.set_dir(\"imgs/\", \"s3://quilt-example/imgs/\")"
    ]
   },
@@ -146,7 +158,8 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "The first parameter to these functions is the *logical key*, which will determine where the file lives within the package. So after running the commands above our package will look like this:"
+    "The first parameter to these functions is the *logical key*, which will determine where the file lives within the package. So after running the commands above our package will look like this:\n",
+    "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -201,7 +214,7 @@
     }
    ],
    "source": [
-    "# assuming data.csv is in the current directory\n",
+    "import quilt3\n",
     "p = quilt3.Package()\n",
     "p.set(\"data.csv\")"
    ]
@@ -210,7 +223,8 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "Another useful trick. Use `\".\"` to set the contents of the package to that of the current directory:"
+    "Another useful trick. Use `\".\"` to set the contents of the package to that of the current directory:\n",
+    "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -232,10 +246,7 @@
     }
    ],
    "source": [
-    "# switch to a test directory and create some test files\n",
-    "import os\n",
-    "%cd data/\n",
-    "os.mkdir(\"stuff\")\n",
+    "# create a test file in test directory\n",
     "with open(\"new_data.csv\", \"w\") as f:\n",
     "    f.write(\"id, value\\na, 42\")\n",
     "\n",
@@ -249,7 +260,9 @@
    "source": [
     "## Deleting data in a package\n",
     "\n",
-    "Use `delete` to remove entries from a package:"
+    "Use `delete` to remove entries from a package:",
+    "\n",
+    "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -281,7 +294,8 @@
     "\n",
     "## Adding metadata to a package\n",
     "\n",
-    "Packages support metadata anywhere in the package. To set metadata on package entries or directories, use the `meta` argument:"
+    "Packages support metadata anywhere in the package. ",
+    "To set metadata on package entries or directories, use the `meta` argument:"
    ]
   },
   {
@@ -294,7 +308,7 @@
       "text/plain": [
        "(local Package)\n",
        " └─data.csv\n",
-       " └─stuff/"
+       " └─subdir/"
       ]
      },
      "execution_count": 9,
@@ -303,16 +317,19 @@
     }
    ],
    "source": [
+    "import quilt3\n",
     "p = quilt3.Package()\n",
     "p.set(\"data.csv\", \"new_data.csv\", meta={\"type\": \"csv\"})\n",
-    "p.set_dir(\"stuff/\", \"stuff/\", meta={\"origin\": \"unknown\"})"
+    "p.set_dir(\"subdir/\", \"subdir/\", meta={\"origin\": \"unknown\"})"
    ]
   },
   {
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "You can also set metadata on the package as a whole using `set_meta`."
+    "You can also set metadata on the package as a whole using `set_meta`.",
+        "\n",
+        "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -325,7 +342,7 @@
       "text/plain": [
        "(local Package)\n",
        " └─data.csv\n",
-       " └─stuff/"
+       " └─subdir/"
       ]
      },
      "execution_count": 10,
@@ -341,7 +358,7 @@
  ],
  "metadata": {
   "kernelspec": {
-   "display_name": "Python 3",
+   "display_name": "Python 3 (ipykernel)",
    "language": "python",
    "name": "python3"
   },
@@ -355,7 +372,7 @@
    "name": "python",
    "nbconvert_exporter": "python",
    "pygments_lexer": "ipython3",
-   "version": "3.7.5"
+   "version": "3.8.9"
   }
  },
  "nbformat": 4,

docs/walkthrough/getting-data-from-a-package.ipynb
@@ -50,7 +50,9 @@
    "source": [
     "## Slicing through a package\n",
     "\n",
-    "Use `dict` key selection to slice into a package tree:"
+    "Use `dict` key selection to slice into a package tree:",
+        "\n",
+        "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -61,7 +63,9 @@
     {
      "data": {
       "text/plain": [
-       "PackageEntry('s3://quilt-example/aleksey/hurdat/requirements.txt?versionId=bQtxuZlaylNVHi0GmxkSMofT5qXJvP95')"
+       "PackageEntry('s3://quilt-example/aleksey/hurdat/requirements.txt?versionId=bQtxuZlaylNVHi0GmxkSMofT5qXJvP95')",
+           "\n",
+           "<!--pytest-codeblocks:cont-->"
       ]
      },
      "execution_count": 2,
@@ -104,7 +108,9 @@
     "\n",
     "## Downloading package data to disk\n",
     "\n",
-    "To download a subset of files from a package directory to a `dest`, use `fetch`:"
+    "To download a subset of files from a package directory to a `dest`, use `fetch`:",
+        "\n",
+        "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -155,7 +161,9 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "`fetch` will default to downloading the files to the current directory, but you can also specify an alternative path:"
+    "`fetch` will default to downloading the files to the current directory, but you can also specify an alternative path:",
+        "\n",
+        "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -191,7 +199,9 @@
    "source": [
     "## Downloading package data into memory\n",
     "\n",
-    "Alternatively, you can download data directly into memory:"
+    "Alternatively, you can download data directly into memory:",
+        "\n",
+        "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -218,7 +228,9 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "To apply a custom deserializer to your data, pass the function as a parameter to the function. For example, to load a hypothetical `yaml` file using `yaml.safe_load`:"
+    "To apply a custom deserializer to your data, pass the function as a parameter to the function. For example, to load a hypothetical `yaml` file using `yaml.safe_load`:",
+        "\n",
+        "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -251,7 +263,9 @@
     "\n",
     "## Getting entry locations\n",
     "\n",
-    "You can get the path to a package entry or directory using `get`:"
+    "You can get the path to a package entry or directory using `get`:",
+        "\n",
+        "<!--pytest-codeblocks:cont-->"
    ]
   },
   {
@@ -281,7 +295,9 @@
    "source": [
     "## Getting metadata\n",
     "\n",
-    "Metadata is available using the `meta` property."
+    "Metadata is available using the `meta` property.",
+        "\n",
+        "<!--pytest-codeblocks:cont-->"
    ]
   },
   {

docs/walkthrough/installing-a-package.ipynb
@@ -6,7 +6,10 @@
    "source": [
     "## Searching for packages\n",
     "\n",
-    "As explained in [\"Uploading a Package\"](uploading-a-package.md), packages are managed using *registries*. There is a one local registry on your machine, and potentially many remote registries elsewhere \"in the world\". Use `list_packages` to see the packages available on a registry:"
+    "As explained in [\"Uploading a Package\"](uploading-a-package.md), ",
+    "packages are managed using *registries*. ",
+    "There is a one local registry on your machine, and potentially many remote registries elsewhere \"in the world\". ",
+    "Use `list_packages` to see the packages available on a registry:"
    ]
   },
   {
@@ -28,9 +31,7 @@
     }
    ],
    "source": [
-    "import quilt3\n",
-    "\n",
-    "# list local packages\n",
+    "import quilt3 # list local packages\n",
     "list(quilt3.list_packages())"
    ]
   },
@@ -56,7 +57,7 @@
     }
    ],
    "source": [
-    "# list remote packages\n",
+    "import quilt3 # list remote packages\n",
     "list(quilt3.list_packages(\"s3://quilt-example\"))"
    ]
   },
@@ -99,6 +100,7 @@
     }
    ],
    "source": [
+    "import quilt3 # install package\n",
     "quilt3.Package.install(\n",
     "    \"examples/hurdat\",\n",
     "    \"s3://quilt-example\",\n",
@@ -110,7 +112,7 @@
    "metadata": {},
    "source": [
     "Note that unless this registry is public, you will need to be logged into a user who has read access to this registry in order to install from it:\n",
-    "\n",
+    "<!--pytest.mark.skip-->\n",
     "```python\n",
     "# only need to run this once\n",
     "# ie quilt3.config('https://your-catalog-homepage/')\n",
@@ -152,6 +154,7 @@
     }
    ],
    "source": [
+    "import quilt3  # install package into a specific folder\n",
     "quilt3.Package.install(\n",
     "    \"examples/hurdat\", \n",
     "    \"s3://quilt-example\", \n",
@@ -195,6 +198,7 @@
     }
    ],
    "source": [
+    "import quilt3  # install specific version of package\n",
     "quilt3.Package.install(\n",
     "    \"examples/hurdat\", \n",
     "    \"s3://quilt-example\", \n",
@@ -226,7 +230,7 @@
     }
    ],
    "source": [
-    "# load a package manifest from a remote registry\n",
+    "import quilt3 # load a package manifest from a remote registry\n",
     "p = quilt3.Package.browse(\"examples/hurdat\", \"s3://quilt-example\")\n",
     "\n",
     "# load a package manifest from the default remote registry\n",
@@ -272,7 +276,7 @@
  ],
  "metadata": {
   "kernelspec": {
-   "display_name": "Python 3",
+   "display_name": "Python 3 (ipykernel)",
    "language": "python",
    "name": "python3"
   },
@@ -286,7 +290,7 @@
    "name": "python",
    "nbconvert_exporter": "python",
    "pygments_lexer": "ipython3",
-   "version": "3.7.5"
+   "version": "3.8.9"
   }
  },
  "nbformat": 4,

commit 3f1245202e728f384aa1f7501a7215bf60af940d

Fix docs for quilt3.config() autoconfiguration call
Was broken in d6d9bdc44a9458789d66a050b3001f0243bf9e7e

api/python/quilt3/api.py
@@ -97,7 +97,7 @@ def config(*catalog_url, **config_values):
     To trigger autoconfiguration, call with just the navigator URL:
 
         import quilt3
-        quilt3.config(navigator_url='https://example.com')
+        quilt3.config('https://example.com')
 
     To set config values, call with one or more key=value pairs:
 