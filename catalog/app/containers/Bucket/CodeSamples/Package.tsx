7d1dd5cb21922c435fa7d18fc1373065982e9c1a
Maxim Chervonny <fiskus@chervonny.ru>
2023-06-14 13:57:10 +0300

    memoize and reduce nesting

--- catalog/app/containers/Bucket/CodeSamples/Package.tsx
+++ catalog/app/containers/Bucket/CodeSamples/Package.tsx
@@ -10,6 +10,41 @@ import * as s3paths from 'utils/s3paths'
 import Code from '../Code'
 import type { SectionProps } from '../Section'
 
+const TEMPLATES = {
+  PY: (bucket: string, path: string, hashDisplay: string) => {
+    const pathPy = path && `, path="${s3paths.ensureNoSlash(path)}"`
+    const hashPy = hashDisplay && `, top_hash="${hashDisplay}"`
+    return dedent`
+      import quilt3 as q3
+      # Browse [[${docs}/api-reference/package#package.browse]]
+      p = q3.Package.browse("${name}"${hashPy}, registry="s3://${bucket}")
+      # make changes to package adding individual files [[${docs}/api-reference/package#package.set]]
+      p.set("data.csv", "data.csv")
+      # or whole directories [[${docs}/api-reference/package#package.set_dir]]
+      p.set_dir("subdir", "subdir")
+      # and push changes [[${docs}/api-reference/package#package.push]]
+      p.push("${name}", registry="s3://${bucket}", message="Hello World")
+
+      # Download (be mindful of large packages) [[${docs}/api-reference/package#package.push]]
+      q3.Package.install("${name}"${pathPy}${hashPy}, registry="s3://${bucket}", dest=".")
+    `
+  },
+  CLI_DOWNLOAD: (bucket: string, path: string, hashDisplay: string) => {
+    const pathCli = path && ` --path "${s3paths.ensureNoSlash(path)}"`
+    const hashCli = hashDisplay && ` --top-hash ${hashDisplay}`
+    return dedent`
+      # Download package [[${docs}/api-reference/cli#install]]
+      quilt3 install "${name}"${pathCli}${hashCli} --registry s3://${bucket} --dest .
+    `
+  },
+  CLI_UPLOAD: (bucket: string, name: string) =>
+    dedent`
+      # Upload package [[${docs}/api-reference/cli#push]]
+      echo "Hello World" > README.md
+      quilt3 push "${name}" --registry s3://${bucket} --dir .
+    `,
+}
+
 interface PackageCodeSamplesProps extends Partial<SectionProps> {
   bucket: string
   name: string
@@ -26,50 +61,30 @@ export default function PackageCodeSamples({
   path,
   ...props
 }: PackageCodeSamplesProps) {
-  const pathCli = path && ` --path "${s3paths.ensureNoSlash(path)}"`
-  const pathPy = path && `, path="${s3paths.ensureNoSlash(path)}"`
   const hashDisplay = hashOrTag === 'latest' ? '' : R.take(10, hash)
-  const hashPy = hashDisplay && `, top_hash="${hashDisplay}"`
-  const hashCli = hashDisplay && ` --top-hash ${hashDisplay}`
-  const code = [
-    {
-      label: 'Python',
-      hl: 'python',
-      contents: dedent`
-        import quilt3 as q3
-        # Browse [[${docs}/api-reference/package#package.browse]]
-        p = q3.Package.browse("${name}"${hashPy}, registry="s3://${bucket}")
-        # make changes to package adding individual files [[${docs}/api-reference/package#package.set]]
-        p.set("data.csv", "data.csv")
-        # or whole directories [[${docs}/api-reference/package#package.set_dir]]
-        p.set_dir("subdir", "subdir")
-        # and push changes [[${docs}/api-reference/package#package.push]]
-        p.push("${name}", registry="s3://${bucket}", message="Hello World")
-
-        # Download (be mindful of large packages) [[${docs}/api-reference/package#package.push]]
-        q3.Package.install("${name}"${pathPy}${hashPy}, registry="s3://${bucket}", dest=".")
-      `,
-    },
-    {
-      label: 'CLI',
-      hl: 'bash',
-      contents:
-        dedent`
-          # Download package [[${docs}/api-reference/cli#install]]
-          quilt3 install "${name}"${pathCli}${hashCli} --registry s3://${bucket} --dest .
-        ` +
-        (!path
-          ? dedent`\n
-              # Upload package [[${docs}/api-reference/cli#push]]
-              echo "Hello World" > README.md
-              quilt3 push "${name}" --registry s3://${bucket} --dir .
-            `
-          : ''),
-    },
-    {
-      label: 'URI',
-      contents: PackageUri.stringify({ bucket, name, hash, path }),
-    },
-  ]
+  const code = React.useMemo(
+    () => [
+      {
+        label: 'Python',
+        hl: 'python',
+        contents: TEMPLATES.PY(bucket, path, hashDisplay),
+      },
+      {
+        label: 'CLI',
+        hl: 'bash',
+        contents: [
+          TEMPLATES.CLI_DOWNLOAD(bucket, path, hashDisplay),
+          !path ? TEMPLATES.CLI_UPLOAD(bucket, name) : '',
+        ]
+          .filter(Boolean)
+          .join('\n'),
+      },
+      {
+        label: 'URI',
+        contents: PackageUri.stringify({ bucket, name, hash, path }),
+      },
+    ],
+    [bucket, name, hashDisplay],
+  )
   return <Code {...props}>{code}</Code>
 }
