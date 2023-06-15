655401d9b413b99340cd17c56e4ab40da620e196
Maxim Chervonny <fiskus@chervonny.ru>
2023-06-15 16:08:03 +0300

    move adding script to on general function

--- catalog/app/utils/gtm.tsx
+++ catalog/app/utils/gtm.tsx
@@ -2,20 +2,27 @@ import * as React from 'react'
 
 import cfg from 'constants/config'
 
-function loadScript(src: string) {
-  const s = window.document.createElement('script')
-  s.type = 'text/javascript'
-  s.async = true
-  s.src = src
-
-  const x = window.document.getElementsByTagName('script')[0]
-  x.parentNode?.insertBefore(s, x)
+interface SupportedAttrs {
+  async?: boolean
+  innerText: string
+  src: string
 }
 
-function addScript(content: string) {
+function addScript(props: Pick<SupportedAttrs, 'innerText'>): void
+function addScript(props: Pick<SupportedAttrs, 'async' | 'src'>): void
+function addScript({ async, innerText, src }: Partial<SupportedAttrs>) {
   const s = window.document.createElement('script')
   s.type = 'text/javascript'
-  s.innerText = content
+
+  if (innerText) {
+    s.innerText = innerText
+  }
+  if (async) {
+    s.async = async
+  }
+  if (src) {
+    s.src = src
+  }
   const x = window.document.getElementsByTagName('script')[0]
   x.parentNode?.insertBefore(s, x)
 }
@@ -28,13 +35,18 @@ export default function GTMLoader({ children }: GTMLoaderProps) {
   const gtmId = cfg.gtmId
   React.useEffect(() => {
     if (!gtmId) return
-    loadScript(`https://www.googletagmanager.com/gtag/js?id=${gtmId}`)
-    addScript(`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${gtmId}');
-    `)
+    addScript({
+      async: true,
+      src: `https://www.googletagmanager.com/gtag/js?id=${gtmId}`,
+    })
+    addScript({
+      innerText: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtmId}');
+      `.replace(/\n/g, ' '),
+    })
   }, [gtmId])
   return <>{children}</>
 }
