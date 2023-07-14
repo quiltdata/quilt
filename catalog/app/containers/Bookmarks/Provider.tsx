62576e5f9919fa1b2fe46192f65421a1f37a8537
Maksim Chervonnyi <max@quiltdata.io>
2023-07-14 16:26:01 +0300

    fix hook dependencies

--- catalog/app/containers/Bookmarks/Provider.tsx
+++ catalog/app/containers/Bookmarks/Provider.tsx
@@ -125,7 +125,7 @@ export function Provider({ children }: ProviderProps) {
         setUpdates(true)
       }
     },
-    [groups, isOpened, updateGroups],
+    [isOpened, updateGroups],
   )
   const clear = React.useCallback(
     (groupName: GroupName) => {
