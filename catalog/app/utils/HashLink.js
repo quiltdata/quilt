0042f10454736f98acc882468a1e2eea1361ca4d
Maxim Chervonny <fiskus@chervonny.ru>
2023-07-26 13:21:39 +0300

    adjust createHashLink usage

--- catalog/app/utils/HashLink.js
+++ catalog/app/utils/HashLink.js
@@ -1,5 +1,4 @@
-import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { genericHashLink } from 'react-router-hash-link'
 
-export default React.forwardRef((props, ref) => genericHashLink({ ...props, ref }, Link))
+export default genericHashLink(Link)
