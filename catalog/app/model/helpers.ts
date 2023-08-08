71254ca7a4f28c19b858fbf23ade40c9af65cfaf
Maxim Chervonny <fiskus@chervonny.ru>
2023-08-08 15:09:34 +0300

    cosmetic

--- catalog/app/model/helpers.ts
+++ catalog/app/model/helpers.ts
@@ -1,14 +1,16 @@
 import type { PackageHash, PackageHashAlias, PackageRevision } from './index'
 
-export function isPackageHash(h: PackageRevision): h is PackageHash {
-  return !!h.value
+export function isPackageHash(rev: PackageRevision): rev is PackageHash {
+  return !!rev.value
 }
 
-export function isPackageHashAlias(h: PackageRevision): h is PackageHashAlias {
-  return !!h.alias
+export function isPackageHashAlias(rev: PackageRevision): rev is PackageHashAlias {
+  return !!rev.alias
 }
 
 export function hashOrTag(rev: PackageRevision): string {
+  // TODO
+  // return rev.value || rev.alias
   if (isPackageHash(rev)) return rev.value
   return rev.alias
 }
