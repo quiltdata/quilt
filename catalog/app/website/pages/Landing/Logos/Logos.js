98add134d05cc7285293f2f48eeded6936a895c7
Maxim Chervonny <fiskus@chervonny.ru>
2023-09-06 11:22:49 +0300

    Remove VIR logo from landing

--- catalog/app/website/pages/Landing/Logos/Logos.js
+++ catalog/app/website/pages/Landing/Logos/Logos.js
@@ -8,16 +8,11 @@ import logoNeumora from './logo-neumora.png'
 import logoObsidian from './logo-obsidian.png'
 import logoSight from './logo-sight.png'
 import logoStemson from './logo-stemson.png'
-import logoVir from './logo-vir.png'
 
 const logos = [
   {
     src: logoCelsius,
     title: 'Celsius Therapeutics',
-  },
-  {
-    src: logoVir,
-    title: 'Vir Bio',
   },
   {
     src: logoNeumora,
