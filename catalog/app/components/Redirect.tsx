349ba5928009a730bca05a1f38279dcc00270619
Maxim Chervonny <fiskus@chervonny.ru>
2023-10-02 11:05:20 +0300

    tsify Redirect

--- catalog/app/components/Redirect.tsx
+++ catalog/app/components/Redirect.tsx
@@ -1,10 +1,13 @@
-/* Redirect */
 import * as React from 'react'
 
 import redirect from 'utils/redirect'
 import Working from 'components/Working'
 
-export default ({ url }) => {
+interface RedirectProps {
+  url: string
+}
+
+export default function Redirect({ url }: RedirectProps) {
   React.useEffect(() => {
     redirect(url)
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
