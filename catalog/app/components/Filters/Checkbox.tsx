75530560a5b50e1578ec09a8c0929eebfb002111
Maksim Chervonnyi <max@quiltdata.io>
2023-10-06 13:58:52 +0300

    better layout for boolean/checkbox

--- catalog/app/components/Filters/Checkbox.tsx
+++ catalog/app/components/Filters/Checkbox.tsx
@@ -1,11 +1,15 @@
+import cx from 'classnames'
 import * as React from 'react'
 import * as M from '@material-ui/core'
 
-const useStyles = M.makeStyles({
-  checkbox: {
-    margin: '-2px',
+const useStyles = M.makeStyles((t) => ({
+  root: {
+    paddingLeft: t.spacing(0.5),
   },
-})
+  checkbox: {
+    margin: t.spacing(-1, 0),
+  },
+}))
 
 interface CheckboxFilterProps {
   value: boolean | undefined
@@ -16,15 +20,21 @@ interface CheckboxProps
   extends Omit<M.FormControlLabelProps, keyof CheckboxFilterProps | 'control'>,
     CheckboxFilterProps {}
 
-export default function Checkbox({ value, onChange, ...props }: CheckboxProps) {
+export default function Checkbox({
+  className,
+  value,
+  onChange,
+  ...props
+}: CheckboxProps) {
   const classes = useStyles()
   return (
     <M.FormControlLabel
+      className={cx(classes.root, className)}
       {...props}
       control={
         <M.Checkbox
-          className={classes.checkbox}
           checked={value}
+          className={classes.checkbox}
           onChange={(event, newValue) => onChange(newValue)}
           size="small"
         />
