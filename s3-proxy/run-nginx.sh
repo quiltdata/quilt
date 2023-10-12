827b74bc917fe6ea5d4ac949e9cb07e74110a1a3
Dima Ryazanov <dima@gmail.com>
2023-10-11 18:45:53 -0700

    Remove workarounds for connecting to an ELB
    No longer needed, since s3-proxy will be connecting to the registry directly.

--- s3-proxy/run-nginx.sh
+++ s3-proxy/run-nginx.sh
@@ -13,12 +13,6 @@ then
     exit 1
 fi
 
-cp /etc/ssl/cert.pem /etc/nginx/certs.pem
-if [ $CERTIFICATE_ARN ]
-then
-    aws --region $(echo $CERTIFICATE_ARN | cut -d ":" -f 4) acm get-certificate --certificate-arn $CERTIFICATE_ARN --output text --query "join('', [Certificate, CertificateChain])" >> /etc/nginx/certs.pem
-fi
-
 INTERNAL_REGISTRY_URL=${INTERNAL_REGISTRY_URL%%/} # Remove a trailing slash.
 
 # Get the DNS server from /etc/resolv.conf
@@ -32,6 +26,6 @@ fi
 
 export NAMESERVER=$nameserver
 
-envsubst '$REGISTRY_HOST $INTERNAL_REGISTRY_URL $NAMESERVER' < /root/nginx.conf.tmpl > /etc/nginx/nginx.conf
+envsubst '$INTERNAL_REGISTRY_URL $NAMESERVER' < /root/nginx.conf.tmpl > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
