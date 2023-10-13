1e542b6e3802b34cd3c1af9f25b70d409ae229d1
nl_0 <nl.imbecile@gmail.com>
2023-10-13 10:47:13 +0200

    rm model/Search

--- catalog/app/model/Search.ts
+++
@@ -1,5 +0,0 @@
-// import * as Types from 'utils/types'
-
-// model of faceted search domain
-
-export interface DUMMY {}
--- catalog/app/model/index.ts
+++ catalog/app/model/index.ts
@@ -8,7 +8,6 @@ import * as GQLTypes from './graphql/types.generated'
 export * as GQLTypes from './graphql/types.generated'
 
 export * as S3 from './S3'
-export * as Search from './Search'
 
 export const BucketPermissionLevel = Types.enum(
   GQLTypes.BucketPermissionLevel,
