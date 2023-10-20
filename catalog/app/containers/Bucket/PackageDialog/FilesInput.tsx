shabss/quiltdata-quilt

multipart checksums

catalog/app/containers/Bucket/PackageDialog/FilesInput.tsx
@@ -1,5 +1,4 @@
 import cx from 'classnames'
-import pLimit from 'p-limit'
 import * as R from 'ramda'
 import * as React from 'react'
 import { useDropzone, FileWithPath } from 'react-dropzone'
@@ -12,6 +11,7 @@ import * as urls from 'constants/urls'
 import type * as Model from 'model'
 import StyledLink from 'utils/StyledLink'
 import assertNever from 'utils/assertNever'
+import computeFileChecksum from 'utils/checksums'
 import dissocBy from 'utils/dissocBy'
 import useDragging from 'utils/dragging'
 import { withoutPrefix } from 'utils/s3paths'
@@ -35,20 +35,18 @@ const COLORS = {
 interface FileWithHash extends File {
   hash: {
     ready: boolean
-    value?: string
+    value?: Model.PackageEntryHash
     error?: Error
-    promise: Promise<string | undefined>
+    promise: Promise<Model.PackageEntryHash | undefined>
   }
   meta?: Types.JsonRecord
 }
 
 const hasHash = (f: File): f is FileWithHash => !!f && !!(f as FileWithHash).hash
 
-const hashLimit = pLimit(2)
-
 export function computeHash(f: File) {
   if (hasHash(f)) return f
-  const hashP = hashLimit(PD.hashFile, f)
+  const hashP = computeFileChecksum(f)
   const fh = f as FileWithHash
   fh.hash = { ready: false } as any
   fh.hash.promise = hashP
@@ -61,10 +59,10 @@ export function computeHash(f: File) {
       fh.hash.ready = true
       return undefined
     })
-    .then((hash) => {
-      fh.hash.value = hash
+    .then((checksum) => {
+      fh.hash.value = checksum
       fh.hash.ready = true
-      return hash
+      return checksum
     })
   return fh
 }
@@ -306,7 +304,7 @@ const computeEntries = ({
           // eslint-disable-next-line no-nested-ternary
           state = !a.hash.ready
             ? ('hashing' as const)
-            : a.hash.value === hash
+            : R.equals(a.hash.value, hash)
             ? ('unchanged' as const)
             : ('modified' as const)
         }
@@ -1473,7 +1471,7 @@ export function FilesInput({
       (acc, [path, f]) => {
         if (S3FilePicker.isS3File(f)) return acc // dont count s3 files
         const e = existing[path]
-        if (e && (!f.hash.ready || f.hash.value === e.hash)) return acc
+        if (e && (!f.hash.ready || R.equals(f.hash.value, e.hash))) return acc
         return R.evolve({ count: R.inc, size: R.add(f.size) }, acc)
       },
       { count: 0, size: 0 },

catalog/app/model/index.ts
@@ -48,11 +48,18 @@ export type Collaborators = ReadonlyArray<
 // Note that the actual user-defined meta is in the `user_meta` field
 export type EntryMeta = (Types.JsonRecord & { user_meta?: Types.JsonRecord }) | null
 
+export const HASH_TYPE_SP = 'SHA256' as const
+export const HASH_TYPE_MP = 'QuiltMultipartSHA256' as const
+export interface PackageEntryHash {
+  type: typeof HASH_TYPE_SP | typeof HASH_TYPE_MP
+  value: string
+}
+
 export interface PackageEntry {
   // TODO: replace with { address: { physicalKey: string }}
   //       so, you can merge PackageEntry and S3File
   physicalKey: string
-  hash: string
+  hash: PackageEntryHash
   meta?: EntryMeta
   size: number
 }

quiltdata/quilt

catalog/app/containers/Bucket/PackageDialog/FilesInput.tsx
@@ -1,5 +1,4 @@
 import cx from 'classnames'
-import pLimit from 'p-limit'
 import * as R from 'ramda'
 import * as React from 'react'
 import { useDropzone, FileWithPath } from 'react-dropzone'
@@ -11,6 +10,7 @@ import * as urls from 'constants/urls'
 import type * as Model from 'model'
 import StyledLink from 'utils/StyledLink'
 import assertNever from 'utils/assertNever'
+import computeFileChecksum from 'utils/checksums'
 import dissocBy from 'utils/dissocBy'
 import useDragging from 'utils/dragging'
 import { readableBytes } from 'utils/string'
@@ -33,20 +33,18 @@ const COLORS = {
 interface FileWithHash extends File {
   hash: {
     ready: boolean
-    value?: string
+    value?: Model.Checksum
     error?: Error
-    promise: Promise<string | undefined>
+    promise: Promise<Model.Checksum | undefined>
   }
   meta?: Types.JsonRecord
 }
 
 const hasHash = (f: File): f is FileWithHash => !!f && !!(f as FileWithHash).hash
 
-const hashLimit = pLimit(2)
-
 export function computeHash(f: File) {
   if (hasHash(f)) return f
-  const hashP = hashLimit(PD.hashFile, f)
+  const hashP = computeFileChecksum(f)
   const fh = f as FileWithHash
   fh.hash = { ready: false } as any
   fh.hash.promise = hashP
@@ -59,10 +57,10 @@ export function computeHash(f: File) {
       fh.hash.ready = true
       return undefined
     })
-    .then((hash) => {
-      fh.hash.value = hash
+    .then((checksum) => {
+      fh.hash.value = checksum
       fh.hash.ready = true
-      return hash
+      return checksum
     })
   return fh
 }
@@ -296,7 +294,7 @@ const computeEntries = ({
           // eslint-disable-next-line no-nested-ternary
           state = !a.hash.ready
             ? ('hashing' as const)
-            : a.hash.value === hash
+            : R.equals(a.hash.value, hash)
             ? ('unchanged' as const)
             : ('modified' as const)
         }
@@ -1455,7 +1453,7 @@ export function FilesInput({
       (acc, [path, f]) => {
         if (S3FilePicker.isS3File(f)) return acc // dont count s3 files
         const e = existing[path]
-        if (e && (!f.hash.ready || f.hash.value === e.hash)) return acc
+        if (e && (!f.hash.ready || R.equals(f.hash.value, e.hash))) return acc
         return R.evolve({ count: R.inc, size: R.add(f.size) }, acc)
       },
       { count: 0, size: 0 },

catalog/app/model/index.ts
@@ -48,11 +48,18 @@ export type Collaborators = ReadonlyArray<
 // Note that the actual user-defined meta is in the `user_meta` field
 export type EntryMeta = (Types.JsonRecord & { user_meta?: Types.JsonRecord }) | null
 
+export const CHECKSUM_TYPE_SP = 'SHA256' as const
+export const CHECKSUM_TYPE_MP = 'QuiltMultipartSHA256' as const
+export interface Checksum {
+  type: typeof CHECKSUM_TYPE_SP | typeof CHECKSUM_TYPE_MP
+  value: string
+}
+
 export interface PackageEntry {
   // TODO: replace with { address: { physicalKey: string }}
   //       so, you can merge PackageEntry and S3File
   physicalKey: string
-  hash: string
+  hash: Checksum
   meta?: EntryMeta
   size: number
 }