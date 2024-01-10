ef7bc0f6b05a96ef4b58cd557a332510f4042396
Maksim Chervonnyi <max@quiltdata.io>
2024-01-10 13:09:43 +0100

    split checksums tests into two test, so i can mock `cfg` effortlessly

---
+++ catalog/app/utils/checksums-legacy.spec.ts
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import path from 'path'
+import util from 'util'
+
+import computeFileChecksumLimit from './checksums'
+
+describe('utils/checksums', () => {
+  describe('computeFileChecksumLimit, legacy and singlepart hashing', () => {
+    test('Test singlepart hashing', async () => {
+      // Package manifest: https://open.quiltdata.com/b/allencell/tree/.quilt/packages/7acdd948d565d1f22c10f0d5ec4ae99742f04a4849c5e1498f252a0ac1ddeb04
+      // File in that package: https://open.quiltdata.com/b/allencell/tree/aics/wtc11_short_read_genome_sequence/README.md?version=qt7oZnXdqJ0vokH1MpXksOiwgqOPPHV2
+      const fileContents = '# This is a test package\n'
+      const file = new File([Buffer.from(fileContents)], 'junk/s3.js')
+      await expect(computeFileChecksumLimit(file)).resolves.toEqual({
+        value: 'e5c0b36103e96037f4892e73e457ad62753c1c5ad50c3bb0610fad268666f1ea',
+        type: 'SHA256',
+      })
+    })
+    test('Test legacy >8Mb hashing', async () => {
+      // Package manifest: https://open.quiltdata.com/b/allencell/tree/.quilt/packages/38886848f1bad99396b96157101dd52520fa6aae0479adb9de4bde2b12997d92
+      // File in that package: https://open.quiltdata.com/b/allencell/tree/.quilt/packages/38886848f1bad99396b96157101dd52520fa6aae0479adb9de4bde2b12997d92
+      const readFile = util.promisify(fs.readFile)
+      const contents = await readFile(path.join(__dirname, './checksums-11mb-test.png'))
+      const file = new File(
+        [contents],
+        'master/diagnosticsheets/diagnostic_sheets/ProteinDisplayName_Alpha-actinin-1_1.png',
+      )
+      await expect(computeFileChecksumLimit(file)).resolves.toEqual({
+        value: '8fd93402941dead341a75ac813d4894e9121ac3ee4107adfd5848ec09a318c84',
+        type: 'SHA256',
+      })
+    })
+  })
+})
--- catalog/app/utils/checksums.spec.ts
+++ catalog/app/utils/checksums.spec.ts
@@ -1,17 +1,38 @@
+import fs from 'fs'
+import path from 'path'
+import util from 'util'
+
 import computeFileChecksumLimit from './checksums'
 
-describe('utils/checksums', () => {
-  describe('computeFileChecksumLimit', () => {
-    test('test', async () => {
-      const fileContents = `document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('s3').textContent = 'Yes!'
-})
+jest.mock(
+  'constants/config',
+  jest.fn(() => ({
+    multipartChecksums: true,
+  })),
+)
 
-`
+describe('utils/checksums', () => {
+  describe('computeFileChecksumLimit, multipart hashing', () => {
+    test('Test singlepart hashing', async () => {
+      // Package manifest: https://open.quiltdata.com/b/allencell/tree/.quilt/packages/7acdd948d565d1f22c10f0d5ec4ae99742f04a4849c5e1498f252a0ac1ddeb04
+      // File in that package: https://open.quiltdata.com/b/allencell/tree/aics/wtc11_short_read_genome_sequence/README.md?version=qt7oZnXdqJ0vokH1MpXksOiwgqOPPHV2
+      const fileContents = '# This is a test package\n'
       const file = new File([Buffer.from(fileContents)], 'junk/s3.js')
       await expect(computeFileChecksumLimit(file)).resolves.toEqual({
-        value: '8f843e82ea8248383fac42cdf9419955d0c7371e18665abb97e0d4ff02048f36',
+        value: 'e5c0b36103e96037f4892e73e457ad62753c1c5ad50c3bb0610fad268666f1ea',
         type: 'SHA256',
+      })
+    })
+    test('Test multipart >8Mb hashing', async () => {
+      const readFile = util.promisify(fs.readFile)
+      const contents = await readFile(path.join(__dirname, './checksums-11mb-test.png'))
+      const file = new File(
+        [contents],
+        'master/diagnosticsheets/diagnostic_sheets/ProteinDisplayName_Alpha-actinin-1_1.png',
+      )
+      await expect(computeFileChecksumLimit(file)).resolves.toEqual({
+        value: '7y6ba70KDRATjq9HezYMTKLluvYXP5g+CSJL9EwnjiY=-2',
+        type: 'QuiltMultipartSHA256',
       })
     })
   })
