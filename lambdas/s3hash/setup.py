3358878fac872598f7545ca62801b936ee009ab9
nl_0 <nl.imbecile@gmail.com>
2024-01-11 15:09:11 +0100

    s3hash: dockerify

--- lambdas/s3hash/setup.py
+++ lambdas/s3hash/setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    name='s3hash',
-    version='0.0.1',
-    py_modules=['index'],
+    name="t4_lambda_s3hash",
+    version="0.0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "aiobotocore==2.7.0",
+        "botocore==1.31.64",
+        "pydantic==1.10.13",
+        "tenacity==6.2.0",
+        "types-aiobotocore[s3]==2.7.0",
+    ],
 )
