20189a1a60b2be4b3030b0fc6357f993a4a24677
nl_0 <nl.imbecile@gmail.com>
2024-01-11 15:17:28 +0100

    dockerify pkgpush lambda

--- lambdas/pkgpush/setup.py
+++ lambdas/pkgpush/setup.py
@@ -1,8 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='t4_lambda_pkgpush',
-    version='0.0.1',
+    name="t4_lambda_pkgpush",
+    version="0.0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_requires=[
+        "boto3==1.28.67",
+        "pydantic==1.10.13",
+        "quilt3==5.4.0",
+    ],
 )
