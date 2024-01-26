7fef9511564f03eb011c48656e79505369259888
nl_0 <nl.imbecile@gmail.com>
2024-01-26 12:43:00 +0100

    pkgpush: add py-shared, relax deps, pip-compile

--- lambdas/pkgpush/setup.py
+++ lambdas/pkgpush/setup.py
@@ -6,8 +6,10 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "boto3==1.28.67",
-        "pydantic==1.10.13",
-        "quilt3==5.4.0",
+        "boto3~=1.28",
+        "pydantic~=1.10",
+        "quilt3~=5.4",
+        "quilt_shared[pydantic,boto,quilt] @ git+https://github.com/quiltdata/quilt@37bd19c580c73f5272f4c85d4e3de3fdc92c561b#subdirectory=py-shared",
+
     ],
 )
