1aa9101e5c40608309f1cdc9647a8653687ba337
nl_0 <nl.imbecile@gmail.com>
2024-01-26 15:50:21 +0100

    s3hash: depend on quilt-shared, update dependencies

--- lambdas/s3hash/setup.py
+++ lambdas/s3hash/setup.py
@@ -6,10 +6,15 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "aiobotocore==2.7.0",
-        "botocore==1.31.64",
-        "pydantic==1.10.13",
-        "tenacity==6.2.0",
-        "types-aiobotocore[s3]==2.7.0",
+        "aiobotocore ~= 2.11",
+        "botocore ~= 1.31",
+        "pydantic ~= 1.10",
+        "tenacity ~= 6.2",
+        "types-aiobotocore[s3] ~= 2.11",
+        (
+            "quilt_shared[pydantic,boto,quilt] @ git+https://github.com/quiltdata/quilt@"
+            "e95cde71e45458c5eca4aa521678b6ba75b6528f"
+            "#subdirectory=py-shared"
+        ),
     ],
 )
