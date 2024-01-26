2b0ce17d73ce5e8079b1c73bc7556445fd702156
nl_0 <nl.imbecile@gmail.com>
2024-01-26 13:17:13 +0100

    cut up the line

--- lambdas/pkgpush/setup.py
+++ lambdas/pkgpush/setup.py
@@ -9,7 +9,10 @@ setup(
         "boto3~=1.28",
         "pydantic~=1.10",
         "quilt3~=5.4",
-        "quilt_shared[pydantic,boto,quilt] @ git+https://github.com/quiltdata/quilt@37bd19c580c73f5272f4c85d4e3de3fdc92c561b#subdirectory=py-shared",
-
+        (
+            "quilt_shared[pydantic,boto,quilt] @ git+https://github.com/quiltdata/quilt@"
+            "37bd19c580c73f5272f4c85d4e3de3fdc92c561b"
+            "#subdirectory=py-shared"
+        ),
     ],
 )
