ec20a41af18d9d32b2c9827d2237c1cf4d20bdfc
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2024-02-09 15:42:14 +0400

    bump quilt3 in pkgpush (#3870)

--- lambdas/pkgpush/CHANGELOG.md
+++ lambdas/pkgpush/CHANGELOG.md
@@ -16,5 +16,6 @@ where verb is one of
 
 ## Changes
 
+- [Changed] Bump quilt3 to set max_pool_connections, this improves performance ([#3870](https://github.com/quiltdata/quilt/pull/3870))
 - [Changed] Compute multipart checksums ([#3402](https://github.com/quiltdata/quilt/pull/3402))
 - [Added] Bootstrap the change log ([#3402](https://github.com/quiltdata/quilt/pull/3402))
--- lambdas/pkgpush/setup.py
+++ lambdas/pkgpush/setup.py
@@ -8,7 +8,11 @@ setup(
     install_requires=[
         "boto3 ~= 1.28",
         "pydantic ~= 1.10",
-        "quilt3 ~= 5.4",
+        (
+            "quilt3 @ git+https://github.com/quiltdata/quilt@"
+            "299b1da851004386ab43423172c4405997fd9c53"
+            "#subdirectory=api/python"
+        ),
         (
             "quilt_shared[pydantic,boto,quilt] @ git+https://github.com/quiltdata/quilt@"
             "33b7e601baee6dff4ef32342b493f55d3dd16210"
