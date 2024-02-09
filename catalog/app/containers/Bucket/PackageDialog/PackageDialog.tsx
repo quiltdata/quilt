f14137c4c2cba5d6f213cf12b6c6923cc22373bb
Alexei Mochalov <nl_0@quiltdata.io>
2024-02-09 16:18:25 +0100

    catalog: adjust package / file size limits (#3871)

--- catalog/app/containers/Bucket/PackageDialog/PackageDialog.tsx
+++ catalog/app/containers/Bucket/PackageDialog/PackageDialog.tsx
@@ -33,9 +33,10 @@ import PACKAGE_EXISTS_QUERY from './gql/PackageExists.generated'
 
 export const MAX_UPLOAD_SIZE = 20 * 1000 * 1000 * 1000 // 20GB
 // XXX: keep in sync w/ the backend
+// NOTE: these limits are lower than the actual "hard" limits on the backend
 export const MAX_S3_SIZE = cfg.multipartChecksums
-  ? 50 * 2 ** 40 // 100 TiB
-  : 50 * 10 ** 9 // 50GB
+  ? 10 * 10 ** 12 // 10 TB
+  : 50 * 10 ** 9 // 50 GB
 export const MAX_FILE_COUNT = 1000
 
 export const ERROR_MESSAGES = {
