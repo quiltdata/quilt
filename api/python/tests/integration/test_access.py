d8562ac605c9b33ae4b862c497d6b645df91810a
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2024-04-02 16:40:50 -0700

    write to quilt-sales-staging

--- api/python/tests/integration/test_access.py
+++ api/python/tests/integration/test_access.py
@@ -9,7 +9,8 @@ from tests.utils import QuiltTestCase
 
 NOW = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 BKT = "850787717197-1867753-fepwgrx9iujr5b9pkjudkhpgxwbuhuse1b-s3alias"
-REG = f"s3://{BKT}"
+SOURCE = f"s3://{BKT}"
+DEST = "s3://quilt-sales-staging"
 FOLDER = "850787717197/sequenceStore/1867753048/readSet/5447294294"
 FILE = "U0a_CGATGT_L001_R1_004.fastq.gz"
 KEY = f"{FOLDER}/{FILE}"
@@ -32,22 +33,20 @@ class AccessTest(QuiltTestCase):
         print(f"list_bucket: {list_bucket}")
 
     def test_list_object_versions(self):
-        print(f"test_list_object_versions for REG: {REG}")
-        versions = list_object_versions(REG, FOLDER+"/", False)
+        print(f"test_list_object_versions for SOURCE: {SOURCE}")
+        versions = list_object_versions(SOURCE, FOLDER+"/", False)
         print(f"versions: {versions}")
         assert versions
 
     def test_package(self):
-        # /Users/ernest/GitHub/quilt/api/python/quilt3/workflows/__init__.py:297: in validate
-
-        uri = f"{REG}/{KEY}"
+        uri = f"{SOURCE}/{KEY}"
         split = FOLDER.split("/")
         pkg_name = f"{split[-2]}/{split[-1]}"
         msg = f"Today's Date: {NOW}"
 
         pkg = q3.Package()
         try:
-            pkg = pkg.browse(pkg_name, registry=REG)
+            pkg = pkg.browse(pkg_name, registry=SOURCE)
         except Exception as e:
             print(f"Error browsing package: {e}")
         print(f"Package: {pkg}")
@@ -55,16 +54,14 @@ class AccessTest(QuiltTestCase):
         print(f"S3 URI: {uri} @ {msg}")
         pkg.set_dir("/", uri, meta={"timestamp": NOW})
 
-        print(f"Package.push: {pkg_name}")
-        rc = pkg.push(pkg_name, registry=REG, message=msg)
+        PKG_URI = f"quilt+{DEST}#package={pkg_name}"
+        print(f"Pushing {pkg_name} to {DEST}: {PKG_URI}")
+        rc = pkg.push(pkg_name, registry=DEST, message=msg)
         assert rc
-
-        PKG_URI = f"quilt+{REG}#package={pkg_name}"
-        print(f"Package {pkg_name} pushed to {REG}: {PKG_URI}")
 
     @pytest.mark.skip(reason="Not sure this is needed")
     def test_bucket(self):
-        registry = REG
+        registry = SOURCE
         path = KEY
         os.environ["AWS_PROFILE"] = "sales"
         client = getattr(S3ClientProvider(), "standard_client")
