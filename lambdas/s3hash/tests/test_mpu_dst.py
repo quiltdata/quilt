22103f48976f5a521d40f1e11b4cc9d6ae48c04c
nl_0 <nl.imbecile@gmail.com>
2024-04-05 12:23:46 +0200

    more tests

--- lambdas/s3hash/tests/test_mpu_dst.py
+++ lambdas/s3hash/tests/test_mpu_dst.py
@@ -4,7 +4,6 @@ import pytest
 import t4_lambda_s3hash as s3hash
 
 
-@pytest.mark.asyncio
 async def test_get_bucket_region_valid(s3_stub: Stubber):
     expected_region = "us-west-2"
     bucket_name = "test-bucket"
@@ -21,7 +20,6 @@ async def test_get_bucket_region_valid(s3_stub: Stubber):
     assert region == expected_region
 
 
-@pytest.mark.asyncio
 async def test_get_bucket_region_exist_error(s3_stub: Stubber):
     bucket_name = "existent-bucket"
     expected_region = "us-west-2"
@@ -38,7 +36,6 @@ async def test_get_bucket_region_exist_error(s3_stub: Stubber):
     assert region == expected_region
 
 
-@pytest.mark.asyncio
 async def test_get_bucket_region_nonexist_error(s3_stub: Stubber):
     bucket_name = "non-existent-bucket"
     s3_stub.add_client_error(
@@ -50,3 +47,41 @@ async def test_get_bucket_region_nonexist_error(s3_stub: Stubber):
     with pytest.raises(Exception) as exc_info:
         await s3hash.get_bucket_region(bucket_name)
     assert "Not Found" in str(exc_info.value)
+
+
+async def test_get_mpu_dst_for_location_valid(s3_stub: Stubber):
+    src_loc = s3hash.S3ObjectSource(bucket="source-bucket", key="source-key", version="source-version")
+    scratch_buckets = {"us-west-2": "scratch-bucket-us-west-2"}
+    expected_dst = s3hash.S3ObjectDestination(bucket="scratch-bucket-us-west-2", key=s3hash.SCRATCH_KEY)
+
+    s3_stub.add_response(
+        method="head_bucket",
+        expected_params={"Bucket": src_loc.bucket},
+        service_response={
+            "ResponseMetadata": {
+                "HTTPHeaders": {"x-amz-bucket-region": "us-west-2"},
+            },
+        },
+    )
+
+    dst = await s3hash.get_mpu_dst_for_location(src_loc, scratch_buckets)
+    assert dst == expected_dst
+
+
+async def test_get_mpu_dst_for_location_no_scratch_bucket(s3_stub: Stubber):
+    src_loc = s3hash.S3ObjectSource(bucket="source-bucket", key="source-key", version="source-version")
+    scratch_buckets = {"us-east-1": "scratch-bucket-us-east-1"}
+
+    s3_stub.add_response(
+        method="head_bucket",
+        expected_params={"Bucket": src_loc.bucket},
+        service_response={
+            "ResponseMetadata": {
+                "HTTPHeaders": {"x-amz-bucket-region": "us-west-2"},
+            },
+        },
+    )
+
+    with pytest.raises(Exception) as exc_info:
+        await s3hash.get_mpu_dst_for_location(src_loc, scratch_buckets)
+    assert "ScratchBucketNotFound" in str(exc_info.value)
