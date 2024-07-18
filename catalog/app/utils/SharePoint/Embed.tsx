f2e30176ae8a7cdc6dec6c2847211b5a7cd9b6a9
Maksim Chervonnyi <mail@redmax.dev>
2024-07-18 16:09:30 +0200

    skeleton

--- catalog/app/utils/SharePoint/Embed.tsx
+++ catalog/app/utils/SharePoint/Embed.tsx
@@ -1,12 +1,42 @@
 import * as React from 'react'
 import * as M from '@material-ui/core'
 
+import Skeleton from 'components/Skeleton'
 import cfg from 'constants/config'
 import type * as Model from 'model'
 
 import { useSharePoint } from './Provider'
 import { postSigned } from './requests'
 import getToken from './token'
+
+const useEmbedSkeletonStyles = M.makeStyles((t) => ({
+  root: {
+    // display: 'flex',
+    // flexDirection: 'column',
+  },
+  header: {
+    height: t.spacing(6),
+  },
+  body: {
+    padding: '20px',
+  },
+  content: {
+    height: t.spacing(70),
+  },
+}))
+
+function EmbedSkeleton() {
+  const classes = useEmbedSkeletonStyles()
+
+  return (
+    <div className={classes.root}>
+      <Skeleton className={classes.header} />
+      <div className={classes.body}>
+        <Skeleton className={classes.content} />
+      </div>
+    </div>
+  )
+}
 
 async function getEmbedUrl(
   authToken: string,
@@ -34,13 +64,5 @@ export default function Embed({ loc }: EmbedProps) {
     }
     loadEmbedUrl()
   }, [loc, msal])
-  return (
-    <h1>
-      {uri ? (
-        <iframe width="100%" height="600px" src={uri} />
-      ) : (
-        <M.CircularProgress size={96} />
-      )}
-    </h1>
-  )
+  return uri ? <iframe width="100%" height="600px" src={uri} /> : <EmbedSkeleton />
 }
