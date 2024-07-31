f329fd568b7af2e5810d6de4e7fad7f1ee1972db
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2024-07-31 14:10:09 +0200

    Use GitHub zip archives instead of git for deps (#4061)

--- lambdas/s3hash/setup.py
+++ lambdas/s3hash/setup.py
@@ -11,8 +11,8 @@ setup(
         "pydantic ~= 1.10",
         "types-aiobotocore[s3] ~= 2.11",
         (
-            "quilt_shared[pydantic,boto,quilt] @ git+https://github.com/quiltdata/quilt@"
-            "7c6edd14fbe8a26613bc26b1bbdc0b956132ef8c"
+            "quilt_shared[pydantic,boto,quilt] @ https://github.com/quiltdata/quilt/archive/"
+            "7c6edd14fbe8a26613bc26b1bbdc0b956132ef8c.zip"
             "#subdirectory=py-shared"
         ),
     ],
