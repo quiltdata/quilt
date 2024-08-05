89b8d62a1307f30a1c9e85c8001101d7a67f7667
nl_0 <nl.imbecile@gmail.com>
2024-08-05 12:57:02 +0200

    route en/decoding test

--- catalog/app/components/Assistant/Model/GlobalContext/navigation.spec.ts
+++ catalog/app/components/Assistant/Model/GlobalContext/navigation.spec.ts
@@ -17,17 +17,37 @@ describe('components/Assistant/Model/GlobalTools/navigation', () => {
     })
   })
   describe('NavigableRouteSchema', () => {
-    it('should decode input', () => {
+    it('should decode input', async () => {
       const routeInput = {
         name: 'search',
         params: {
+          searchString: '',
+          buckets: [],
+          order: 'NEWEST',
           params: {
-            resultType: 'o',
+            resultType: 'p',
+            filter: [],
+            userMetaFilters: [
+              {
+                path: '/author',
+                predicate: {
+                  type: 'KeywordEnum',
+                  value: {
+                    terms: ['Aneesh', 'Maksim'],
+                  },
+                },
+              },
+            ],
           },
         },
       }
       const routeDecoded = Schema.decodeUnknownSync(nav.NavigableRouteSchema)(routeInput)
       expect(routeDecoded).toMatchSnapshot()
+
+      const route = nav.routes[routeDecoded.name]
+      // @ts-expect-error
+      const loc = await Schema.encodePromise(route.paramsSchema)(routeDecoded.params)
+      expect(loc).toMatchSnapshot()
     })
   })
 })
