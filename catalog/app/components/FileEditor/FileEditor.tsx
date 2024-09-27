e0c72a8097cd9aca8b4d9083c85da0ee8ec3b738
Maksim Chervonnyi <mail@redmax.dev>
2024-09-27 13:52:36 +0200

    add one level of abstraction for previewing

--- catalog/app/components/FileEditor/FileEditor.tsx
+++ catalog/app/components/FileEditor/FileEditor.tsx
@@ -2,11 +2,15 @@ import cx from 'classnames'
 import * as React from 'react'
 import * as M from '@material-ui/core'
 
-import Markdown from 'components/Markdown'
+import {
+  TextPreviewProps,
+  Skeleton as TextPreviewSkeleton,
+} from 'components/Preview/text'
 import * as PreviewUtils from 'components/Preview/loaders/utils'
 import PreviewDisplay from 'components/Preview/Display'
 import type * as Model from 'model'
 import AsyncResult from 'utils/AsyncResult'
+import * as RT from 'utils/reactTools'
 
 import Skeleton from './Skeleton'
 import { EditorState } from './State'
@@ -16,6 +20,11 @@ import { loadMode } from './loader'
 import { EditorInputType } from './types'
 
 export { detect, isSupportedFileType } from './loader'
+
+const TextPreview: React.FC<TextPreviewProps> = RT.mkLazy(
+  () => import('components/Preview/text'),
+  TextPreviewSkeleton,
+)
 
 interface EditorProps extends EditorState {
   editing: EditorInputType
@@ -102,9 +111,11 @@ export function Editor(props: EditorProps) {
       <div className={cx(classes.tab, { [classes.active]: !props.preview })}>
         <EditorSuspended {...props} />
       </div>
-      <div className={cx(classes.tab, { [classes.active]: props.preview })}>
-        <Markdown data={props.value} />
-      </div>
+      {props.preview && (
+        <div className={cx(classes.tab, classes.active)}>
+          <TextPreview type={props.editing} value={props.value} />
+        </div>
+      )}
     </React.Suspense>
   )
 }
---
+++ catalog/app/components/Preview/text/index.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import * as M from '@material-ui/core'
+
+import * as FileEditor from 'components/FileEditor'
+import Markdown from 'components/Markdown'
+import Skel from 'components/Skeleton'
+
+function NoValue() {
+  return <h1>No value</h1>
+}
+
+function NoPreview() {
+  return <h1>No preview</h1>
+}
+
+const useSkeletonStyles = M.makeStyles((t) => ({
+  line: {
+    height: t.spacing(3),
+    marginBottom: t.spacing(1),
+  },
+}))
+
+export function Skeleton() {
+  const classes = useSkeletonStyles()
+  const lines = [80, 50, 100, 60, 30, 80, 50, 100, 60, 30, 20, 70]
+  return (
+    <div>
+      {lines.map((width, index) => (
+        <Skel className={classes.line} width={`${width}%`} key={width + index} />
+      ))}
+    </div>
+  )
+}
+
+export interface TextPreviewProps {
+  type: FileEditor.EditorInputType
+  value?: string
+}
+
+export default function TextPreview({ type, value }: TextPreviewProps) {
+  if (!value) return <NoValue />
+
+  switch (type.brace) {
+    case 'markdown':
+      return <Markdown data={value} />
+    default:
+      return <NoPreview />
+  }
+}
