7ed6573b827d465bdd54b6c571ffd5869f8a5725
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-10-16 16:26:42 -0700

    datetime.timezone.utc

--- api/python/tests/test_session.py
+++ api/python/tests/test_session.py
@@ -75,7 +75,7 @@ class TestSession(QuiltTestCase):
     @patch('quilt3.session._load_credentials')
     def test_create_botocore_session(self, mock_load_credentials, mock_save_credentials):
         # Test good credentials.
-        future_date = datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1)
+        future_date = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)
 
         mock_load_credentials.return_value = dict(
             access_key='access-key',
@@ -94,7 +94,7 @@ class TestSession(QuiltTestCase):
         mock_save_credentials.assert_not_called()
 
         # Test expired credentials.
-        past_date = datetime.datetime.now(datetime.UTC) - datetime.timedelta(minutes=5)
+        past_date = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=5)
 
         mock_load_credentials.return_value = dict(
             access_key='access-key',
