ff4aed8fe4237284615b4168af466d2d2f4af4b8
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-10-16 17:01:05 -0700

    cleanup tabulator.py

--- docs/advanced-features/tabulator.py
+++ docs/advanced-features/tabulator.py
@@ -3,33 +3,33 @@ import time
 
 DOMAIN = 'stable'
 WORKGROUP = f'QuiltUserAthena-tf-{DOMAIN}-NonManagedRoleWorkgroup'
-TABLE = 'ccle_tsv'
+QUERY = f'SELECT * FROM "quilt-tf-{DOMAIN}-tabulator"."udp-spec"."ccle_tsv" LIMIT 10'
 
 quilt3.config(f'https://{DOMAIN}.quilttest.com/')
 quilt3.login()
 session = quilt3.get_boto3_session()
 athena_client = session.client('athena')
 
-request = athena_client.start_query_execution(
-    QueryString=f'SELECT * FROM "{TABLE}" LIMIT 10',
-    QueryExecutionContext={
-        'Catalog': f'quilt-tf-{DOMAIN}-tabulator',
-        'Database': 'udp-spec'
-    },
+response = athena_client.start_query_execution(
+    QueryString=QUERY,
     WorkGroup=WORKGROUP
 )
-query_execution_id = request['QueryExecutionId']
+query_execution_id = response['QueryExecutionId']
 print(f'Query execution ID: {query_execution_id}')
 
 while True:
-    response = athena_client.get_query_execution(QueryExecutionId=query_execution_id)
-    state = response['QueryExecution']['Status']['State']
+    execution_response = athena_client.get_query_execution(QueryExecutionId=query_execution_id)
+    state = execution_response['QueryExecution']['Status']['State']
     if state in ('SUCCEEDED', 'FAILED', 'CANCELLED'):
         break
     print(f'\tQuery state: {state}')
     time.sleep(1)
 print(f'Query finished with state: {state}')
 
-results = athena_client.get_query_results(QueryExecutionId=query_execution_id)
-for row in results['ResultSet']['Rows']:
-    print([field.get('VarCharValue') for field in row['Data']])
+if state == 'SUCCEEDED':
+    results = athena_client.get_query_results(QueryExecutionId=query_execution_id)
+    for row in results['ResultSet']['Rows']:
+        print([field.get('VarCharValue') for field in row['Data']])
+else:
+    print(f'Query did not succeed. Final state: {state}')
+
