9559915558dad19a3eb31221cc3c1e7fa55bb328
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2024-11-20 13:16:14 +0100

    Cleanup after migration from S3 select (#4229)

--- lambdas/s3select/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='t4_lambda_s3select',
-    version='0.0.1',
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-)
