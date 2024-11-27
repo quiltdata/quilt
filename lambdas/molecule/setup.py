263974d31682d48f67ebcc6d729025b52983c6a9
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2024-11-27 17:05:21 +0100

    molecule

--- lambdas/molecule/setup.py
+++ lambdas/molecule/setup.py
@@ -6,6 +6,11 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "requests==2.32.2",
+        "requests ~= 2.32.2",
+        (
+            "t4_lambda_shared[lambda] @ https://github.com/quiltdata/quilt/archive/"
+            "f45d8ab51f2d60e98efda7510322f94d822e4eb4.zip"
+            "#subdirectory=lambdas/shared"
+        ),
     ],
 )
