8e7d343118cf9034070e4111cf49f5e448b5beb5
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2024-11-27 14:40:07 +0100

    add deps for lambda shared

--- lambdas/shared/setup.py
+++ lambdas/shared/setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+
+mem_deps = [
+    "psutil ~= 6.1",
+]
+
 setup(
     name="t4-lambda-shared",
     version="0.0.3",
@@ -11,6 +16,16 @@ setup(
         "tests": [
             "pytest",
             "pytest-cov",
+        ],
+        "mem": mem_deps,  # for t4_lambda_shared.utils.get_available_memory()
+        # for t4_lambda_shared.preview
+        "preview": [
+            *mem_deps,
+            "fcsparser ~= 0.2.1",
+            "openpyxl ~= 3.1",
+            "pandas ~= 2.2",
+            "pyarrow ~= 18.0",
+            "xlrd ~= 2.0",
         ],
         "lambda": [
             "awslambdaric>=2,<3",
