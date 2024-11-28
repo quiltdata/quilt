1c2ecd75eba9daa3fe09c7b86343f85c3f417e47
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2024-11-28 13:28:03 +0100

    numpy < 2

--- lambdas/tabular_preview/setup.py
+++ lambdas/tabular_preview/setup.py
@@ -11,6 +11,11 @@ setup(
         "xlrd >=2,< 3",
         "openpyxl >=3,<4 ",
         "fsspec[http] >= 2022.1.0",
+        # Stripping numpy.libs in numpy 2 results in
+        # libscipy_openblas64_-ff651d7f.so: ELF load command address/offset not properly aligned
+        # which is probably caused by some bug in glibc (it happens on Ubuntu 22.04, Amazon Linux 2/2023,
+        # but not on Ubuntu 24.04).
+        "numpy < 2",
         (
             "t4_lambda_shared @ https://github.com/quiltdata/quilt/archive/"
             "f45d8ab51f2d60e98efda7510322f94d822e4eb4.zip"
