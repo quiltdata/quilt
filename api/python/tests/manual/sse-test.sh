639bc67f1ea2d5f73ca68e86be8c496884f1b932
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-12-27 10:56:32 -0800

    use kms policy

--- api/python/tests/manual/sse-test.sh
+++ api/python/tests/manual/sse-test.sh
@@ -2,3 +2,7 @@
  aws s3 ls $TEST_BUCKET
  aws s3 cp TEST.md $TEST_BUCKET/
  aws s3 ls --recursive $TEST_BUCKET
+ aws s3 cp TEST.md $TEST_BUCKET/with-kms/TEST.md
+# upload failed: ./TEST.md to s3://test-kms-policies/with-kms/TEST.md An error occurred (AccessDenied) when calling the PutObject operation: User: arn:aws:iam::712023778557:user/ernest-staging is not authorized to perform: s3:PutObject on resource: "arn:aws:s3:::test-kms-policies/with-kms/TEST.md" with an explicit deny in a resource-based policy
+aws s3 cp TEST.md $TEST_BUCKET/with-kms/TEST.md --sse aws:kms
+aws s3 ls --recursive $TEST_BUCKET
