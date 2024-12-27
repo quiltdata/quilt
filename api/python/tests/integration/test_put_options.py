b66c754f9cb8cb684b8dfa8e4d2f87ff5723a15d
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-12-27 11:26:41 -0800

    bucket.put_file fails
    E           botocore.exceptions.ClientError: An error occurred (404) when calling the HeadObject operation: Not Found

--- api/python/tests/integration/test_put_options.py
+++ api/python/tests/integration/test_put_options.py
@@ -1,22 +1,23 @@
-from tests.utils import QuiltTestCase
 import pathlib
-
-from quilt3 import Bucket, Package
+from quilt3 import Bucket
 
 DATA_DIR = pathlib.Path(__file__).parent / 'data'
+TEST_BUCKET = "s3://test-kms-policies"
+TEST_FILE = "foo.txt"
+TEST_SRC = f"{DATA_DIR / TEST_FILE}"
 
-class TestPutOptions(QuiltTestCase):
-    TEST_BUCKET = "s3://test-kms-policies"
-    TEST_FILE = "foo.txt"
-    TEST_SRC = f"{DATA_DIR / TEST_FILE}"
+print(f"TEST_BUCKET: {TEST_BUCKET}")
+print(f"TEST_SRC: {TEST_SRC}")
 
-    def dest_file(self, test_name):
-        return f"{self.TEST_BUCKET}/test/{test_name}"
+def dest_file(test_name):
+    return f"{TEST_BUCKET}/test/{test_name}"
 
-    def test_bucket_put_file(self):
-        print(f"TEST_BUCKET: {self.TEST_BUCKET}")
-        print(f"TEST_SRC: {self.TEST_SRC}")
-        bucket = Bucket(self.TEST_BUCKET)
-        bucket.put_file(self.TEST_SRC, self.dest_file("test_bucket_put_file"))
+def test_bucket_put_file():
+    dest = dest_file("test_bucket_put_file")
+    print(f"DEST: {dest}")
+    bucket = Bucket(TEST_BUCKET)
 
-    
+#    bucket.put_file(TEST_SRC, dest_file("test_bucket_put_file"))
+    bucket.put_file(TEST_SRC,
+                    dest_file("test_bucket_put_file"),
+                    put_options={"ServerSideEncryption": "aws:kms"})
