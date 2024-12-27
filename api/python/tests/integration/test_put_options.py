454698fdbf9538fe8eec32c0a2ea1c7d578c64dc
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-12-27 15:58:50 -0800

    pkg_entry.fetch

--- api/python/tests/integration/test_put_options.py
+++ api/python/tests/integration/test_put_options.py
@@ -3,9 +3,11 @@ import pathlib
 import boto3
 from botocore.exceptions import ClientError
 from pytest import raises
+from tempfile import TemporaryDirectory
 
 from quilt3 import Bucket, Package
 
+ERR_MSG = r"An error occurred \(AccessDenied\) when calling the PutObject operation:.*"
 DATA_DIR = pathlib.Path(__file__).parent / 'data'
 TEST_BUCKET = "test-kms-policies"
 TEST_BUCKET_URI = f"s3://{TEST_BUCKET}"
@@ -32,9 +34,7 @@ def test_bucket_put_file():
     dest = dest_key("test_bucket_put_file")
     print(f"test_bucket_put_file.dest: {dest}")
 
-    with raises(ClientError,
-                match=r"An error occurred \(AccessDenied\) when calling "
-                "the PutObject operation:.*"):
+    with raises(ClientError, match=ERR_MSG):
         S3_BUCKET.put_file(dest, TEST_SRC)
 
     S3_BUCKET.put_file(dest, TEST_SRC, put_options=USE_KMS)
@@ -51,9 +51,7 @@ def test_bucket_put_dir():
     dest = dest_dir("test_bucket_put_dir")
     print(f"test_bucket_put_dir.dest: {dest}")
 
-    with raises(ClientError,
-                match=r"An error occurred \(AccessDenied\) when calling "
-                "the PutObject operation:.*"):
+    with raises(ClientError, match=ERR_MSG):
         S3_BUCKET.put_dir(dest, DATA_DIR)
 
     S3_BUCKET.put_dir(dest, DATA_DIR, put_options=USE_KMS)
@@ -77,24 +75,38 @@ def test_package_push():
     pkg = Package()
     pkg.set(TEST_FILE, TEST_SRC)
 
-    with raises(ClientError,
-                match=r"An error occurred \(AccessDenied\) when calling "
-                "the PutObject operation:.*"):
+    with raises(ClientError, match=ERR_MSG):
         pkg.push(pkg_name, TEST_BUCKET_URI, force=True)
 
     pkg.push(pkg_name, TEST_BUCKET_URI, put_options=USE_KMS, force=True)
 
     # Use boto3 to verify the object was uploaded with the correct encryption
-    response = S3_CLIENT.head_object(Bucket=TEST_BUCKET, Key=f"{pkg_name}/{TEST_FILE}")
+    response = S3_CLIENT.head_object(Bucket=TEST_BUCKET,
+                                     Key=f"{pkg_name}/{TEST_FILE}")
     assert response['ServerSideEncryption'] == 'aws:kms'
 
     # Read package entry
     pkg2 = Package.browse(pkg_name, registry=TEST_BUCKET_URI)
     assert TEST_FILE in pkg2
-    pkg_entry = pkg2.get(TEST_FILE)
+    pkg_entry = pkg2[TEST_FILE]
+    print(f"pkg_entry: {pkg_entry}")
     assert pkg_entry is not None
+
+    # fetch to temp location in S3 bucket
+    fetch_dir = dest_dir("test_package_fetch")
+    dest_uri = f"s3://{TEST_BUCKET}/{fetch_dir}"
+    with raises(ClientError, match=ERR_MSG):
+        pkg_entry.fetch(dest_uri)
+
+    new_entry = pkg_entry.fetch(dest_uri, put_options=USE_KMS)
+    assert new_entry is not None
+
+    # Use boto3 to verify the object was uploaded with the correct encryption
+    response = S3_CLIENT.head_object(Bucket=TEST_BUCKET,
+                                     Key=f"{fetch_dir}/{TEST_FILE}")
+    assert response['ServerSideEncryption'] == 'aws:kms'
 
     # Cleanup
     # pkg.delete(pkg_name)
-    # S3_CLIENT.delete_object(Bucket=TEST_BUCKET, Key=f"{pkg_name}/{TEST_FILE}")
+    # S3_CLIENT.delete_object(Bucket=TEST_BUCKET, Key=f"{fetch_dir}")
     # S3_CLIENT.delete_object(Bucket=TEST_BUCKET, Key=f"{pkg_name}/")
