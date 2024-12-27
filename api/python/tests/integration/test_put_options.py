64068cac2a9a0f45f2f46c9be63c5acfb6e56060
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-12-27 12:14:30 -0800

    fail test_package_push

--- api/python/tests/integration/test_put_options.py
+++ api/python/tests/integration/test_put_options.py
@@ -4,11 +4,12 @@ import boto3
 from botocore.exceptions import ClientError
 from pytest import raises
 
-from quilt3 import Bucket
+from quilt3 import Bucket, Package
 
 DATA_DIR = pathlib.Path(__file__).parent / 'data'
 TEST_BUCKET = "test-kms-policies"
-S3_BUCKET = Bucket(f"s3://{TEST_BUCKET}")
+TEST_BUCKET_URI = f"s3://{TEST_BUCKET}"
+S3_BUCKET = Bucket(TEST_BUCKET_URI)
 TEST_FILE = "foo.txt"
 TEST_SRC = f"{DATA_DIR / TEST_FILE}"
 
@@ -26,14 +27,15 @@ def dest_key(test_name):
 
 def test_bucket_put_file():
     dest = dest_key("test_bucket_put_file")
-    print(f"DEST: {dest}")
+    print(f"test_bucket_put_file.dest: {dest}")
 
     with raises(ClientError,
                 match=r"An error occurred \(AccessDenied\) when calling "
                 "the PutObject operation:.*"):
         S3_BUCKET.put_file(dest, TEST_SRC)
 
-    S3_BUCKET.put_file(dest, TEST_SRC, put_options={"ServerSideEncryption": "aws:kms"})
+    S3_BUCKET.put_file(dest, TEST_SRC,
+                       put_options={"ServerSideEncryption": "aws:kms"})
 
     # Use boto3 to verify the object was uploaded with the correct encryption
     s3 = boto3.client('s3')
@@ -46,14 +48,15 @@ def test_bucket_put_file():
 
 def test_bucket_put_dir():
     dest = dest_dir("test_bucket_put_dir")
-    print(f"DEST: {dest}")
+    print(f"test_bucket_put_dir.dest: {dest}")
 
     with raises(ClientError,
                 match=r"An error occurred \(AccessDenied\) when calling "
                 "the PutObject operation:.*"):
         S3_BUCKET.put_dir(dest, DATA_DIR)
 
-    S3_BUCKET.put_dir(dest, DATA_DIR, put_options={"ServerSideEncryption": "aws:kms"})
+    S3_BUCKET.put_dir(dest, DATA_DIR,
+                      put_options={"ServerSideEncryption": "aws:kms"})
 
     # Use boto3 to verify the object was uploaded with the correct encryption
     s3 = boto3.client('s3')
@@ -66,3 +69,19 @@ def test_bucket_put_dir():
     for obj in response.get('Contents', []):
         s3.delete_object(Bucket=TEST_BUCKET, Key=obj['Key'])
     s3.delete_object(Bucket=TEST_BUCKET, Key=dest)
+
+
+def test_package_push():
+    pkg_name = dest_dir("test_package_push")
+    print(f"test_package_push.pkg_name: {pkg_name}")
+
+    pkg = Package()
+    pkg.set(TEST_FILE, TEST_SRC)
+
+    with raises(ClientError,
+                match=r"An error occurred \(AccessDenied\) when calling "
+                "the PutObject operation:.*"):
+        pkg.push(pkg_name, TEST_BUCKET_URI)
+
+    pkg.push(pkg_name, TEST_BUCKET_URI,
+             put_options={"ServerSideEncryption": "aws:kms"})
