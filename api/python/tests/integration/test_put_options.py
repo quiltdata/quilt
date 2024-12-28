1ec19fd0e8d2737e55c8ef4d92ee73bc6015b001
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-12-27 16:41:39 -0800

    delete_key

--- api/python/tests/integration/test_put_options.py
+++ api/python/tests/integration/test_put_options.py
@@ -3,11 +3,11 @@ import pathlib
 import boto3
 from botocore.exceptions import ClientError
 from pytest import raises
-from tempfile import TemporaryDirectory
 
-from quilt3 import Bucket, Package
+from quilt3 import Bucket, Package, delete_package
 
-ERR_MSG = r"An error occurred \(AccessDenied\) when calling the PutObject operation:.*"
+ERR_MSG = r"An error occurred \(AccessDenied\)" +\
+           r" when calling the PutObject operation:.*"
 COPY_MSG = ERR_MSG.replace("PutObject", "CopyObject")
 DATA_DIR = pathlib.Path(__file__).parent / 'data'
 TEST_BUCKET = "test-kms-policies"
@@ -31,6 +31,15 @@ def dest_key(test_name):
     return f"{dest_dir(test_name)}/{TEST_FILE}"
 
 
+def delete_key(key: str):
+    response = S3_CLIENT.list_objects_v2(Bucket=TEST_BUCKET, Prefix=key)
+    if "Contents" in response:
+        objs = [{"Key": obj["Key"]} for obj in response["Contents"]]
+        S3_CLIENT.delete_objects(Bucket=TEST_BUCKET, Delete={"Objects": objs})
+    else:
+        S3_CLIENT.delete_object(Bucket=TEST_BUCKET, Key=key)
+
+
 def test_bucket_put_file():
     dest = dest_key("test_bucket_put_file")
     print(f"test_bucket_put_file.dest: {dest}")
@@ -45,7 +54,7 @@ def test_bucket_put_file():
     assert response['ServerSideEncryption'] == 'aws:kms'
 
     # Use boto3 to clean up
-    S3_CLIENT.delete_object(Bucket=TEST_BUCKET, Key=dest)
+    delete_key(dest)
 
 
 def test_bucket_put_dir():
@@ -59,14 +68,10 @@ def test_bucket_put_dir():
 
     # Use boto3 to verify the object was uploaded with the correct encryption
     response = S3_CLIENT.head_object(Bucket=TEST_BUCKET,
-                              Key=f"{dest}/{TEST_FILE}")
+                                     Key=f"{dest}/{TEST_FILE}")
     assert response['ServerSideEncryption'] == 'aws:kms'
 
-    # Use boto3 to remove directory and its contents
-    response = S3_CLIENT.list_objects_v2(Bucket=TEST_BUCKET, Prefix=dest)
-    for obj in response.get('Contents', []):
-        S3_CLIENT.delete_object(Bucket=TEST_BUCKET, Key=obj['Key'])
-    S3_CLIENT.delete_object(Bucket=TEST_BUCKET, Key=dest)
+    delete_key(dest)
 
 
 def test_package_push():
@@ -90,7 +95,6 @@ def test_package_push():
     pkg2 = Package.browse(pkg_name, registry=TEST_BUCKET_URI)
     assert TEST_FILE in pkg2
     pkg_entry = pkg2[TEST_FILE]
-    print(f"pkg_entry: {pkg_entry}")
     assert pkg_entry is not None
 
     # fetch entry to remote URI
@@ -102,11 +106,13 @@ def test_package_push():
     new_entry = pkg_entry.fetch(dest_uri, put_options=USE_KMS)
     assert new_entry is not None
 
-    # Use boto3 to verify the object was uploaded with the correct encryption
-    response = S3_CLIENT.head_object(Bucket=TEST_BUCKET, Key=f"{fetch_dir}/{TEST_FILE}")
+    # Use boto3 to verify the object was fetched with the correct encryption
+    response = S3_CLIENT.head_object(Bucket=TEST_BUCKET,
+                                     Key=f"{fetch_dir}/{TEST_FILE}")
     assert response['ServerSideEncryption'] == 'aws:kms'
     S3_CLIENT.delete_object(Bucket=TEST_BUCKET, Key=fetch_dir)
 
     # Cleanup
-    Package.delete(pkg_name)
-    # S3_CLIENT.delete_object(Bucket=TEST_BUCKET, Key=f"{pkg_name}/")
+    delete_package(pkg_name, registry=TEST_BUCKET_URI)
+    delete_key(pkg_name)
+    delete_key(fetch_dir)
