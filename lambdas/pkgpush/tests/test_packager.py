3e115df0291b681e75664fb36b52ce923bdc2cc2
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2025-02-19 12:36:14 +0100

    lint

--- lambdas/pkgpush/tests/test_packager.py
+++ lambdas/pkgpush/tests/test_packager.py
@@ -1,9 +1,7 @@
 import pytest
 
-import quilt3
 import quilt3.util
 import t4_lambda_pkgpush
-
 
 DEFAULT_PKG_NAME_PREFIX = "quilt-packager"
 DEFAULT_PKG_NAME_SUFFIX = "pkg"
@@ -15,8 +13,8 @@ DEFAULT_PKG_NAME_SUFFIX = "pkg"
         ("", f"{DEFAULT_PKG_NAME_PREFIX}/{DEFAULT_PKG_NAME_SUFFIX}"),
         ("////", f"{DEFAULT_PKG_NAME_PREFIX}/{DEFAULT_PKG_NAME_SUFFIX}"),
         ("//f*0//", f"{DEFAULT_PKG_NAME_PREFIX}/f_0"),
-        ("//f*0//b@r//", f"f_0/b_r"),
-        ("//f*0//b@r//b@z//", f"b_r/b_z"),
+        ("//f*0//b@r//", "f_0/b_r"),
+        ("//f*0//b@r//b@z//", "b_r/b_z"),
     ],
 )
 def test_infer_pkg_name_from_prefix(prefix, expected):
