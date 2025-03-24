ea4f801d99c934f39bc767c5f7d3a7c65cf261dd
Maksim Chervonnyi <mail@redmax.dev>
2025-03-24 11:48:19 +0100

    remove unnecessary blocks

--- catalog/app/website/pages/OpenLanding/OpenLanding.js
+++ catalog/app/website/pages/OpenLanding/OpenLanding.js
@@ -8,13 +8,9 @@ import * as LinkedData from 'utils/LinkedData'
 import MetaTitle from 'utils/MetaTitle'
 
 import Layout from 'website/components/Layout'
-import Contribute from 'website/components/Contribute'
-import Videos from 'website/components/Videos'
 
 import Buckets from './Buckets'
 import Search from './Search'
-import Showcase from './Showcase'
-import QuiltIsDifferent from './QuiltIsDifferent'
 
 export default function OpenLanding() {
   return (
@@ -26,11 +22,7 @@ export default function OpenLanding() {
       <NavBar.Provider>
         <Search />
       </NavBar.Provider>
-      <Showcase />
       <Buckets />
-      <Videos />
-      <QuiltIsDifferent />
-      <Contribute />
     </Layout>
   )
 }
