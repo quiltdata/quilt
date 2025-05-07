d7cab3888e5a643329c74b431c821832ec3ddf46
Maksim Chervonnyi <mail@redmax.dev>
2025-05-07 15:02:47 +0200

    use pause and always call `isDirectory`, catch errors consistently, remove unnecessary function wrappers

--- catalog/app/containers/Bucket/HandleNoSlashDir.tsx
+++ catalog/app/containers/Bucket/HandleNoSlashDir.tsx
@@ -11,29 +11,27 @@ import * as requests from './requests'
 import { displayError } from './errors'
 
 // If object exists, then this is 100% an object page
-function useIsObject(handle: Model.S3.S3ObjectLocation) {
-  const [exists, setExists] = React.useState(null)
+function useIsObject(handle: Model.S3.S3ObjectLocation): null | boolean | Error {
+  const [exists, setExists] = React.useState<null | boolean | Error>(null)
 
   const s3 = AWS.S3.use()
   React.useEffect(() => {
     const { bucket, key, version } = handle
-    function resolveObjectExistence() {
-      requests
-        .getObjectExistence({
-          s3,
-          bucket,
-          key,
-          version,
-        })
-        .then(
-          requests.ObjectExistence.case({
-            Exists: () => true,
-            _: () => false,
-          }),
-        )
-        .then(setExists)
-    }
-    resolveObjectExistence()
+    requests
+      .getObjectExistence({
+        s3,
+        bucket,
+        key,
+        version,
+      })
+      .then(
+        requests.ObjectExistence.case({
+          Exists: () => true,
+          _: () => false,
+        }),
+      )
+      .then(setExists)
+      .catch(setExists)
   }, [s3, handle])
 
   return exists
@@ -42,43 +40,37 @@ function useIsObject(handle: Model.S3.S3ObjectLocation) {
 // If prefix contains at least something, then it is a directory.
 // S3 can not have empty directories, because directories are virtual,
 //    they are based on paths for existing keys (file)
-function useIsDirectory(handle: Model.S3.S3ObjectLocation) {
+function useIsDirectory(
+  handle: Model.S3.S3ObjectLocation,
+  pause: boolean,
+): null | boolean | Error {
   const bucketListing = requests.useBucketListing()
-  return React.useCallback(async () => {
+  const [isDir, setIsDir] = React.useState<null | boolean | Error>(null)
+  React.useEffect(() => {
+    if (pause) return
+
     const { bucket, key } = handle
     const path = s3paths.ensureSlash(key)
-    const { dirs, files } = await bucketListing({ bucket, path, maxKeys: 1 })
-    return !!dirs.length || !!files.length
-  }, [bucketListing, handle])
+    bucketListing({ bucket, path, maxKeys: 1 })
+      .then(({ dirs, files }) => setIsDir(!!dirs.length || !!files.length))
+      .catch((e) => setIsDir(e instanceof Error ? e : new Error(`${e}`)))
+  }, [bucketListing, handle, pause])
+  return isDir
 }
 
 function useHandleNoSlashDir(
   handle: Model.S3.S3ObjectLocation,
-): 'loading' | 'dir' | 'file' | Error {
+): null | 'dir' | 'file' | Error {
   const isObject = useIsObject(handle)
-  const requestIsDirectory = useIsDirectory(handle)
+  const isDirectory = useIsDirectory(handle, isObject === null)
 
-  const [pageType, setPageType] = React.useState<'loading' | 'dir' | 'file' | Error>(
-    'loading',
-  )
+  if (isObject === null || isObject instanceof Error) return isObject
 
-  React.useEffect(() => {
-    function resolvePageType() {
-      if (isObject === null) return
+  if (isObject) return 'file'
 
-      if (isObject) {
-        setPageType('file')
-        return
-      }
+  if (isDirectory === null || isDirectory instanceof Error) return isDirectory
 
-      requestIsDirectory()
-        .then((isDir) => setPageType(isDir ? 'dir' : 'file'))
-        .catch((e) => setPageType(e instanceof Error ? e : new Error(`${e}`)))
-    }
-    resolvePageType()
-  }, [handle, isObject, requestIsDirectory])
-
-  return pageType
+  return isDirectory ? 'dir' : 'file'
 }
 
 interface HandleNoSlashDirProps {
@@ -92,7 +84,7 @@ export default function HandleNoSlashDir({ children, handle }: HandleNoSlashDirP
   const pageType = useHandleNoSlashDir(handle)
 
   switch (pageType) {
-    case 'loading':
+    case null:
       return <Placeholder color="text.secondary" />
     case 'dir':
       const dirPage = urls.bucketDir(handle.bucket, s3paths.ensureSlash(handle.key))
