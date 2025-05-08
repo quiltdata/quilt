715a86259c73cc4890c977915e4959101ece7056
nl_0 <nl.imbecile@gmail.com>
2025-05-08 16:32:35 +0200

    and attempt to make it somewhat more ergonomic

--- catalog/app/containers/Bucket/FallbackToDir.tsx
+++ catalog/app/containers/Bucket/FallbackToDir.tsx
@@ -6,7 +6,6 @@ import Placeholder from 'components/Placeholder'
 import * as Model from 'model'
 import * as AWS from 'utils/AWS'
 import { useData } from 'utils/Data'
-import AsyncResult from 'utils/AsyncResult'
 import * as NamedRoutes from 'utils/NamedRoutes'
 import * as s3paths from 'utils/s3paths'
 
@@ -24,52 +23,32 @@ export default function FallbackToDir({ children, handle }: FallbackToDirProps) 
 
   const objData = useData(requests.getObjectExistence, { s3, ...handle })
 
-  const noAutoFetch = React.useMemo(
-    () =>
-      AsyncResult.case(
-        {
-          Ok: requests.ObjectExistence.case({ Exists: R.T, _: R.F }),
-          _: R.T,
-        },
-        objData.result,
-      ),
-    [objData.result],
-  )
+  const noAutoFetch = objData.case({
+    Ok: requests.ObjectExistence.case({ Exists: R.T, _: R.F }),
+    _: R.T,
+  })
 
   const dirData = useData(requests.bucketListing, { s3, ...handle }, { noAutoFetch })
 
-  const redirectResult = React.useMemo(
-    () =>
-      AsyncResult.case(
-        {
-          Ok: requests.ObjectExistence.case({
-            Exists: R.F,
-            _: () =>
-              AsyncResult.mapCase(
-                {
-                  Ok: (isDirectory: boolean) =>
-                    isDirectory
-                      ? urls.bucketDir(handle.bucket, s3paths.ensureSlash(handle.key))
-                      : undefined,
-                },
-                dirData.result,
-              ),
-          }),
-          Err: AsyncResult.Err,
-          Pending: AsyncResult.Pending,
-          Init: AsyncResult.Init,
-        },
-        objData.result,
-      ),
-    [objData.result, dirData.result, handle, urls],
-  )
+  const shouldRedirect: null | Error | boolean = objData.case({
+    Ok: requests.ObjectExistence.case({
+      Exists: () => false,
+      _: () =>
+        dirData.case({
+          Ok: ({ dirs, files }: any) => !!dirs.length || !!files.length,
+          Err: (e: Error) => e,
+          _: () => null,
+        }),
+    }),
+    Err: (e: Error) => e,
+    _: () => null,
+  })
 
-  return AsyncResult.case(
-    {
-      Ok: (to?: string) => (to ? <RRDom.Redirect to={to} /> : children),
-      Err: displayError(),
-      _: () => <Placeholder color="text.secondary" />,
-    },
-    redirectResult,
+  if (shouldRedirect === null) return <Placeholder color="text.secondary" />
+  if (shouldRedirect instanceof Error) return displayError()(shouldRedirect)
+  return shouldRedirect ? (
+    <RRDom.Redirect to={urls.bucketDir(handle.bucket, s3paths.ensureSlash(handle.key))} />
+  ) : (
+    children
   )
 }
