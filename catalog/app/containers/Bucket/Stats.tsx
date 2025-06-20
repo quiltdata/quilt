70ea46adc7fa06a9e7583703aae38e4a857eee2c
Maksim Chervonnyi <mail@redmax.dev>
2025-06-20 17:09:20 +0200

    show bucket stats in bucket nav

--- catalog/app/containers/Bucket/Stats.tsx
+++ catalog/app/containers/Bucket/Stats.tsx
@@ -1,15 +1,64 @@
 import cx from 'classnames'
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 import * as M from '@material-ui/core'
 
+import { Popup } from 'components/Collaborators'
 import type * as Model from 'model'
 import * as GQL from 'utils/GraphQL'
+import * as APIConnector from 'utils/APIConnector'
 import AsyncResult from 'utils/AsyncResult'
+import * as AWS from 'utils/AWS'
+import { useData } from 'utils/Data'
+import * as NamedRoutes from 'utils/NamedRoutes'
 import StyledTooltip from 'utils/StyledTooltip'
+import { formatQuantity, readableBytes } from 'utils/string'
 
-import { Popup } from 'components/Collaborators'
+import * as requests from './requests'
 
+import BUCKET_OVERVIEW_URL_QUERY from './gql/BucketOverviewUrl.generated'
 import COLLABORATORS_QUERY from './gql/Collaborators.generated'
+
+function PackagesIcon() {
+  return (
+    <M.SvgIcon
+      fontSize="small"
+      height="32"
+      style={{ width: '16px', height: '16px' }}
+      viewBox="0 0 32 32"
+      width="32"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6,2 L26,2 L32,13 L0,13 Z"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="3"
+      />
+
+      <line x1="16" y1="2" x2="16" y2="13" stroke="currentColor" strokeWidth="3" />
+      <path
+        d="M0,13 H32 V28 A3,3 0 0 1 29,31 H3 A3,3 0 0 1 0,28 Z"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="3"
+      />
+    </M.SvgIcon>
+  )
+}
+
+function useBucketStats(bucket: string) {
+  const s3 = AWS.S3.use()
+  const req = APIConnector.use()
+  const { bucketConfig } = GQL.useQueryS(BUCKET_OVERVIEW_URL_QUERY, { bucket })
+  const overviewUrl = bucketConfig?.overviewUrl
+  return useData(requests.bucketStats, { req, s3, bucket, overviewUrl })
+}
+
+function usePackagesStats(bucket: string) {
+  const req = APIConnector.use()
+  return useData(requests.countPackageRevisions, { req, bucket })
+}
 
 function NoCollaborators({ className }: { className: string }) {
   return (
@@ -116,13 +165,44 @@ function ChipError({ className, error }: ChipErrorProps) {
   )
 }
 
+interface ChipLinkProps {
+  className: string
+  label: React.ReactNode
+  to: string
+  icon: string | React.ReactNode
+  title: string
+}
+
+function ChipLink({ className, icon, label, to }: ChipLinkProps) {
+  return (
+    <StyledTooltip title="Total size">
+      <M.Button
+        className={className}
+        color="inherit"
+        component={Link}
+        size="small"
+        startIcon={
+          typeof icon === 'string' ? <M.Icon fontSize="small">{icon}</M.Icon> : icon
+        }
+        to={to}
+      >
+        {label}
+      </M.Button>
+    </StyledTooltip>
+  )
+}
+
 const useStyles = M.makeStyles((t) => ({
   root: {
     color: t.palette.text.secondary,
-    display: 'flex',
+    display: 'grid',
     alignItems: 'center',
+    gridAutoFlow: 'column',
+    gridColumnGap: t.spacing(2.5),
   },
-  chip: {},
+  chip: {
+    minWidth: 'auto',
+  },
 }))
 
 interface StatsProps {
@@ -132,10 +212,69 @@ interface StatsProps {
 
 export default function Stats({ className, bucket }: StatsProps) {
   const classes = useStyles()
+
+  const { urls } = NamedRoutes.use()
+
+  const { result: stats } = useBucketStats(bucket)
+  const { result: pkgs } = usePackagesStats(bucket)
   const collaborators = useCollaborators(bucket)
 
   return (
     <div className={cx(classes.root, className)}>
+      {AsyncResult.case(
+        {
+          Ok: ({ totalBytes }: { totalBytes: number }) => (
+            <ChipLink
+              className={classes.chip}
+              icon="pie_chart_outlined"
+              label={readableBytes(totalBytes)}
+              title="Total size"
+              to={urls.bucketOverview(bucket)}
+            />
+          ),
+          _: () => null,
+          Err: (error: Error) => <ChipError className={classes.chip} error={error} />,
+          Pending: () => <ChipLoading className={classes.chip} />,
+        },
+        stats,
+      )}
+
+      {AsyncResult.case(
+        {
+          Ok: ({ totalObjects }: { totalObjects: number }) => (
+            <ChipLink
+              className={classes.chip}
+              icon={<M.Icon fontSize="small">insert_drive_file_outlined</M.Icon>}
+              label={readableBytes(totalObjects)}
+              title="Number of objects in the bucket"
+              to={urls.bucketDir(bucket)}
+            />
+          ),
+          _: () => null,
+          Err: (error: Error) => <ChipError className={classes.chip} error={error} />,
+          Pending: () => <ChipLoading className={classes.chip} />,
+        },
+        stats,
+      )}
+
+      {AsyncResult.case(
+        {
+          Ok: (data: number) => (
+            <ChipLink
+              className={classes.chip}
+              icon={<PackagesIcon />}
+              label={formatQuantity(data)}
+              title="Number of packages in the bucket"
+              to={urls.bucketPackageList(bucket)}
+            />
+          ),
+          _: () => null,
+          Err: (error: Error) => <ChipError className={classes.chip} error={error} />,
+          Pending: () => <ChipLoading className={classes.chip} />,
+        },
+        pkgs,
+      )}
+
       {AsyncResult.case(
         {
           Ok: (data: ReadonlyArray<Model.GQLTypes.CollaboratorBucketConnection>) =>
