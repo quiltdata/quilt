ccd193d1b47c5d381b0a56afd3a7d4ad5d689a29
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2025-07-01 23:50:22 +0200

    cleanup

--- lambdas/es_ingest/src/t4_lambda_es_ingest/__init__.py
+++ lambdas/es_ingest/src/t4_lambda_es_ingest/__init__.py
@@ -14,6 +14,59 @@ es = make_elastic(os.environ["ES_ENDPOINT"])
 logger = get_quilt_logger()
 
 
+class BulkDocumentError(Exception):
+    pass
+
+
+class TooManyRequestsError(Exception):
+    pass
+
+
+def sleep_until_timeout(context):
+    """Sleep until the lambda timeout"""
+    remaining = context.get_remaining_time_in_millis() / 1000 - 1
+    logger.warning("Sleeping for %s seconds just before lambda timeout", remaining)
+    # good night, sweet prince
+    time.sleep(remaining)
+
+
+def bulk(context, es, data: bytes):
+    t0 = time.time()
+    try:
+        resp = es.bulk(
+            data,
+            filter_path="errors",
+            # wait as much as possible because it's better die trying than just die
+            # leave a second to avoid lambda timeout
+            request_timeout=context.get_remaining_time_in_millis() / 1000 - 1,
+        )
+    except elasticsearch.exceptions.TransportError as e:
+        if e.status_code == 429:
+            # at this point ES seems to be *very* overloaded, so we just sleep until lambda timeout
+            logger.warning("Got a 429 Too Many Requests error, sleeping until lambda timeout")
+            sleep_until_timeout(context)
+            raise TooManyRequestsError
+        raise
+
+    t1 = time.time()
+    delta = t1 - t0
+    logger.info("Bulk request took %s seconds", delta)
+    overtime = delta - 10
+    if overtime > 0:
+        # if the request took so long ES seems to be overloaded, so it's better to sleep
+        # now to avoid 429 Too Many Requests error later causing lambda failure and retry
+        time_to_sleep = min(
+            random.uniform(overtime / 2, overtime) + 15,
+            context.get_remaining_time_in_millis() / 1000 - 1,
+        )
+        logger.warning("Sleeping for %s seconds to avoid ES overload", time_to_sleep)
+        time.sleep(time_to_sleep)
+    if resp["errors"]:
+        # TODO: log errors from items.*.error?
+        # TODO: ignore index_not_found_exception for delete operations?
+        raise BulkDocumentError
+
+
 def handler(event, context):
     logger.debug("Batch indexer handler called with event: %s", event)
     assert len(event["Records"]) == 1, "Batch indexer handler expects exactly one record"
@@ -24,73 +77,7 @@ def handler(event, context):
 
     bucket = event["s3"]["bucket"]["name"]
     key = event["s3"]["object"]["key"]
-    # XXX: use version?
-
-    class BulkDocumentError(Exception):
-        pass
-
-    class TooManyRequestsError(Exception):
-        pass
-
-    class LambdaTimeoutError(Exception):
-        pass
-
-    def sleep_until_timeout():
-        """Sleep until the lambda timeout"""
-        remaining = context.get_remaining_time_in_millis() / 1000 - 1
-        logger.warning("Sleeping for %s seconds just before lambda timeout", remaining)
-        # good night, sweet prince
-        time.sleep(remaining)
-
-    # # it looks like ES internal bulk API has a 60s timeout by default
-    # # so we should wait hoping ES will be able to process the request
-    # read_timeout = 61
-
-    # @tenacity.retry(
-    #     reraise=True,
-    #     wait=tenacity.wait_random_exponential(min=8, multiplier=8),
-    #     retry=tenacity.retry_if_exception_type(TooManyRequestsError),
-    #     before_sleep=tenacity.before_log(logger, logging.WARNING),
-    # )
-    def bulk(context, es, data: bytes):
-        # if context.get_remaining_time_in_millis() < read_timeout * 1000:
-        #     logger.warning("Not enough time left to process bulk request, sleeping till lambda timeout")
-        #     sleep_until_timeout()
-        #     raise LambdaTimeoutError
-        t0 = time.time()
-        try:
-            resp = es.bulk(
-                data,
-                filter_path="errors",
-                request_timeout=context.get_remaining_time_in_millis() / 1000 - 1,
-            )
-        # except elasticsearch.exceptions.ConnectionTimeout as e:
-        #     # At this point ES seems to start returning 5xx errors
-        #     logger.warning("We got a connection timeout, sleeping until lambda timeout")
-        #     sleep_until_timeout()
-        #     raise
-        except elasticsearch.exceptions.TransportError as e:
-            if e.status_code == 429:
-                logger.warning("Got a 429 Too Many Requests error, sleeping until lambda timeout")
-                sleep_until_timeout()
-                raise TooManyRequestsError
-            raise
-
-        t1 = time.time()
-        delta = t1 - t0
-        logger.info("Bulk request took %s seconds", delta)
-        overtime = delta - 10
-        if overtime > 0:
-            time_to_sleep = min(
-                random.uniform(overtime / 2, overtime) + 15,
-                context.get_remaining_time_in_millis() / 1000 - 1,
-            )
-            logger.warning("Sleeping for %s seconds to avoid ES overload", time_to_sleep)
-            time.sleep(time_to_sleep)
-        if resp["errors"]:
-            # TODO: log errors from items.*.error?
-            # TODO: ignore index_not_found_exception for delete operations?
-            raise BulkDocumentError
+    # XXX: try to get version ID from the event, if available?
 
     data = s3_client.get_object(Bucket=bucket, Key=key)["Body"].read()
     bulk(context, es, data)
