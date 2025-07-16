eeb1c945ecbbb425a97129d16305c61880d3bd62
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2025-07-16 18:08:27 +0200

    isort

--- lambdas/shared/tests/test_utils.py
+++ lambdas/shared/tests/test_utils.py
@@ -6,7 +6,7 @@ import logging
 import os
 import sys
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 from testfixtures import LogCapture
