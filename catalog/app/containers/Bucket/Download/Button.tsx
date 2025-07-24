499266df7e57c566736efe26305b66df8b5fbfdd
Maksim Chervonnyi <mail@redmax.dev>
2025-07-24 12:07:11 +0200

    use Buttons.WithPopover for download button

--- catalog/app/containers/Bucket/Download/Button.tsx
+++ catalog/app/containers/Bucket/Download/Button.tsx
@@ -1,72 +1,13 @@
 import * as React from 'react'
-import * as M from '@material-ui/core'
 
 import * as Buttons from 'components/Buttons'
 
-const useStyles = M.makeStyles((t) => ({
-  root: {
-    position: 'relative',
-  },
-  popup: {
-    position: 'absolute',
-    right: 0,
-    top: '100%',
-    transform: `translateY(${t.spacing(0.5)}px)`,
-    zIndex: t.zIndex.modal,
-    animation: t.transitions.create('$slideDown'),
-    [t.breakpoints.down('xs')]: {
-      width: 'calc(100vw - 16px)',
-    },
-  },
-  backdrop: {
-    backgroundColor: t.palette.action.disabledBackground,
-    zIndex: t.zIndex.modal - 1,
-  },
-  '@keyframes slideDown': {
-    '0%': {
-      opacity: 0.7,
-      transform: `matrix(0.9, 0, 0, 0.9, 10, ${t.spacing(-0.5)})`,
-    },
-    '100%': {
-      opacity: 1,
-      transform: `matrix(1, 0, 0, 1, 0, ${t.spacing(0.5)})`,
-    },
-  },
-}))
-
 interface ButtonProps {
-  label?: string
-  className?: string
   children: NonNullable<React.ReactNode>
+  className?: string
+  label?: string
 }
 
-export default function Button({
-  className,
-  children,
-  label = 'Get files',
-}: ButtonProps) {
-  const classes = useStyles()
-  const [opened, setOpened] = React.useState(false)
-  const handleClick = React.useCallback(() => setOpened((o) => !o), [])
-  const handleClose = React.useCallback(() => setOpened(false), [])
-  return (
-    <div className={classes.root}>
-      <Buttons.Iconized
-        className={className}
-        endIcon={<M.Icon>arrow_drop_down</M.Icon>}
-        icon="download"
-        label={label}
-        onClick={handleClick}
-        size="small"
-        variant="outlined"
-      />
-
-      <M.Backdrop open={opened} className={classes.backdrop} onClick={handleClose} />
-      {opened && (
-        <M.Paper className={classes.popup} elevation={4}>
-          {children}
-        </M.Paper>
-      )}
-    </div>
-  )
+export default function Button({ label = 'Get files', ...props }: ButtonProps) {
+  return <Buttons.WithPopover icon="download" label={label} {...props} />
 }
