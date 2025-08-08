c0dd884fab50cbe1202f45f653caffccf174fb38
Maksim Chervonnyi <mail@redmax.dev>
2025-08-08 10:59:22 +0200

    use single state instead of value/error

--- catalog/app/components/Filters/DateField.tsx
+++ catalog/app/components/Filters/DateField.tsx
@@ -4,9 +4,73 @@ import * as M from '@material-ui/core'
 
 import Log from 'utils/Logging'
 
+const InputLabelProps = { shrink: true }
+
+// TODO: return Value?
 const ymdToDate = (ymd: string): Date => new Date(ymd)
 
+// TODO: return Value?
 const dateToYmd = (date: Date): string => dateFns.format(date, 'yyyy-MM-dd')
+
+const dateToYmdSafe = (date?: Date): string | undefined => {
+  if (!date) return undefined
+  try {
+    return dateToYmd(date)
+  } catch (e) {
+    Log.error(e)
+    return undefined
+  }
+}
+
+type ValueOk = { _tag: 'ok'; value: string }
+
+type ValueErr = { _tag: 'error'; value: string; error: Error }
+
+type Value = ValueOk | ValueErr
+
+const Ok = (value: string): ValueOk => ({
+  _tag: 'ok',
+  value,
+})
+
+const Err = (value: string, error: unknown): ValueErr => ({
+  _tag: 'error',
+  value,
+  error: error instanceof Error ? error : new Error('Invalid date'),
+})
+
+function useDateInput(date: Date | null): {
+  state: Value
+  setValue: (v: string) => Error | Date
+} {
+  const [state, setState] = React.useState<ValueOk | ValueErr>(Ok(''))
+
+  React.useEffect(() => {
+    if (!date) {
+      setState(Err('', new Error('Empty date')))
+      return
+    }
+    try {
+      setState(Ok(dateToYmd(date)))
+    } catch (e) {
+      setState(Err('', e))
+    }
+  }, [date])
+
+  const setValue = React.useCallback((value: string) => {
+    const d = ymdToDate(value)
+    if (!Number.isNaN(d.valueOf())) {
+      setState(Ok(value))
+      return d
+    }
+
+    const invalid = new Error(d.toString())
+    setState(Err(value, invalid))
+    return invalid
+  }, [])
+
+  return { state, setValue }
+}
 
 const useDateFieldStyles = M.makeStyles((t) => ({
   input: {
@@ -30,71 +94,35 @@ export default function DateField({
 }: Omit<M.TextFieldProps, 'value' | 'onChange'> & DateFieldProps) {
   const classes = useDateFieldStyles()
 
-  const [value, setValue] = React.useState('')
-  const [error, setError] = React.useState<Error | null>(null)
-
-  React.useEffect(() => {
-    if (!date) {
-      setError(new Error('Empty date'))
-      setValue('')
-      return
-    }
-    try {
-      const ymd = dateToYmd(date)
-      setError(null)
-      setValue(ymd)
-    } catch (e) {
-      setError(e instanceof Error ? e : new Error('Failed to format'))
-      setValue('')
-    }
-  }, [date])
-
+  const { state, setValue } = useDateInput(date)
   const handleChange = React.useCallback(
     (event) => {
-      setValue(event.target.value)
-
-      const d = ymdToDate(event.target.value)
-      if (Number.isNaN(d.valueOf())) {
-        setError(new Error(d.toString()))
-      } else {
-        onChange(d)
-      }
+      const dateOrError = setValue(event.target.value)
+      if (dateOrError instanceof Error) return
+      onChange(dateOrError)
     },
-    [onChange],
+    [onChange, setValue],
   )
 
-  const min = React.useMemo(() => {
-    if (!extents.min) return undefined
-    try {
-      return dateToYmd(extents.min)
-    } catch (e) {
-      Log.error(e)
-      return undefined
-    }
-  }, [extents])
+  const inputProps = React.useMemo(
+    () => ({ min: dateToYmdSafe(extents.min), max: dateToYmdSafe(extents.max) }),
+    [extents],
+  )
 
-  const max = React.useMemo(() => {
-    if (!extents.max) return undefined
-    try {
-      return dateToYmd(extents.max)
-    } catch (e) {
-      Log.error(e)
-      return undefined
-    }
-  }, [extents])
+  const InputProps = React.useMemo(() => ({ classes }), [classes])
 
   return (
     <M.TextField
-      InputLabelProps={{ shrink: true }}
-      InputProps={{ classes }}
-      inputProps={{ min, max }}
+      InputLabelProps={InputLabelProps}
+      InputProps={InputProps}
       className={className}
-      error={!!error}
-      helperText={error?.message}
+      error={state._tag === 'error'}
+      helperText={state._tag === 'error' && state.error.message}
+      inputProps={inputProps}
       onChange={handleChange}
       size="small"
       type="date"
-      value={value}
+      value={state.value}
       variant="outlined"
       {...props}
     />
