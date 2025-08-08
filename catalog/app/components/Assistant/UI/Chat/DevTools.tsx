4dad88893345cd782a460c85d828c55fe1de4157
nl_0 <nl.imbecile@gmail.com>
2025-08-08 15:54:05 +0200

    model id override

--- catalog/app/components/Assistant/UI/Chat/DevTools.tsx
+++ catalog/app/components/Assistant/UI/Chat/DevTools.tsx
@@ -1,10 +1,66 @@
 import * as Eff from 'effect'
 import * as React from 'react'
 import * as M from '@material-ui/core'
+import ClearIcon from '@material-ui/icons/Clear'
 
 import JsonDisplay from 'components/JsonDisplay'
 
 import * as Model from '../../Model'
+import * as Bedrock from '../../Model/Bedrock'
+
+const useModelIdOverrideStyles = M.makeStyles((t) => ({
+  root: {
+    margin: t.spacing(2, 0),
+    padding: t.spacing(0, 2),
+  },
+}))
+
+function ModelIdOverride() {
+  const classes = useModelIdOverrideStyles()
+
+  const [modelId, setModelId] = React.useState(Bedrock.getModelIdOverride)
+
+  const handleModelIdChange = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setModelId(event.target.value)
+    },
+    [],
+  )
+
+  React.useEffect(() => Bedrock.setModelIdOverride(modelId), [modelId])
+
+  const handleClear = React.useCallback(() => setModelId(''), [])
+
+  return (
+    <div className={classes.root}>
+      <M.TextField
+        label="Bedrock Model ID"
+        placeholder={Bedrock.DEFAULT_MODEL_ID}
+        value={modelId}
+        onChange={handleModelIdChange}
+        fullWidth
+        helperText="Leave empty to use default"
+        InputLabelProps={{ shrink: true }}
+        InputProps={{
+          endAdornment: modelId ? (
+            <M.InputAdornment position="end">
+              <M.IconButton
+                aria-label="Clear model ID override"
+                onClick={handleClear}
+                edge="end"
+                size="small"
+              >
+                <M.Tooltip arrow title="Clear model ID override">
+                  <ClearIcon />
+                </M.Tooltip>
+              </M.IconButton>
+            </M.InputAdornment>
+          ) : null,
+        }}
+      />
+    </div>
+  )
+}
 
 const useStyles = M.makeStyles((t) => ({
   root: {
@@ -52,6 +108,7 @@ export default function DevTools({ state }: DevToolsProps) {
     <section className={classes.root}>
       <h1 className={classes.heading}>Qurator Developer Tools</h1>
       <div className={classes.contents}>
+        <ModelIdOverride />
         <JsonDisplay className={classes.json} name="Context" value={context} />
         <JsonDisplay className={classes.json} name="State" value={state} />
         <JsonDisplay className={classes.json} name="Prompt" value={prompt} />
