8874e26cd459e29edb632905a882ee2bdfa2e9d2
nl_0 <nl.imbecile@gmail.com>
2025-08-12 13:36:12 +0200

    sign/proxy s3 requests in OPEN mode for authenticated users

--- catalog/app/utils/s3paths.ts
+++ catalog/app/utils/s3paths.ts
@@ -3,6 +3,7 @@ import { parse as parseUrl } from 'url'
 
 import * as R from 'ramda'
 
+import cfg from 'constants/config'
 import type { S3ObjectLocation } from 'model/S3'
 import { mkSearch } from 'utils/NamedRoutes'
 
@@ -104,12 +105,23 @@ export const parseS3Url = (url: string): S3ObjectLocation => {
 export const resolveKey = (from: string, to: string) =>
   resolve(`/${getPrefix(from)}`, to).substring(1)
 
+interface UriOptions {
+  proxy?: boolean
+  region?: string
+}
+
 // AWS docs (https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingBucket.html) state that
 // "buckets created in Regions launched after March 20, 2019 are not reachable via the
 // `https://bucket.s3.amazonaws.com naming scheme`", so probably we need to support
 // `https://bucket.s3.aws-region.amazonaws.com` scheme as well.
-export const handleToHttpsUri = ({ bucket, key, version }: S3ObjectLocation) =>
-  `https://${bucket}.s3.amazonaws.com/${encode(key)}${mkSearch({ versionId: version })}`
+export const handleToHttpsUri = (
+  { bucket, key, version }: S3ObjectLocation,
+  opts: UriOptions = {},
+) => {
+  const prefix = opts.proxy ? `${cfg.s3Proxy}/` : 'https://'
+  const region = opts.region ? `.${opts.region}` : ''
+  return `${prefix}${bucket}.s3${region}.amazonaws.com/${encode(key)}${mkSearch({ versionId: version })}`
+}
 
 export const handleToS3Url = ({ bucket, key, version = undefined }: S3ObjectLocation) =>
   `s3://${bucket}/${encode(key)}${mkSearch({ versionId: version })}`
