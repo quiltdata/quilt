4c49fcec4fa7276e408ec78671527a49ffa244b6
Maksim Chervonnyi <mail@redmax.dev>
2025-08-13 11:17:06 +0200

    Fix package count when zero packages

--- catalog/app/containers/Bucket/Overview/Header.tsx
+++ catalog/app/containers/Bucket/Overview/Header.tsx
@@ -311,6 +311,7 @@ function useStats(bucket: string, overviewUrl?: string | null) {
         data: ({ searchPackages: r }) => {
           switch (r.__typename) {
             case 'EmptySearchResultSet':
+              return formatQuantity(0)
             case 'InvalidInput':
               return '?'
             case 'PackagesSearchResultSet':
