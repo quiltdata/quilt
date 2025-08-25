31547d8185b79d9f1e5654128d803e182a01350c
Maksim Chervonnyi <mail@redmax.dev>
2025-08-25 14:30:46 +0200

    fix border layout jump

--- catalog/app/containers/Bucket/Toolbar/CreatePackage/index.tsx
+++ catalog/app/containers/Bucket/Toolbar/CreatePackage/index.tsx
@@ -1,4 +1,7 @@
+import cx from 'classnames'
 import * as React from 'react'
+import * as M from '@material-ui/core'
+
 import * as Buttons from 'components/Buttons'
 
 import { useSelection } from '../../Selection/Provider'
@@ -6,18 +9,26 @@ import { useSelection } from '../../Selection/Provider'
 export { default as BucketDirOptions } from './BucketDirOptions'
 export { default as useSuccessors } from './useSuccessors'
 
+const useStyles = M.makeStyles({
+  root: {
+    transitionProperty: 'background-color, box-shadow', // Disable border-width transition
+  },
+})
+
 interface ButtonProps {
   children: NonNullable<React.ReactNode>
   className?: string
   label?: string
 }
 
-export function Button(props: ButtonProps) {
+export function Button({ className, ...props }: ButtonProps) {
+  const classes = useStyles()
   const slt = useSelection()
   // FIXME: if slt == null => File button
   // FIXME: make WithPopover#icon optional
   return (
     <Buttons.WithPopover
+      className={cx(classes.root, className)}
       label="Create package"
       variant={slt.isEmpty ? 'outlined' : 'contained'}
       color={slt.isEmpty ? 'default' : 'primary'}
