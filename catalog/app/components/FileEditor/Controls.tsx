0fa03cef643c7fc9202aafd5b870fe975d48fe0a
Maksim Chervonnyi <mail@redmax.dev>
2025-09-12 11:48:53 +0200

    Fileeditor.Controls are used only in editing mode; remove dead code

--- catalog/app/components/FileEditor/Controls.tsx
+++ catalog/app/components/FileEditor/Controls.tsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 import * as M from '@material-ui/core'
 
 import * as Buttons from 'components/Buttons'
+
 import type { EditorState } from './State'
-import type { EditorInputType } from './types'
 
 interface AddFileButtonProps {
   onClick: () => void
@@ -37,85 +37,21 @@ export function PreviewButton({ className, preview, onPreview }: PreviewButtonPr
   )
 }
 
-const LIST_ITEM_TYPOGRAPHY_PROPS = { noWrap: true }
-
-interface ListItemProps {
-  onEdit: (type: EditorInputType | null) => void
-  type: EditorInputType
-}
-function ListItem({ onEdit, type }: ListItemProps) {
-  const handleEdit = React.useCallback(() => onEdit(type), [onEdit, type])
-  return (
-    <M.ListItem onClick={handleEdit} button>
-      <M.ListItemText
-        primary={type.title || 'Edit file'}
-        primaryTypographyProps={LIST_ITEM_TYPOGRAPHY_PROPS}
-      />
-    </M.ListItem>
-  )
-}
-
 interface ControlsProps extends EditorState {
   className?: string
 }
 
-export function Controls({
-  className,
-  editing,
-  onCancel,
-  onEdit,
-  saving,
-  onSave,
-  types,
-}: ControlsProps) {
+export function Controls({ className, onCancel, saving, onSave }: ControlsProps) {
   const disabled = saving
-  const handleEditClick = React.useCallback(() => onEdit(types[0]), [onEdit, types])
-  if (editing) {
-    return (
-      <M.ButtonGroup disabled={disabled} className={className} size="small">
-        <Buttons.Iconized icon="undo" onClick={onCancel} label="Cancel" />
-        <Buttons.Iconized
-          color="primary"
-          icon="save"
-          label="Save"
-          onClick={onSave}
-          variant="contained"
-        />
-      </M.ButtonGroup>
-    )
-  }
-
-  if (types.length > 1) {
-    return (
-      <Buttons.WithPopover
-        className={className}
-        disabled={disabled}
-        icon="edit"
-        label="Edit"
-      >
-        <M.List dense>
-          {types.map((type) => (
-            <ListItem key={type.brace} onEdit={onEdit} type={type} />
-          ))}
-        </M.List>
-      </Buttons.WithPopover>
-    )
-  }
-
   return (
     <M.ButtonGroup disabled={disabled} className={className} size="small">
+      <Buttons.Iconized icon="undo" onClick={onCancel} label="Cancel" />
       <Buttons.Iconized
-        disabled={disabled}
-        icon="edit"
-        label="Edit"
-        onClick={handleEditClick}
-      />
-      <Buttons.Iconized
-        className={className}
-        disabled={disabled}
-        icon="delete"
-        label="Delete"
-        onClick={() => alert('not implemented!')}
+        color="primary"
+        icon="save"
+        label="Save"
+        onClick={onSave}
+        variant="contained"
       />
     </M.ButtonGroup>
   )
