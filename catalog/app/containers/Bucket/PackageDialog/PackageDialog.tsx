7ab1ad9e4c02f55b0e59050bd3c38a458c22029e
Maksim Chervonnyi <mail@redmax.dev>
2025-09-25 17:05:33 +0200

    remove now unused code

--- catalog/app/containers/Bucket/PackageDialog/PackageDialog.tsx
+++ catalog/app/containers/Bucket/PackageDialog/PackageDialog.tsx
@@ -1,12 +1,9 @@
-import type { ErrorObject } from 'ajv'
 import * as R from 'ramda'
 import * as React from 'react'
 import * as M from '@material-ui/core'
 
 import cfg from 'constants/config'
 import { mkFormError } from 'utils/formTools'
-import { JsonRecord } from 'utils/types'
-import * as workflows from 'utils/workflows'
 
 export const MAX_UPLOAD_SIZE = 20 * 1000 * 1000 * 1000 // 20GB
 // XXX: keep in sync w/ the backend
@@ -15,54 +12,6 @@ export const MAX_S3_SIZE = cfg.chunkedChecksums
   ? 5 * 10 ** 12 // 5 TB
   : 50 * 10 ** 9 // 50 GB
 export const MAX_FILE_COUNT = 1000
-
-interface FieldProps {
-  error?: string
-  helperText?: React.ReactNode
-  validating?: boolean
-}
-
-const useFieldInputStyles = M.makeStyles({
-  root: {
-    // It hides M.CircularProgress (spinning square) overflow
-    overflow: 'hidden',
-  },
-})
-
-// TODO: re-use components/Form/TextField
-export function Field({
-  error,
-  helperText,
-  validating,
-  ...rest
-}: FieldProps & M.TextFieldProps) {
-  const inputClasses = useFieldInputStyles()
-  const props = {
-    InputLabelProps: { shrink: true },
-    InputProps: {
-      endAdornment: validating && <M.CircularProgress size={20} />,
-      classes: inputClasses,
-    },
-    error: !!error,
-    helperText: error || helperText,
-    ...rest,
-  }
-  return <M.TextField {...props} />
-}
-
-export const defaultWorkflowFromConfig = (wcfg?: workflows.WorkflowsConfig) =>
-  wcfg?.workflows.find((item) => item.isDefault)
-
-export function useWorkflowValidator(workflowsConfig?: workflows.WorkflowsConfig) {
-  return React.useMemo(
-    () => (workflow?: workflows.Workflow) => {
-      if (!workflowsConfig?.isWorkflowRequired) return undefined
-      if (workflow && workflow.slug !== workflows.notSelected) return undefined
-      return 'required'
-    },
-    [workflowsConfig?.isWorkflowRequired],
-  )
-}
 
 export function useCryptoApiValidation() {
   return React.useCallback(
@@ -114,19 +63,3 @@ export function DialogWrapper({
   )
   return <M.Dialog {...props} />
 }
-
-export function isEntryError(e: Error | ErrorObject): e is EntryValidationError {
-  return !!(e as EntryValidationError)?.data?.logical_key
-}
-
-export interface ValidationEntry {
-  logical_key: string
-  size: number
-  meta?: JsonRecord
-}
-
-interface EntryValidationError extends ErrorObject {
-  data: ValidationEntry
-}
-
-export type EntriesValidationErrors = (Error | EntryValidationError)[]
