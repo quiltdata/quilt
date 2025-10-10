e698ff4b9d498a098fe7b79dda8bca5433e655a9
Maksim Chervonnyi <mail@redmax.dev>
2025-10-10 11:07:04 +0200

    consistently trim hash

--- catalog/app/containers/Bucket/PackageCompare/Diff/Revisioned.tsx
+++ catalog/app/containers/Bucket/PackageCompare/Diff/Revisioned.tsx
@@ -2,7 +2,7 @@ import cx from 'classnames'
 import * as React from 'react'
 import * as M from '@material-ui/core'
 
-import { trimCenter } from 'utils/string'
+import * as Hash from 'components/Hash'
 
 const useStyles = M.makeStyles((t) => ({
   root: {
@@ -29,7 +29,7 @@ export default function Revisioned({ className, children, hash }: RevisionedProp
   const classes = useStyles()
   return (
     <div className={cx(classes.root, className)}>
-      <span className={classes.hash}>{trimCenter(hash, 12)}</span>
+      <Hash.Trimmed className={classes.hash}>{hash}</Hash.Trimmed>
       {children}
     </div>
   )
