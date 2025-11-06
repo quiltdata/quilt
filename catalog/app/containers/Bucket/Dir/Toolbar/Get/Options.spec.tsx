6e067161cb19341c2a0757d5fd7ab49af47e20e2
Maksim Chervonnyi <mail@redmax.dev>
2025-11-06 18:36:10 +0100

    tests for code samples

---
+++ catalog/app/containers/Bucket/Dir/Toolbar/Get/Options.spec.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import { render } from '@testing-library/react'
+
+import * as CodeSamples from 'containers/Bucket/CodeSamples'
+import { DirHandleCreate } from 'containers/Bucket/Toolbar'
+
+import Options from './Options'
+
+jest.mock('constants/config', () => ({}))
+
+jest.mock('containers/Bucket/CodeSamples', () => ({
+  Quilt3List: jest.fn(() => <div />),
+  Quilt3Fetch: jest.fn(() => <div />),
+  CliList: jest.fn(() => <div />),
+  CliFetch: jest.fn(() => <div />),
+}))
+
+jest.mock(
+  'containers/Bucket/Toolbar/GetOptions',
+  () =>
+    ({ code }: { code: React.ReactNode }) => <>{code}</>,
+)
+
+const { Quilt3Fetch, CliFetch } = CodeSamples as jest.Mocked<typeof CodeSamples>
+
+describe('containers/Bucket/Dir/Toolbar/Get/Options', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  const props = {
+    bucket: expect.any(String),
+    className: expect.any(String),
+    path: expect.any(String),
+  }
+
+  it('should render CodeSamples components with correct dest for nested path', () => {
+    const handle = DirHandleCreate('test-bucket', 'folder/subfolder/file.txt')
+
+    render(<Options handle={handle} />)
+
+    expect(Quilt3Fetch).toHaveBeenCalledWith({ ...props, dest: 'file.txt' }, {})
+    expect(CliFetch).toHaveBeenCalledWith({ ...props, dest: 'file.txt' }, {})
+  })
+
+  it('should render CodeSamples components with bucket as dest for root path', () => {
+    const handle = DirHandleCreate('test-bucket', '')
+
+    render(<Options handle={handle} />)
+
+    expect(Quilt3Fetch).toHaveBeenCalledWith({ ...props, dest: 'test-bucket' }, {})
+    expect(CliFetch).toHaveBeenCalledWith({ ...props, dest: 'test-bucket' }, {})
+  })
+})
