f0f87246d46adfde8f894b5ac2053c57f8276038
Maksim Chervonnyi <mail@redmax.dev>
2025-11-07 11:34:36 +0100

    simplify code

--- catalog/app/containers/Bucket/Dir/Toolbar/Get/Options.spec.tsx
+++ catalog/app/containers/Bucket/Dir/Toolbar/Get/Options.spec.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { render } from '@testing-library/react'
 
-import * as CodeSamples from 'containers/Bucket/CodeSamples'
+import { Quilt3Fetch, CliFetch } from 'containers/Bucket/CodeSamples'
 import { DirHandleCreate } from 'containers/Bucket/Toolbar'
 
 import Options from './Options'
@@ -9,10 +9,10 @@ import Options from './Options'
 jest.mock('constants/config', () => ({}))
 
 jest.mock('containers/Bucket/CodeSamples', () => ({
-  Quilt3List: jest.fn(() => <div />),
-  Quilt3Fetch: jest.fn(() => <div />),
-  CliList: jest.fn(() => <div />),
-  CliFetch: jest.fn(() => <div />),
+  Quilt3List: jest.fn(() => <></>),
+  Quilt3Fetch: jest.fn(() => <></>),
+  CliList: jest.fn(() => <></>),
+  CliFetch: jest.fn(() => <></>),
 }))
 
 jest.mock(
@@ -20,8 +20,6 @@ jest.mock(
   () =>
     ({ code }: { code: React.ReactNode }) => <>{code}</>,
 )
-
-const { Quilt3Fetch, CliFetch } = CodeSamples as jest.Mocked<typeof CodeSamples>
 
 describe('containers/Bucket/Dir/Toolbar/Get/Options', () => {
   const props = {
@@ -31,18 +29,14 @@ describe('containers/Bucket/Dir/Toolbar/Get/Options', () => {
   }
 
   it('should render CodeSamples components with correct dest for nested path', () => {
-    const handle = DirHandleCreate('test-bucket', 'folder/subfolder/')
-
-    render(<Options handle={handle} />)
+    render(<Options handle={DirHandleCreate('test-bucket', 'folder/subfolder/')} />)
 
     expect(Quilt3Fetch).toHaveBeenCalledWith({ ...props, dest: 'subfolder' }, {})
     expect(CliFetch).toHaveBeenCalledWith({ ...props, dest: 'subfolder' }, {})
   })
 
   it('should render CodeSamples components with bucket as dest for root path', () => {
-    const handle = DirHandleCreate('test-bucket', '')
-
-    render(<Options handle={handle} />)
+    render(<Options handle={DirHandleCreate('test-bucket', '')} />)
 
     expect(Quilt3Fetch).toHaveBeenCalledWith({ ...props, dest: 'test-bucket' }, {})
     expect(CliFetch).toHaveBeenCalledWith({ ...props, dest: 'test-bucket' }, {})
