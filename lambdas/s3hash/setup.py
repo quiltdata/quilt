d68303c3d735c511817f03455f678a1ea73a6635
Alexei Mochalov <alexei.a.mochalov@gmail.com>
2025-11-19 17:29:04 +0100

    Update s3hash dependencies
    - Update quilt_shared to commit 71d289f3 (with ChecksumAlgorithm enum)
    - Regenerate requirements.txt

--- lambdas/s3hash/setup.py
+++ lambdas/s3hash/setup.py
@@ -7,12 +7,12 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         "aiobotocore ~= 2.15",
+        "types-aiobotocore[s3] ~= 2.15",
         "botocore >= 1.35.50",
         "pydantic ~= 2.10",
-        "types-aiobotocore[s3] ~= 2.15",
         (
             "quilt_shared[pydantic,boto,quilt] @ https://github.com/quiltdata/quilt/archive/"
-            "27752e5efe7f981947d5cc0c7353bcf3f4dadade.zip"
+            "71d289f3c43e509f35e390a5696a0195a24d3e9e.zip"
             "#subdirectory=py-shared"
         ),
     ],
