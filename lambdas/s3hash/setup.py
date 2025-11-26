054e178d0cfa8c15abed9f4c213283ec4c6183fa
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2025-11-26 13:03:55 +0700

    bump shared

--- lambdas/s3hash/setup.py
+++ lambdas/s3hash/setup.py
@@ -13,7 +13,7 @@ setup(
         "quilt3 >= 7, < 8",
         (
             "quilt_shared[pydantic,boto,quilt] @ https://github.com/quiltdata/quilt/archive/"
-            "a860e9936efccb5160fe476894ad1030ebdbf863.zip"
+            "a4b310300bfe1cd5f2b319930b03f5264e82d5e5.zip"
             "#subdirectory=py-shared"
         ),
     ],
