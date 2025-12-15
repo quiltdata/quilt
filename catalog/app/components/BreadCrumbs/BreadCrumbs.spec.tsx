e5172430a880ae5333870335be5de2f865e5ad8b
Maksim Chervonnyi <mail@redmax.dev>
2025-12-15 12:42:40 +0100

    replace snapshots with querying in Breadcrumbs

--- catalog/app/components/BreadCrumbs/BreadCrumbs.spec.tsx
+++ catalog/app/components/BreadCrumbs/BreadCrumbs.spec.tsx
@@ -1,64 +1,73 @@
 import * as React from 'react'
-import { render } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
-import { describe, it, expect, vi } from 'vitest'
-
-import { makeStyles } from 'utils/makeStyles.spec'
-
-vi.mock('@material-ui/core', async () => ({
-  ...(await vi.importActual('@material-ui/core')),
-  makeStyles: makeStyles('BreadCrumbs'),
-}))
+import { describe, it, expect, afterEach } from 'vitest'
 
 import * as BreadCrumbs from './'
 
 describe('components/BreadCrumbs', () => {
+  afterEach(cleanup)
+
   describe('Segment', () => {
     it('should render EMPTY', () => {
-      const { container } = render(<BreadCrumbs.Segment />)
-      expect(container).toMatchSnapshot()
+      const { getByText } = render(<BreadCrumbs.Segment />)
+      expect(getByText('<EMPTY>').textContent).toBe('<EMPTY>')
     })
+
     it('should render label', () => {
-      const { container } = render(<BreadCrumbs.Segment label="Lorem ipsum" />)
-      expect(container).toMatchSnapshot()
+      const { getByText } = render(<BreadCrumbs.Segment label="Lorem ipsum" />)
+      expect(getByText('Lorem ipsum').textContent).toBe('Lorem ipsum')
     })
+
     it('should render link', () => {
-      const { container } = render(
+      const { getByText } = render(
         <MemoryRouter>
           <BreadCrumbs.Segment label="A" to="/a" />
         </MemoryRouter>,
       )
-      expect(container).toMatchSnapshot()
+      expect(getByText('A').getAttribute('href')).toBe('/a')
     })
   })
+
   describe('render', () => {
     it('basic breadcrumbs', () => {
       const crumbs = BreadCrumbs.getCrumbs('aa a/bb-b/c/d_d', (x) => x, 'ROOT')
-      const { container } = render(
+      const { container, getByText } = render(
         <MemoryRouter>{BreadCrumbs.render(crumbs)}</MemoryRouter>,
       )
-      expect(container).toMatchSnapshot()
+      expect(getByText('ROOT').getAttribute('to')).toBe('')
+      expect(getByText('aa a').getAttribute('href')).toBe('/aa a/')
+      expect(getByText('bb-b').getAttribute('href')).toBe('/aa a/bb-b/')
+      expect(getByText('c').getAttribute('href')).toBe('/aa a/bb-b/c/')
+      expect(container.textContent).toContain('d_d')
     })
+
     it('and transform links', () => {
       const crumbs = BreadCrumbs.getCrumbs('a/b', (x) => x, 'ROOT')
       const getLinkProps = ({ to }: { to?: string }) => ({
         href: `https://quiltdata.com/${to || ''}`,
       })
-      const { container } = render(
+      const { container, getByText } = render(
         <MemoryRouter>{BreadCrumbs.render(crumbs, { getLinkProps })}</MemoryRouter>,
       )
-      expect(container).toMatchSnapshot()
+      expect(getByText('ROOT').getAttribute('href')).toBe('https://quiltdata.com/')
+      expect(getByText('a').getAttribute('href')).toBe('https://quiltdata.com/a/')
+      expect(container.textContent).toContain('b')
     })
+
     it('and make links for every crumb', () => {
       const crumbs = BreadCrumbs.getCrumbs('a/b', (x) => x, 'ROOT', { tailLink: true })
       const getLinkProps = ({ to }: { to?: string }) => ({
         href: `https://quiltdata.com/${to || ''}`,
       })
-      const { container } = render(
+      const { getByText } = render(
         <MemoryRouter>{BreadCrumbs.render(crumbs, { getLinkProps })}</MemoryRouter>,
       )
-      expect(container).toMatchSnapshot()
+      expect(getByText('ROOT').getAttribute('href')).toBe('https://quiltdata.com/')
+      expect(getByText('a').getAttribute('href')).toBe('https://quiltdata.com/a/')
+      expect(getByText('b').getAttribute('href')).toBe('https://quiltdata.com/a/b')
     })
+
     it('and end with separator', () => {
       const crumbs = BreadCrumbs.getCrumbs('a/b', (x) => x, 'ROOT', {
         tailLink: true,
@@ -67,19 +76,26 @@ describe('components/BreadCrumbs', () => {
       const getLinkProps = ({ to }: { to?: string }) => ({
         href: `https://quiltdata.com/${to || ''}`,
       })
-      const { container } = render(
+      const { container, getByText } = render(
         <MemoryRouter>{BreadCrumbs.render(crumbs, { getLinkProps })}</MemoryRouter>,
       )
-      expect(container).toMatchSnapshot()
+      expect(getByText('ROOT').getAttribute('href')).toBe('https://quiltdata.com/')
+      expect(getByText('a').getAttribute('href')).toBe('https://quiltdata.com/a/')
+      expect(getByText('b').getAttribute('href')).toBe('https://quiltdata.com/a/b')
+      expect(container.textContent).toBe('ROOT / a / b / ')
     })
+
     it('without root label', () => {
       const crumbs = BreadCrumbs.getCrumbs('a/b/c', (x) => x)
-      const { container } = render(
+      const { getByText, queryByText } = render(
         <MemoryRouter>{BreadCrumbs.render(crumbs)}</MemoryRouter>,
       )
-      expect(container).toMatchSnapshot()
+      expect(getByText('a').getAttribute('href')).toBe('/a/')
+      expect(getByText('b').getAttribute('href')).toBe('/a/b/')
+      expect(queryByText('ROOT')).toBeFalsy()
     })
   })
+
   it('copyWithoutSpaces', () => {
     const input = `ROOT / aa a
       / bb-b / c / <EMPTY> / d_d`
