8872765f673e1a1e85a72c91ddbd99cb0f47e9d2
Maksim Chervonnyi <mail@redmax.dev>
2026-05-11 15:19:44 +0200

    test(catalog): switch Markdown renderer assertions to inline snapshots
    Pin exact HTML output via toMatchInlineSnapshot instead of hand-written
    output strings. Maintenance under markdown-it / highlight.js upgrades
    is now `vitest --update` plus a PR diff review, instead of editing the
    expectations by hand and risking copy-paste drift.

--- catalog/app/components/Markdown/Markdown.spec.ts
+++ catalog/app/components/Markdown/Markdown.spec.ts
@@ -19,14 +19,15 @@ describe('components/Markdown', () => {
 <span href="anything" src="anything">don't touch</span>
 <a href="anything">link</a>
 <img src="anything"/>`
-      const output = `<p>Something</p>
-<p><a rel="nofollow" href="LINK anything">link</a>
-<img alt="" src="IMAGE anything"></p>
-<p><span src="anything" href="anything">don’t touch</span>
-<a rel="nofollow" href="LINK anything">link</a>
-<img src="IMAGE anything"></p>
-`
-      expect(render(input)).toBe(output)
+      expect(render(input)).toMatchInlineSnapshot(`
+        "<p>Something</p>
+        <p><a rel="nofollow" href="LINK anything">link</a>
+        <img alt="" src="IMAGE anything"></p>
+        <p><span src="anything" href="anything">don’t touch</span>
+        <a rel="nofollow" href="LINK anything">link</a>
+        <img src="IMAGE anything"></p>
+        "
+      `)
     })
 
     it('Preserves HTML attributes', () => {
@@ -38,14 +39,15 @@ describe('components/Markdown', () => {
 <span href="anything" src="anything" data-dont-touch>don't touch</span>
 <a rel="nofollow base" title="Link title" href="anything">link</a>
 <img alt="Alternative text" src="anything"/>`
-      const output = `<p>Something</p>
-<p><a rel="nofollow" href="LINK anything">link</a>
-<img alt="Alternative text" src="IMAGE anything"></p>
-<p><span data-dont-touch="" src="anything" href="anything">don’t touch</span>
-<a href="LINK anything" title="Link title" rel="nofollow base nofollow">link</a>
-<img src="IMAGE anything" alt="Alternative text"></p>
-`
-      expect(render(input)).toBe(output)
+      expect(render(input)).toMatchInlineSnapshot(`
+        "<p>Something</p>
+        <p><a rel="nofollow" href="LINK anything">link</a>
+        <img alt="Alternative text" src="IMAGE anything"></p>
+        <p><span data-dont-touch="" src="anything" href="anything">don’t touch</span>
+        <a href="LINK anything" title="Link title" rel="nofollow base nofollow">link</a>
+        <img src="IMAGE anything" alt="Alternative text"></p>
+        "
+      `)
     })
     it('Avoids XSS', () => {
       const hack = getRenderer({
@@ -53,7 +55,10 @@ describe('components/Markdown', () => {
         processLink: () => 'javascript:alert(0)',
         win: win as $TSFixMe,
       })
-      expect(hack('<a href="anything">l</a>')).toBe('<p><a rel="nofollow">l</a></p>\n')
+      expect(hack('<a href="anything">l</a>')).toMatchInlineSnapshot(`
+        "<p><a rel="nofollow">l</a></p>
+        "
+      `)
     })
 
     it('Strips invalid attributes', () => {
@@ -69,9 +74,10 @@ describe('components/Markdown', () => {
 
       const input = `[title](link-url) ![](img-url)`
       expect(() => withInvalidAttributes(input)).not.toThrow()
-
-      const output = `<p><a rel="nofollow">title</a> <img alt=""></p>\n`
-      expect(withInvalidAttributes(input)).toBe(output)
+      expect(withInvalidAttributes(input)).toMatchInlineSnapshot(`
+        "<p><a rel="nofollow">title</a> <img alt=""></p>
+        "
+      `)
     })
 
     const renderPlain = getRenderer({
@@ -82,50 +88,68 @@ describe('components/Markdown', () => {
 
     it('Highlights fenced code via highlight.js', () => {
       const input = '```js\nconst x = 1\n```'
-      const output = `<pre><code class="language-js"><span class="hljs-keyword">const</span> x = <span class="hljs-number">1</span>\n</code></pre>\n`
-      expect(renderPlain(input)).toBe(output)
+      expect(renderPlain(input)).toMatchInlineSnapshot(`
+        "<pre><code class="language-js"><span class="hljs-keyword">const</span> x = <span class="hljs-number">1</span>
+        </code></pre>
+        "
+      `)
     })
 
     it('Autolinks bare URLs', () => {
       const input = 'see https://example.com'
-      const output = `<p>see <a rel="nofollow" href="https://example.com">https://example.com</a></p>\n`
-      expect(renderPlain(input)).toBe(output)
+      expect(renderPlain(input)).toMatchInlineSnapshot(`
+        "<p>see <a rel="nofollow" href="https://example.com">https://example.com</a></p>
+        "
+      `)
     })
 
     it('Adds nofollow rel to links', () => {
       const input = '[x](https://example.com)'
-      const output = `<p><a rel="nofollow" href="https://example.com">x</a></p>\n`
-      expect(renderPlain(input)).toBe(output)
+      expect(renderPlain(input)).toMatchInlineSnapshot(`
+        "<p><a rel="nofollow" href="https://example.com">x</a></p>
+        "
+      `)
     })
 
     it('Renders tasklist glyphs', () => {
       const input = '- [x] done\n- [ ] todo\n- [] none'
-      const output = `<ul>\n<li>☑ done</li>\n<li>☐ todo</li>\n<li>☐ none</li>\n</ul>\n`
-      expect(renderPlain(input)).toBe(output)
+      expect(renderPlain(input)).toMatchInlineSnapshot(`
+        "<ul>
+        <li>☑ done</li>
+        <li>☐ todo</li>
+        <li>☐ none</li>
+        </ul>
+        "
+      `)
     })
 
     it('Skips tasklist glyph for escaped brackets', () => {
       const input = '\\[x] not a checkbox'
-      const output = `<p>[x] not a checkbox</p>\n`
-      expect(renderPlain(input)).toBe(output)
+      expect(renderPlain(input)).toMatchInlineSnapshot(`
+        "<p>[x] not a checkbox</p>
+        "
+      `)
     })
 
     it('Skips tasklist glyph mid-word', () => {
       const input = 'foo[x]bar'
-      const output = `<p>foo[x]bar</p>\n`
-      expect(renderPlain(input)).toBe(output)
+      expect(renderPlain(input)).toMatchInlineSnapshot(`
+        "<p>foo[x]bar</p>
+        "
+      `)
     })
 
     it('Applies typographer replacements', () => {
       const input = '(c) -- test'
-      const output = `<p>© – test</p>\n`
-      expect(renderPlain(input)).toBe(output)
+      expect(renderPlain(input)).toMatchInlineSnapshot(`
+        "<p>© – test</p>
+        "
+      `)
     })
 
     it('Strips <script> via DOMPurify', () => {
       const input = '<script>alert(1)</script>'
-      const output = ``
-      expect(renderPlain(input)).toBe(output)
+      expect(renderPlain(input)).toMatchInlineSnapshot(`""`)
     })
   })
 })
