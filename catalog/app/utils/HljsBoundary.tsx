25a1ac17195a0aa4a7ac8a3638dd6c1aa47a415c
Maksim Chervonnyi <mail@redmax.dev>
2026-06-25 14:11:19 +0200

    fix(hljs): keep HljsBoundary fallback as ReactNode

--- catalog/app/utils/HljsBoundary.tsx
+++ catalog/app/utils/HljsBoundary.tsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 
 interface HljsBoundaryProps {
   children: React.ReactNode
-  fallback: React.ReactElement | null
+  fallback: React.ReactNode
 }
 
 export default function HljsBoundary({ children, fallback }: HljsBoundaryProps) {
-  return <React.Suspense fallback={fallback}>{children}</React.Suspense>
+  return <React.Suspense fallback={fallback ?? null}>{children}</React.Suspense>
 }
