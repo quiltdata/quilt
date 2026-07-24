quiltdata/quilt

commit ae8521ffed445efc676da7752b0269bbc593a824

PackageRegistry v2 (#1678)

api/python/quilt3/backends/local.py
@@ -1,9 +1,11 @@
+import operator
 import os
 import shutil
+from datetime import datetime
 
 from quilt3.data_transfer import delete_url
 
-from .base import PackageRegistryV1
+from .base import PackageRegistryV1, PackageRegistryV2
 
 
 def safe_listdir(path):
@@ -20,18 +22,52 @@ class LocalPackageRegistryV1(PackageRegistryV1):
             for pkg in safe_listdir(os.path.join(pointers_dir_path, usr)):
                 yield f'{usr}/{pkg}'
 
-    def list_package_versions(self, pkg_name: str):
+    def list_package_pointers(self, pkg_name: str):
         pointers_dir_path = self.pointers_dir(pkg_name).path
         for path in safe_listdir(pointers_dir_path):
             with open(os.path.join(pointers_dir_path, path)) as f:
                 yield path, f.read()
 
-    def delete_package(self, pkg_name: str, top_hash: str = None):
-        package_path = self.pointers_dir(pkg_name)
-        if top_hash is None:
-            shutil.rmtree(package_path.path)
-        else:
-            super().delete_package(pkg_name, top_hash)
-            delete_url(package_path)
-        usr = pkg_name.partition('/')[0]
-        delete_url(self.pointers_global_dir.join(usr))
+    def delete_package(self, pkg_name: str):
+        shutil.rmtree(self._pointers_usr_dir(pkg_name).path)
+
+    def delete_package_version(self, pkg_name: str, top_hash: str):
+        super().delete_package_version(pkg_name, top_hash)
+        delete_url(self.pointers_dir(pkg_name))
+        delete_url(self._pointers_usr_dir(pkg_name))
+
+
+class LocalPackageRegistryV2(PackageRegistryV2):
+    _from_path_to_package_name = operator.methodcaller('replace', '@', '/')
+
+    def list_packages(self):
+        return map(self._from_path_to_package_name, safe_listdir(self.manifests_global_dir.path))
+
+    list_package_pointers = LocalPackageRegistryV1.list_package_pointers
+
+    def list_package_versions_with_timestamps(self, pkg_name: str):
+        try:
+            scanner = os.scandir(self.manifests_package_dir(pkg_name).path)
+        except FileNotFoundError:
+            return
+        with scanner as entries:
+            for entry in entries:
+                yield datetime.fromtimestamp(entry.stat().st_mtime), entry.name
+
+    def delete_package(self, pkg_name: str):
+        shutil.rmtree(self.manifests_package_dir(pkg_name).path)
+        shutil.rmtree(self.pointers_dir(pkg_name).path)
+
+    def delete_package_version(self, pkg_name: str, top_hash: str):
+        shutil.rmtree(self._manifest_parent_pk(pkg_name, top_hash).path)
+        super().delete_package_version(pkg_name, top_hash)
+        delete_url(self.pointers_dir(pkg_name))
+        delete_url(self.manifests_package_dir(pkg_name))
+
+
+def get_package_registry(version: int):
+    if version == 1:
+        return LocalPackageRegistryV1
+    if version == 2:
+        return LocalPackageRegistryV2
+    raise ValueError()

api/python/quilt3/api.py
@@ -34,7 +34,11 @@ def delete_package(name, registry=None, top_hash=None):
         top_hash (str): Optional. A package hash to delete, instead of the whole package.
     """
     validate_package_name(name)
-    get_package_registry(registry).delete_package(name, top_hash)
+    registry = get_package_registry(registry)
+    if top_hash is None:
+        registry.delete_package(name)
+    else:
+        registry.delete_package_version(name, registry.resolve_top_hash(name, top_hash))
 
 
 @ApiTelemetry("api.list_packages")

api/python/quilt3/backends/__init__.py
@@ -1,8 +1,8 @@
 from quilt3.util import PhysicalKey, get_from_config, fix_url
 
 from .base import PackageRegistry
-from .local import LocalPackageRegistryV1
-from .s3 import S3PackageRegistryV1
+from . import local
+from . import s3
 
 
 def get_package_registry(path=None) -> PackageRegistry:
@@ -16,4 +16,6 @@ def get_package_registry(path=None) -> PackageRegistry:
         path = PhysicalKey.from_url(
             get_from_config('default_local_registry') if path is None else fix_url(path)
         )
-    return (LocalPackageRegistryV1 if path.is_local() else S3PackageRegistryV1)(path)
+    return (local if path.is_local() else s3).get_package_registry(
+        version=int(get_from_config('default_registry_version')),
+    )(path)

api/python/quilt3/backends/base.py
@@ -1,4 +1,5 @@
 import abc
+import operator
 import time
 
 from quilt3.data_transfer import list_url, delete_url, copy_file, put_bytes, get_bytes
@@ -7,6 +8,8 @@ from quilt3.util import PhysicalKey, QuiltException
 
 class PackageRegistry(abc.ABC):
     latest_tag_name = 'latest'
+    top_hash_len = 64
+    revision_pointers = False
 
     def __init__(self, base: PhysicalKey):
         self.base = base
@@ -40,9 +43,8 @@ class PackageRegistry(abc.ABC):
     def pointer_pk(self, pkg_name: str, pointer_name: str) -> PhysicalKey:
         pass
 
-    @abc.abstractmethod
     def pointer_latest_pk(self, pkg_name: str) -> PhysicalKey:
-        pass
+        return self.pointer_pk(pkg_name, self.latest_tag_name)
 
     @property
     @abc.abstractmethod
@@ -62,11 +64,19 @@ class PackageRegistry(abc.ABC):
         pass
 
     @abc.abstractmethod
+    def list_package_pointers(self, pkg_name: str):
+        pass
+
+    @abc.abstractmethod
     def list_package_versions(self, pkg_name: str):
         pass
 
     @abc.abstractmethod
-    def delete_package(self, pkg_name: str, top_hash: str = None):
+    def delete_package(self, pkg_name: str):
+        pass
+
+    @abc.abstractmethod
+    def delete_package_version(self, pkg_name: str, top_hash: str):
         pass
 
     @abc.abstractmethod
@@ -84,6 +94,7 @@ class PackageRegistry(abc.ABC):
 
 class PackageRegistryV1(PackageRegistry):
     root_path = '.quilt'
+    revision_pointers = True
 
     @property
     def pointers_global_dir(self) -> PhysicalKey:
@@ -92,11 +103,11 @@ class PackageRegistryV1(PackageRegistry):
     def pointers_dir(self, pkg_name: str) -> PhysicalKey:
         return self.root.join(f'named_packages/{pkg_name}/')
 
+    def _pointers_usr_dir(self, pkg_name: str) -> PhysicalKey:
+        return self.root.join(f'named_packages/{pkg_name.partition("/")[0]}/')
+
     def pointer_pk(self, pkg_name: str, pointer_name: str) -> PhysicalKey:
         return self.root.join(f'named_packages/{pkg_name}/{pointer_name}')
-
-    def pointer_latest_pk(self, pkg_name: str) -> PhysicalKey:
-        return self.pointer_pk(pkg_name, self.latest_tag_name)
 
     @property
     def manifests_global_dir(self) -> PhysicalKey:
@@ -108,32 +119,6 @@ class PackageRegistryV1(PackageRegistry):
     def manifest_pk(self, pkg_name: str, top_hash: str) -> PhysicalKey:
         return self.root.join(f'packages/{top_hash}')
 
-    def delete_package(self, pkg_name: str, top_hash: str = None):
-        package_path = self.pointers_dir(pkg_name)
-        paths = list(list_url(package_path))
-        if not paths:
-            raise QuiltException("No such package exists in the given directory.")
-
-        if top_hash is not None:
-            top_hash = self.resolve_top_hash(pkg_name, top_hash)
-            deleted = []
-            remaining = []
-            for path, _ in paths:
-                pkg_hash = get_bytes(self.pointer_pk(pkg_name, path)).decode()
-                (deleted if pkg_hash == top_hash else remaining).append(path)
-            if not deleted:
-                raise QuiltException("No such package version exists in the given directory.")
-            for path in deleted:
-                delete_url(self.pointer_pk(pkg_name, path))
-            if 'latest' in deleted and remaining:
-                # Create a new "latest". Technically, we need to compare numerically,
-                # but string comparisons will be fine till year 2286.
-                new_latest = max(remaining)
-                copy_file(self.pointer_pk(pkg_name, new_latest), self.pointer_latest_pk(pkg_name))
-        else:
-            for path, _ in paths:
-                delete_url(self.pointer_pk(pkg_name, path))
-
     def push_manifest(self, pkg_name: str, top_hash: str, manifest_data: bytes):
         put_bytes(manifest_data, self.manifest_pk(pkg_name, top_hash))
         hash_bytes = top_hash.encode()
@@ -141,11 +126,15 @@ class PackageRegistryV1(PackageRegistry):
         put_bytes(hash_bytes, self.pointer_pk(pkg_name, str(int(time.time()))))
         put_bytes(hash_bytes, self.pointer_latest_pk(pkg_name))
 
+    @staticmethod
+    def _top_hash_from_path(path: str) -> str:
+        return path
+
     def resolve_top_hash(self, pkg_name: str, hash_prefix: str) -> str:
         if len(hash_prefix) == 64:
             top_hash = hash_prefix
         elif 6 <= len(hash_prefix) < 64:
-            matching_hashes = [h for h, _
+            matching_hashes = [self._top_hash_from_path(h) for h, _
                                in list_url(self.manifests_package_dir(pkg_name))
                                if h.startswith(hash_prefix)]
             if not matching_hashes:
@@ -159,10 +148,12 @@ class PackageRegistryV1(PackageRegistry):
         # TODO: verify that name is correct with respect to this top_hash
         return top_hash
 
+    resolve_top_hash_requires_pkg_name = False
+
     def shorten_top_hash(self, pkg_name: str, top_hash: str) -> str:
         min_shorthash_len = 7
 
-        matches = [h for h, _ in list_url(self.manifests_package_dir(pkg_name))
+        matches = [self._top_hash_from_path(h) for h, _ in list_url(self.manifests_package_dir(pkg_name))
                    if h.startswith(top_hash[:min_shorthash_len])]
         if len(matches) == 0:
             raise ValueError(f"Tophash {top_hash} was not found in registry {self.base}")
@@ -171,3 +162,91 @@ class PackageRegistryV1(PackageRegistry):
             matches = [h for h in matches if h.startswith(potential_shorthash)]
             if len(matches) == 1:
                 return potential_shorthash
+
+    def list_package_versions(self, pkg_name: str):
+        return self.list_package_pointers(pkg_name)
+
+    def delete_package_version(self, pkg_name: str, top_hash: str):
+        deleted = []
+        remaining = []
+        for path, pkg_hash in self.list_package_versions(pkg_name):
+            (deleted if pkg_hash == top_hash else remaining).append(path)
+        if not deleted:
+            raise QuiltException("No such package version exists in the given directory.")
+        for path in deleted:
+            delete_url(self.pointer_pk(pkg_name, path))
+        if 'latest' in deleted and remaining:
+            # Create a new "latest". Technically, we need to compare numerically,
+            # but string comparisons will be fine till year 2286.
+            new_latest = max(remaining)
+            copy_file(self.pointer_pk(pkg_name, new_latest), self.pointer_latest_pk(pkg_name))
+
+
+class PackageRegistryV2(PackageRegistry):
+    """
+    All metadata files live under `.quilt/v2`:
+    * the manifests use `.quilt/v2/manifests/<usr>@<pkg>/<top_hash>/manifest.json` format for path
+    * the tag files (or pointers) live under `.quilt/v2/tags/<usr>@<pkg>/<tag_name>`, each of these files
+      contains the top hash of one of package manifests.
+    """
+
+    root_path = '.quilt/v2'
+
+    _package_name_to_path = operator.methodcaller('replace', '/', '@')
+
+    @property
+    def pointers_global_dir(self) -> PhysicalKey:
+        return self.root.join('tags/')
+
+    def pointers_dir(self, pkg_name: str) -> PhysicalKey:
+        return self.root.join(f'tags/{self._package_name_to_path(pkg_name)}/')
+
+    def pointer_pk(self, pkg_name: str, pointer_name: str) -> PhysicalKey:
+        return self.root.join(f'tags/{self._package_name_to_path(pkg_name)}/{pointer_name}')
+
+    @property
+    def manifests_global_dir(self) -> PhysicalKey:
+        return self.root.join('manifests/')
+
+    def manifests_package_dir(self, pkg_name: str) -> PhysicalKey:
+        return self.root.join(f'manifests/{self._package_name_to_path(pkg_name)}/')
+
+    def manifest_pk(self, pkg_name: str, top_hash: str) -> PhysicalKey:
+        return self.root.join(f'manifests/{self._package_name_to_path(pkg_name)}/{top_hash}/manifest.jsonl')
+
+    def _manifest_parent_pk(self, pkg_name: str, top_hash: str) -> PhysicalKey:
+        return self.root.join(f'manifests/{self._package_name_to_path(pkg_name)}/{top_hash}/')
+
+    @abc.abstractmethod
+    def list_package_versions_with_timestamps(self, pkg_name: str):
+        """
+        Returns:
+            An iterable of tuples containing the datetime and hash for the package.
+        """
+
+    def list_package_versions(self, pkg_name: str):
+        for dt, top_hash in self.list_package_versions_with_timestamps(pkg_name):
+            yield str(int(dt.timestamp())), top_hash
+
+    def push_manifest(self, pkg_name: str, top_hash: str, manifest_data: bytes):
+        put_bytes(manifest_data, self.manifest_pk(pkg_name, top_hash))
+        put_bytes(top_hash.encode(), self.pointer_latest_pk(pkg_name))
+
+    @staticmethod
+    def _top_hash_from_path(path: str) -> str:
+        return path.rsplit('/', 2)[-2]
+
+    resolve_top_hash = PackageRegistryV1.resolve_top_hash
+    resolve_top_hash_requires_pkg_name = True
+    shorten_top_hash = PackageRegistryV1.shorten_top_hash
+
+    def delete_package_version(self, pkg_name: str, top_hash: str):
+        delete_url(self.manifest_pk(pkg_name, top_hash))
+        if get_bytes(self.pointer_latest_pk(pkg_name)).decode() == top_hash:
+            delete_url(self.pointer_latest_pk(pkg_name))
+            timestamp, new_latest = max(self.list_package_versions_with_timestamps(pkg_name), default=(None, None))
+            if new_latest:
+                put_bytes(new_latest.encode(), self.pointer_latest_pk(pkg_name))
+        for pointer, pointer_top_hash in self.list_package_pointers(pkg_name):
+            if pointer_top_hash == top_hash:
+                delete_url(self.pointer_pk(pkg_name, pointer))

api/python/quilt3/backends/s3.py
@@ -1,6 +1,19 @@
-from quilt3.data_transfer import list_url, get_bytes
+from quilt3.data_transfer import list_url, get_bytes, S3ClientProvider, S3Api
+from quilt3.util import PhysicalKey
 
-from .base import PackageRegistryV1
+from .base import PackageRegistryV1, PackageRegistryV2
+
+
+def s3_list_objects(**kwargs):
+    s3_client = S3ClientProvider().find_correct_client(S3Api.LIST_OBJECTS_V2, kwargs['Bucket'], kwargs)
+    return s3_client.get_paginator('list_objects_v2').paginate(**kwargs)
+
+
+def delete_url_recursively(src: PhysicalKey):
+    s3_client = S3ClientProvider().standard_client
+    for resp in s3_list_objects(Bucket=src.bucket, Prefix=src.path):
+        for key in resp.get('Contents', ()):
+            s3_client.delete_object(Bucket=src.bucket, Key=key['Key'])
 
 
 class S3PackageRegistryV1(PackageRegistryV1):
@@ -13,8 +26,46 @@ class S3PackageRegistryV1(PackageRegistryV1):
                 prev_pkg = pkg
                 yield pkg
 
-    def list_package_versions(self, pkg_name: str):
+    def list_package_pointers(self, pkg_name: str):
         package_dir = self.pointers_dir(pkg_name)
         for path, _ in list_url(package_dir):
             pkg_hash = get_bytes(package_dir.join(path))
             yield path, pkg_hash.decode().strip()
+
+    def delete_package(self, pkg_name: str):
+        delete_url_recursively(self.pointers_dir(pkg_name))
+
+
+class S3PackageRegistryV2(PackageRegistryV2):
+    def list_packages(self):
+        prefix = self.manifests_global_dir.path
+        prefix_len = len(prefix)
+        for resp in s3_list_objects(
+            Bucket=self.manifests_global_dir.bucket,
+            Prefix=prefix,
+            Delimiter='/',
+        ):
+            for obj in resp.get('CommonPrefixes', ()):
+                yield obj['Prefix'][prefix_len:-1].replace('@', '/')
+
+    list_package_pointers = S3PackageRegistryV1.list_package_pointers
+
+    def list_package_versions_with_timestamps(self, pkg_name: str):
+        manifest_dir_pk = self.manifests_package_dir(pkg_name)
+        prefix = manifest_dir_pk.path
+        s = slice(len(prefix), None)
+        for response in s3_list_objects(Bucket=manifest_dir_pk.bucket, Prefix=prefix):
+            for obj in response.get('Contents', ()):
+                yield obj['LastModified'], self._top_hash_from_path(obj['Key'][s])
+
+    def delete_package(self, pkg_name: str):
+        delete_url_recursively(self.manifests_package_dir(pkg_name))
+        delete_url_recursively(self.pointers_dir(pkg_name))
+
+
+def get_package_registry(version: int):
+    if version == 1:
+        return S3PackageRegistryV1
+    if version == 2:
+        return S3PackageRegistryV2
+    raise ValueError()

api/python/quilt3/packages.py
@@ -511,14 +511,18 @@ class Package:
         except TypeError:
             name, registry, hash_prefix = cls._parse_resolve_hash_args(*args, **kwargs)
             validate_package_name(name)
+            return get_package_registry(registry).resolve_top_hash(name, hash_prefix)
         else:
+            registry = get_package_registry(registry)
+            if registry.resolve_top_hash_requires_pkg_name:
+                raise TypeError(f'Package name is required for resolving top hash at {registry.root}.')
             warnings.warn(
                 "Calling resolve_hash() without the 'name' parameter is deprecated.",
                 category=RemovedInQuilt4Warning,
                 stacklevel=2,
             )
+            return registry.resolve_top_hash(name, hash_prefix)
 
-        return get_package_registry(registry).resolve_top_hash(name, hash_prefix)
     # This is needed for nice signature in docs.
     resolve_hash.__func__.__signature__ = inspect.signature(_parse_resolve_hash_args.__func__)
 

api/python/quilt3/util.py
@@ -66,6 +66,8 @@ apiGatewayEndpoint:
 # Binary API Gateway endpoint (e.g., for preview)
 binaryApiGatewayEndpoint:
 
+default_registry_version: 1
+
 """.format(BASE_PATH.as_uri() + '/packages')
 
 

commit 205cab28dc24e03561676c971fede8fe4ce3cafb

Fix local delete_package wiping the whole user namespace
LocalPackageRegistryV1.delete_package rmtree'd _pointers_usr_dir, i.e.
named_packages/<usr>/, so deleting one package also deleted every sibling
package sharing its namespace. Target the package-scoped pointers_dir
instead, matching LocalPackageRegistryV2 and S3PackageRegistryV1, and clean
up the now-possibly-empty parent dir with delete_url (a no-op unless empty),
as delete_package_version already does.

Regressed in ae8521ff (#1678), which split delete_package/
delete_package_version and changed the rmtree target; the pre-split code
removed pointers_dir.

The existing test only ever built one package per namespace, so it passed
either way; the new test builds two siblings and fails on the old code.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

api/python/quilt3/backends/local.py
@@ -29,7 +29,8 @@ class LocalPackageRegistryV1(PackageRegistryV1):
                 yield path, f.read()
 
     def delete_package(self, pkg_name: str):
-        shutil.rmtree(self._pointers_usr_dir(pkg_name).path)
+        shutil.rmtree(self.pointers_dir(pkg_name).path)
+        delete_url(self._pointers_usr_dir(pkg_name))
 
     def delete_package_version(self, pkg_name: str, top_hash: str):
         super().delete_package_version(pkg_name, top_hash)

api/python/tests/integration/test_packages.py
@@ -1034,6 +1034,18 @@ class PackageTest(QuiltTestCase):
         quilt3.delete_package('Quilt/Test')
         assert 'Quilt/Test' not in quilt3.list_packages()
 
+    def test_local_package_delete_keeps_sibling_packages(self):
+        """delete_package must remove only the named package, not the whole user namespace."""
+        Package().build("Quilt/Test1")
+        Package().build("Quilt/Test2")
+        assert {"Quilt/Test1", "Quilt/Test2"} <= set(quilt3.list_packages())
+
+        quilt3.delete_package("Quilt/Test1")
+
+        packages = set(quilt3.list_packages())
+        assert "Quilt/Test1" not in packages
+        assert "Quilt/Test2" in packages
+
     def test_local_delete_package_revision(self):
         pkg_name = 'Quilt/Test'
         top_hash1 = 'top_hash1'